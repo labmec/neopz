c8b86732b2bba1cdde205ce49a8025f478113eb7
cantao <cantao@a693bd38-532a-e730-8659-88a8c11ee2f8>
2003-02-10 18:03:02 +0000

    Fixed a missing const.

--- Material/pzmaterial.cc
+++ Material/pzmaterial.cc
@@ -10,77 +10,77 @@
 REAL TPZMaterial::gBigNumber = 1.e12;
 
 TPZMaterial::TPZMaterial(int id) {
-  fId = id;
-  fForcingFunction = 0;
+   fId = id;
+   fForcingFunction = 0;
 }
 
 TPZMaterial::TPZMaterial(TPZMaterial &material) {
-  fId = material.fId;
-  fForcingFunction = material.fForcingFunction;
+   fId = material.fId;
+   fForcingFunction = material.fForcingFunction;
 }
 
 void TPZMaterial::Print(ostream & out) {
-  out << endl << "Material Id = " << fId << endl;
+   out << endl << "Material Id = " << fId << endl;
 }
 
 int TPZMaterial::VariableIndex(char *name) {
-  if(!strcmp(name,"state")) return 0;
-  if(!strcmp(name,"POrder")) return 99;
-  if(!strcmp(name,"Error")) return 100;
-  if(!strcmp(name,"TrueError")) return 101;
-  if(!strcmp(name,"EffectivityIndex")) return 102;
+   if(!strcmp(name,"state")) return 0;
+   if(!strcmp(name,"POrder")) return 99;
+   if(!strcmp(name,"Error")) return 100;
+   if(!strcmp(name,"TrueError")) return 101;
+   if(!strcmp(name,"EffectivityIndex")) return 102;
 
 
-  return -1;
+   return -1;
 }
 
 int TPZMaterial::NSolutionVariables(int index) {
-  if(index == 0) return NStateVariables();
-  if(index == 99) return 1;
-  if(index == 100) return 1;
-  if(index == 101) return 1;
-  if(index == 102) return 1;
-  PZError << "TPZMaterial::NSolutionVariables called index = " << index << "\n";
-  return 0;
+   if(index == 0) return NStateVariables();
+   if(index == 99) return 1;
+   if(index == 100) return 1;
+   if(index == 101) return 1;
+   if(index == 102) return 1;
+   PZError << "TPZMaterial::NSolutionVariables called index = " << index << "\n";
+   return 0;
 }
 
 void TPZMaterial::Solution(TPZVec<REAL> &Sol,TPZFMatrix &/*DSol*/,TPZFMatrix &/*axes*/,int var,
 			   TPZVec<REAL> &Solout){
-  if(var == 0) Solout = Sol;
-  else if(var == 99 || var == 100 || var == 101 || var == 102) {
-    //  	PZError << "TPZMaterial var = "<< var << " the element should treat this case\n";
-    Solout[0] = Sol[0]; // = 0.;
-  } else Solout.Resize(0);
+   if(var == 0) Solout = Sol;
+   else if(var == 99 || var == 100 || var == 101 || var == 102) {
+      //  	PZError << "TPZMaterial var = "<< var << " the element should treat this case\n";
+      Solout[0] = Sol[0]; // = 0.;
+   } else Solout.Resize(0);
 }
 
 TPZBndCond *TPZMaterial::CreateBC(int id, int typ, TPZFMatrix &val1, TPZFMatrix &val2) {
-  return new TPZBndCond(this,id,typ,val1,val2);
+   return new TPZBndCond(this,id,typ,val1,val2);
 }
 
 void TPZMaterial::SetData(istream &data) {
-  PZError << "TPZMaterial::SetData is called.\n";
-  data >> fId;
+   PZError << "TPZMaterial::SetData is called.\n";
+   data >> fId;
 }
 
 TPZMaterial *TPZMaterial::NewMaterial() {
-  PZError << "TPZMaterial::NewMaterial is called.\n";
-  return 0;
+   PZError << "TPZMaterial::NewMaterial is called.\n";
+   return 0;
 }
 void TPZMaterial::Contribute(TPZVec<REAL> &x,TPZFMatrix &jacinv, TPZVec<REAL> &sol,TPZFMatrix &dsol,REAL weight,TPZFMatrix &axes,TPZFMatrix &phi,TPZFMatrix &dphi,TPZFMatrix &ef){
-    TPZFMatrix ek(ef.Rows(),ef.Rows(),0.);
-    Contribute(x,jacinv,sol,dsol,weight,axes,phi,dphi,ek,ef);
+   TPZFMatrix ek(ef.Rows(),ef.Rows(),0.);
+   Contribute(x,jacinv,sol,dsol,weight,axes,phi,dphi,ek,ef);
 }
 
 void TPZMaterial::Clone(TPZAdmChunkVector<TPZMaterial *> &matvec) {
-  int matid = Id();
-  int nmat = matvec.NElements();
-  int m;
-  for(m=0; m<nmat; m++) {
-    TPZMaterial *mat = matvec[m];
-    if(!mat) continue;
-    if(mat->Id() == matid) return;
-  }
-  int vecpos = matvec.AllocateNewElement();
-  TPZMaterial *newmat = NewMaterial();
-  matvec[vecpos] = newmat;
+   int matid = Id();
+   int nmat = matvec.NElements();
+   int m;
+   for(m=0; m<nmat; m++) {
+      TPZMaterial *mat = matvec[m];
+      if(!mat) continue;
+      if(mat->Id() == matid) return;
+   }
+   int vecpos = matvec.AllocateNewElement();
+   TPZMaterial *newmat = NewMaterial();
+   matvec[vecpos] = newmat;
 }
--- Material/pzmaterial.h
+++ Material/pzmaterial.h
@@ -1,143 +1,173 @@
+/** @file pzmaterial.h
+ *
+ * Header file for abstract class TPZMaterial.
+ */
 
 #ifndef PZMATERIALHPP
 #define PZMATERIALHPP
-/**File : pzmaterial.h
-
-Header file for abstract class TPZMaterial.
-*/
-
-#include "pzreal.h"
 
 #include <iostream>
+#include <string.h>
+
+#include "pzreal.h"
+#include "pzvec.h"
+#include "pzmatrix.h"
+#include "pzfmatrix.h"
+#include "pzadmchunk.h"
 
 using namespace std;
 
-#include <string.h>
-
-   //pl = LerMaterial("placa2.dat");
-   //secondmesh->InsertMaterialObject(pl);
-   //pl = LerMaterial("placa3.dat");
-   //secondmesh->InsertMaterialObject(pl);
-   //CC : condicões de contorno
-
 class TPZBndCond;
-
-template <class T>
-class TPZVec;
-class TPZMatrix;
-class TPZFMatrix;
 class TPZMaterial;
-template <class T>
-class TPZAdmChunkVector;
 
+class  TPZMaterial
+{
+      int fId;
 
-class  TPZMaterial  {
+   protected:
+      void (*fForcingFunction)(TPZVec<REAL> &loc,TPZVec<REAL> &result);
 
-  int	fId;
+   public:
 
- protected:
-  void (*fForcingFunction)(TPZVec<REAL> &loc,TPZVec<REAL> &result);
+      static REAL gBigNumber;
 
-  public:
+      /** Creates a material object and inserts it in the vector of
+       *  material pointers of the mesh. Upon return vectorindex
+       *  contains the index of the material object within the
+       *  vector
+       */
+      TPZMaterial(int id);
 
-  static REAL gBigNumber;
+      /** Creates a material object based on the referred object and
+       *  inserts it in the vector of material pointers of the mesh.
+       *  Upon return vectorindex contains the index of the material
+       *  object within the vector
+       */
+      TPZMaterial(TPZMaterial &mat);
 
-  /**Creates a material object and inserts it in the vector of material pointers
-  of the mesh.
-  Upon return vectorindex contains the index of the material object within the vector*/
-  TPZMaterial(int id);
+      virtual ~TPZMaterial() {}
 
-  /**Creates a material object based on the referred object and inserts it in the vector of material pointers
-  of the mesh.
-  Upon return vectorindex contains the index of the material object within the vector*/
-  TPZMaterial(TPZMaterial &mat);
+      /** returns the name of the material*/
+      virtual char *Name() { return "no_name"; }
 
-  virtual ~TPZMaterial() {}
+      /**returns the integrable dimension of the material*/
+      virtual int Dimension() = 0;
 
-  /** returns the name of the material*/
-  virtual char *Name() { return "no_name"; }
+      int Id() const { return fId; }
+      void SetId(int id) { fId = id; }
 
-  /**returns the integrable dimension of the material*/
-  virtual int Dimension() = 0;
+      /** returns the number of state variables associated with the material*/
+      virtual int NStateVariables() = 0;
 
-  int Id() { return fId; }
-  void SetId(int id) { fId = id; }
+      /** return the number of components which form the flux function*/
+      virtual int NFluxes() {return 0;}
 
-  /** returns the number of state variables associated with the material*/
-  virtual int NStateVariables() = 0;
+      /** print out the data associated with the material*/
+      virtual void Print(ostream &out = std::cout);
 
-  /** return the number of components which form the flux function*/
-  virtual int NFluxes() {return 0;}
+      /**returns the variable index associated with the name*/
+      virtual int VariableIndex(char *name);
 
-  /** print out the data associated with the material*/
-  virtual void Print(ostream &out = std::cout);
+      /** returns the number of variables associated with the variable
+	  indexed by var.  var is obtained by calling VariableIndex*/
+      virtual int NSolutionVariables(int var);
 
-  /**returns the variable index associated with the name*/
-  virtual int VariableIndex(char *name);
+      /**returns the solution associated with the var index based on
+       * the finite element approximation*/
+      virtual void Solution(TPZVec<REAL> &Sol, TPZFMatrix &DSol,
+			    TPZFMatrix &axes, int var, TPZVec<REAL> &Solout);
 
-  /** returns the number of variables associated with the variable indexed by var.
-      var is obtained by calling VariableIndex*/
-  virtual int NSolutionVariables(int var);
+      /**compute the value of the flux function to be used by ZZ error
+       * estimator*/
+      virtual void Flux(TPZVec<REAL> &x, TPZVec<REAL> &Sol, 
+			TPZFMatrix &DSol, TPZFMatrix &axes,
+			TPZVec<REAL> &flux) {}
 
-  /**returns the solution associated with the var index based on the finite element approximation*/
-  virtual void Solution(TPZVec<REAL> &Sol,TPZFMatrix &DSol,TPZFMatrix &axes,int var,TPZVec<REAL> &Solout);
+      /**Create an object TPZBndCond derived of TPZMaterial*/
+      virtual TPZBndCond *CreateBC(int id, int typ, TPZFMatrix &val1,
+				   TPZFMatrix &val2);
 
-  /**compute the value of the flux function to be used by ZZ error estimator*/
-  virtual void Flux(TPZVec<REAL> &x, TPZVec<REAL> &Sol, TPZFMatrix &DSol, TPZFMatrix &axes, TPZVec<REAL> &flux) {}
+      /**Compute contribution to the stiffness matrix and right hand
+       * side at an integration point*/
+      virtual void Contribute(TPZVec<REAL> &x, TPZFMatrix &jacinv,
+			      TPZVec<REAL> &sol, TPZFMatrix &dsol,
+			      REAL weight,TPZFMatrix &axes,
+			      TPZFMatrix &phi, TPZFMatrix &dphi,
+			      TPZFMatrix &ek, TPZFMatrix &ef) = 0;
 
-  /**Create an object TPZBndCond derived of TPZMaterial*/
-  virtual TPZBndCond *CreateBC(int id, int typ, TPZFMatrix &val1, TPZFMatrix &val2);
+      /** Compute contribution to the stiffness matrix and right hand
+       * side at the integration point of a boundary*/
+      virtual void ContributeBC(TPZVec<REAL> &x, TPZVec<REAL> &sol,
+				REAL weight, TPZFMatrix &axes,
+				TPZFMatrix &phi, TPZFMatrix &ek,
+				TPZFMatrix &ef, TPZBndCond &bc) = 0;
 
-  /**Compute contribution to the stiffness matrix and right hand side at an integration point*/
-  virtual void Contribute(TPZVec<REAL> &x,TPZFMatrix &jacinv,TPZVec<REAL> &sol,TPZFMatrix &dsol,
-	REAL weight,TPZFMatrix &axes,TPZFMatrix &phi,TPZFMatrix &dphi,TPZFMatrix &ek,TPZFMatrix &ef) = 0;
+      /**Set a procedure as source function for the material.  loc
+	 corresponds to the coordinate of the point where the source
+	 function is applied*/
+      void SetForcingFunction(void (*fp)(TPZVec<REAL> &loc,
+					 TPZVec<REAL> &result))
+      {
+	 fForcingFunction = fp;
+      }
 
-  /*Compute contribution to the stiffness matrix and right hand side at the integration point of a boundary*/
-  virtual void ContributeBC(TPZVec<REAL> &x,TPZVec<REAL> &sol,REAL weight,TPZFMatrix &axes,
-			    TPZFMatrix &phi,TPZFMatrix &ek,TPZFMatrix &ef,TPZBndCond &bc) = 0;
+      /**Compute the error due to the difference between the
+	 interpolated flux and the flux computed based on the
+	 derivative of the solution*/
+      virtual void Errors(TPZVec<REAL> &x, TPZVec<REAL> &sol, TPZFMatrix &dsol,
+			  TPZFMatrix &axes, TPZVec<REAL> &flux,
+			  TPZVec<REAL> &uexact, TPZFMatrix &duexact,
+			  TPZVec<REAL> &val) {}
 
-  /**Set a procedure as source function for the material.
-     loc corresponds to the coordinate of the point where the source function is applied*/
-  void SetForcingFunction(void (*fp)(TPZVec<REAL> &loc,TPZVec<REAL> &result)){
-    fForcingFunction = fp;
-  }
+      /**Incremented by Jorge*/
+      /**To create another material of the same type*/
+      virtual TPZMaterial *NewMaterial();
 
-  /**Compute the error due to the difference between the interpolated flux and
-  the flux computed based on the derivative of the solution*/
-  virtual void Errors(TPZVec<REAL> &x,TPZVec<REAL> &sol,TPZFMatrix &dsol, TPZFMatrix &axes, TPZVec<REAL> &flux,
-			TPZVec<REAL> &uexact,TPZFMatrix &duexact,TPZVec<REAL> &val) {}
+      /**Read data of the material from a istream (file data)*/
+      virtual void SetData(istream &data);
 
-  /**Incremented by Jorge*/
-  /**To create another material of the same type*/
-  virtual TPZMaterial *NewMaterial();
-  /**Read data of the material from a istream (file data)*/
-  virtual void SetData(istream &data);
+      /**Compute contribution to the stiffness matrix and right hand
+       * side at an integration point*/
+      virtual void Contribute(TPZVec<REAL> &x, TPZFMatrix &jacinv,
+			      TPZVec<REAL> &sol, TPZFMatrix &dsol, REAL weight,
+			      TPZFMatrix &axes, TPZFMatrix &phi,
+			      TPZFMatrix &dphi, TPZFMatrix &ef);
 
-  /**Compute contribution to the stiffness matrix and right hand side at an integration point*/
-  virtual void Contribute(TPZVec<REAL> &x,TPZFMatrix &jacinv, TPZVec<REAL> &sol,TPZFMatrix &dsol, REAL weight, TPZFMatrix &axes,TPZFMatrix &phi,TPZFMatrix &dphi,TPZFMatrix &ef);
-
-  /**
-   * Create a copy of the material object and put it in the vector which is passed on
-   */
-  virtual void Clone(TPZAdmChunkVector<TPZMaterial *> &matvec);
+      /**
+       * Create a copy of the material object and put it in the vector
+       * which is passed on
+       */
+      virtual void Clone(TPZAdmChunkVector<TPZMaterial *> &matvec);
 	
-  /**Jorge 17/09/99*/
-  /**To return a numerical flux type to apply over the interfaces of the elements*/
-  virtual int FluxType() { return 2; }
-	/** Factor to diffussive term*/
-  virtual int IdBC(double *x) { return 5; }
-  /**Compute the contribution to stiffness matrix and load vector of the
-     fluxes over the interface between two computational elements*/
-  virtual void ContributeOverInterface(TPZVec<REAL> &x,TPZVec<REAL> &leftsol,TPZVec<REAL> &rightsol,
-          REAL weight,REAL area,int type,TPZFMatrix &axes,TPZFMatrix &phileft,TPZFMatrix &phiright,TPZVec<REAL> &normal,
-          TPZFMatrix &ek,TPZFMatrix &ef);
+      /**Jorge 17/09/99*/
+      /**To return a numerical flux type to apply over the interfaces
+       * of the elements*/
+      virtual int FluxType() { return 2; }
 
+      /** Factor to diffussive term*/
+      virtual int IdBC(double *x) { return 5; }
+
+      /**Compute the contribution to stiffness matrix and load vector
+	 of the fluxes over the interface between two computational
+	 elements*/
+      virtual void ContributeOverInterface(TPZVec<REAL> &x,
+					   TPZVec<REAL> &leftsol,
+					   TPZVec<REAL> &rightsol,
+					   REAL weight, REAL area, int type,
+					   TPZFMatrix &axes,
+					   TPZFMatrix &phileft,
+					   TPZFMatrix &phiright,
+					   TPZVec<REAL> &normal,
+					   TPZFMatrix &ek, TPZFMatrix &ef);
 };
 
-inline void TPZMaterial::ContributeOverInterface(TPZVec<REAL> &/*x*/,TPZVec<REAL> &/*leftsol*/,TPZVec<REAL> &/*rightsol*/,
-          REAL /*weight*/,REAL /*area*/,int /*type*/,TPZFMatrix &/*axes*/,TPZFMatrix &/*phileft*/,TPZFMatrix &/*phiright*/,
-          TPZVec<REAL> &/*normal*/,TPZFMatrix &/*ek*/,TPZFMatrix &/*ef*/) {
-  cout << "TPZMaterial::ContributeOverInterface is called.\n";
+inline void TPZMaterial::ContributeOverInterface(
+   TPZVec<REAL> &/*x*/, TPZVec<REAL> &/*leftsol*/, TPZVec<REAL> &/*rightsol*/,
+   REAL /*weight*/, REAL /*area*/, int /*type*/, TPZFMatrix &/*axes*/,
+   TPZFMatrix &/*phileft*/, TPZFMatrix &/*phiright*/, TPZVec<REAL> &/*normal*/,
+   TPZFMatrix &/*ek*/, TPZFMatrix &/*ef*/)
+{
+   cout << "TPZMaterial::ContributeOverInterface is called.\n";
 }
 
 #endif
