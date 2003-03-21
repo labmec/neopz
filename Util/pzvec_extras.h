1520f37e1c60cac4cb678303c1471567342b3734
tiago <tiago@a693bd38-532a-e730-8659-88a8c11ee2f8>
2003-03-21 13:31:48 +0000

    Functions sscal (Scale a vector) and sdot (scale product).

--- Util/pzvec_extras.h
+++ Util/pzvec_extras.h
@@ -2,7 +2,7 @@
  * @file pzvec_extra.h
  * @brief Extra utilities for TPZVec.
  */
-// $Id: pzvec_extras.h,v 1.1.1.1 2003-02-04 16:45:27 cantao Exp $
+// $Id: pzvec_extras.h,v 1.2 2003-03-21 13:31:48 tiago Exp $
 
 #ifndef PZVEC_EXTRAS_H
 #define PZVEC_EXTRAS_H
@@ -34,6 +34,76 @@ void saxpy( TPZVec< T1 >& x, const TPZVec< T2 >& y, Scalar s )
    }
 }
 
-#endif //PZVEC_EXTRAS_H
 
+/**
+ * Performs a sscal operation: x <- x * s.
+ *
+ * @since Mar 20, 2003
+ * @author Tiago Forti
+ * @author Erick Santos
+ */
+template< class T1, class Scalar >
+void sscal(TPZVec< T1 > & x, const Scalar s)
+{
+#ifdef BLAS
+//The blas method's call shall be done here.
+#endif  //BLAS
+
+   int size = x.NElements();
+
+   for( int ii = 0; ii < size; ii++)
+   {
+   	x[ii] *= s;
+   }
+}
+
+/**
+ * Performs a sdot operation: dot <- Transpose[x] * y
+ *
+ * @since Mar 20, 2003
+ * @author Tiago Forti
+ * @author Erick Santos
+ */
+template< class T1 >
+double sdot(TPZVec< T1 > & x, TPZVec< T1 > & y)
+{
+#ifdef BLAS
+//The blas method's call shall be done here.
+#endif  //BLAS
+
+   int size = x.NElements();
+   double sum = 0.0;
+
+#ifndef NOTDEBUG
+   if( size != y.NElements() ) {
+      PZError << "SDOT error!" << endl
+	      << "Vectors with different sizes #x = " << size
+	      << ", #y = " << y.NElements() << endl;
+
+      PZError.flush();
+   }
+#endif
+
+   for( int ii = 0; ii < size; ii++)
+   {
+   	sum += x[ii] * y[ii];
+   }
+
+   return sum;
+}
+
+
+#endif //PZVEC_EXTRAS_H
 //--| PZ |----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
