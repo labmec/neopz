81d60a816cac04f760435fa1c73fa748e845e8ac
cantao <cantao@a693bd38-532a-e730-8659-88a8c11ee2f8>
2003-04-02 13:56:48 +0000

    Tested with PZStack also.

--- Projects/sorting/sort.cc
+++ Projects/sorting/sort.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "pzvec.h"
+#include "pzstack.h"
 #include "pzvec_extras.h"
 
 #define S 10
@@ -14,21 +15,25 @@ int main()
    cout << "Vector sorting test using STL (std::sort)..." << endl << endl;
 
    typedef TPZVec< int > Vector_t;
+   typedef TPZStack< int > Stack_t;
 
    Vector_t v( S );
+   Stack_t s;
 
    for( int ii = 0; ii < S; ii++ )
    {
       v[ ii ] = S - ii;
+
+      s.Push( S - ii );
    }
 
    cout.precision( 2 );
 
    cout << "Original vector:" << endl << setw( 6 ) << v << endl;
+   cout << "Sorted vector:"   << endl << setw( 6 ) << Sort( v ) << endl;
 
-   cout << "Sorted vector:" << endl << setw( 6 ) << Sort( v ) << endl;
-
-   cout << v << endl;
+   cout << "Original stack:" << endl << setw( 6 ) << s << endl;
+   cout << "Sorted stack:"   << endl << setw( 6 ) << Sort( s ) << endl;
 
    return 0;
 };
