d2cf3e919b1dfe72ea522781eecf0ce7ed0528fc
cedric <cedric@a693bd38-532a-e730-8659-88a8c11ee2f8>
2003-05-26 22:48:23 +0000

    building discontinuous project

---
+++ Projects/descontinuo/TPZIterativeAnalysis.h
@@ -0,0 +1,32 @@
+
+#ifndef ITERATIVEANALYSISH
+#define ITERATIVEANALYSISH
+
+#include <iostream>
+#include "pzanalysis.h"
+#include "pzreal.h"
+using namespace std;
+class TPZCompMesh;
+class TPZCompEl;
+class TPZMaterial;
+/******************/
+
+class TPZIterativeAnalysis : public TPZAnalysis {
+
+  clock_t fBegin,fInit;
+
+public:
+
+  TPZIterativeAnalysis(TPZCompMesh *mesh,std::ostream &out = cout);
+
+  void IterativeProcess(ostream &out,REAL tol,int numiter,TPZMaterial *mat,int marcha=1,int resolution=0);
+
+  void IterativeProcessTest(ostream &out,REAL tol,int numiter,TPZMaterial *mat,int marcha,int resolution=0);
+
+  void CoutTime(clock_t &start,char *title);
+
+  void SetDeltaTime(TPZCompMesh *CompMesh,TPZMaterial *mat);
+};
+
+#endif
+
