5e28cb6aff077ecd207898060a5f9acc225b312e
longhin <longhin@a693bd38-532a-e730-8659-88a8c11ee2f8>
2003-09-23 21:25:58 +0000

    *** empty log message ***

---
+++ Projects/CursoPZ/Class_1/main.cc
@@ -0,0 +1,70 @@
+//We are use vectors and integration rules routines
+#include "pzvec.h"
+#include "pzquad.h"
+
+//IO
+#include <iostream>
+using namespace std;
+
+//Functions declaration
+REAL Funcao (TPZVec<REAL> &pt, int i, int j, int k, int p);
+REAL Alfa (int i, int j, int k, int p);
+
+
+int main(){
+        int i = 1;
+        int j = 1;
+        int k = 1;
+        int p = 1;
+        TPZVec<REAL> point(3,0.);
+        REAL weight = 0.;
+        REAL integral = 0.;
+
+	//=====Regra 1D=====================================
+	TPZInt1d ordem1d (p);
+        int npoints = ordem1d.NPoints();
+
+        int it;
+        for (it=0;it<npoints;it++){
+        	ordem1d.Point(it,point,weight);
+                integral += weight * Funcao(point,i,j,k,p);
+        }
+	//=====Final Regra 1D================================
+
+        cout << "Integral = " << integral << endl;
+        return 0;
+}
+
+
+//Functions definition
+REAL Funcao(TPZVec<REAL> &pt, int i, int j, int k, int p){
+	int r,s,t;
+	REAL result=0.;
+        REAL x = 0.;
+        REAL y = 0.;
+        REAL z = 0.;
+
+	//Para tratar com 1D e 2D tb.
+        int dim = pt.NElements();
+        if (dim > 2) z = pt[2];
+        if (dim > 1) y = pt[1];
+        x = pt[0];
+
+        //Atenção --> A função foi alterada aqui para que 1D e 2D tenham resultados diferentes de zero!
+	for (r=0;r<=i;r++){
+		for(s=0;s<=j;s++){
+        		for (t=0;t<=k;t++){
+				result +=  Alfa(r,s,t,p) * (pow (x,r) + pow (y,s) + pow (z,t));
+   			}
+		}
+	}
+	return result;
+};
+
+REAL Alfa(int i, int j, int k, int p){
+	int n = i*p*p + j*p + k;
+        REAL alfa = sin((REAL)n);
+        //cout << "alfa = " << alfa << endl;
+	return alfa;
+}
+
---
+++ Projects/CursoPZ/Class_2/main.cc
@@ -0,0 +1,83 @@
+//We are using matrices and solvers
+#include "pzvec.h"
+#include "pzmatrix.h"
+#include "pzfmatrix.h"
+#include <pzskylmat.h>
+#include <pzstepsolver.h> 
+//IO
+#include <iostream>
+using namespace std;
+
+void FillMatrix(TPZMatrix &mat,int neq, int banda);
+void FillF(TPZFMatrix &f, int neq, int nst);
+
+int main(){
+  int neq=10;
+  int banda=2;
+  int i,j;
+ 
+  TPZFMatrix  cheia(neq,neq, 0.);
+  FillMatrix(cheia,neq,banda);
+
+  TPZVec <int> skyvec(neq,0);
+  for (i=0;i<neq-banda;i++);{
+    if (i >= banda && i <neq-banda ) skyvec[i]=2*banda-1;
+    else if (i < banda) skyvec[i]=banda+i;
+    else skyvec[i] = banda+neq-i;
+  }
+
+  TPZSkylMatrix  skyline(neq,skyvec);
+  FillMatrix(skyline,neq,banda);
+
+  cheia.Print( "Matriz cheia ",cout);
+  skyline.Print( "Matriz skyline ",cout);
+
+  TPZFMatrix F;
+  FillF(F,neq,1);
+  F.Print("Vetor de Carga ",cout);
+
+  TPZFMatrix resultcheia;
+  /*  TPZStepSolver direct(&cheia);
+  direct.SetDirect(ECholesky);
+  direct.Solve(F,resultcheia);*/
+
+  TPZStepSolver step(&cheia);
+  TPZStepSolver precond(step);
+  int numiterpre =5;
+  int numiter = 20;
+  double overrelax = 1.1;
+  double tol = 1e-8;
+  precond.SetSSOR(numiterpre,overrelax,tol,1);
+  step.SetCG(numiter,precond,tol,1);
+  step.Solve(F,resultcheia);
+
+  resultcheia.Print("Solucão ",cout);
+   
+  cout << "Integral = " << endl;
+  return 0;
+}
+
+void FillMatrix(TPZMatrix &mat,int neq, int banda){
+  int i,j;
+  for ( i=0;i<neq;i++){
+    mat(i,i)=2000.;
+    int col = i+banda;
+    if (col >= neq ) col = neq; 
+    for ( j=i+1;j<col;j++){
+      mat(i,j)=500./(j-i);
+      mat(j,i)=500./(j-i);
+    }
+  }
+}
+
+void FillF(TPZFMatrix &f,int neq, int nst){
+  double PI = 3.1416;
+  f.Resize(neq,nst);
+  int i,j;
+  for (i=0;i<neq;i++){
+    for (j=0;j<nst;j++){
+      double val = (double)(i%10);
+      f(i,j)=PI*val;
+    }
+  }
+}
