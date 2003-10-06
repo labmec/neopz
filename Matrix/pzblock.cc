3075b39f2b007c9138c6328de4ece0ef26e5f4d2
cantao <cantao@a693bd38-532a-e730-8659-88a8c11ee2f8>
2003-10-06 20:52:39 +0000

    Fixed a small bug on MaxBlocks.

--- Matrix/pzblock.cc
+++ Matrix/pzblock.cc
@@ -110,7 +110,7 @@ TPZBlock::SetNBlocks(const int num_of_blocks )
   //modified Philippe 24/7/97
   // a small optimization
   int MaxBlocks = fBlock.NElements();
-  if(num_of_blocks >= MaxBlocks) fBlock.Expand((int) (MaxBlocks*1.2));
+  if(num_of_blocks >= MaxBlocks) fBlock.Expand((int) (num_of_blocks*1.2));
   TNode copy;
   fBlock.Resize(num_of_blocks,copy);
   if(num_of_blocks == MaxBlocks) return 1;
