5f621bba335920ec5eba958e1c14ba13e89dd8c1
erick <erick@a693bd38-532a-e730-8659-88a8c11ee2f8>
2003-10-16 20:08:46 +0000

    Computational mesh to work with CFD.

---
+++ Mesh/pzflowcmesh..cc
@@ -0,0 +1,105 @@
+#include "pzflowcmesh.h"
+
+
+TPZFlowCompMesh::TPZFlowCompMesh(TPZGeoMesh* gr) : TPZCompMesh(gr) {
+
+}
+
+REAL TPZFlowCompMesh::MaxVelocityOfMesh(){
+
+  int nel = ElementVec().NElements(), i, nstate, dim, elDim;
+  TPZManVector<REAL> density(1), sol, velocity(1);// sol(nstate);
+  REAL maxvel = 0.0, veloc, sound, press, gamma;
+  TPZVec<REAL> param(3,0.);
+
+  // loop over all elements
+  for(i=0;i<nel;i++){
+    TPZCompEl *pElComp = ElementVec()[i];
+    if(!pElComp)PZErr << "TPZFlowCompMesh::MaxVelocityOfMesh: No associated computation element\n".;
+
+    TPZMaterial *mat = pElComp->Material();
+    TPZGeoEl *pElGeo = pElComp->Reference();
+    if(!mat || !pElGeo)PZErr << "TPZFlowCompMesh::MaxVelocityOfMesh ERROR: null material or element.\n";
+
+    dim = mat->Dimension();
+    elDim = pElGeo->Dimension();
+    if(elDim != dim)PZErr << "TPZFlowCompMesh::MaxVelocityOfMesh2 ERROR: element and material with different dimensions.\n";
+
+    TPZConservationLaw2 *law = dynamic_cast<TPZConservationLaw2 *>(mat);
+    if(!law)PZErr << "TPZFlowCompMesh::MaxVelocityOfMesh2 ERROR: non-fluid material.\n";
+
+    // number of state variables for this material.
+    nstate = law->NStateVariables();
+
+    // getting the center point of the internal side.
+    pElGeo->CenterPoint(pElGeo->NSides()-1,param);//com->Solution(sol,j+100,sol2);
+    // verifying the density answer
+    pElComp->Solution(param,1,density);
+    if(density[0] < 0.0)PZErr << "TPZFlowCompMesh::MaxVelocityOfMesh: Negative density\n";
+
+    // Getting the velocity answers
+    pElComp->Solution(param,6,velocity);
+    // getting the whole vector of solutions
+    sol.Resize(nstate);
+    pElComp->Solution(param,5,sol);
+
+    press = law->Pressure(sol);
+    if(press < 0.0)PZerr << "TPZFlowCompMesh::MaxVelocityOfMesh minus pressure\n";
+
+    // retrieving the constant of gas.
+    gamma = law->Gamma();
+
+    sound = sqrt(gamma*press/density[0]);
+
+    // maximal eigenvalue velocity
+    veloc = velocity[0] + sound;
+    if(veloc > maxvel) maxvel = veloc;
+  }
+  return maxvel;
+}
+
+void TPZFlowCompMesh::CollectFluidMaterials()
+{
+   int i, NumMat;
+   TPZAdmChunkVector< TPZMaterial * > * pVecMat;
+
+   pVecMat = fCompMesh->MaterialVec();
+   NumMat = pVecMat->NElements();
+
+   // buffering the fluid materials
+   for(i = 0; i < NumMat; i++)
+   {
+      TPZConservationLaw2 * pConsLaw;
+      pConsLaw = dynamic_cast<TPZConservationLaw2 *>(pVecMat->operator[](i));
+      if(pConsLaw)
+      {
+         int index =  fFluidMaterial.AllocateNewElement();
+	 fFluidMaterial[index] = pConsLaw;
+      }
+   }
+}
+
+
+void TPZFlowCompMesh::ComputeTimeStep()
+{
+    REAL maxVel = MaxVelocityOfMesh();
+    REAL deltax = LesserEdgeOfMesh();
+
+    TPZCompElDisc *disc;
+    int degree = disc->gDegree;
+
+    int i, NumFluid = fFluidMaterial.NElements();
+    for(i = 0; i < NumFluid; i++)
+    {
+       fFluidMaterial[i]->SetTimeStep(maxVel, deltax, degree);
+    }
+}
+
+void TPZFlowCompMesh::SetCFL(REAL CFL)
+{
+    int i, NumFluid = fFluidMaterial.NElements();
+    for(i = 0; i < NumFluid; i++)
+    {
+       fFluidMaterial[i]->SetCFL(CFL);
+    }
+}
---
+++ Mesh/pzflowcmesh.h
@@ -0,0 +1,71 @@
+#include "pzcompel.h"
+#include "pzgeoel.h"
+#include "TPZConservationLaw.h"
+
+
+#ifndef TPZFLOWCOMPMESH_H
+#define TPZFLOWCOMPMESH_H
+
+class TPZMaterial;
+class TPZGeoMesh;
+#include <iostream>
+#include "pzcmesh.h"
+using namespace std;
+/*******************/
+/**
+ * This class implements the additions behavior and
+ * data to handle CFD problems as computional meshes.
+ *
+ */
+class TPZFlowCompMesh : public TPZCompMesh
+{
+
+public:
+
+  //TPZFlowCompMesh();/**empty constructor*/
+
+  TPZFlowCompMesh(TPZGeoMesh* gr);
+
+  ~TPZFlowCompMesh(){};
+
+  /**
+   * According to each material settings, it returns
+   * the greatest velocity in the mesh.
+   */
+  REAL MaxVelocityOfMesh();
+
+  /**
+   * Computes the current time step for the mesh.
+   * User must call CollectFluidMaterials and
+   * SetCFL first.
+   */
+  void ComputeTimeStep();
+
+  /**
+   * Informs all materials the CFL number.
+   * User must call CollectFluidMaterials first.
+   */
+  void SetCFL(REAL CFL);
+
+  /**
+   * Should be called after all materials have been
+   * added to the mesh. This method collects all
+   * fluid materials and stores a pointer to them
+   * in the vector fFluidMaterial.
+   */
+  void CollectFluidMaterials();
+
+protected:
+
+   /**
+    * This vector of pointers represent the collection
+    * of all fluid materials in the mesh. These are the
+    * materials that deserve special attention during
+    * the contribution processes.
+    */
+   TPZAdmChunkVector< TPZConservationLaw2 * > fFluidMaterial;
+
+};
+
+#endif
+
