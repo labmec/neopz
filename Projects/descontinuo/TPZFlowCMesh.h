825d9a4b6188bb635671d48c03c4bf56dcbe722a
cedric <cedric@a693bd38-532a-e730-8659-88a8c11ee2f8>
2003-10-17 19:34:20 +0000

    *** empty log message ***

--- Projects/descontinuo/TPZFlowCMesh.h
+++ Projects/descontinuo/TPZFlowCMesh.h
@@ -1,7 +1,3 @@
-#include "pzcompel.h"
-#include "pzgeoel.h"
-#include "TPZConservationLaw.h"
-
 
 #ifndef TPZFLOWCOMPMESHPP
 #define TPZFLOWCOMPMESHPP
@@ -26,51 +22,6 @@ class TPZFlowCompMesh : public TPZCompMesh {
 
   void SetDeltaTime(TPZMaterial *mat);
 };
-
-
-
-inline TPZFlowCompMesh::TPZFlowCompMesh(TPZGeoMesh* gr) : TPZCompMesh(gr) {
-
-}
-
-
-inline REAL TPZFlowCompMesh::MaxVelocityOfMesh(int nstate,REAL gamma){
-
-  int nel = ElementVec().NElements(),i;
-  TPZManVector<REAL> density(1),sol(nstate),velocity(1);
-  REAL maxvel = 0.0,veloc,sound,press;
-  TPZVec<REAL> param(3,0.);
-  for(i=0;i<nel;i++){
-    TPZCompEl *com = ElementVec()[i];
-    if(!com) continue;
-    TPZMaterial *mat = com->Material();
-    TPZGeoEl *geo = com->Reference();
-    if(!mat || !geo){
-      cout << "TPZFlowCompMesh::MaxVelocityOfMesh ERROR: null material or element\n";
-      continue;
-    }
-    int dim = mat->Dimension();
-    int dimel = geo->Dimension();
-    if(dimel != dim) continue;
-    geo->CenterPoint(geo->NSides()-1,param);//com->Solution(sol,j+100,sol2);
-    com->Solution(param,1,density);
-    if(density[0] < 0.0){
-      cout << "TPZFlowCompMesh::MaxVelocityOfMesh minus density\n";
-    }
-    com->Solution(param,6,velocity);
-    com->Solution(param,5,sol);
-    TPZConservationLaw *law = dynamic_cast<TPZConservationLaw *>(mat);
-    press = law->Pressure(sol);
-    if(press < 0.0){
-      cout << "TPZFlowCompMesh::MaxVelocityOfMesh minus pressure\n";
-    }
-    sound = sqrt(gamma*press/density[0]);
-    veloc = velocity[0] + sound;
-    if(veloc > maxvel) maxvel = veloc;
-  }
-  return maxvel;
-
-}
 
 #endif
 
