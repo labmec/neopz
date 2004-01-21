25b0565b1c139250df618d22a3fbd866fcc2f756
erick <erick@a693bd38-532a-e730-8659-88a8c11ee2f8>
2004-01-21 00:24:51 +0000

    Included SUPG and FAD tests

--- Projects/ArtDiff/main.cpp
+++ Projects/ArtDiff/main.cpp
@@ -9,7 +9,13 @@
 #include "pzvec.h"
 #include "fadType.h"
 
+#define Using_FAD
+
+#ifdef Using_FAD
+typedef FADREAL Number;
+#else
 typedef REAL Number;
+#endif
 
 void error(char * teste)
 {
@@ -18,187 +24,169 @@ void error(char * teste)
 
 int main()
 {
- {// Rotation Tests
-   TPZDiffMatrix<Number> Rot, RotT, Temp;
-   TPZVec<Number> sol;
-   Number us;
 
-   cout << "Testing Rotation 2d\n";
+   TPZDiffMatrix<Number> Rot, RotT,
+			 X, Xi,
+			 Y, Yi,
+			 M, Mi,
+			 Temp, Temp2,
+			 LambdaSUPG, LambdaB;
 
-   sol.Resize(4);
-   sol[0] = 1.;
-   sol[1] = 3.3;
-   sol[2] = 2.3;
-   sol[3] = 5;
-   TPZEulerConsLaw2::uRes(sol, us);
+ {// 2d Tests
 
-   TPZArtDiff::RotMatrix(sol, us, Rot, RotT);
-
-   cout << "Rot" << Rot << endl;
-   cout << "RotT" << RotT << endl;
-   Rot.Multiply(RotT, Temp);
-   cout << "Rot.RotT" << Temp << endl;
-
-
-   cout << "Testing Rotation 3d\n";
-
-   sol.Resize(5);
-   sol[0] = 1.;
-   sol[1] = 3.3;
-   sol[2] = 2.3;
-   sol[3] = 4.5;
-   sol[4] = 5;
-   TPZEulerConsLaw2::uRes(sol, us);
-
-   TPZArtDiff::RotMatrix(sol, us, Rot, RotT);
-
-   cout << "Rot" << Rot << endl;
-   cout << "RotT" << RotT << endl;
-   Rot.Multiply(RotT, Temp);
-   cout << "Rot.RotT" << Temp << endl;
- }
-
- {
-   TPZDiffMatrix<Number> M, Mi, Temp;
-   TPZVec<Number> sol;
-   Number us;
-
-   cout << "Testing Matrix M 2d\n";
-
-   sol.Resize(4);
-   sol[0] = 1.;
-   sol[1] = 3.3;
-   sol[2] = 2.3;
-   sol[3] = 5;
-   TPZEulerConsLaw2::uRes(sol,us);
-
-   TPZArtDiff::MMatrix(sol, us, 1.4, M, Mi);
-
-   cout << "M" << M << endl;
-   cout << "Mi" << Mi << endl;
-   M.Multiply(Mi, Temp);
-   cout << "M.Mi" << Temp << endl;
-
-
-   cout << "Testing Matrix M 3d\n";
-
-   sol.Resize(5);
-   sol[0] = 1.;
-   sol[1] = 3.3;
-   sol[2] = 2.3;
-   sol[3] = 4.5;
-   sol[4] = 5;
-   TPZEulerConsLaw2::uRes(sol,us);
-
-   TPZArtDiff::MMatrix(sol, us, 1.4, M, Mi);
-
-   cout << "M" << M << endl;
-   cout << "Mi" << Mi << endl;
-   M.Multiply(Mi, Temp);
-   cout << "M.Mi" << Temp << endl;
-
-  }
-
-
- {
-   TPZDiffMatrix<Number> X, Xi, Lambda, Temp;
-   TPZVec<Number> sol;
-   Number us, c;
-
-   cout << "Testing Matrix X 2d\n";
-
-   sol.Resize(4);
-   sol[0] = 1.;
-   sol[1] = 3.3;
-   sol[2] = 2.3;
-   sol[3] = 10;
-   TPZEulerConsLaw2::uRes(sol,us);
-
-   TPZEulerConsLaw2::cSpeed(sol, 1.4, c);
-
-   TPZArtDiff::EigenSystemSUPG(sol, us, c, 1.4, X, Xi, Lambda);
-
-   cout << "X" << X << endl;
-   cout << "Xi" << Xi << endl;
-   X.Multiply(Xi, Temp);
-   cout << "X.Xi" << Temp << endl;
-
-
-   cout << "Testing Matrix X 3d\n";
-
-   sol.Resize(5);
-   sol[0] = 1.;
-   sol[1] = 3.3;
-   sol[2] = 2.3;
-   sol[3] = 4.5;
-   sol[4] = 20;
-   TPZEulerConsLaw2::uRes(sol,us);
-
-   TPZEulerConsLaw2::cSpeed(sol, 1.4, c);
-
-   TPZArtDiff::EigenSystemSUPG(sol, us, c, 1.4, X, Xi, Lambda);
-
-   cout << "X" << X << endl;
-   cout << "Xi" << Xi << endl;
-   X.Multiply(Xi, Temp);
-   cout << "X.Xi" << Temp << endl;
-
-  }
-
-
- {
-   TPZDiffMatrix<Number> Y, Yi, Lambda, Temp;
    TPZVec<Number> sol, aaS;
    Number us, c;
 
-   cout << "Testing Matrix X 2d\n";
-
+   //creating data
    sol.Resize(4);
-   sol[0] = 1.;
-   sol[1] = 3.3;
-   sol[2] = 2.3;
-   sol[3] = 10;
+   sol[0] = 1.1;
+   sol[1] = 3.4;
+   sol[2] = 2.7;
+   sol[3] = 10.;
+
+#ifdef Using_FAD
+      sol[0].diff(0,4);
+      sol[1].diff(1,4);
+      sol[2].diff(2,4);
+      sol[3].diff(3,4);
+#endif
+
+   TPZEulerConsLaw2::uRes(sol, us);
+   TPZEulerConsLaw2::cSpeed(sol, 1.4, c);
+
+   cout << "Testing Rotation 2d\n";
+
+   TPZArtDiff::RotMatrix(sol, us, Rot, RotT);
+
+   cout << "Rot" << Rot << endl;
+   cout << "RotT" << RotT << endl;
+   Rot.Multiply(RotT, Temp);
+   cout << "Rot.RotT" << Temp << endl;
+
+   cout << "Testing MMatrix 2d\n";
+
+   TPZArtDiff::MMatrix(sol, us, 1.4, M, Mi);
+
+   cout << "M" << M << endl;
+   cout << "Mi" << Mi << endl;
+   M.Multiply(Mi, Temp);
+   cout << "M.Mi" << Temp << endl;
+
+   cout << "Testing SUPG 2d\n";
+
+   TPZArtDiff::EigenSystemSUPG(sol, us, c, 1.4, X, Xi, LambdaSUPG);
+
+   cout << "X" << X << endl;
+   cout << "Xi" << Xi << endl;
+   X.Multiply(Xi, Temp);
+   cout << "X.Xi" << Temp << endl;
+
+   cout << "LambdaSUPG" << LambdaSUPG;
+
+   cout << "Testing whole SUPG decomposition\n";
+   RotT. Multiply(M, Temp);
+   Temp. Multiply(X, Temp2);
+   Temp2.Multiply(LambdaSUPG, Temp);
+   Temp. Multiply(Xi, Temp2);
+   Temp2.Multiply(Mi, Temp);
+   Temp. Multiply(Rot, Temp2);
+
+   cout << "Result of: RotT.M.X.LambdaSUPG.Xi.Mi.Rot (should be 1/Sqr(A2+B2))\n";
+   cout << Temp2;
+
+   cout << "Testing Bornhaus 2d\n";
 
    aaS.Resize(2);
    aaS[0] = 2.1;
    aaS[1] = 2.3;
 
-   TPZEulerConsLaw2::uRes(sol,us);
-
-   TPZEulerConsLaw2::cSpeed(sol, 1.4, c);
-
-   TPZArtDiff::EigenSystemBornhaus(sol, us, c, 1.4, aaS, Y, Yi, Lambda);
+   TPZArtDiff::EigenSystemBornhaus(sol, us, c, 1.4, aaS, Y, Yi, LambdaB);
 
    cout << "Y" << Y << endl;
    cout << "Yi" << Yi << endl;
    Y.Multiply(Yi, Temp);
    cout << "Y.Yi" << Temp << endl;
 
-   cout << "Testing Matrix X 3d\n";
+ }
 
+ {// 3d Tests
+
+   TPZVec<Number> sol, aaS;
+   Number us, c;
+
+   //creating data
    sol.Resize(5);
-   sol[0] = 1.;
-   sol[1] = 3.3;
-   sol[2] = 2.3;
-   sol[3] = 4.5;
-   sol[4] = 20;
+   sol[0] = 1.1;
+   sol[1] = 3.4;
+   sol[2] = 2.7;
+   sol[3] = 4.9;
+   sol[4] = 20.;
+
+
+#ifdef Using_FAD
+      sol[0].diff(0,5);
+      sol[1].diff(1,5);
+      sol[2].diff(2,5);
+      sol[3].diff(3,5);
+      sol[4].diff(4,5);
+#endif
+
+   TPZEulerConsLaw2::uRes(sol, us);
+   TPZEulerConsLaw2::cSpeed(sol, 1.4, c);
+
+   cout << "Testing Rotation 3d\n";
+
+   TPZArtDiff::RotMatrix(sol, us, Rot, RotT);
+
+   cout << "Rot" << Rot << endl;
+   cout << "RotT" << RotT << endl;
+   Rot.Multiply(RotT, Temp);
+   cout << "Rot.RotT" << Temp << endl;
+
+   cout << "Testing MMatrix 3d\n";
+
+   TPZArtDiff::MMatrix(sol, us, 1.4, M, Mi);
+
+   cout << "M" << M << endl;
+   cout << "Mi" << Mi << endl;
+   M.Multiply(Mi, Temp);
+   cout << "M.Mi" << Temp << endl;
+
+   cout << "Testing SUPG 3d\n";
+
+   TPZArtDiff::EigenSystemSUPG(sol, us, c, 1.4, X, Xi, LambdaSUPG);
+
+   cout << "X" << X << endl;
+   cout << "Xi" << Xi << endl;
+   X.Multiply(Xi, Temp);
+   cout << "X.Xi" << Temp << endl;
+   TPZEulerConsLaw2::cSpeed(sol, 1.4, c);
+
+   cout << "LambdaSUPG" << LambdaSUPG;
+
+   cout << "Testing whole SUPG decomposition\n";
+   RotT. Multiply(M, Temp);
+   Temp. Multiply(X, Temp2);
+   Temp2.Multiply(LambdaSUPG, Temp);
+   Temp. Multiply(Xi, Temp2);
+   Temp2.Multiply(Mi, Temp);
+   Temp. Multiply(Rot, Temp2);
+
+   cout << Temp2;
+
+   cout << "Testing Bornhaus 3d\n";
 
    aaS.Resize(3);
    aaS[0] = 2.1;
    aaS[1] = 2.3;
    aaS[2] = 1.7;
 
-   TPZEulerConsLaw2::uRes(sol,us);
-
-   TPZEulerConsLaw2::cSpeed(sol, 1.4, c);
-
-   TPZArtDiff::EigenSystemSUPG(sol, us, c, 1.4, Y, Yi, Lambda);
+   TPZArtDiff::EigenSystemBornhaus(sol, us, c, 1.4, aaS, Y, Yi, LambdaB);
 
    cout << "Y" << Y << endl;
    cout << "Yi" << Yi << endl;
    Y.Multiply(Yi, Temp);
    cout << "Y.Yi" << Temp << endl;
-
-  }
+ }
 }
 
