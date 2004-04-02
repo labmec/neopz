28b43ba8f96c01e3978506a9e54f70fd7f88b400
rgdamas <rgdamas@a693bd38-532a-e730-8659-88a8c11ee2f8>
2004-04-02 16:14:14 +0000

    Program to test and watch the matrix in Data Explorer.

---
+++ Projects/visualmatrix/main.cpp
@@ -0,0 +1,16 @@
+#include <pzvisualmatrix.h>
+#include <fstream.h>
+
+main()
+{
+  cout << "visual matrix." << endl;
+  TPZFMatrix matrix(3,3);
+  int i, j;
+  for(i=0; i< matrix.Cols(); i++){
+    for(j=0; j<matrix.Rows(); j++){
+      matrix(i,j)= i*3 +j;
+    }
+  }
+  VisualMatrix(matrix, "visualmatrix.dx");
+
+}
