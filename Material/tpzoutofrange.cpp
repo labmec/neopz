0d81fcc0c8556be2c85996e973bdc70ce077b556
phil <phil@a693bd38-532a-e730-8659-88a8c11ee2f8>
2004-06-07 21:54:08 +0000

    Included class for exception throw

---
+++ Material/tpzoutofrange.cpp
@@ -0,0 +1,23 @@
+//
+// C++ Implementation: tpzoutofrange
+//
+// Description: 
+//
+//
+// Author: Philippe Devloo <phil@fec.unicamp.br>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "tpzoutofrange.h"
+
+TPZOutofRange::TPZOutofRange()
+{
+}
+
+
+TPZOutofRange::~TPZOutofRange()
+{
+}
+
+
