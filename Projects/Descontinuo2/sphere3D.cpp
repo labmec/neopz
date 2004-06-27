37c8710ae637e86ca57cc871884a23c47b6a62c9
erick <erick@a693bd38-532a-e730-8659-88a8c11ee2f8>
2004-06-27 00:38:03 +0000

    changed boundaries - working well

--- Projects/Descontinuo2/sphere3D.cpp
+++ Projects/Descontinuo2/sphere3D.cpp
@@ -12,9 +12,12 @@
 #include <iostream>
 #include <fstream>
 #include "TPZGeoElement.h"
+#include "pzshapecube.h"
 #include "pzshapequad.h"
 #include "pzgeoquad.h"
+#include "TPZGeoCube.h"
 #include "pzrefquad.h"
+#include "TPZRefCube.h"
 #include "TPZGeoLinear.h"
 #include "TPZRefLinear.h"
 #include "pzbstrmatrix.h"
@@ -27,9 +30,9 @@ void SpherePoints(TPZVec< TPZVec<REAL> > & pt, TPZVec< TPZVec< int> > &elms, int
 
    int index;
    int nn = nSubdiv + 1;
-   int nLayerPts = nn;
+   int nLayerPts = nSubdiv * 2;
 
-   REAL ri = 1., ro = 5.;
+   REAL ri = 1., ro = 25.;
 
    // the shape of the quarter of the sphere is made
    // based on the shape below:
@@ -67,27 +70,44 @@ void SpherePoints(TPZVec< TPZVec<REAL> > & pt, TPZVec< TPZVec< int> > &elms, int
 		v7(3,0.);
 
    TPZVec<REAL> coord(3), ref1(3), ref2(3);
-   REAL alpha = 1. / ((double) nn + 1);
+   REAL alpha = 1. / ((double) nn) * .7;
    REAL beta = 1. - alpha;
-   REAL alpha2 = 1. / ((double) nn);
+   REAL alpha2 = 1. / ((double) nn) * 1.2;
    REAL beta2 = 1. - alpha2;
    REAL alphan, betan;
    int i, j, k;
 
    v1[2] = 1.;
-   v2[1] = 1.;
+
+   v2[1] = 1./sqrt(2.);
+   v2[2] = 1./sqrt(2.);
+
    v3[0] = -1.;
 
+   REAL size4 = 0., size5 = 0.;
+   // computing v4 and v5 normalized
    for(i = 0; i < 3; i++)
    {
       v4[i] = v3[i] * alpha  + v1[i] * beta;
+      size4 += v4[i] * v4[i];
       v5[i] = v3[i] * alpha2 + v2[i] * beta2;
-      v6[i] = v2[i];
-      v7[i] = v5[i];
+      size5 += v5[i] * v5[i];
    }
+   size5 = sqrt(size5);
+   size4 = sqrt(size4);
+   for(i = 0; i < 3; i++)
+   {
+      v4[i] /= size4;
+      v5[i] /= size5;
+   }
+
+   v6 = v2;
+   v7 = v5;
+
 
    v7[1] *= -1.;
    v6[1] *= -1.;
+
 
    // notable measures
    int nPtsPerLayer = 2 * nn + 1;
@@ -184,8 +204,12 @@ void SpherePoints(TPZVec< TPZVec<REAL> > & pt, TPZVec< TPZVec< int> > &elms, int
       }
    }
 
+   // moving point with ref number = 4 - less deformed elements
+   pt[refIndex[4]][0] *= .7;
+   pt[refIndex[4]+2][0] *= .7;
+
    //normalizing vectors, so that they fit in a sphere
-   for(i = 0; i < nPtsPerLayer; i++)
+   for(i = 0; i < nPtsPerSphereLayer; i++)
    {
       REAL size = pt[i][0] * pt[i][0] +
                   pt[i][1] * pt[i][1] +
@@ -196,19 +220,79 @@ void SpherePoints(TPZVec< TPZVec<REAL> > & pt, TPZVec< TPZVec< int> > &elms, int
       pt[i][2] /= size;
    }
 
-   //creating all points
-   for(i = nLayerPts-1; i >= 0; i--)
+   //creating internal / external points
+   for(j = 0; j < nPtsPerSphereLayer; j++)
    {
-      alpha = ri + xpg(4., i, nLayerPts-1) * (ri - ro);
-      for(j = 0; j < nPtsPerLayer; j++)
+      coord[0] = pt[j][0];
+      coord[1] = pt[j][1];
+      coord[2] = pt[j][2];
+
+      // adjusting points to cones:
+      REAL a,b,c, delta, sol1, sol2;
+      a = - coord[0] * coord[0] +
+            coord[1] * coord[1] +
+	    coord[2] * coord[2];
+      b = fabs(coord[0]) * 2.;
+      c = -1;
+      delta = b * b - 4. * a * c;
+      delta = sqrt(delta);
+
+      if(fabs(a) < 1e-4)
       {
-         pt[i * nLayerPts + j][0] = pt[i][0] * alpha;
-	 pt[i * nLayerPts + j][1] = pt[i][1] * alpha;
-	 pt[i * nLayerPts + j][2] = pt[i][2] * alpha;
+         sol1 = 1. / b;
+      }else{
+         sol1 =( -b + delta) / 2. / a;
+	 sol2 =( -b - delta) / 2. / a;
+
+	 if(sol1 * sol2 < 0.)
+	 {
+            sol1 = max(sol1, sol2);
+	 }else{
+            sol1 = min(sol1, sol2);
+	 }
+      }
+
+      coord[0] *= ro * sol1;
+      coord[1] *= ro * sol1;
+      coord[2] *= ro * sol1;
+      pt[(nLayerPts - 1) * nPtsPerSphereLayer + j] = coord;
+
+      coord[0] = pt[j][0] * ri;
+      coord[1] = pt[j][1] * ri;
+      coord[2] = pt[j][2] * ri;
+      pt[j] = coord;
+
+      // other points
+      for(i = 1; i < nLayerPts-1; i++)
+      {
+         alpha = xpg(pow((ro-ri),1./((double) nLayerPts-1)), i, nLayerPts-1);
+	 beta = 1. - alpha;
+         coord[0] = pt[(nLayerPts - 1) * nPtsPerSphereLayer + j][0] * alpha +
+	            pt[j][0] * beta;
+         coord[1] = pt[(nLayerPts - 1) * nPtsPerSphereLayer + j][1] * alpha +
+	            pt[j][1] * beta;
+         coord[2] = pt[(nLayerPts - 1) * nPtsPerSphereLayer + j][2] * alpha +
+	            pt[j][2] * beta;
+         pt[i * nPtsPerSphereLayer + j] = coord;
       }
    }
 
-// quadrilateral data
+/*
+   //creating all points
+   for(i = nLayerPts-1; i >= 0; i--)
+   {
+      alpha = ri + xpg(pow(ro,1./((double) nLayerPts-1)), i, nLayerPts-1) * (ro - ri);
+      for(j = 0; j < nPtsPerSphereLayer; j++)
+      {
+         coord[0] = pt[j][0] * alpha;
+	 coord[1] = pt[j][1] * alpha;
+	 coord[2] = pt[j][2] * alpha;
+
+	 pt[i * nPtsPerSphereLayer + j] = coord;
+      }
+   }
+*/
+// cube data
 
    int nElsPerLayer = nn * 2;
    int nElsPerSphereLayer = nSubdiv * nElsPerLayer + nSubdiv * 2;
@@ -236,11 +320,11 @@ void SpherePoints(TPZVec< TPZVec<REAL> > & pt, TPZVec< TPZVec< int> > &elms, int
 	    elms[index + k] = nodes;
 	 }
       }
-      index = i * nElsPerSphereLayer + nSubDiv * nElsPerLayer;
+      index = i * nElsPerSphereLayer + nSubdiv * nElsPerLayer;
       for(j = 0; j < nSubdiv; j++)
       {
          // left elements
-	 nodes[0] = i * nPtsPerLayer + nSubdiv * nPtsPerLayer + j;
+	 nodes[0] = i * nPtsPerSphereLayer + nSubdiv * nPtsPerLayer + j;
 	 nodes[1] = nodes[0] + 1;
 	 nodes[2] = (i+1) * nPtsPerSphereLayer - nSubdiv + j + 1;
 	 nodes[3] = nodes[2] - 1;
@@ -255,7 +339,7 @@ void SpherePoints(TPZVec< TPZVec<REAL> > & pt, TPZVec< TPZVec< int> > &elms, int
 	 elms[index + j] = nodes;
 
 	 // right elements
-	 nodes[0] = i * nPtsPerSphereLayer + nn * nPtsPerLayer - j - 1;
+	 nodes[0] = i * nPtsPerSphereLayer + nn * nPtsPerLayer - j - 2;
 	 nodes[1] = nodes[0] + 1;
 	 int tmp  = nodes[2];
 	 nodes[2] = nodes[3];
@@ -268,11 +352,10 @@ void SpherePoints(TPZVec< TPZVec<REAL> > & pt, TPZVec< TPZVec< int> > &elms, int
 	 nodes[5] = nodes[1] + nPtsPerSphereLayer;
 	 nodes[6] = nodes[2] + nPtsPerSphereLayer;
 	 nodes[7] = nodes[3] + nPtsPerSphereLayer;
-	 elms[index + nSubdiv * 2 - j] = nodes;
+	 elms[index + nSubdiv * 2 - j - 1] = nodes;
       }
 
    }
-
 }
 
 TPZGeoMesh * CreateSphereGeoMesh(TPZVec< TPZVec< REAL > > & nodes,
@@ -293,7 +376,7 @@ TPZGeoMesh * CreateSphereGeoMesh(TPZVec< TPZVec< REAL > > & nodes,
 
    for( i = 0; i < elms.NElements(); i++)
    {
-      gEls[i] = gmesh->CreateGeoElement(ElType, elms[i], matId, i, nSubdiv);
+      gEls[i] = gmesh->CreateGeoElement(ElType, elms[i], matId, i);
    }
 
 // Constructing neighborhood
@@ -325,7 +408,10 @@ TPZFlowCompMesh *
    TPZGeoElement<TPZShapeLinear,TPZGeoLinear,TPZRefLinear>
                 ::SetCreateFunction(TPZCompElDisc::CreateDisc);
 
-   const int dim = 2;
+   TPZGeoElement<TPZShapeCube,TPZGeoCube,TPZRefCube>
+                ::SetCreateFunction(TPZCompElDisc::CreateDisc);
+
+   const int dim = 3;
 //   int interfdim = dim -1;
 //   TPZCompElDisc::gInterfaceDimension = interfdim;
 
@@ -337,7 +423,7 @@ TPZFlowCompMesh *
    SpherePoints(nodes, elms, nSubdiv);
 
 // Creating the geometric mesh
-   TPZGeoMesh * gmesh = CreateSphereGeoMesh(nodes, elms, EQuadrilateral, 1, gElem, nSubdiv);
+   TPZGeoMesh * gmesh = CreateSphereGeoMesh(nodes, elms, /*EQuadrilateral*/ECube, 1, gElem, nSubdiv);
 
    TPZFlowCompMesh * cmesh = new TPZFlowCompMesh(gmesh);
 
@@ -370,35 +456,84 @@ TPZFlowCompMesh *
 // Boundary conditions
 
    TPZBndCond * bc;
-   TPZFMatrix val1(4,4), val2(4,1);
-   REAL ro = 1.7,
-	//u = 5.5,
-	//v = 0,//3.3,
-	p = 2.;
-	//vel2 = u*u + v*v;
+   TPZFMatrix val1(5,5), val2(5,1);
 
-// copiado do Cedric
-   //CC Todas as arestas: DIRICHLET
+   int nn = nSubdiv + 1;
+   int nElsPerLayer = nn * 2;
+   int nElsPerSphereLayer = nSubdiv * nElsPerLayer + nSubdiv * 2;
+   int nLayerEls = nSubdiv * 2 - 1;
+   int i, j;
+
+   //Sphere faces: Wall
    val1.Zero();
    val2.Zero();
-   val2(0,0) = ro;
-   val2(1,0) = /*ro */ .5;
-   val2(2,0) = /*ro */ 0.;
-   val2(3,0) = p;///(gamma-1.0) + 0.5 * ro * vel2;
-/*
-   val2(0,0) = ro;
-   val2(1,0) = ro * u;
-   val2(2,0) = ro * v;
-   val2(3,0) = p/(gamma-1.0) + 0.5 * ro * vel2;*/
-   TPZGeoElBC((TPZGeoEl *)gElem[0],4,-1,*gmesh);
-   TPZGeoElBC((TPZGeoEl *)gElem[0],5,-1,*gmesh);
-   TPZGeoElBC((TPZGeoEl *)gElem[0],6,-1,*gmesh);
-   TPZGeoElBC((TPZGeoEl *)gElem[0],7,-1,*gmesh);
-   bc = mat->CreateBC(-1,9,val1,val2);
+   for( i = 0; i < nElsPerSphereLayer; i++)
+   {
+      TPZGeoElBC((TPZGeoEl *)gElem[i],20,-1,*gmesh);
+   }
+
+   // Symmetry faces: slipwall
+   val1.Zero();
+   val2.Zero();
+   // upper faces
+   for( i = 0; i < nLayerEls; i++)
+      for( j = 0; j < nElsPerLayer; j++)
+      {
+         TPZGeoElBC((TPZGeoEl *)gElem[i*nElsPerSphereLayer + j],21,-1,*gmesh);
+      }
+   // lower faces
+   for( i = 0; i < nLayerEls; i++)
+      for( j = 0; j < nn; j++)
+      {
+         // left elms.
+         TPZGeoElBC((TPZGeoEl *)gElem[i*nElsPerSphereLayer + j * nElsPerLayer],24,-1,*gmesh);
+	 // right elms.
+	 if(j < nn-1)
+	 {
+            TPZGeoElBC((TPZGeoEl *)gElem[i*nElsPerSphereLayer + (j+1) * nElsPerLayer - 1],22,-1,*gmesh);
+	 }else{
+            TPZGeoElBC((TPZGeoEl *)gElem[(i+1)*nElsPerSphereLayer - 1],22,-1,*gmesh);
+	 }
+      }
+   // slipwall
+   bc = mat->CreateBC(-1,5,val1,val2);
+   cmesh->InsertMaterialObject(bc);
+
+   //external sphere faces: inflow/outflow
+   REAL Mach;
+   cout << "\nMach number\n";
+   cin >> Mach;
+
+   val2(0,0) = 1.;// rho
+   val2(1,0) = Mach;// Machx
+   val2(2,0) = 0.;// Machy
+   val2(3,0) = 0.;// Machz
+   val2(4,0) = 1.;// pressure
+
+   TPZVec<REAL> param(3,0.), X(3,0.);
+   for( i = 0; i < nElsPerSphereLayer; i++)
+   {
+
+      TPZGeoEl * pEl = gElem[i +  (nLayerEls - 1) * nElsPerSphereLayer];
+      pEl -> X(param, X);
+
+      if(X[0] > 0.)
+      {
+      // outflow
+         TPZGeoElBC(pEl,25,-2,*gmesh);
+      }else{
+      // inflow
+         TPZGeoElBC(pEl,25,-3,*gmesh);
+      }
+   }
+   // outflow
+   bc = mat->CreateBC(-2,9,val1,val2);
+   cmesh->InsertMaterialObject(bc);
+   // inflow
+   bc = mat->CreateBC(-3,9,val1,val2);
    cmesh->InsertMaterialObject(bc);
 
    cmesh->AutoBuild();
-   //cmesh->AdjustBoundaryElements();
 
 // printing meshes
 
@@ -415,9 +550,8 @@ TPZFlowCompMesh *
    Solution.Zero();
 
    int nVars = Solution.Rows();
-   int k;
-   for(k = 0; k < nVars; k++)Solution(k,0) = 0;//.05;
-   int j, NSolutionBlocks;
+   for(i = 0; i < nVars; i++)Solution(i,0) = 0;
+   int NSolutionBlocks;
    //TPZBlock * pBlock = cmesh->Block();
    NSolutionBlocks = cmesh->Block().NBlocks();
    int nShape = Solution.Rows() / NSolutionBlocks / (dim + 2);
@@ -426,29 +560,17 @@ TPZFlowCompMesh *
    {
       int blockOffset = cmesh->Block().Position(j) + lastShapeFun;
 
-      REAL ro = 1.7,
-	   u = .5,
-	   v = 0.01,//0.0821588,//3.5,
-	   p = 2.,
-	   c = sqrt(1.4 * p / ro);
-      REAL vel2 = (u*u + v*v) * c*c;
-      Solution(blockOffset  ,0) = ro;
-      Solution(blockOffset+1,0) = ro * u * c;
-      Solution(blockOffset+2,0) = ro * v * c;
-      Solution(blockOffset+3,0) = p/(gamma-1.0) + 0.5 * ro * vel2;
- /*     ro = 1.4;
-      u = 3.3;
-      v = 5.5;
-      p = 4.;
-      vel2 = u*u + v*v;
-      Solution(blockOffset  ,0) = ro;
-      Solution(blockOffset+1,0) = ro * u;
-      Solution(blockOffset+2,0) = ro * v;
-      Solution(blockOffset+3,0) = (p/(gamma-1.0) + 0.5 * ro * vel2);
-*/
-/*      int nVars = Solution.Rows();
-      int k;
-      for(k = 4; k < nVars; k++)Solution(k,0) = .05;*/
+      REAL rho = 1.0,
+           p = 1.,
+	   u = sqrt(1.4 * p / rho) * Mach,
+	   v = 0.,
+	   w = 0.,
+	   vel2 = u*u + v*v + w*w;
+      Solution(blockOffset  ,0) = rho;
+      Solution(blockOffset+1,0) = rho * u;
+      Solution(blockOffset+2,0) = rho * v;
+      Solution(blockOffset+3,0) = rho * w;
+      Solution(blockOffset+4,0) = p/(gamma-1.0) + 0.5 * rho * vel2;
    }
 
    cmesh->LoadSolution(Solution);
