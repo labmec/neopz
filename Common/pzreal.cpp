8ea5cde92abd55d39832879ab292c8c0bd7db2c2
phil <phil@a693bd38-532a-e730-8659-88a8c11ee2f8>
2004-09-07 23:47:34 +0000

    O PZ agora vem com opcao de contar o numero de operacoes de ponto flutuante! Baste redefinir o typedef dentro do pzreal.h Para compilar, tive que explicitar instanciacao de certas variaveis. FUNCIONA!!

---
+++ Common/pzreal.cpp
@@ -0,0 +1,50 @@
+//
+// C++ Implementation: pzreal
+//
+// Description: 
+//
+//
+// Author: Philippe R. B. Devloo <phil@fec.unicamp.br>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "pzreal.h"
+#include <string>
+using namespace std;
+
+TPZCounter TPZFlopCounter::gCount;
+
+static string names[] = {
+  "sum",
+  "prod",
+  "div",
+  "sqrt",
+  "pow",
+  "cos",
+  "sin",
+  "acos",
+  "asin",
+  "atan",
+  "exp",
+  "log"
+};
+
+void TPZCounter::Print(std::ostream &out) const
+{
+  int i;
+  for(i=0; i<gNumOp; i++)
+    {
+      out << names[i] << " " << fCount[i] << endl;
+    }
+}
+
+std::ostream &operator<<(std::ostream &out,const TPZCounter &count)
+{
+  int i;
+  for(i=0; i<gNumOp; i++)
+    {
+      out << names[i] << "/" << count.fCount[i] << "\t";
+    }
+  return out;
+}
