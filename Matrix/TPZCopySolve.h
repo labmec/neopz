508f1ccf0d2a71f624745fdb444997a6c82de6b1
phil <phil@a693bd38-532a-e730-8659-88a8c11ee2f8>
2004-12-17 13:35:25 +0000

    compiling copy

--- Matrix/TPZCopySolve.h
+++ Matrix/TPZCopySolve.h
@@ -5,21 +5,33 @@
 #include "pzsolve.h"
 class TPZFMatrix;
 class TPZCopySolve : public TPZMatrixSolver {
-public:  
+public:
 
+  TPZCopySolve(TPZMatrix *mat) : TPZMatrixSolver(mat)
+  {
+  }
+  TPZCopySolve(const TPZCopySolve &other) : TPZMatrixSolver(other)
+  {
+  }
     /**
      *      Solves the system of linear equations stored in current matrix
      *      As this class implements only a copy operation, it just copies u to F;
      *      @param F contains Force vector
      *      @param result contains the solution
      */
-  virtual void Solve(const TPZFMatrix &F, TPZFMatrix &result, TPZFMatrix * ){result=F;}  
+    void Solve(const TPZFMatrix &F, TPZFMatrix &result, TPZFMatrix *residual)
+      {
+        result=F;
+      }  
 
     /**
      *      Clones the current object returning a pointer of type TPZSolver
       */
-  virtual TPZSolver *Clone() const {return new TPZCopySolve;}
+    TPZSolver *Clone() const 
+    {
+      return new TPZCopySolve(*this);
+    }
 
-;
+
 };
 #endif //TPZCOPYSOLVE_H
