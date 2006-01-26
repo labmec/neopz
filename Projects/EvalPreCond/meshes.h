0b42958f36b54476a2b4bfeea5caeb65f452dc0e
tiago <tiago@a693bd38-532a-e730-8659-88a8c11ee2f8>
2006-01-26 15:35:18 +0000

    *** empty log message ***

---
+++ Projects/EvalPreCond/meshes.h
@@ -0,0 +1,44 @@
+// -*- c++ -*-
+
+//$Id: meshes.h,v 1.1 2006-01-26 15:35:18 tiago Exp $
+
+class TPZCompMesh;
+#include "pzvec.h"
+
+/** Barra tracionada - Validado
+ */
+TPZCompMesh * BarraTracionada(int h, int p); 
+
+/** Barra tracionada sem Dirichlet, apenas Neumann */
+TPZCompMesh * BarraTracionadaNeumann(int h, int p); 
+
+/** Barra tracionada girada de 10 graus em torno do eixo x.
+ * int cubo = 1 faz um cubo - Validado
+ * int cubo = 2 faz um prisma - Validado
+ */
+TPZCompMesh * BarraTracionadaGirada(int h, int p);
+ 
+/** Viga engastada com momento concentrado na extremidade livre - Validado
+ */
+TPZCompMesh * VigaEngastada(int h, int p);
+void MomentoExtremidade(TPZVec<REAL> &pto, TPZVec<REAL> &force);
+
+/** Viga engastada com forca de corpo (volumetrica) - Validado
+ */
+TPZCompMesh * VigaEngastadaForcaVolume(int h, int p);
+
+
+ 
+/** Viga bi-engastada
+ */
+// TPZCompMesh * VigaBiEngastada(int h, int p);
+ 
+// /** Vigas em balanćo
+//  */
+// TPZCompMesh * VigasBalanco(int h, int p);
+//  
+// /** Bloco confinado
+//  */
+// TPZCompMesh * BlocoConfinado(int h, int p);
+
+void SetPOrder(int p);
