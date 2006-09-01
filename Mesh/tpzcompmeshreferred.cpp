b988018d610f43f2b267c540b7b6c83ab3d2cc89
joao <joao@a693bd38-532a-e730-8659-88a8c11ee2f8>
2006-09-01 19:21:57 +0000

    First version of Referred Computational Mesh

---
+++ Mesh/tpzcompmeshreferred.cpp
@@ -0,0 +1,65 @@
+//
+// C++ Implementation: tpzcompmeshreferred
+//
+// Description: 
+//
+//
+// Author: Philippe R. B. Devloo <phil@fec.unicamp.br>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "tpzcompmeshreferred.h"
+#include "pzgmesh.h"
+#include "pzcompel.h"
+
+TPZCompMeshReferred::TPZCompMeshReferred(TPZGeoMesh *gmesh)
+ : TPZCompMesh(gmesh), fReferredIndices(0), fReferred(0)
+{
+}
+
+TPZCompMeshReferred::TPZCompMeshReferred(const TPZCompMeshReferred &copy)
+ : TPZCompMesh(*this), fReferredIndices(copy.fReferredIndices), fReferred(copy.fReferred)
+{
+}
+
+TPZCompMeshReferred::~TPZCompMeshReferred()
+{
+}
+
+
+void TPZCompMeshReferred::LoadReferred(TPZCompMesh *mesh)
+{
+  fReferredIndices.resize(this->NElements());
+  fReferred = mesh;
+  TPZGeoMesh *gmesh = mesh->Reference();
+  gmesh->ResetReference();
+  mesh->LoadReferences();
+  int iel,nel = NElements();
+  for(iel=0; iel<nel; iel++)
+  {
+    TPZCompEl *cel = fElementVec[iel];
+    fReferredIndices[iel] = -1;
+    TPZGeoEl *gel = cel->Reference();
+    if(!gel)continue;
+    TPZCompEl *cel2 = gel->Reference();
+    if(!cel2) continue;
+    fReferredIndices[iel] = cel2->Index();
+  }
+}
+
+void TPZCompMeshReferred::ResetReferred()
+{
+  fReferredIndices.resize(0);
+  fReferred = 0;
+}
+
+TPZCompEl *TPZCompMeshReferred::ReferredEl(int index)
+{
+  if(!fReferred) return 0;
+  int celindex = fReferredIndices[index];
+  if(celindex <0) return 0;
+  TPZCompEl *cel = fReferred->ElementVec()[celindex];
+  return cel;
+}
+
---
+++ Mesh/tpzcompmeshreferred.h
@@ -0,0 +1,50 @@
+//
+// C++ Interface: tpzcompmeshreferred
+//
+// Description: 
+//
+//
+// Author: Philippe R. B. Devloo <phil@fec.unicamp.br>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef TPZCOMPMESHREFERRED_H
+#define TPZCOMPMESHREFERRED_H
+
+#include <pzcmesh.h>
+#include <vector>
+
+/**
+This class implements the structure to allow one mesh to refer to the solution of another
+
+@author Philippe R. B. Devloo
+*/
+class TPZCompMeshReferred : public TPZCompMesh
+{
+
+  std::vector<int> fReferredIndices;
+
+  TPZCompMesh *fReferred;
+
+public:
+    TPZCompMeshReferred(TPZGeoMesh *gmesh);
+
+    TPZCompMeshReferred(const TPZCompMeshReferred &compmesh);
+
+    virtual ~TPZCompMeshReferred();
+
+    void LoadReferred(TPZCompMesh *mesh);
+
+    void ResetReferred();
+
+    TPZCompEl *ReferredEl(int index);
+
+    TPZCompMesh *ReferredMesh()
+    {
+      return fReferred;
+    }
+
+};
+
+#endif
