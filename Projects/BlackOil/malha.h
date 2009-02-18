d61b3a4e9bd5d83b3388c631cedfaf7a58f9463c
fortiago <fortiago@a693bd38-532a-e730-8659-88a8c11ee2f8>
2009-02-18 11:54:03 +0000

    Curso do Prof. Denis

--- Projects/BlackOil/malha.h
+++ Projects/BlackOil/malha.h
@@ -1,4 +1,4 @@
-//$Id: malha.h,v 1.2 2008-11-25 13:28:15 fortiago Exp $
+//$Id: malha.h,v 1.3 2009-02-18 11:54:03 fortiago Exp $
 
 #include "pzcmesh.h"
 
@@ -11,3 +11,18 @@ TPZCompMesh *Unidimensional(int h, double deltaT);
   * O problema eh agua em cima e oleo em baixo que devem migrar.
   */
 TPZCompMesh *UnidimensionalGravidade(int h, double deltaT);
+
+TPZGeoMesh * QuarterFiveSpot(int ndiv_xy, int ndiv_z, double lxy, double lz, double wellDiam, double factor);
+
+TPZGeoMesh * QuarterFiveSpotReg(double lxy, double lz);
+
+TPZCompMesh * QuarterFiveSpot(TPZGeoMesh * gmesh, double deltaT);
+
+double AreaPoco(TPZGeoMesh * gmesh, int matid);
+
+void DivideMalha(TPZGeoMesh * gmesh);
+
+void DivideTornoPocos(TPZGeoMesh * gmesh);
+
+void ScaleVec(TPZVec<REAL> &NodeIni, TPZVec<REAL> &NodeFin, double Norm, TPZVec<REAL> &OriginVec, TPZVec<REAL> &OutputVec);
+
