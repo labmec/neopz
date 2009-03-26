ae869a8067ae0dc3f115d3a60545b393f1435a93
fortiago <fortiago@a693bd38-532a-e730-8659-88a8c11ee2f8>
2009-03-26 20:35:33 +0000

    Trabalhos do Caju em mapeamentos nÃ£o-lineares

---
+++ Geom/tpzgeoblend.h
@@ -0,0 +1,106 @@
+#ifndef TPZGEOBLEND_H
+#define TPZGEOBLEND_H
+
+#include "pzgeotriangle.h"
+#include "TPZGeoCube.h"
+#include "pzgeoprism.h"
+#include "pzgeoel.h"
+#include "pznoderep.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace pzgeom;
+using namespace pztopology;
+
+  /**
+  / Class made by Paulo Cesar de Alvarenga Lucci (Caju)
+  / LabMeC - FEC - UNICAMP
+  / 2007
+ */
+
+template <class TGeo>
+class TPZGeoBlend : public TGeo {
+
+public:
+
+     bool IsLinearMapping() const { 
+          return false; 
+     }
+     bool IsGeoBlendEl() const { 
+          return true; 
+     }
+
+     TPZGeoBlend(TPZVec<int> &nodeindexes, TPZGeoMesh &mesh) : TGeo(nodeindexes,mesh) {
+     }
+
+     TPZGeoBlend() : TGeo() {
+     }
+
+     TPZGeoBlend(const TPZGeoBlend &cp,std::map<int,int> & gl2lcNdMap) : TGeo(cp,gl2lcNdMap) {
+     }
+
+     TPZGeoBlend(const TPZGeoBlend &cp) : TGeo(cp) {
+     }
+
+     TPZGeoBlend(const TPZGeoBlend &cp, TPZGeoMesh &) : TGeo(cp) {
+     }
+
+     void SetNeighbourInfo(int side, TPZGeoElSide &neigh, TPZTransform &trans) {
+          fNeighbours[side-TGeo::NNodes] = neigh;
+          fTrans[side - TGeo::NNodes] = trans;
+     }
+
+     TPZGeoElSide Neighbour(int side) {
+          return fNeighbours[side-TGeo::NNodes];
+     }
+
+     TPZTransform TransfBetweenNeigh(int side) {
+          return fTrans[side - TGeo::NNodes];
+     }
+
+     /**
+      * returns the type name of the element
+      */
+    static std::string TypeName() { return TGeo::TypeName();} 
+
+    void X(TPZFMatrix & coord, TPZVec<REAL>& par, TPZVec<REAL> &result);
+
+    void Jacobian(TPZFMatrix & coord, TPZVec<REAL>& par, TPZFMatrix &jacobian, TPZFMatrix &axes,REAL &detjac,TPZFMatrix &jacinv);
+
+    /**Print all relevant data of the element to cout*/
+    void Print(std::ostream & out = std::cout);
+
+    /**
+    / Este método percorre todos os lados do elemento fornecido (el) e, para cada lado,
+    / percorre todos os vizinhos até retornar a si próprio. Se neste processo encontrar algum elemento que
+    / seja quadrático e não seja do tipo TPZGeoBlend, recorre ao método SetNeighbourInfo() para estabelecer que este
+    / elemento encontrado será seu vizinho pelo respectivo lado.
+   */
+    void Initialize(TPZGeoEl *refel);
+
+    void Initialize(TPZVec<int> &nodeindexes, TPZGeoMesh &mesh);
+
+   /**
+    * Method which creates a geometric boundary condition 
+    * element based on the current geometric element, 
+    * a side and a boundary condition number
+    */
+  TPZGeoEl *CreateBCGeoEl(TPZGeoEl *orig, int side,int bc);
+
+  TPZGeoEl *CreateBCGeoBlendEl(TPZGeoEl *orig,int side,int bc);
+
+
+  TPZGeoEl *CreateGeoBlend(TPZGeoMesh &mesh, MElementType type, TPZVec<int>& nodeindexes, int matid, int& index);
+
+
+protected:
+
+    void MapToNeighSide(int side, TPZVec<REAL> &InternalPar, TPZVec<REAL> &NeighPar, TPZFMatrix &JacNeighSide);
+    TPZGeoElSide fNeighbours[1+TGeo::NSides - TGeo::NNodes];
+    TPZTransform fTrans[1+TGeo::NSides - TGeo::NNodes];
+};
+
+TPZGeoEl *CreateGeoBlendElement(TPZGeoMesh &mesh, MElementType type, TPZVec<int>& nodeindexes, int matid, int& index);
+
+#endif
---
+++ SpecialMaps/tpzellipse.cpp
@@ -0,0 +1,162 @@
+#include "tpzellipse.h"
+
+#include "TPZGeoLinear.h"
+#include "pzshapelinear.h"
+
+#include "pzfmatrix.h"
+#include "pzvec.h"
+#include "pzgmesh.h"
+#include "pzgeoel.h"
+#include "pznoderep.h"
+#include "pzgnode.h"
+#include "pzreal.h"
+
+using namespace std;
+using namespace pzgeom;
+using namespace pzshape;
+
+void TPZEllipse::SetAxes(double a, double b)
+{
+    fA = a;
+    fB = b;
+}
+
+/// Este método estabelece o mapeamento de um elemento linear contido num
+/// hexaedro de dimensões (2.fA,2.fB,h) para a elipse inscrita no plano xy deste hexaedro
+/// ou seja: elipse -> (semi-eixos fA e fB e extrudada no valor de h)
+void TPZEllipse::X(TPZFMatrix &nodes,TPZVec<REAL> &loc,TPZVec<REAL> &result)
+{
+    //Mapeando o ponto do elemento mestre para a malha.
+    TPZGeoLinear::X(nodes,loc,result);
+
+    //Parametrizando para o elemento mestre do hexaedro (mantém coordenada z, i.e. result[2]).
+    double  qsi = result[0]/fA;
+    double  eta = result[1]/fB;
+
+#if Debug
+if(fabs(qsi) > 1. || fabs(eta) > 1.)
+{
+    cout << "\nThe linear element don't belong to the domain of the ellipse circumscribed hexahedron [(-fA,-fB,-h/2),(fA,fB,h/2)]\n";
+    cout << "See TPZEllipse class!\n";
+    exit(-1);
+}
+#endif
+
+    //Realizando o mapeamento no plano xy
+    if(fabs(qsi) < 1.E-8 && fabs(eta) < 1.E-8)
+    {
+        result[0] = 0.;
+        result[1] = 0.;
+        return;
+    }
+    if( fabs(qsi) <= fabs(eta) )
+    {
+        result[0] = fA*fabs(eta)*qsi/sqrt(qsi*qsi + eta*eta);
+        result[1] = fB*fabs(eta)*eta/sqrt(qsi*qsi + eta*eta);
+        return;
+    }
+    if( fabs(qsi) > fabs(eta) )
+    {
+        result[0] = fA*fabs(qsi)*qsi/sqrt(qsi*qsi + eta*eta);
+        result[1] = fB*fabs(qsi)*eta/sqrt(qsi*qsi + eta*eta);
+        return;
+    }
+}
+
+void TPZEllipse::Jacobian(TPZFMatrix &coord, TPZVec<REAL> &par, TPZFMatrix &jacobian, TPZFMatrix &axes, REAL &detjac, TPZFMatrix &jacinv)
+{
+    TPZVec<REAL> result(3);
+    TPZGeoLinear::X(coord,par,result);
+    double  qsi = result[0]/fA;
+    double  eta = result[1]/fB;
+
+    TPZGeoLinear::Jacobian(coord, par, jacobian, axes, detjac, jacinv);
+    TPZFMatrix JacLine(3,1);
+
+    JacLine(0,0) = jacobian(0,0)*axes(0,0)/fA;
+    JacLine(1,0) = jacobian(0,0)*axes(0,1)/fB;
+    JacLine(2,0) = jacobian(0,0)*axes(0,2);
+
+    TPZFMatrix JacEllipse(3,3);
+
+    if(fabs(qsi) < 1.E-8 && fabs(eta) < 1.E-8)
+    {
+        JacEllipse(0,0) = fA;
+        JacEllipse(0,1) = 0.;
+        JacEllipse(0,2) = 0.;
+
+        JacEllipse(1,0) = 0.;
+        JacEllipse(1,1) = fB;
+        JacEllipse(1,2) = 0.;
+
+        JacEllipse(2,0) = 0.;
+        JacEllipse(2,1) = 0.;
+        JacEllipse(2,2) = 1.;
+    }
+    else if( fabs(qsi) <= fabs(eta) )
+    {
+        JacEllipse(0,0) =  fA*pow(eta*eta,1.5)/pow(eta*eta + qsi*qsi,1.5);
+        JacEllipse(0,1) =  fA*eta*qsi*qsi*qsi/(fabs(eta)*pow(eta*eta + qsi*qsi,1.5));
+        JacEllipse(0,2) =  0.;
+
+        JacEllipse(1,0) = -fB*eta*fabs(eta)*qsi/pow(eta*eta + qsi*qsi,1.5);
+        JacEllipse(1,1) =  fB*fabs(eta)*(eta*eta + 2.*qsi*qsi)/pow(eta*eta + qsi*qsi,1.5);
+        JacEllipse(1,2) =  0.;
+
+        JacEllipse(2,0) =  0.;
+        JacEllipse(2,1) =  0.;
+        JacEllipse(2,2) =  1.;
+    }
+    else if( fabs(qsi) > fabs(eta) )
+    {
+        JacEllipse(0,0) =  fA*fabs(qsi)*(2.*eta*eta + qsi*qsi)/pow(eta*eta + qsi*qsi,1.5);
+        JacEllipse(0,1) = -fA*eta*qsi*fabs(qsi)/pow(eta*eta + qsi*qsi,1.5);
+        JacEllipse(0,2) =  0.;
+
+        JacEllipse(1,0) =  fB*eta*eta*eta*qsi/(fabs(qsi)*pow(eta*eta + qsi*qsi,1.5));
+        JacEllipse(1,1) =  fB*pow(qsi*qsi,1.5)/pow(eta*eta + qsi*qsi,1.5);
+        JacEllipse(1,2) =  0.;
+
+        JacEllipse(2,0) =  0.;
+        JacEllipse(2,1) =  0.;
+        JacEllipse(2,2) =  1.;
+    }
+
+    TPZFMatrix JacTemp(3,1);
+    JacEllipse.Multiply(JacLine,JacTemp);
+    TPZFMatrix axest;
+    JacTemp.GramSchmidt(axest,jacobian);
+    axest.Transpose(&axes);
+
+    detjac = jacobian(0,0);
+    jacinv.Resize(1,1);
+    jacinv(0,0) = 1./detjac;
+}
+
+
+TPZGeoEl *TPZEllipse::CreateBCGeoEl(TPZGeoEl *orig, int side,int bc)
+{
+     if(side==2)
+     {
+          TPZManVector<int> nodes(3);
+          nodes[0] = orig->SideNodeIndex(side,0); nodes[1] = orig->SideNodeIndex(side,1); nodes[2] = orig->SideNodeIndex(side,2); int index;
+          TPZGeoEl *gel = orig->Mesh()->CreateGeoElement(EOned,nodes,bc,index);
+          TPZGeoElSide(gel,0).SetConnectivity(TPZGeoElSide(orig,TPZShapeLinear::SideConnectLocId(side,0)));
+          TPZGeoElSide(gel,1).SetConnectivity(TPZGeoElSide(orig,TPZShapeLinear::SideConnectLocId(side,1)));
+          TPZGeoElSide(gel,2).SetConnectivity(TPZGeoElSide(orig,side));
+          return gel;
+     }
+
+     else if(side==0 || side==1)
+     {
+          TPZManVector<int> nodeindexes(1);
+          nodeindexes[0] = orig->SideNodeIndex(side,0); int index;
+          TPZGeoEl *gel = orig->Mesh()->CreateGeoElement(EPoint,nodeindexes,bc,index);
+          TPZGeoElSide(gel,0).SetConnectivity(TPZGeoElSide(orig,side));
+          return gel;
+     }
+
+     else PZError << "\nTPZGeoLinear::CreateBCGeoEl. Side = " << side << endl;
+     return 0;
+}
+
