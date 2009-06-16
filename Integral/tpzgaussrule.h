4257b7e03205c7e0700a2dc2ae3ecaf61ac6e117
erick <erick@a693bd38-532a-e730-8659-88a8c11ee2f8>
2009-06-16 02:48:56 +0000

    Implemented the integration rule of gauss-lobatto, which places the first and last integration points at the boundaries of the integration interval.

---
+++ Integral/tpzgaussrule.h
@@ -0,0 +1,41 @@
+//
+// C++ Interface: tpzintrule
+//
+// Description: 
+//
+//
+// Author: Philippe R. B. Devloo <phil@fec.unicamp.br>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef TPZGAUSSRULE_H
+#define TPZGAUSSRULE_H
+
+#include "pzreal.h"
+
+/**
+	@author Philippe R. B. Devloo <phil@fec.unicamp.br>
+*/
+class TPZGaussRule{
+public:
+
+  enum {NUMINT_RULES = 38};
+    friend class TPZIntRuleList;
+    short	   fNumInt;		// number of integration points for this object
+    REAL	*fLocation;	// location of the integration point
+    REAL	*fWeight;	// weight of the integration point
+
+    TPZGaussRule(int i);
+    ~TPZGaussRule();
+
+    public:
+
+      short NInt(){ return fNumInt;}	//return number of integration points
+
+      REAL Loc(int i);						//return location of the ith pot
+
+      REAL W(int i);						//return weight for the ith point
+  };
+
+#endif
