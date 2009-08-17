9100bbc2a11b529bfccf5553be97a0d59e7114d0
longhin <longhin@a693bd38-532a-e730-8659-88a8c11ee2f8>
2009-08-17 19:57:56 +0000

    No big changes in code, only minor warnings

--- Matrix/TPZCopySolve.h
+++ Matrix/TPZCopySolve.h
@@ -4,34 +4,36 @@
 #define TPZCOPYSOLVE_H
 #include "pzsolve.h"
 class TPZFMatrix;
-class TPZCopySolve : public TPZMatrixSolver {
+class TPZCopySolve: public TPZMatrixSolver
+{
 public:
 
-  TPZCopySolve(TPZMatrix *mat) : TPZMatrixSolver(mat)
+  TPZCopySolve(TPZMatrix *mat) :
+    TPZMatrixSolver(mat)
   {
   }
-  TPZCopySolve(const TPZCopySolve &other) : TPZMatrixSolver(other)
+  TPZCopySolve(const TPZCopySolve &other) :
+    TPZMatrixSolver(other)
   {
   }
-    /**
-     *      Solves the system of linear equations stored in current matrix
-     *      As this class implements only a copy operation, it just copies u to F;
-     *      @param F contains Force vector
-     *      @param result contains the solution
-     */
-    void Solve(const TPZFMatrix &F, TPZFMatrix &result, TPZFMatrix *residual)
-      {
-        result=F;
-      }  
+  /**
+   *      Solves the system of linear equations stored in current matrix
+   *      As this class implements only a copy operation, it just copies u to F;
+   *      @param F contains Force vector
+   *      @param result contains the solution
+   */
+  void Solve(const TPZFMatrix &F, TPZFMatrix &result, TPZFMatrix *residual)
+  {
+    result = F;
+  }
 
-    /**
-     *      Clones the current object returning a pointer of type TPZSolver
-      */
-    TPZSolver *Clone() const 
-    {
-      return new TPZCopySolve(*this);
-    }
-
+  /**
+   *      Clones the current object returning a pointer of type TPZSolver
+   */
+  TPZSolver *Clone() const
+  {
+    return new TPZCopySolve(*this);
+  }
 
 };
 #endif //TPZCOPYSOLVE_H
