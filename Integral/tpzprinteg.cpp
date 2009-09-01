a5b6cbba14a06207239055ead35bfcd463aa5f79
phil <phil@a693bd38-532a-e730-8659-88a8c11ee2f8>
2009-09-01 19:42:21 +0000

    Removed some instantiations when compiling with borland

--- Integral/tpzprinteg.cpp
+++ Integral/tpzprinteg.cpp
@@ -19,24 +19,25 @@ TPZPrInteg<TFather>::~TPZPrInteg()
 }
 
 template<class TFather>
-    inline TPZIntPoints *TPZPrInteg<TFather>::PrismExtend(int order)
+TPZIntPoints *TPZPrInteg<TFather>::PrismExtend(int order)
 {
   std::cout << "Please implement me " << __PRETTY_FUNCTION__;
   return 0;
 }
 
+#ifndef BORLAND
 template<>
-    inline TPZIntPoints *TPZPrInteg<TPZInt1Point>::PrismExtend(int order)
+TPZIntPoints *TPZPrInteg<TPZInt1Point>::PrismExtend(int order)
 {
   return new TPZPrInteg<TPZPrInteg<TPZInt1Point> >(order);
 }
 
 template<>
-    inline TPZIntPoints *TPZPrInteg< TPZPrInteg<TPZInt1Point> >::PrismExtend(int order)
+TPZIntPoints *TPZPrInteg< TPZPrInteg<TPZInt1Point> >::PrismExtend(int order)
 {
   return new TPZPrInteg<TPZPrInteg<TPZPrInteg<TPZInt1Point> > >(order);
 }
-
+#endif
 template class TPZPrInteg<TPZInt1d>;
 template class TPZPrInteg<TPZIntTriang>;
 template class TPZPrInteg<TPZIntQuad>;
@@ -44,4 +45,5 @@ template class TPZPrInteg<TPZIntCube3D>;
 template class TPZPrInteg<TPZIntTetra3D>;
 template class TPZPrInteg<TPZIntPrism3D>;
 template class TPZPrInteg<TPZIntPyram3D>;
+template class TPZPrInteg<TPZInt1Point>;
 
