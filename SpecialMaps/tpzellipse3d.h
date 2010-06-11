cd4ce6fec5cd32245e2747b883567926f5e1dd33
caju <caju@a693bd38-532a-e730-8659-88a8c11ee2f8>
2010-06-11 14:59:37 +0000

    implementado metodos que retornam atributos privados

--- SpecialMaps/tpzellipse3d.h
+++ SpecialMaps/tpzellipse3d.h
@@ -23,19 +23,19 @@ using namespace pzgeom;
 using namespace pztopology;
 
 class TPZEllipse3D : public TPZNodeRep<2,TPZLine> {
-	
+
 public:
-	
+
 	enum {NNodes = 2};
-	
+
     bool IsLinearMapping() const
 	{
 		return false;
 	}
-	
+
     TPZEllipse3D(const TPZEllipse3D &cp,std::map<int,int> & gl2lcNdMap) : TPZNodeRep<NNodes,pztopology::TPZLine>(cp,gl2lcNdMap){
     }
-	
+
     TPZEllipse3D() : TPZNodeRep<NNodes,pztopology::TPZLine>(){
     }
 
@@ -43,17 +43,17 @@ public:
     {
 
     }
-	
+
     TPZEllipse3D(const TPZEllipse3D &cp) : TPZNodeRep<NNodes,pztopology::TPZLine>(cp){
     }
-	
+
     TPZEllipse3D(const TPZEllipse3D &cp, TPZGeoMesh &) : TPZNodeRep<NNodes, pztopology::TPZLine>(cp){
     }
-	
+
 	TPZEllipse3D(TPZVec<int> &nodeindexes, TPZGeoMesh &mesh) : TPZNodeRep<NNodes,pztopology::TPZLine>(nodeindexes){
 	}
-	
-    /** 
+
+    /**
 	 * Adjust node coordinates in case of does not belong to the
 	 * ellipse arc defined by the given origin and semiAxes
      */
@@ -72,12 +72,12 @@ public:
       * ========> SEGUINDO A REGRA DA MAO DIREITA (sentido anti-horario em relacao ao sistema local definido pelos vetores dos semi-eixos X e Y).
       */
     void X(TPZFMatrix &nodeCoord,TPZVec<REAL> &qsi,TPZVec<REAL> &x);
-	
+
      /**
       * Este metodo estabelece a matriz jacobiana do mapeamento X (acima declarado)
       */
     void Jacobian(TPZFMatrix &nodeCoord, TPZVec<REAL> &qsi, TPZFMatrix &jac, TPZFMatrix &axes, REAL &detjac, TPZFMatrix &jacinv);
-	
+
     static std::string TypeName() { return "Linear";}
     static TPZGeoEl * CreateBCGeoEl(TPZGeoEl *orig, int side,int bc);
 
@@ -88,41 +88,62 @@ public:
 									  TPZVec<int>& nodeindexes,
 									  int matid,
 									  int& index);
-	
-	
+
+	void GetNodesCoords(TPZGeoMesh &mesh, TPZFMatrix &nodes);
+	void SetNodesCoords(TPZGeoMesh &mesh, TPZFMatrix &nodes);
+
+	TPZVec<REAL> SemiAxeX()
+	{
+		return this->fSemiAxeX;
+	}
+	double sAxeX()
+	{
+		return this->fsAxeX;
+	}
+	TPZVec<REAL> SemiAxeY()
+	{
+		return this->fSemiAxeY;
+	}
+	double sAxeY()
+	{
+		return this->fsAxeY;
+	}
+	TPZVec<REAL> Origin()
+	{
+		return this->fOrigin;
+	}
 
 private://metodos utilizados apenas pelos metodos publicos desta classe!
-	
+
    /**
     * Este metodo retorna o angulo[ang(vini),ang(vfin)] que corresponde a um qsi[-1,1]
     */
     double Angle(double qsi, TPZFMatrix &vini, TPZFMatrix &vfin);
-	
+
    /**
     * Este metodo retorna a derivada do angulo[ang(vini),ang(vfin)] com respeito a qsi[-1,1]
     */
     double DAngleDqsi(TPZFMatrix &vini, TPZFMatrix &vfin);
-	
+
    /**
     * Equacao em R2 da elipse em funcao do angulo[ 0 , 2pi]
-    */	
+    */
     TPZFMatrix EllipseR2equation(double ang);
-	
+
     /**
      * Derivada da equacao em R2 da elipse em funcao com respeito ao angulo
-     */	
+     */
     TPZFMatrix DEllipseR2equationDang(double ang);
-		
-	void GetNodesCoords(TPZGeoMesh &mesh, TPZFMatrix &nodes);
-	void SetNodesCoords(TPZGeoMesh &mesh, TPZFMatrix &nodes);
-	
+
+	private:
+
     /**
      * fSemiAxeX @param - Half of Ellipse Axe in X direction
      * fSemiAxeY @param - Half of Ellipse Axe in Y direction
      */
     TPZVec<REAL> fSemiAxeX;
     double fsAxeX;//norma de fSemiAxeX
-	
+
     TPZVec<REAL> fSemiAxeY;
     double fsAxeY;//norma de fSemiAxeY
 
