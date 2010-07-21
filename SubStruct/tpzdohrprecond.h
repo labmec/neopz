e0278d5cf734c2648030b5bbb7183f27ed02047b
phil <phil@a693bd38-532a-e730-8659-88a8c11ee2f8>
2010-07-21 03:13:53 +0000

    Moved the DohrAssembleData and DohrAssembleList to a separate file

--- SubStruct/tpzdohrprecond.h
+++ SubStruct/tpzdohrprecond.h
@@ -27,6 +27,7 @@
 #include "tpzdohrsubstruct.h"
 #include "tpzdohrmatrix.h"
 #include "tpzdohrassembly.h"
+#include "tpzdohrassemblelist.h"
 
 /**
 This class represents a matrix which computes the preconditioner developed by Dohrmann
@@ -135,17 +136,6 @@ struct TPZDohrPrecondThreadV1Data {
 };
 
 
-struct TPZDohrAssembleItem {
-	/// Initialize the assemble item based on the submesh index and size of the local contribution
-	TPZDohrAssembleItem(int subindex, int size) : fSubIndex(subindex), fAssembleData(size,1,0.)
-	{
-	}
-	/// substructure index
-	int fSubIndex;
-	/// the data which should be assembled
-	TPZFMatrix fAssembleData;
-};
-
 template <class TSubStruct> 
 struct TPZDohrPrecondV2SubData {
 	
@@ -230,36 +220,5 @@ struct TPZDohrPrecondV2SubDataList {
 	static void *ThreadWork(void *voidptr);
 };
 
-struct TPZDohrAssembleList {
-	
-	/// constructor indicating the number of items that will be assembled and the target matrix
-	TPZDohrAssembleList(int numitems, TPZFMatrix &output, TPZAutoPointer<TPZDohrAssembly> assembly);
-	/// destructor
-	~TPZDohrAssembleList();
-	/// the number of items that will be assembled before returning
-	int fNumItems;
-	// semaphore (to wake up assembly thread)
-#ifdef MACOSX
-	sem_t *fSemaphore;
-#else
-	sem_t fSemaphore;
-#endif
-	/// this is the mutex which controls the access to the list
-	pthread_mutex_t fListAccessLock;
-	/// this is the mutex which controls the assembly
-	pthread_mutex_t fAssemblyLock;
-	/// list of objects needed to be assembled
-	std::list<TPZAutoPointer<TPZDohrAssembleItem> > fWork;
-	/// Add an item to the list in a thread safe way
-	void AddItem(TPZAutoPointer<TPZDohrAssembleItem> assembleItem);
-	/// remove an item from the list
-	TPZAutoPointer<TPZDohrAssembleItem> PopItem();
-	/// Assembly indexes
-	TPZAutoPointer<TPZDohrAssembly> fAssembleIndexes;
-	/// Target Matrix
-	TPZFMatrix *fOutput;
-	/// Procedure which performs the assembly process
-	static void *Assemble(void *voidptr);
-};
 
 #endif
