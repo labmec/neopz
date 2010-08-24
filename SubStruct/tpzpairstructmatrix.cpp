c53163127908d32bd5ba6cca94f1d8302443383b
phil <phil@a693bd38-532a-e730-8659-88a8c11ee2f8>
2010-08-24 17:01:56 +0000

    Made the pair structmatrix multithreaded

--- SubStruct/tpzpairstructmatrix.cpp
+++ SubStruct/tpzpairstructmatrix.cpp
@@ -12,6 +12,8 @@
 #include "pzelmat.h"
 #include "pzcompel.h"
 #include "pzstrmatrix.h"
+#include "pzsubcmesh.h"
+#include "pzanalysis.h"
 
 using namespace std;
 
@@ -22,7 +24,16 @@ static LoggerPtr logger(Logger::getLogger("pz.strmatrix.tpzpairstructmatrix"));
 static LoggerPtr loggerel(Logger::getLogger("pz.strmatrix.element"));
 #endif
 
-void TPZPairStructMatrix::Assemble(int mineq, int maxeq, TPZMatrix *first, TPZMatrix *second, TPZFMatrix &rhs)
+int TPZPairStructMatrix::gNumThreads = 0;
+
+TPZPairStructMatrix::TPZPairStructMatrix(TPZCompMesh *mesh, TPZVec<int> &permutescatter)
+{
+	fMesh = mesh;
+	fPermuteScatter = permutescatter;
+	fNumThreads = gNumThreads;
+}
+
+void TPZPairStructMatrix::SerialAssemble(int mineq, int maxeq, TPZMatrix *first, TPZMatrix *second, TPZFMatrix &rhs)
 {
 	int iel;
 	TPZCompMesh &mesh = *fMesh;
@@ -102,5 +113,372 @@ void TPZPairStructMatrix::PermuteScatter(TPZVec<int> &index)
 	for(iel = 0; iel<nel; iel++)
 	{
 		index[iel] = fPermuteScatter[index[iel]];
+	}
+}
+
+void TPZPairStructMatrix::Assemble(int mineq, int maxeq, TPZMatrix *first, TPZMatrix *second, TPZFMatrix &rhs)
+{
+	std::cout << "Assembling the system of equations with " << fNumThreads << " threads\n";
+	if(fNumThreads <= 0)
+	{
+		SerialAssemble(mineq,maxeq,first,second,rhs);
+		return;
+	}
+	else 
+	{
+		MultiThread_Assemble(mineq, maxeq, first, second, rhs);
+	}
+}
+
+void TPZPairStructMatrix::MultiThread_Assemble(int mineq, int maxeq, TPZMatrix *first, TPZMatrix *second, TPZFMatrix &rhs)
+{
+	ThreadData threaddata(*fMesh,*first,*second,rhs,mineq,maxeq);
+	threaddata.fPermuteScatter = fPermuteScatter;
+	const int numthreads = this->fNumThreads;
+	std::vector<pthread_t> allthreads(numthreads+1);
+	int itr;
+	for(itr=0; itr<numthreads; itr++)
+	{
+		pthread_create(&allthreads[itr], NULL,ThreadData::ThreadWork, &threaddata);
+	}
+	
+	// assemble the first matrix
+	pthread_create(&allthreads[itr], NULL,ThreadData::ThreadAssembly1, &threaddata);
+	
+	// assemble the second matrix
+	ThreadData::ThreadAssembly2(&threaddata);
+	
+	for(itr=0; itr<numthreads+1; itr++)
+	{
+		pthread_join(allthreads[itr],NULL);
+	}
+}
+
+TPZPairStructMatrix::ThreadData::ThreadData(TPZCompMesh &mesh, TPZMatrix &mat1, TPZMatrix &mat2, 
+										TPZFMatrix &rhs, int mineq, int maxeq)
+: fNextElement(0), fMesh(&mesh),
+fGlobMatrix1(&mat1), fGlobMatrix2(&mat2), fGlobRhs(&rhs),
+fMinEq(mineq), fMaxEq(maxeq)
+{	
+	pthread_mutex_init(&fAccessElement,NULL);
+}
+
+TPZPairStructMatrix::ThreadData::~ThreadData()
+{
+	pthread_mutex_destroy(&fAccessElement);
+}
+
+void *TPZPairStructMatrix::ThreadData::ThreadWork(void *datavoid)
+{
+	ThreadData *data = (ThreadData *) datavoid;
+	// compute the next element (this method is threadsafe)
+	int iel = data->NextElement();
+	TPZCompMesh *cmesh = data->fMesh;
+	int nel = cmesh->NElements();
+	while(iel < nel)
+	{
+		
+		TPZAutoPointer<TPZElementMatrix> ek = new TPZElementMatrix(cmesh,TPZElementMatrix::EK);
+		TPZAutoPointer<TPZElementMatrix> ef = new TPZElementMatrix(cmesh,TPZElementMatrix::EF);
+		
+		TPZCompEl *el = cmesh->ElementVec()[iel];
+		TPZElementMatrix *ekp = ek.operator->();
+		TPZElementMatrix *efp = ef.operator->();
+		TPZElementMatrix &ekr = *ekp;
+		TPZElementMatrix &efr = *efp;
+		el->CalcStiff(ekr,efr);
+		
+		
+		if(!el->HasDependency()) {
+			ek->ComputeDestinationIndices();
+			
+			if(data->fMinEq != -1 || data->fMaxEq != -1)
+			{
+				TPZStructMatrix::FilterEquations(ek->fSourceIndex,ek->fDestinationIndex,data->fMinEq,data->fMaxEq);
+			}
+#ifdef LOG4CXX
+			if(loggerel->isDebugEnabled())
+			{
+				std::stringstream sout;
+				ek->fMat.Print("Element stiffness matrix",sout);
+				ef->fMat.Print("Element right hand side", sout);
+				LOGPZ_DEBUG(loggerel,sout.str())
+			}
+#endif
+		} else {
+			// the element has dependent nodes
+			ek->ApplyConstraints();
+			ef->ApplyConstraints();
+			ek->ComputeDestinationIndices();
+			if(data->fMinEq != -1 || data->fMaxEq != -1)
+			{
+				TPZStructMatrix::FilterEquations(ek->fSourceIndex,ek->fDestinationIndex,data->fMinEq,data->fMaxEq);
+			}
+		}
+		
+		
+		// put the elementmatrices on the stack to be assembled (threadsafe)
+		data->ComputedElementMatrix(iel,ek,ef);
+		// compute the next element (this method is threadsafe)
+		iel = data->NextElement();
+	}
+	data->fAssembly1.Post();
+	data->fAssembly2.Post();
+	return 0;
+}
+
+void TPZPairStructMatrix::ThreadData::PermuteScatter(TPZVec<int> &index)
+{
+	int nel = index.NElements();
+	int iel;
+	for(iel = 0; iel<nel; iel++)
+	{
+		index[iel] = fPermuteScatter[index[iel]];
+	}
+}
+
+
+// The function which will compute the assembly
+void *TPZPairStructMatrix::ThreadData::ThreadAssembly1(void *threaddata)
+{
+	ThreadData *data = (ThreadData *) threaddata;
+	TPZCompMesh *cmesh = data->fMesh;
+	int nel = cmesh->NElements();
+	pthread_mutex_lock(&(data->fAccessElement));
+	int nextel = data->fNextElement;
+	int numprocessed = data->fProcessed1.size();
+	bool globalresult = true;
+	while(nextel < nel || numprocessed)
+	{
+		std::map<int, std::pair< TPZAutoPointer<TPZElementMatrix>, TPZAutoPointer<TPZElementMatrix> > >::iterator itavail;
+		std::set<int>::iterator itprocess;
+		bool keeplooking = false;
+		if(data->fSubmitted1.size() && data->fProcessed1.size())
+		{
+			itavail = data->fSubmitted1.begin();
+			itprocess = data->fProcessed1.begin();
+			if(itavail->first == *itprocess)
+			{
+				// make sure we come back to look for one more element
+				keeplooking = true;
+				// Get a hold of the data
+				int iel = *itprocess;
+				data->fProcessed1.erase(itprocess);
+				TPZAutoPointer<TPZElementMatrix> ek = itavail->second.first;
+				TPZAutoPointer<TPZElementMatrix> ef = itavail->second.second;
+				data->fSubmitted1.erase(itavail);
+#ifdef LOG4CXX
+				std::stringstream sout;
+				sout << "Assembling element " << iel;
+				LOGPZ_DEBUG(logger,sout.str())
+#endif
+				// Release the mutex
+				pthread_mutex_unlock(&data->fAccessElement);
+				// Assemble the matrix
+				if(!ek->HasDependency())
+				{
+					data->fGlobMatrix1->AddKel(ek->fMat,ek->fSourceIndex,ek->fDestinationIndex);
+					data->fGlobRhs->AddFel(ef->fMat,ek->fSourceIndex,ek->fDestinationIndex);				
+				}
+				else
+				{
+					data->fGlobMatrix1->AddKel(ek->fConstrMat,ek->fSourceIndex,ek->fDestinationIndex);
+					data->fGlobRhs->AddFel(ef->fConstrMat,ek->fSourceIndex,ek->fDestinationIndex);				
+				}
+				// acquire the mutex
+				pthread_mutex_lock(&data->fAccessElement);
+			}
+		}
+		if(!keeplooking)
+		{
+			pthread_mutex_unlock(&data->fAccessElement);
+			LOGPZ_DEBUG(logger,"Going to sleep within assembly")
+			// wait for a signal
+			data->fAssembly1.Wait();
+			LOGPZ_DEBUG(logger,"Waking up for assembly")
+			pthread_mutex_lock(&data->fAccessElement);
+		}
+		nextel = data->fNextElement;
+		numprocessed = data->fProcessed1.size();
+		
+	}
+	pthread_mutex_unlock(&data->fAccessElement);
+	return 0;	
+}		
+
+// The function which will compute the assembly
+void *TPZPairStructMatrix::ThreadData::ThreadAssembly2(void *threaddata)
+{
+	ThreadData *data = (ThreadData *) threaddata;
+	TPZCompMesh *cmesh = data->fMesh;
+	int nel = cmesh->NElements();
+	pthread_mutex_lock(&(data->fAccessElement));
+	int nextel = data->fNextElement;
+	int numprocessed = data->fProcessed2.size();
+	bool globalresult = true;
+	while(nextel < nel || numprocessed)
+	{
+		std::map<int, TPZAutoPointer<TPZElementMatrix> >::iterator itavail;
+		std::set<int>::iterator itprocess;
+		bool keeplooking = false;
+		if(data->fSubmitted2.size() && data->fProcessed2.size())
+		{
+			itavail = data->fSubmitted2.begin();
+			itprocess = data->fProcessed2.begin();
+			if(itavail->first == *itprocess)
+			{
+				// make sure we come back to look for one more element
+				keeplooking = true;
+				// Get a hold of the data
+				int iel = *itprocess;
+				data->fProcessed2.erase(itprocess);
+				TPZAutoPointer<TPZElementMatrix> ek = itavail->second;
+				data->fSubmitted2.erase(itavail);
+#ifdef LOG4CXX
+				std::stringstream sout;
+				sout << "Assembling element " << iel;
+				LOGPZ_DEBUG(logger,sout.str())
+#endif
+				// Release the mutex
+				pthread_mutex_unlock(&data->fAccessElement);
+				TPZManVector<int,300> destindex(ek->fDestinationIndex);
+				data->PermuteScatter(destindex);
+
+				// Assemble the matrix
+				if(!ek->HasDependency())
+				{
+					data->fGlobMatrix2->AddKel(ek->fMat,ek->fSourceIndex,destindex);
+				}
+				else
+				{
+					data->fGlobMatrix2->AddKel(ek->fConstrMat,ek->fSourceIndex,destindex);
+				}
+				// acquire the mutex
+				pthread_mutex_lock(&data->fAccessElement);
+			}
+		}
+		if(!keeplooking)
+		{
+			pthread_mutex_unlock(&data->fAccessElement);
+			LOGPZ_DEBUG(logger,"Going to sleep within assembly")
+			// wait for a signal
+			data->fAssembly2.Wait();
+			LOGPZ_DEBUG(logger,"Waking up for assembly")
+			pthread_mutex_lock(&data->fAccessElement);
+		}
+		nextel = data->fNextElement;
+		numprocessed = data->fProcessed2.size();
+		
+	}
+	pthread_mutex_unlock(&data->fAccessElement);
+	return 0;	
+}		
+
+int TPZPairStructMatrix::ThreadData::NextElement()
+{
+	pthread_mutex_lock(&fAccessElement);
+	int iel;
+	int nextel = fNextElement;
+	TPZCompMesh *cmesh = fMesh;
+	TPZAdmChunkVector<TPZCompEl *> &elementvec = cmesh->ElementVec();
+	int nel = elementvec.NElements();
+	for(iel=fNextElement; iel < nel; iel++)
+	{
+		TPZCompEl *el = elementvec[iel];
+		if(!el) continue;
+		if(fMaterialIds.size() == 0) break;
+		TPZAutoPointer<TPZMaterial> mat = el->Material();
+		TPZSubCompMesh *submesh = dynamic_cast<TPZSubCompMesh *> (el);
+		if(!mat)
+		{
+			if(!submesh)
+			{
+				continue;
+			}
+			else if(submesh->NeedsComputing(fMaterialIds) == false) continue;
+		}
+		else 
+		{
+			int matid = mat->Id();
+			if(this->ShouldCompute(matid) == false) continue;
+		}
+		break;
+	}
+	fNextElement = iel+1;
+	nextel = iel;
+	if(iel<nel) 
+	{
+		fProcessed1.insert(iel);
+		fProcessed2.insert(iel);
+	}
+	pthread_mutex_unlock(&fAccessElement);
+#ifdef LOG4CXX
+	{
+		std::stringstream sout;
+		sout << __PRETTY_FUNCTION__ << " returning " << nextel << " fNextElement " << fNextElement;
+		LOGPZ_DEBUG(logger,sout.str())
+	}
+#endif
+	return nextel;
+}
+
+// put the computed element matrices in the map
+void TPZPairStructMatrix::ThreadData::ComputedElementMatrix(int iel, TPZAutoPointer<TPZElementMatrix> &ek, TPZAutoPointer<TPZElementMatrix> &ef)
+{
+	pthread_mutex_lock(&fAccessElement);
+	std::pair< TPZAutoPointer<TPZElementMatrix>, TPZAutoPointer<TPZElementMatrix> > el(ek,ef);
+	fSubmitted1[iel] = el;
+	fSubmitted2[iel] = ek;
+	fAssembly1.Post();
+	fAssembly2.Post();
+	pthread_mutex_unlock(&fAccessElement);	
+	
+}
+
+/// Set the set of material ids which will be considered when assembling the system
+void TPZPairStructMatrix::SetMaterialIds(const std::set<int> &materialids)
+{
+	fMaterialIds = materialids;
+#ifdef LOG4CXX
+	{
+		std::set<int>::const_iterator it;
+		std::stringstream sout;
+		sout << "setting input material ids ";
+		for(it=materialids.begin(); it!= materialids.end(); it++)
+		{
+			sout << *it << " ";
+		}
+		LOGPZ_DEBUG(logger,sout.str())
+	}
+#endif
+	if(!fMesh)
+	{
+		LOGPZ_WARN(logger,"SetMaterialIds called without mesh")
+		return;
+	}
+	int iel;
+	TPZAdmChunkVector<TPZCompEl*> &elvec = fMesh->ElementVec();
+	int nel = elvec.NElements();
+	for(iel=0; iel<nel; iel++)
+	{
+		TPZCompEl *cel = elvec[iel];
+		if(!cel) continue;
+		TPZSubCompMesh *subcmesh = dynamic_cast<TPZSubCompMesh *> (cel);
+		if(!subcmesh) continue;
+		TPZAnalysis *anal = subcmesh->GetAnalysis();
+		if(!anal)
+		{
+			LOGPZ_ERROR(logger,"SetMaterialIds called for substructure without analysis object")
+			DebugStop();
+			//subcmesh->SetAnalysis();
+			//anal=subcmesh->GetAnalysis();
+		}
+		TPZAutoPointer<TPZStructMatrix> str = anal->StructMatrix();
+		if(!str)
+		{
+			LOGPZ_WARN(logger,"SetMaterialIds called for substructure without structural matrix")
+			continue;
+		}
+		str->SetMaterialIds(materialids);
 	}
 }
--- SubStruct/tpzpairstructmatrix.h
+++ SubStruct/tpzpairstructmatrix.h
@@ -13,24 +13,96 @@
 #include "pzvec.h"
 #include "tpzautopointer.h"
 #include "pzmatrix.h"
+#include "TPZGuiInterface.h"
+#include "pzelmat.h"
+#include "TPZSemaphore.h"
 
 class TPZPairStructMatrix
 {
 	TPZCompMesh *fMesh;
 	TPZVec<int> fPermuteScatter;
+	std::set<int> fMaterialIds;
+	int fNumThreads;
 	
 	void PermuteScatter(TPZVec<int> &index);
 	
 public:
 	
-	TPZPairStructMatrix(TPZCompMesh *mesh, TPZVec<int> &permutescatter)
+	static int gNumThreads;
+	
+	TPZPairStructMatrix(TPZCompMesh *mesh, TPZVec<int> &permutescatter);
+
+	void SetNumThreads(int numthreads)
 	{
-		fMesh = mesh;
-		fPermuteScatter = permutescatter;
+		fNumThreads = numthreads;
 	}
 	
-	
+	/// Set the set of material ids which will be considered when assembling the system
+	void SetMaterialIds(const std::set<int> &materialids);
+
 	void Assemble(int mineq, int maxeq, TPZMatrix *first, TPZMatrix *second, TPZFMatrix &rhs);
+
+	void SerialAssemble(int mineq, int maxeq, TPZMatrix *first, TPZMatrix *second, TPZFMatrix &rhs);
+	
+	void MultiThread_Assemble(int mineq, int maxeq, TPZMatrix *first, TPZMatrix *second, TPZFMatrix &rhs);
+	
+	struct ThreadData
+	{
+		// Initialize the mutex semaphores and others
+		ThreadData(TPZCompMesh &mesh,TPZMatrix &mat1, TPZMatrix &mat2, TPZFMatrix &rhs, int mineq, int maxeq);
+		// Destroy the mutex semaphores and others
+		~ThreadData();
+		// current structmatrix object
+		TPZCompMesh *fMesh;
+		// mutexes (to choose which element is next)
+		pthread_mutex_t fAccessElement;
+		// semaphore (to wake up the first assembly thread)
+		TPZSemaphore fAssembly1;
+		// semaphore (to wake up the second assembly thread)
+		TPZSemaphore fAssembly2;
+		// global matrix1
+		TPZMatrix *fGlobMatrix1;
+		// global matrix2
+		TPZMatrix *fGlobMatrix2;
+		// global rhs
+		TPZFMatrix *fGlobRhs;
+		// minimum equation to be assembled
+		int fMinEq;
+		// maximum equation to be assembled
+		int fMaxEq;
+		/// material identifiers which need to be computed
+		std::set<int> fMaterialIds;
+		/// vector which defines the permutation of all equations to internal equations
+		TPZVec<int> fPermuteScatter;
+		// list of computed element matrices (autopointers?)
+		std::map<int, std::pair< TPZAutoPointer<TPZElementMatrix>, TPZAutoPointer<TPZElementMatrix> > > fSubmitted1;
+		// list of computed element matrices (autopointers?)
+		std::map<int, TPZAutoPointer<TPZElementMatrix> > fSubmitted2;
+		// elements which are being processed maintained by the first global matrix
+		std::set<int> fProcessed1;
+		// elements which are being processed maintained by the second global matrix
+		std::set<int> fProcessed2;
+		// current element
+		int fNextElement;
+		// look for an element index which needs to be computed and put it on the stack
+		int NextElement();
+		// put the computed element matrices in the map
+		void ComputedElementMatrix(int iel, TPZAutoPointer<TPZElementMatrix> &ek, TPZAutoPointer<TPZElementMatrix> &ef);
+		// The function which will compute the matrices
+		static void *ThreadWork(void *threaddata);
+		// The function which will compute the assembly
+		static void *ThreadAssembly1(void *threaddata);
+		// The function which will compute the assembly
+		static void *ThreadAssembly2(void *threaddata);
+		
+		/// Establish whether the element should be computed
+		bool ShouldCompute(int matid)
+		{
+			return fMaterialIds.size()==0 || fMaterialIds.find(matid) != fMaterialIds.end();
+		}
+		void PermuteScatter(TPZVec<int> &index);
+		
+	};
 	
 };
 
