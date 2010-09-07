fe56d6fcac1bb7620f07e1aea8b01655243abaa9
erick <erick@a693bd38-532a-e730-8659-88a8c11ee2f8>
2010-09-07 22:15:12 +0000

    *** empty log message ***

--- SubStruct/TPZfTime.cpp
+++ SubStruct/TPZfTime.cpp
@@ -1,10 +1,44 @@
+/*
+ *  ftime.cpp
+ *  SubStruct
+ *
+ *  Created by Bandit on 7/20/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
 #include "TPZfTime.h"
 
+using namespace std;
 
-TPZfTime::TPZfTime(){}
+TPZfTime::TPZfTime()
+{
+	ftime(&finicio);
+}
 
-TPZfTime::~TPZfTime(){}
+TPZfTime::~TPZfTime()
+{
+}
 
-std:string TPZfTime::ReturnTimeString(){}
-double TPZfTime::ReturnTimeDouble(){}
+std::string TPZfTime::ReturnTimeString()
+{ 
 
+	ftime(&ffinal);
+	double time = ((double) ffinal.time + ((double) ffinal.millitm * 0.001)) - ((double) finicio.time + ((double) finicio.millitm * 0.001));
+	stringstream oss;
+	string str;
+	
+	oss << time << " seconds\n";
+	str = oss.str();
+	
+   // printf("%f seconds\n", ((double) ffinal.time + ((double) ffinal.millitm * 0.001)) - ((double) finicio.time + ((double) finicio.millitm * 0.001)));
+	return str;
+}
+
+double TPZfTime::ReturnTimeDouble()
+{
+	ftime(&ffinal);
+	double time = ((double) ffinal.time + ((double) ffinal.millitm * 0.001)) - ((double) finicio.time + ((double) finicio.millitm * 0.001));
+	
+	return time;
+}
--- SubStruct/TPZfTime.h
+++ SubStruct/TPZfTime.h
@@ -1,21 +1,48 @@
+#ifndef TPZFTIME_H
+#define TPZFTIME_H
+
+/*
+ *  ftime.h
+ *  SubStruct
+ *
+ *  Created by Bandit on 7/20/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+
 #include <iostream>
 #include <sys/timeb.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
-#include <strstream>
+#include <sstream>
 
+/// Calculate the Times
 class TPZfTime
 {
-
+	
 public:
 
-TPZfTime();
+	/// Start the timer when the object is created
+	TPZfTime();
+	
+	~TPZfTime();
+	
+	/// When called, returns the time since the creation of the object in a string
+	std::string ReturnTimeString();
+	
+	/// When called, returns the time since the creation of the object in a double
+	double ReturnTimeDouble();
+	
+	
+	
+	
+private:
 
-~TPZfTime();
+	struct timeb finicio, ffinal;
 
-std::strstream ReturnTimeString();
-double ReturnTimeDouble();
-
-
+	
 };
+
+#endif
