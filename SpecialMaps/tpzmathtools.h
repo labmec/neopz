9e17b3037e6fa9c7a3cc606386f5ac3f748c7e79
phil <phil@a693bd38-532a-e730-8659-88a8c11ee2f8>
2010-11-08 15:51:04 +0000

    Removed unnecessary include statements

--- SpecialMaps/tpzmathtools.h
+++ SpecialMaps/tpzmathtools.h
@@ -1,61 +1,8 @@
 #ifndef TPZMATHTOOLS_H
 #define TPZMATHTOOLS_H
 
-#include <iostream>
-#include <cstdlib>
-
-#include "pzgmesh.h"
 #include "pzvec.h"
-#include "pzgnode.h"
 #include "pzgeoel.h"
-#include "pzadmchunk.h"
-#include "pzcmesh.h"
-#include "pzcompel.h"
-#include "pzmaterial.h"
-#include "pzerror.h"
-#include "pzmatrix.h"
-#include "pzfmatrix.h"
-#include "TPZGeoElement.h"
-#include "TPZRefPattern.h"
-#include "tpzgeoelrefpattern.h"
-#include "TPZGeoCube.h"
-#include "pzshapecube.h"
-#include "TPZRefCube.h"
-#include "pzshapelinear.h"
-#include "TPZGeoLinear.h"
-#include "TPZRefLinear.h"
-#include "pzrefquad.h"
-#include "pzshapequad.h"
-#include "pzgeoquad.h"
-#include "pzshapetriang.h"
-#include "pzreftriangle.h"
-#include "pzgeotriangle.h"
-#include "pzshapeprism.h"
-#include "pzrefprism.h"
-#include "pzgeoprism.h"
-#include "pzshapetetra.h"
-#include "pzreftetrahedra.h"
-#include "pzgeotetrahedra.h"
-#include "pzshapepiram.h"
-#include "pzrefpyram.h"
-#include "pzgeopyramid.h"
-#include "pzrefpoint.h"
-#include "pzgeopoint.h"
-#include "pzshapepoint.h"
-#include "pzgmesh.h"
-#include "tpzgeoelmapped.h"
-#include "tpzcurvedtriangle.h"
-#include "tpzquadratictrig.h"
-#include "tpzquadratictetra.h"
-#include "tpzarc3d.h"
-#include "pzshapetriang.h"
-#include "pzgeotriangle.h"
-#include "pzlog.h"
-#include <sstream>
-#include "pzgeoelside.h"
-
-using namespace std;
-using namespace pzgeom;
 
   /**
   / Class made by Paulo Cesar de Alvarenga Lucci (Caju)
