df0ab9ede661b488a4c55cfec7cf6eefabc92d6b
phil <phil@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-05-11 03:01:35 +0000

    Removed stray characters from the beginning of the file

--- Pre/pzreadmeshhr.cpp
+++ Pre/pzreadmeshhr.cpp
@@ -1,4 +1,4 @@
-﻿/*****************************************************************************
+/*****************************************************************************
  * O contedo desse arquivo �de propriedade do LabMeC-DES-FEC-UNICAMP e do
  * CENPES-Petrobras.
  * O uso de qualquer parte ou do todo est�condicionado �expressa autoriza�o
