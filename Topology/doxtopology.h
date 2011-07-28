d2833266b8aad903f51208adcb167ecba4ca94fd
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-07-28 20:50:02 +0000

    New subgroup to utilities for topology

--- Topology/doxtopology.h
+++ Topology/doxtopology.h
@@ -4,5 +4,10 @@
 
 /** @defgroup topology The master element topology classes.
  *
- * @brief Defines the estructure of the master elements.
+ * @brief Defines the structure of the master elements.
+ */
+
+/** @defgroup topologyutils The utilities for topology classes.
+ * @ingroup topology
+ * @brief Utilities classes operating on data related to master element
  */
