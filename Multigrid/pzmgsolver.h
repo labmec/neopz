3b939d4d7fcd944df06d23a9d576f47d512e4aad
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-07-28 20:45:50 +0000

    Doxygen documentation

--- Multigrid/pzmgsolver.h
+++ Multigrid/pzmgsolver.h
@@ -7,47 +7,52 @@
 
 #define TPZMGSOLVER_ID 28291008
 
-
 class TPZFMatrix;
 class TPZTransfer;
+
+/**
+ * @ingroup solver
+ * @brief Represents a solution process in three steps : transfer of the residual, execute a solver on the coarse mesh, extend the solution
+ */
 class TPZMGSolver: public TPZMatrixSolver
 {
 public:
-  TPZMGSolver() : TPZMatrixSolver() {}
-  TPZMGSolver(TPZAutoPointer<TPZTransfer> trf, const TPZMatrixSolver &sol,
-      int nvar, TPZAutoPointer<TPZMatrix> refmat);
-  TPZMGSolver(TPZAutoPointer<TPZTransfer> trf, const TPZMatrixSolver &sol,
-      int nvar);
-
-  TPZMGSolver(const TPZMGSolver & copy);
-
-  ~TPZMGSolver();
-
-  void SetTransferMatrix(TPZAutoPointer<TPZTransfer> Refmat);
-
-  void ResetTransferMatrix();
-
-  TPZAutoPointer<TPZTransfer> TransferMatrix()
-  {
-    return fStep;
-  }
-
-  TPZSolver * Clone() const;
-
-  void Solve(const TPZFMatrix &F, TPZFMatrix &result, TPZFMatrix *residual = 0);
-
-  virtual int ClassId() const
-  {
-    return TPZMGSOLVER_ID;
-  }
-  virtual void Write(TPZStream &buf, int withclassid);
-  virtual void Read(TPZStream &buf, void *context);
-
-
+	TPZMGSolver() : TPZMatrixSolver() {}
+	TPZMGSolver(TPZAutoPointer<TPZTransfer> trf, const TPZMatrixSolver &sol,
+				int nvar, TPZAutoPointer<TPZMatrix> refmat);
+	TPZMGSolver(TPZAutoPointer<TPZTransfer> trf, const TPZMatrixSolver &sol,
+				int nvar);
+	
+	TPZMGSolver(const TPZMGSolver & copy);
+	
+	~TPZMGSolver();
+	
+	void SetTransferMatrix(TPZAutoPointer<TPZTransfer> Refmat);
+	
+	void ResetTransferMatrix();
+	
+	TPZAutoPointer<TPZTransfer> TransferMatrix()
+	{
+		return fStep;
+	}
+	
+	TPZSolver * Clone() const;
+	
+	void Solve(const TPZFMatrix &F, TPZFMatrix &result, TPZFMatrix *residual = 0);
+	
+	virtual int ClassId() const
+	{
+		return TPZMGSOLVER_ID;
+	}
+	virtual void Write(TPZStream &buf, int withclassid);
+	virtual void Read(TPZStream &buf, void *context);
+	
+	
 private:
-  TPZMatrixSolver * fCoarse;
-  int fNVar;
-  TPZAutoPointer<TPZTransfer> fStep;
-  //    TPZMatrixSolver::TPZContainer *fTransfer;
+	TPZMatrixSolver * fCoarse;
+	int fNVar;
+	TPZAutoPointer<TPZTransfer> fStep;
+	//    TPZMatrixSolver::TPZContainer *fTransfer;
 };
+
 #endif //TPZMGSOLVER_H
