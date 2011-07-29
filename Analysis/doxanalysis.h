bacf9bb1315be0d636583d26673943c2ef31843e
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-07-29 22:29:41 +0000

    To doxygen documentation

---
+++ Analysis/doxanalysis.h
@@ -0,0 +1,8 @@
+/**
+ * doxanalysis.h
+ */
+
+/** @defgroup analysis The Analysis classes.
+ *
+ * @brief Objects of this classes implement the analysis procedures.
+ */
---
+++ SubStruct/doxsubstrmatrix.h
@@ -0,0 +1,8 @@
+
+/**
+ * @defgroup substructure Matrix divided in sub structure classes
+ 
+ * This module contains all classes which defines Matrices divided in sub structures.
+ * 
+ * Was implemented Dohrmann algorithm and preconditioner.
+ */
