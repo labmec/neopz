fe5ccbcf6ad7bc9e4725f67fab0ccbcf79f43352
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-07-30 19:14:10 +0000

    Adding Unit Testing Directories to matrix and topology modules

---
+++ UnitTest_PZ/TestMatrix/TestMatrix.cpp
@@ -0,0 +1,195 @@
+// Jorge at 2011, July.
+
+#include "pzmatrix.h"
+#include "pzfmatrix.h"
+#include "pzbndmat.h"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN pz matrix tests
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+/** 
+* @brief Tests wheter a matrix matr is diagonally dominant. Checks Aii > Soma(Aij) on j!=i, for all i.
+* @param matr Matrix to check, it can to be non square matrix.
+* @note matx is a class of the type matrix matr.
+*/
+template <class matx>
+int TestingGeneratingDiagonalDominantMatrix(matx &matr) {
+	REAL sum;
+	for(int i=0;i<matr.Rows();i++) {
+		sum = 0.0;
+		for(int j=0;j<matr.Cols();j++) {
+			if(i!=j)
+				sum += fabs(matr.GetVal(i,j));
+		}
+		if(!fabs(matr.GetVal(i,i)) > sum) return 0;
+	}
+	return 1;
+}
+
+/**
+* @brief Tests the Inverse method of the matrix to any matrix types. It uses the AutoFill method to create a square matrix with 
+*
+* @param dim Dimension of the square matrix to be build.
+* @note Process: build square matrix with randomic values, compute its inverse and the inverse of the inverse.
+* Then, checks whether the first and last matrices are identical.
+*/
+/** Thanks it:
+ * NOTE1: I knowed that Inverse function change data of the current matrix. It stores some decomposition (LU or Cholesky ...)
+ * NOTE2: If a matrix is decomposed, all times when you call Inverse, it isn't calculated because the matrix has decomposed flag as TRUE 
+ */
+template <class matx>
+void TestingInverseWithAutoFill(int dim) {
+	int i, j;
+	
+	matx ma(dim,dim);
+	ma.AutoFill();
+	BOOST_CHECK_EQUAL(TestingGeneratingDiagonalDominantMatrix<TPZFMatrix>(ma),1);
+	// Making ma copy because ma is modified by Inverse method (it's decomposed)
+	matx cpy(ma);
+	matx inv(dim,dim);
+	// getting inverse twice
+	cpy.Inverse(inv);
+	inv.Inverse(cpy);
+	
+	/// Checking whether the res matrix is identical to m1 matrix
+	for(i=0;i<dim;i++)
+		for(j=0;j<dim;j++)
+			BOOST_CHECK(IsZero(ma.GetVal(i,j) - cpy.GetVal(i,j)));
+}
+
+/**
+* @brief Tests the operator * of the matrix, using AutoFill to build a square matrix of dimension dim (user defined)
+*
+* @param dim Dimension of the square matrix to be build.
+* @note Process: build square matrix with randomic values, compute the product using * operator and then checks whether it is a identity matrix.
+*/
+template <class matx>
+void TestingMultiplyOperatorWithAutoFill(int dim) {
+	int i, j;
+	REAL val;
+	// ma times inv must to be a identity matrix
+	matx ma(dim,dim);
+	ma.AutoFill();
+
+	matx inv(dim,dim);
+	matx macpy(ma);
+
+	macpy.Inverse(inv);
+	macpy = ma*inv;
+	// Checking whether result matrix is the identity matrix
+	for(i=0;i<dim;i++) {
+		for(j=0;j<dim;j++) {
+			val = fabs(macpy.GetVal(i,j));
+			if(i==j) BOOST_CHECK(IsZero(val - 1.0));
+			else BOOST_CHECK(IsZero(val));
+		}
+	}
+}
+
+/**
+* @brief Tests the Multiply method of the matrix, using AutoFill to build a square matrix of dimension dim (user defined)
+*
+* @param dim Dimension of the square matrix to be build.
+* @note Process: build square matrix with randomic values, compute its inverse then calculate the product of the first and inverse.
+* Check if the result is a identity matrix.
+*/
+template <class matx>
+void TestingMultiplyWithAutoFill(int dim) {
+	int i, j;
+	REAL val;
+	// ma times inv must to be a identity matrix
+	matx ma(dim,dim);
+	ma.AutoFill();
+
+	matx inv(dim,dim);
+	matx res(ma);
+
+	res.Inverse(inv);
+	ma.Multiply(inv,res);
+	// Checking whether result matrix is the identity matrix
+	for(i=0;i<dim;i++) {
+		for(j=0;j<dim;j++) {
+			val = fabs(res.GetVal(i,j));
+			if(i==j) BOOST_CHECK(IsZero(val - 1.0));
+			else BOOST_CHECK(IsZero(val));
+		}
+	}
+}
+
+/**
+* @brief Tests the Transpose method of the matrix, using AutoFill to build a square matrix of dimension dim (user defined)
+* @param dim Dimension of the square matrix to be build.
+* @note Process: build square matrix with randomic values, compute its transpose and transpose again then compare the first and last matrices.
+*/
+template <class matx>
+void TestingTransposeWithAutoFill(int rows,int cols) {
+	int i, j;
+	
+	matx ma(rows,cols);
+	ma.AutoFill();
+
+	matx matransp(cols,rows);
+	matx matransptransp(ma);
+
+	// getting inverse twice
+	matransptransp.Transpose(&matransp);
+	matransp.Transpose(&matransptransp);
+	
+	/// Checking whether the res matrix is identical to m1 matrix
+	for(i=0;i<rows;i++)
+		for(j=0;j<cols;j++)
+			BOOST_CHECK(IsZero(ma.GetVal(i,j) - matransptransp.GetVal(i,j)));
+}
+
+BOOST_AUTO_TEST_SUITE(matrix_tests)
+
+BOOST_AUTO_TEST_CASE(diagonaldominant_tests) {
+	int dim, maxiter = 100;
+	for(dim=3;dim<maxiter;dim++) {
+		TPZFMatrix ma(dim,dim);
+		ma.AutoFill();
+		BOOST_CHECK_EQUAL(TestingGeneratingDiagonalDominantMatrix<TPZFMatrix>(ma),1);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(inverse_tests)
+{
+	int dim;
+	for(dim = 3;dim < 100; dim+=5) {
+		TestingInverseWithAutoFill<TPZFMatrix>(dim);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(multiply_tests)
+{
+	int dim;
+	for(dim = 3;dim < 100; dim+=5) {
+		TestingMultiplyWithAutoFill<TPZFMatrix>(dim);
+	}
+}
+BOOST_AUTO_TEST_CASE(multiplyoperator_tests)
+{
+	int dim;
+	for(dim = 3;dim < 100; dim+=5) {
+		TestingMultiplyOperatorWithAutoFill<TPZFMatrix>(dim);
+	}
+}
+BOOST_AUTO_TEST_CASE(transpose_tests)
+{
+	int rows, cols;
+	for(rows = 3;rows < 4; rows+=5) {
+		for(cols = 3;cols < 100;cols+=5) {
+			TestingTransposeWithAutoFill<TPZFMatrix>(rows,cols);
+		}
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+//BOOST_AUTO_TEST_CASE(nonsingular_test)
+//{
+//	TestingDiagonalDominant<TPZFBMatrix>();
+//}
---
+++ UnitTest_PZ/TestTopology/TestTopology.cpp
@@ -0,0 +1,48 @@
+//
+//  TestTopology.cpp
+//  PZ
+//
+//  Created by Philippe Devloo on 5/6/11.
+//  Copyright 2011 UNICAMP. All rights reserved.
+//
+
+#include "pzmanvector.h"
+#include "pztrnsform.h"
+#include "tpzline.h"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN pz topology tests
+
+#include <boost/test/unit_test.hpp>
+
+using namespace pztopology;
+
+template <class top>
+void TestingSideProjections2() {
+    int dim = top::Dimension;
+    int nsides = top::NSides;
+    TPZManVector<REAL,3> pt1,pt2,pt3;
+    int is;
+    for (is=0; is<nsides; is++) {
+        int sidedim = top::SideDimension(is);
+        TPZManVector<REAL,3> pt1(dim),pt2(dim),pt3(sidedim);
+        top::CenterPoint(nsides-1,pt1);
+        TPZTransform tr1 = top::TransformSideToElement(is);
+        TPZTransform tr2 = top::TransformElementToSide(is);
+        TPZTransform tr3;
+        tr3 = tr2.Multiply(tr1);
+        BOOST_CHECK_EQUAL(tr3.Mult().Rows(), sidedim);
+    }
+}
+
+// Tests for the 'voidflux' class.
+BOOST_AUTO_TEST_SUITE(topology_tests2)
+
+BOOST_AUTO_TEST_CASE(projection_tests2)
+{
+    TestingSideProjections2<TPZLine>();
+    // Ensure that subtracting 6 from 8 gives 2.
+    //TPZVoidFlux flux(1,2.);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
