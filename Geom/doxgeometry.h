180fc4bfc2d1ca2381c86458cf1194eecc1783db
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-08-01 15:00:30 +0000

    Doxygen files

---
+++ Geom/doxgeometry.h
@@ -0,0 +1,9 @@
+/**
+ * @file
+ * @brief Creates geometry group for Doxygen documentation.
+ */
+/** 
+ * @defgroup geometry The Geometric approximation classes
+ * @brief The geometry classes are responsible for the aproximation of the geometry of the problem and
+ * for keeping track of the topology of the mesh
+ */
---
+++ Material/doxmaterial.h
@@ -0,0 +1,11 @@
+/**
+ * \file
+ * @brief Creates material group for doxygen documentation.
+ */
+/** 
+ * @defgroup material The Material Classes
+ * @brief Classes which implement the weak statement of the differential equation within the PZ environment. \n
+ * TPZMaterial is the Base Class.
+ * @note It is noteworthy to observe that this definition does not depend on the definition of the interpolation space.
+ */
+
