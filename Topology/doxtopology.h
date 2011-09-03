356cdc37bfe1b1e56d1152b80a7fba36979adf79
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-09-03 19:36:55 +0000

    Formating right

--- Topology/doxtopology.h
+++ Topology/doxtopology.h
@@ -3,12 +3,13 @@
  * @brief Creates topology and topologyutils groups for Doxygen documentation.
  */
 
-/** @defgroup topology The master element topology classes.
- *
+/**
+ * @defgroup topology The master element topology classes.
  * @brief Defines the structure of the master elements.
  */
 
-/** @defgroup topologyutils The utilities for topology classes.
+/**
+ * @defgroup topologyutils The utilities for topology classes.
  * @ingroup topology
  * @brief Utilities classes operating on data related to master element
  */
