43c46ddd801a01a2cc6278b0aed076cc34f52907
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-09-03 19:32:03 +0000

    Documentation of the attributes and methods

--- Pre/pzdatafi.h
+++ Pre/pzdatafi.h
@@ -15,71 +15,43 @@
 class TGeoGrid;
 class TCompGrid;
 
-/* **************************************************************************
- 
- CLASS NAME :
- 
- datafile
- 
- PURPOSE OF THE CLASS :
- 
- datafile is a virtual class from which classes with different data formats can
- be derived. The purpose of the datafile class is to facilitate the opening of an
- input stream and define the methods used to read a grid
- 
- DEPENDENCIES :
- 
- datafile.h :
- 
- pzcondef.h
- pzerror.h
- pzcondef.h
- 
- ERRORS :
- 
- the length of the parameter fn in the constructor is not checked before
- being copied into filename. This may lead to system crashes...
- 
- ANOMALIES :
- 
- 
- WARNING :
- 
- If the input stream could not be opened, the file_error parameter is set to TRUE.
- It is up to the programmer of the derived class to check this data when writing
- the read method
- 
- IMPROVEMENTS :
- 
- The filename variable could be allocated dynamically to fit any filename length
- 
- PROGRAMMERS :
- 
- Jose Sergio Rodrigues Alves Filho
- Philippe Remy Bernard Devloo
- 
- ************************************************************************** */
-
 /** 
  * @ingroup pre
- * @brief datafile is a virtual class from which classes with different data formats can be derived. \ref pre "Getting Data"
+ * @brief TDatafile is a virtual class from which classes with different data formats can be derived. \ref pre "Getting Data"
+ * @author Jose Sergio Rodrigues Alves Filho
+ * @author Philippe Remy Bernard Devloo
  */
 /**
  * The purpose of the datafile class is to facilitate the opening of an
  * input stream and defines the methods used to read a grid.
+ *
+ * \par ERRORS:
+ * The length of the parameter fn in the constructor is not checked before
+ * being copied into filename. This may lead to system crashes...
+ *
+ * \warning
+ * If the input stream could not be opened, the file_error parameter is set to TRUE. \n
+ * It is up to the programmer of the derived class to check this data when writing the read method.
+ *
+ * \par IMPROVEMENTS:
+ * The filename variable could be allocated dynamically to fit any filename length (Who? When?)
  */
 class TDatafile 
 {
 protected:
+	/// File from import data
 	filebuf  fBuffer;
+	/// Data
 	istream  fData;
+	/// File name 
 	char  fFilename[256];
+	/// Flag to report if the file is opening
 	int	fFileError;
-	//------------------------------------------------------------
+
 public:
 	
+	/// Constructor with filename of the data to import
 	TDatafile(char *fn) : fData(&fBuffer) {
-		
 		fFileError = 0;
 		strcpy (fFilename, fn);
 		if ( fBuffer.open( fn, ios::in) == 0) {
@@ -88,16 +60,14 @@ public:
 			fFileError = 1;
 		}
 	}
+	/// Returns if could opening or not the data file
 	int Error() {return fFileError;}
-	
+	/// Destructor
 	virtual ~TDatafile(){ fBuffer.close();}
-	
-	//virtual short Read (grid & malha) = 0;
-	
-	virtual short Read (TCompGrid & malha) = 0;
-	
-};
 
-//------------------------------------------------------------
+	/// To import data into computational mesh
+	virtual short Read (TCompGrid & malha) = 0;
+
+};
 
 #endif
