817b2db43f11b808e5e020ad404ad15f12bb7d24
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-09-08 14:29:48 +0000

    This files contains deprecated classes

---
+++ OutOfDate/Material/TPZConservationLaw.h
@@ -0,0 +1,152 @@
+/**
+ * \file
+ * @brief Contains the TPZConservationLaw class which implements the interface for conservation laws.
+ */
+#ifndef CONSERVATIONLAWHPP
+#define CONSERVATIONLAWHPP
+
+#include <iostream>
+#include "pzmaterial.h"
+#include "pzfmatrix.h"
+#include "pzvec.h"
+
+/**
+ * @deprecated THIS CLASS IS DEPRECATED BY TPZConservationLaw2.
+ * @ingroup material
+ * @brief Implements the interface for conservation laws, keeping track of the timestep as well
+ */
+class TPZConservationLaw  : public TPZMaterial {
+	
+	int fDim;
+	REAL fTimeStep;
+	
+	public :
+	
+	REAL fDelta;
+	
+	TPZConservationLaw(int nummat,REAL delta_t,int dim);
+	
+	/** @brief copy constructor*/
+	TPZConservationLaw(TPZConservationLaw &copy);
+	
+	/** @brief To create another material of the same type*/
+	TPZAutoPointer<TPZMaterial> NewMaterial();
+	
+	~TPZConservationLaw(){};
+	
+	/** @brief compute the boundary condition left solution */
+	virtual void ComputeSolLeft(TPZVec<REAL> &solr,TPZVec<REAL> &soll,TPZVec<REAL> &normal,TPZBndCond *bcleft);
+	
+	/** @brief compute the boundary condition right solution */
+	virtual void ComputeSolRight(TPZVec<REAL> &solr,TPZVec<REAL> &soll,TPZVec<REAL> &normal,TPZBndCond *bcright);
+	
+	/** @brief termodinamic pressure determined by the law of an ideal gas */
+	virtual REAL Pressure(TPZVec<REAL> &U);
+	
+	virtual REAL Gamma();
+	
+	//virtual int IntegrationDegree() = 0;
+	
+	//virtual void SetIntegDegree(int degree) = 0;
+	
+	virtual void SetDelta(REAL delta){fDelta = delta;}
+	
+	virtual void SetDeltaTime(REAL maxveloc,REAL deltax,int degree);
+	
+	REAL Delta();
+	
+	virtual void SetTimeStep(REAL timestep){fTimeStep = timestep;}
+	
+	virtual REAL TimeStep(){return fTimeStep;}
+	
+	int Dimension() { return fDim;}
+	
+	int NStateVariables();
+	
+	virtual void Print(std::ostream & out);
+	
+	virtual std::string Name() { return "TPZConservationLaw"; }
+	
+	virtual void Contribute(TPZMaterialData &data,
+                            REAL weight,
+                            TPZFMatrix &ek,
+                            TPZFMatrix &ef);
+	
+	virtual void ContributeInterface(TPZMaterialData &data,
+                                     REAL weight,
+                                     TPZFMatrix &ek,
+                                     TPZFMatrix &ef);
+	
+	virtual void ContributeBC(TPZMaterialData &data,
+                              REAL weight,
+                              TPZFMatrix &ek,
+                              TPZFMatrix &ef,
+                              TPZBndCond &bc);
+	
+	virtual void ContributeBC(TPZMaterialData &data,
+							  REAL weight,
+							  TPZFMatrix &ef,
+							  TPZBndCond &bc)
+	{
+		TPZMaterial::ContributeBC(data,weight,ef,bc);
+	}
+	
+	virtual void Contribute(TPZMaterialData &data,
+                            REAL weight,
+                            TPZFMatrix &ef);
+	
+	virtual int VariableIndex(const std::string &name);
+	
+	virtual int NSolutionVariables(int var);
+	
+	virtual int NFluxes(){ return 1;}
+	
+protected:
+	virtual void Solution(TPZVec<REAL> &Sol,TPZFMatrix &DSol,TPZFMatrix &axes,int var,TPZVec<REAL> &Solout);
+public:
+	
+	virtual void Solution(TPZMaterialData &data, int var, TPZVec<REAL> &Solout)
+	{
+		TPZMaterial::Solution(data,var,Solout);
+	}
+	
+	virtual void Flux(TPZVec<REAL> &x, TPZVec<REAL> &Sol, TPZFMatrix &DSol, TPZFMatrix &axes, TPZVec<REAL> &flux);
+	
+	void Errors(TPZVec<REAL> &x,TPZVec<REAL> &u,
+				TPZFMatrix &dudx, TPZFMatrix &axes, TPZVec<REAL> &flux,
+				TPZVec<REAL> &u_exact,TPZFMatrix &du_exact,TPZVec<REAL> &values);
+};
+
+
+// inline void TPZConservationLaw::ContributeInterface(TPZMaterialData &data,
+//                                                       REAL ,
+//                                                       TPZFMatrix &){
+//   PZError << "TPZConservationLaw::ContributeInterface it would never have to be called\n";
+// }
+
+inline void TPZConservationLaw::Solution(TPZVec<REAL> &Sol,TPZFMatrix &DSol,TPZFMatrix &axes,int var,TPZVec<REAL> &Solout){
+	PZError << "TPZConservationLaw::Solution it would never have to be called\n";
+}
+
+inline void TPZConservationLaw::SetDeltaTime(REAL maxveloc,REAL deltax,int degree){
+	PZError << "TPZConservationLaw::SetDeltaTime it would never have to be called\n";
+}
+
+inline REAL TPZConservationLaw::Pressure(TPZVec<REAL> &U) {
+	PZError << "TPZConservationLaw::Pressure it would never have to be called\n";
+	return 0.0;
+}
+
+inline REAL TPZConservationLaw::Gamma() {
+	PZError << "TPZConservationLaw::Gamma it would never have to be called\n";
+	return 0.0;
+}
+
+inline void TPZConservationLaw::ComputeSolLeft(TPZVec<REAL> &,TPZVec<REAL> &,TPZVec<REAL> &,TPZBndCond *){
+	PZError << "TPZConservationLaw::ComputeSolLeft it would never have to be called\n";
+}
+
+inline void TPZConservationLaw::ComputeSolRight(TPZVec<REAL> &,TPZVec<REAL> &,TPZVec<REAL> &,TPZBndCond *){
+	PZError << "TPZConservationLaw::ComputeSolRight it would never have to be called\n";
+}
+#endif
---
+++ OutOfDate/Mesh/pzconnect.h
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * @brief Contains declaration of TPZConnect class which represents a set of shape functions associated with a computational element
+ */
+//$Id: pzconnect.h,v 1.19 2010-08-25 03:05:06 phil Exp $
+//HEADER FILE FOR CLASS NODE
+
+#ifndef  PZCONNECTBCH
+#define  PZCONNECTBCH
+
+#include "pzfmatrix.h"
+#include "pzstack.h"
+#include <iostream>
+#include <set>
+
+
+class TPZBndCond;
+class TPZCompMesh;
+class TPZBlock;
+class TPZStream;
+
+
+/**
+ @brief Associate a degree of freedom node with a boundary condition
+ such boundary condition can be dirichlet, point load or mixed boundary condition.
+ @deprecated THIS CLASS IS DEPRECATED
+ */
+struct TPZConnectBC {
+	
+	TPZConnect *fConnect;
+	TPZBndCond *fBC;
+	
+	TPZConnectBC() {
+		fConnect = 0;
+		fBC = 0;
+	}
+	TPZConnectBC(TPZConnect *nd,TPZBndCond *bc) {
+		fConnect = nd;
+		fBC = bc;
+	}
+	
+	void Print(TPZCompMesh &mesh,std::ostream &out = std::cout);
+	
+};
+
+/// Prints data related with connect and boundary condition
+void TPZConnectBC::Print(TPZCompMesh &mesh,std::ostream &out){
+	out << "Connect boundary condition :\n";
+	if(fConnect) fConnect->Print(mesh,out);
+	if(fBC) fBC->Print(out);
+}
+
+#endif
+
