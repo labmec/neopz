569a4d46a1e18e3001d32683b3331946368830c9
agnaldofarias.mg@gmail.com <agnaldofarias.mg@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-09-30 15:44:27 +0000

    new classe: TPZMultiphysicsElement. intermediate between pzcompel and tpzmultiphysiccompel

---
+++ Mesh/tpzmultiphysicselement.cpp
@@ -0,0 +1,17 @@
+/*
+ *  tpzmultiphysicselement.cpp
+ *  PZ
+ *
+ *  Created by Agnaldo on 9/30/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "tpzmultiphysicselement.h"
+#include "pzcompel.h"
+
+//TPZMultiphysicsElement::TPZMultiphysicsElement() :TPZCompEl(){
+//}
+
+//TPZMultiphysicsElement::~TPZMultiphysicsElement(){	
+//}
