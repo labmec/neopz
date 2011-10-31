badb59a14e9e6a2f0b94ab818aa4bd61e3858f8c
agnaldofarias.mg@gmail.com <agnaldofarias.mg@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-10-31 18:52:01 +0000

    class to build mesh multiphysics

---
+++ Mesh/pzbuildmultiphysicsmesh.cpp
@@ -0,0 +1,196 @@
+/*
+ *  pzbuildmultiphysicsmesh.cpp
+ *  PZ
+ *
+ *  Created by Agnaldo on 10/31/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "pzbuildmultiphysicsmesh.h"
+
+#include "pzmultiphysicselement.h"
+
+
+TPZBuildMultiphysicsMesh::TPZBuildMultiphysicsMesh(){
+}
+
+TPZBuildMultiphysicsMesh::~TPZBuildMultiphysicsMesh(){
+}
+
+void TPZBuildMultiphysicsMesh::AddElements(TPZVec<TPZCompMesh *> cmeshVec, TPZCompMesh *MFMesh)
+{
+	TPZGeoMesh *gmesh = MFMesh->Reference();
+	gmesh->ResetReference();
+	int nMFEl = MFMesh->NElements();
+	int nmesh = cmeshVec.size();
+	int imesh;
+	for(imesh = 0; imesh<nmesh; imesh++)
+	{
+		cmeshVec[imesh]->LoadReferences();
+		int iel, is;
+		for(iel=0; iel<nMFEl; iel++)
+		{
+			TPZMultiphysicsElement *mfcel = dynamic_cast<TPZMultiphysicsElement *> (MFMesh->ElementVec()[iel]);
+			if(!mfcel)
+			{
+				DebugStop();
+			}
+			TPZGeoEl *gel = mfcel->Reference();
+			TPZStack<TPZCompElSide> celstack;
+			TPZGeoElSide gelside(gel,gel->NSides()-1);
+			if (gel->Reference()) {
+				celstack.Push(gelside.Reference());
+			}
+			gelside.ConnectedCompElementList(celstack, 0, 0);
+			for(is=0;is<celstack.size();is++) {
+				TPZGeoElSide gelside = celstack[is].Reference();
+				
+				if(gelside.Element()->Dimension()!=gel->Dimension()) {
+					if(is!=celstack.size()-1) {
+						celstack[is] = celstack.Pop();
+						is--;
+					}
+					else {
+						celstack.Pop();
+					}
+					
+				}
+			}
+			if(celstack.size() != 1)
+			{
+				DebugStop();
+			}
+			
+			mfcel->AddElement(celstack[0].Element(), imesh);
+		}
+		gmesh->ResetReference();
+	}
+}
+
+void TPZBuildMultiphysicsMesh::AddConnects(TPZVec<TPZCompMesh *> cmeshVec, TPZCompMesh *MFMesh)
+{
+	int nmeshes = cmeshVec.size();
+	TPZVec<int> FirstConnect(nmeshes,0);
+	int nconnects = 0;
+	int imesh;
+	for (imesh=0; imesh<nmeshes; imesh++) 
+	{
+		FirstConnect[imesh] = nconnects;
+		nconnects += cmeshVec[imesh]->ConnectVec().NElements();
+	}
+	MFMesh->ConnectVec().Resize(nconnects);
+	MFMesh->Block().SetNBlocks(nconnects);
+	int counter = 0;
+	int seqnum = 0;
+	for (imesh=0; imesh<nmeshes; imesh++) 
+	{
+		int ic;
+		int nc = cmeshVec[imesh]->ConnectVec().NElements();
+		for (ic=0; ic<nc; ic++) 
+		{
+			TPZConnect &refcon =  cmeshVec[imesh]->ConnectVec()[ic];
+			MFMesh->ConnectVec()[counter] = refcon;
+			if (refcon.SequenceNumber() >= 0) {
+				MFMesh->ConnectVec()[counter].SetSequenceNumber(seqnum);
+				int ndof = refcon.NDof(*cmeshVec[imesh]);
+				MFMesh->Block().Set(seqnum,ndof);
+				seqnum++;
+			}
+			counter++;
+		}	
+		// ajustar as dependencias
+		for (ic=0; ic<nc; ic++) 
+		{
+			TPZConnect &cn = MFMesh->ConnectVec()[FirstConnect[imesh]+ic];
+			if (cn.HasDependency()) 
+			{
+				TPZConnect::TPZDepend *dep = cn.FirstDepend();
+				while (dep) {
+					dep->fDepConnectIndex = dep->fDepConnectIndex+FirstConnect[imesh];
+					dep = dep->fNext;
+				}
+			}
+		}	
+	}
+	MFMesh->Block().SetNBlocks(seqnum);
+	MFMesh->ExpandSolution();
+	int iel;
+	int nelem = MFMesh->NElements();
+	for (iel = 0; iel < nelem; iel++) 
+	{
+		TPZMultiphysicsElement *cel = dynamic_cast<TPZMultiphysicsElement *> (MFMesh->ElementVec()[iel]);
+		if (!cel) {
+			DebugStop();
+		}
+		TPZStack<int> connectindexes;
+		int imesh;
+		for (imesh=0; imesh < nmeshes; imesh++) {
+			TPZCompEl *celref = cel->ReferredElement(imesh);
+			int ncon = celref->NConnects();
+			int ic;
+			for (ic=0; ic<ncon; ic++) {
+				connectindexes.Push(celref->ConnectIndex(ic)+FirstConnect[imesh]);
+			}
+		}
+		cel->SetConnectIndexes(connectindexes);
+	}
+}
+
+void TPZBuildMultiphysicsMesh::TransferFromMeshes(TPZVec<TPZCompMesh *> &cmeshVec, TPZCompMesh *MFMesh)
+{
+    int imesh;
+    int nmeshes = cmeshVec.size();
+    TPZManVector<int> FirstConnectIndex(nmeshes+1,0);
+    for (imesh = 0; imesh < nmeshes; imesh++) {
+        FirstConnectIndex[imesh+1] = FirstConnectIndex[imesh]+cmeshVec[imesh]->NConnects();
+    }
+    TPZBlock &blockMF = MFMesh->Block();
+    for (imesh = 0; imesh < nmeshes; imesh++) {
+        int ncon = cmeshVec[imesh]->NConnects();
+        TPZBlock &block = cmeshVec[imesh]->Block();
+        int ic;
+        for (ic=0; ic<ncon; ic++) {
+            TPZConnect &con = cmeshVec[imesh]->ConnectVec()[ic];
+            int seqnum = con.SequenceNumber();
+			if(seqnum<0) continue;       // Whether connect was deleted by previous refined process
+            int blsize = block.Size(seqnum);
+            TPZConnect &conMF = MFMesh->ConnectVec()[FirstConnectIndex[imesh]+ic];
+            int seqnumMF = conMF.SequenceNumber();
+            int idf;
+            for (idf=0; idf<blsize; idf++) {
+                blockMF.Put(seqnumMF, idf, 0, block.Get(seqnum, idf, 0));
+            }
+        }
+    }
+}
+
+void TPZBuildMultiphysicsMesh::TransferFromMultiPhysics(TPZVec<TPZCompMesh *> &cmeshVec, TPZCompMesh *MFMesh)
+{
+    int imesh;
+    int nmeshes = cmeshVec.size();
+    TPZManVector<int> FirstConnectIndex(nmeshes+1,0);
+    for (imesh = 0; imesh < nmeshes; imesh++) {
+        FirstConnectIndex[imesh+1] = FirstConnectIndex[imesh]+cmeshVec[imesh]->NConnects();
+    }
+    TPZBlock &blockMF = MFMesh->Block();
+    for (imesh = 0; imesh < nmeshes; imesh++) {
+        int ncon = cmeshVec[imesh]->NConnects();
+        TPZBlock &block = cmeshVec[imesh]->Block();
+        int ic;
+        for (ic=0; ic<ncon; ic++) {
+            TPZConnect &con = cmeshVec[imesh]->ConnectVec()[ic];
+            int seqnum = con.SequenceNumber();
+			if(seqnum<0) continue;       // Whether connect was deleted by previous refined process
+            int blsize = block.Size(seqnum);
+            TPZConnect &conMF = MFMesh->ConnectVec()[FirstConnectIndex[imesh]+ic];
+            int seqnumMF = conMF.SequenceNumber();
+            int idf;
+            for (idf=0; idf<blsize; idf++) {
+                block.Put(seqnum, idf, 0, blockMF.Get(seqnumMF, idf, 0));
+            }
+        }
+    }
+    
+}
+
