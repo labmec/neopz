0e21384e914b417d456a4f58fb3b31090cf911f6
cesar.lucci@mac.com <cesar.lucci@mac.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-11-21 18:55:12 +0000

    Holds PlaneFracture Materials Ids

---
+++ PlaneFracture/TPZPlaneFractureMaterials.h
@@ -0,0 +1,18 @@
+//
+//  TPZPlaneFractureMaterials.h
+//  PZ
+//
+//  Created by Cesar Lucci on 21/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+/** @brief Material index to create TPZGeoElRefPattern */
+const int __aux1DEl_Mat = -1;
+
+/** @brief Material index to create TPZGeoElRefPattern to linear element */
+const int __fractureLine_Mat = -2;
+
+const int __2DfractureMat = 1;//2D elements inside fracture
+const int __2DrockMat = 2;//2D elements outside fracture
+const int __3DRockMat = 3;//3D elements surrounding fracture plane
