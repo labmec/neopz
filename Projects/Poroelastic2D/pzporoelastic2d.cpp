fa7272d487375d8de6d9a1ad3825482dd074690c
agnaldofarias.mg@gmail.com <agnaldofarias.mg@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2011-12-05 10:47:39 +0000

    news methods

--- Projects/Poroelastic2D/pzporoelastic2d.cpp
+++ Projects/Poroelastic2D/pzporoelastic2d.cpp
@@ -7,15 +7,245 @@
  *
  */
 
-#include "pzporoelastic2d.h"
+#include <iostream>
+#include <string>
 
-TPZPoroElastic2d::TPZPoroElastic2d():TPZDiscontinuousGalerkin(), fDim(1){
+#include "pzporoelastic2d.h"
+#include "pzelasmat.h" 
+#include "pzbndcond.h"
+#include "pzaxestools.h"
+
+#include "pzlog.h"
+#ifdef LOG4CXX
+static LoggerPtr logdata(Logger::getLogger("pz.material.poroelastic.data"));
+#endif
+TPZPoroElastic2d::TPZPoroElastic2d():TPZDiscontinuousGalerkin(), ff(0), fnu(0.), fE(0.), falpha(0.), fk(0.), fvisc(0.), fPlaneStress(0), fmatId(0), fDim(2){
+	
+	ff.resize(2);
+	ff[0]=0.;
+	ff[1]=0.;
+	//fnu = 0.;
+//	fE=0.;
+//	fk =0.;
+//	fvisc = 0.;
+//	falpha = 0.;
+	fPlaneStress = 1.;
 	
 }
 
-TPZPoroElastic2d::TPZPoroElastic2d(int matid, int dim):TPZDiscontinuousGalerkin(matid),fDim(dim){
-	
+TPZPoroElastic2d::TPZPoroElastic2d(int matid, int dim):TPZDiscontinuousGalerkin(matid), ff(0), fnu(0.), fE(0.), falpha(0.), fk(0.), fvisc(0.),fPlaneStress(0), fDim(dim){
+	ff.resize(2);
+	ff[0]=0.;
+	ff[1]=0.;
+	//fnu = 0.;
+//	fE=0.;
+//	fk =0.;
+//	fvisc = 0.;
+//	falpha = 0.;
+	fPlaneStress = 1;
+	fmatId = matid;
 }
 
 TPZPoroElastic2d::~TPZPoroElastic2d(){
 }
+
+
+int TPZPoroElastic2d::NStateVariables() {
+	return 1;
+}
+
+
+void TPZPoroElastic2d::Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix &ek, TPZFMatrix &ef){
+	
+	
+	int nref =  datavec.size();
+	if (nref != 2 ) {
+		std::cout << " Erro.!! datavec tem que ser de tamanho 2 \n";
+		DebugStop();
+	}
+	
+	// Matrix size is for each element is  phiu.Rows() plus  phip.Rows()
+	
+	// Setting the size of first block of first problem. Elastic problem
+	TPZFMatrix  &phiu =  datavec[0].phi;
+	TPZFMatrix &dphiu = datavec[0].dphix;
+	int phru = phiu.Rows();
+	
+	// Setting the size of second block of second problem. transport problem 
+	TPZFMatrix  &phip =  datavec[1].phi;
+	TPZFMatrix &dphip = datavec[1].dphix;
+	int phrp = phip.Rows();
+	
+	//Equacao da elasticidade
+	// Calculate the matrix contribution for elastic problem
+	TPZAutoPointer <TPZElasticityMaterial> matelast = new TPZElasticityMaterial(fmatId, fE, fnu, ff[0], ff[1], fPlaneStress);
+	TPZFNMatrix<10000> ekelastic(2*phru,2*phru),efelastic(2*phru,1);
+	matelast->Contribute(datavec[0], weight, ekelastic, efelastic);
+	// It adds Source matrix on current matrix from position (sRow, sCol). 
+	ek.AddSub(0,0, ekelastic);
+	ef.AddSub(0, 0, efelastic);		
+	
+	//Equacao de Poisson: pressao 
+	// Calculate the matrix contribution for transport problem from
+	for(int in = 0; in < phrp; in++) {
+		ef(in+2*phru, 0) += 0.; 
+		
+		for(int jn = 0; jn < phrp; jn++) {
+			for(int kd=0; kd<fDim; kd++) {
+				ek(in+2*phru, jn+2*phru) += weight *dphip(kd,in)*dphip(kd,jn); 
+			}
+		}
+	}
+	
+	
+#ifdef LOG4CXX
+	if(logdata->isDebugEnabled())
+	{
+		std::stringstream sout;
+		ek.Print("ek = ",sout,EMathematicaInput);
+		ef.Print("ef = ",sout,EMathematicaInput);
+		ekelastic.Print("ekelastic = ",sout,EMathematicaInput);
+		efelastic.Print("efelastic = ",sout,EMathematicaInput);
+		LOGPZ_DEBUG(logdata,sout.str())
+	}
+#endif
+			
+}
+
+void TPZPoroElastic2d::ContributeBC(TPZVec<TPZMaterialData> &datavec,REAL weight, TPZFMatrix &ek,
+									   TPZFMatrix &ef,TPZBndCond &bc) 
+{
+		
+	int nref =  datavec.size();
+	if (nref != 2) {
+		std::cout << " Erro.!! datavec tem que ser de tamanho 2 \n";
+		DebugStop();
+	}
+	if (bc.Val2().Rows() != 3) {
+		std::cout << " Erro.!! Neste material precisa-se do valor da condicao de contorno para ux, uy e p.\n";
+		std::cout << "portanto precisa-se passar uma matrix Val2(3,1).\n";
+		DebugStop();
+	}
+	
+	if (bc.Val1().Rows() != 3) {
+		std::cout << " Erro.!! Neste material precisa-se do valor da condicao de contorno para ux, uy e p.\n";
+		DebugStop();
+	}
+	
+	TPZFMatrix  &phiu = datavec[0].phi;
+	TPZFMatrix  &phip = datavec[1].phi;
+	
+	int phru = phiu.Rows();
+	int phrp = phip.Rows();
+	short in,jn;
+	REAL v2[3];
+	v2[0] = bc.Val2()(0,0);
+	v2[1] = bc.Val2()(1,0);
+	v2[2] = bc.Val2()(2,0);
+		
+	switch (bc.Type()) {
+		case 0 :			// Dirichlet condition for two equations
+			//Equacao da elasticidade
+			for(in = 0 ; in < phru; in++) {
+				ef(2*in,0) += gBigNumber * v2[0] *   // x displacement
+				phiu(in,0) * weight;        // forced v2 displacement
+				ef(2*in+1,0) += gBigNumber * v2[1] * // x displacement
+				phiu(in,0) * weight;        // forced v2 displacement
+				for (jn = 0 ; jn < phru; jn++) {
+					ek(2*in,2*jn) += gBigNumber * phiu(in,0) *
+					phiu(jn,0) * weight;
+					ek(2*in+1,2*jn+1) += gBigNumber * phiu(in,0) *
+					phiu(jn,0) * weight;
+				}
+			}
+						
+			//segunda equacao
+			//Equacao de Poisson: pressao 
+			for(in = 0 ; in < phrp; in++) {
+				ef(in+2*phru,0) += gBigNumber * v2[2]*phip(in,0)*weight;
+				for (jn = 0 ; jn < phrp; jn++) {
+					ek(in+2*phru,jn+2*phru) += gBigNumber*phip(in,0)*phip(jn,0)*weight;
+				}
+			}
+			break;
+			
+		case 11 :		// Neumann condition for two equations
+			//Equacao da elasticidade
+			for(in = 0 ; in <phru; in++) {           // componentes da tracao normal ao contorno
+				ef(2*in,0) += v2[0] * phiu(in,0)*weight;   // tracao em x  (ou pressao)
+				ef(2*in+1,0) += v2[1] * phiu(in,0)*weight; // tracao em y (ou pressao) , nula se nao h
+			}      // ou deslocamento nulo  v2 = 0
+			
+			
+			//Equacao de Poisson: pressao 
+			for(in = 0 ; in < phrp; in++) {
+				ef(in+2*phru,0) += v2[2]*phip(in,0) * weight;
+			}
+			break;
+			
+			case 22 : // // Mixed condition for two equations
+			//Equacao da elasticidade			
+			for(in = 0 ; in < phru; in++) {
+				ef(2*in, 0) += v2[0] * phiu(in, 0) * weight;   // Neumann , Sigmaij
+				ef(2*in+1, 0) += v2[1] * phiu(in, 0) * weight; // Neumann
+				
+				for (jn = 0 ; jn < phru; jn++) {
+					ek(2*in,2*jn) += bc.Val1()(0,0)*phiu(in,0)*
+					phiu(jn,0)*weight;         // peso de contorno => integral de contorno
+					ek(2*in+1,2*jn) += bc.Val1()(1,0)*phiu(in,0)*
+					phiu(jn,0)*weight;
+					ek(2*in+1,2*jn+1) += bc.Val1()(1,1)*phiu(in,0)*
+					phiu(jn,0)*weight;
+					ek(2*in,2*jn+1) += bc.Val1()(0,1)*phiu(in,0)*
+					phiu(jn,0)*weight;
+				}
+			} 
+			
+			//Equacao de Poisson: pressao 
+			for(in = 0 ; in < phrp; in++) {
+				ef(in+2*phru, 0) += v2[2] * phip(in, 0)*weight;
+				for (jn = 0 ; jn < phrp; jn++) {
+					ek(in+2*phru,jn+2*phru) += bc.Val1()(2,0)*phip(in,0)*
+					phip(jn,0)*weight;     // peso de contorno => integral de contorno
+				}
+			}
+	}
+	
+}
+
+void TPZPoroElastic2d::Print(std::ostream &out) {
+	out << "name of material : " << Name() << "\n";
+	out << "properties : \n";
+	out << "\t E   = " << fE   << std::endl;
+	out << "\t nu   = " << fnu   << std::endl;
+	out << "\t Forcing function F   = " << ff[0] << ' ' << ff[1]   << std::endl; 
+	out << "Biot constant  = " << falpha << std::endl;
+	out << "Permeabilidade da rocha fK "<< fk << std::endl;
+	out << "Viscosidade do fluido "<< fvisc << std::endl;
+	out << "2D problem " << fPlaneStress << std::endl;
+	out << "Base Class properties :";
+	TPZMaterial::Print(out);
+	out << "\n";
+}
+
+/** Returns the variable index associated with the name */
+int TPZPoroElastic2d::VariableIndex(const std::string &name){
+	if(!strcmp("SolutionU",name.c_str()))        return  1;
+	if(!strcmp("SolutionP",name.c_str()))        return  2;
+	if(!strcmp("DerivateU",name.c_str()))        return  3;
+	if(!strcmp("DerivateP",name.c_str()))        return  4;
+	
+	return TPZMaterial::VariableIndex(name);
+}
+
+
+void TPZPoroElastic2d::ContributeInterface(TPZMaterialData &data, REAL weight, TPZFMatrix &ek, TPZFMatrix &ef){
+	DebugStop();
+}
+
+void TPZPoroElastic2d::ContributeBCInterface(TPZMaterialData &data, REAL weight, TPZFMatrix &ek,TPZFMatrix &ef,TPZBndCond &bc){
+	DebugStop();
+}
+
+
+
--- Projects/Poroelastic2D/pzporoelastic2d.h
+++ Projects/Poroelastic2D/pzporoelastic2d.h
@@ -12,6 +12,7 @@
 
 #include "pzmaterial.h"
 #include "pzdiscgal.h"
+#include "pzvec.h"
 
 #include <iostream>
 
@@ -21,20 +22,24 @@
  * @brief DESCRIBE PLEASE
  */
 /**
- * \f$ fK1 Laplac(u)  = fXf1 ==> Int{fK1 Grad(u).n v}ds - Int{fK1 Grad(u)Grad(v)}dx = Int{fxf1 v}dx   (Eq. 1)  \f$ 
+ **@ingroup equacao da elasticidade
+ * \f$  div(T(u))  + fXf2 = 0  ==> Int{Grad(v).T(u)}dx - Int{v.gN}ds  = Int{ff.v}dx   (Eq. 1) \f$
  *
- * \f$ fK2 Laplac(p)  = fXf2  ==> Int{fK2 Grad(p).n v}ds - Int{fK2 Grad(p)Grad(v)}dx = Int{fxf2 v}dx   (Eq. 2) \f$ 
+ *\f$ T(u) =  tr(E(u) − alpha*p*I)lambda*I + 2*nu*(E(u) − alpha*p*I)\f$
+ *
+ *@ingroup equacao da pressao
+ * \f$ -Laplac(u)  = 0 ==> Int{Grad(u)Grad(v)}dx - Int{Grad(u).n v}ds  = 0   (Eq. 2)  \f$ 
+ *
  */
+ 
 
 
 class TPZPoroElastic2d : public TPZDiscontinuousGalerkin {
 	
 protected:
-	///** @brief Forcing function value */
-	REAL fx;
-
+	
 	/** @brief Forcing vector */
-	REAL ff[2];
+	TPZVec<REAL>  ff;
 	
 	/** @brief Elasticity modulus */
 	REAL fE;
@@ -42,14 +47,23 @@ protected:
 	/** @brief Poison coeficient */
 	REAL fnu;
 	
+	/** @brief constant poroelastic Biot and K is the drained bulk modulus*/
+	REAL falpha; //[1/pa]
 	
-	
-	//	
 	/** @brief Problem dimension */
 	int fDim;
 	
-//	/** @brief Coeficient which multiplies the Laplacian operator. */
-//	REAL fK;
+	/**@ permeability of the rock and fluid viscosity*/
+	REAL fk; 
+	REAL fvisc;
+	
+	/** @brief Uses plain stress 
+	*@ fPlaneStress = 1 => Plain stress state 
+	*@ fPlaneStress != 1 => Plain Strain state 
+	*/
+	int fPlaneStress;
+	
+	REAL fmatId;
 	
 public:
 	TPZPoroElastic2d();
@@ -58,92 +72,126 @@ public:
 	
 	virtual ~TPZPoroElastic2d();
 	
-	//virtual void Print(std::ostream & out);
-//	
-//	virtual std::string Name() { return "TwoUncoupledPoisson"; }
-//	
-//	int Dimension() {return fDim;}
-//	
-//	virtual int NStateVariables();
-//	
-//	void SetParameters(REAL diff1, REAL diff2){
-//		fK1= diff1;
-//		fK2 =diff2;
+	virtual void Print(std::ostream & out);
+	
+	virtual std::string Name() { return "TPZPoroElastic2d"; }
+	
+	int Dimension() {return fDim;}
+	
+	virtual int NStateVariables();
+
+	/** @brief Parameters of rock and fluid: */
+	void SetParameters(REAL perm, REAL visc)
+	{
+		fk = perm;
+		fvisc = visc;
+	}
+	
+	/** 
+	 * @brief Set parameters of elastic material:
+	  * @param E elasticity modulus
+	 * @param nu poisson coefficient
+	 * @param fx forcing function \f$ -x = fx \f$ 
+	 * @param fy forcing function \f$ -y = fy \f$
+	 * @param plainstress \f$ plainstress = 1 \f$ indicates use of plainstress
+	 */
+	void SetParameters(REAL E, REAL nu, REAL falpha, REAL fx, REAL fy)
+	{
+		fE = E;
+		fnu = nu;
+		ff[0] = fx;
+		ff[1] = fy;
+	}
+	
+	/** @brief Set falpha parameter
+	 *@param biot : constant poroelastic Biot [dimensionless]
+	 *@param bulk : drained bulk modulus [Pa]
+	 */
+	void SetAlpha(REAL biot, REAL bulk)
+	{
+		falpha = biot/bulk;
+	}
+	
+	/** @brief Set plane problem  
+	 *@ planestress = 1 => Plain stress state 
+	 *@ planestress != 1 => Plain Strain state 
+	 */
+	void SetfPlaneProblem(int planestress)
+	{
+		fPlaneStress = planestress;
+	}
+	
+	//
+//	virtual TPZAutoPointer<TPZMaterial> NewMaterial(){
+//		return new TPZPoroElastic2d(*this);
 //	}
-//	
-//	void GetParameters(REAL &diff1, REAL &diff2){
-//		diff1 = fK1;
-//		diff2= fK2;
-//	}
-//	
-//	void SetInternalFlux(REAL flux1, REAL flux2)
-//	{
-//		fXf1 = flux1;
-//		fXf2 = flux2;
-//	}
-//	
-//	//virtual TPZAutoPointer<TPZMaterial> NewMaterial(){
-//	//		return new TwoUncoupledPoisson(*this);
-//	//	}
-//	
-//	/**
-//     * @brief It computes a contribution to the stiffness matrix and load vector at one integration point to multiphysics simulation.
-//     * @param datavec [in] stores all input data
-//     * @param weight [in] is the weight of the integration rule
-//     * @param ek [out] is the stiffness matrix
-//     * @param ef [out] is the load vector
-//     */
-//	virtual void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix &ek, TPZFMatrix &ef);
-//	
-//	virtual void ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix &ek,TPZFMatrix &ef,TPZBndCond &bc);
-//	
-//	//void ContributeInterface(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix &ek,TPZFMatrix &ef);
-//	
-//	virtual int VariableIndex(const std::string &name);
-//	
-//	virtual int NSolutionVariables(int var);
-//	
-//	//protected:
-//	//	
-//	//	/**
-//	//     * @brief It return a solution to multiphysics simulation.
-//	//     * @param Sol [in] is the solution 
-//	//     * @param DSol [in] is the Gradient
-//	//     * @param axes  [in] is the points of the coordinate axes
-//	//     * @param var [in] number of solution variables. See  NSolutionVariables() method
-//	//     * @param Solout [out] is the solution vector
-//	//     */	
-//	//	virtual void Solution(TPZVec<REAL> &Sol,TPZFMatrix &DSol,TPZFMatrix &axes, int var,TPZVec<REAL> &Solout);
-//	
-//	//public:
-//	virtual void Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<REAL> &Solout);
-//	
-//	//virtual int IntegrationRuleOrder(TPZVec<int> elPMaxOrder) const;
-//	
-//	/**
-//	 * @brief It computes a contribution to stiffness matrix and load vector at one integration point
-//	 * @param data [in]
-//	 * @param weight [in]
-//	 * @param ek [out] is the stiffness matrix
-//	 * @param ef [out] is the load vector
-//	 * @since April 16, 2007
-//	 */
-//	virtual void ContributeInterface(TPZMaterialData &data, REAL weight, TPZFMatrix &ek, TPZFMatrix &ef);
-//	
-//	/**
-//	 * @brief It computes a contribution to stiffness matrix and load vector at one BC integration point
-//	 * @param data [in]
-//	 * @param weight [in]
-//	 * @param ek [out] is the stiffness matrix
-//	 * @param ef [out] is the load vector
-//	 * @param bc [in] is the boundary condition object
-//	 * @since April 16, 2007
-//	 */
-//	virtual void ContributeBCInterface(TPZMaterialData &data, REAL weight, TPZFMatrix &ek,TPZFMatrix &ef,TPZBndCond &bc);
 	
+	/**
+     * @brief It computes a contribution to the stiffness matrix and load vector at one integration point to multiphysics simulation.
+     * @param datavec [in] stores all input data
+     * @param weight [in] is the weight of the integration rule
+     * @param ek [out] is the stiffness matrix
+     * @param ef [out] is the load vector
+     */
+	virtual void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix &ek, TPZFMatrix &ef);
 	
+	virtual void ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix &ek,TPZFMatrix &ef,TPZBndCond &bc);
 	
+		
+	//void ContributeInterface(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix &ek,TPZFMatrix &ef);
 	
+	virtual int VariableIndex(const std::string &name)
+	{
+	DebugStop();
+	}
+	
+	virtual int NSolutionVariables(int var)
+	{ 
+	DebugStop();
+	}
+	
+	//protected:
+	//	
+	//	/**
+	//     * @brief It return a solution to multiphysics simulation.
+	//     * @param Sol [in] is the solution 
+	//     * @param DSol [in] is the Gradient
+	//     * @param axes  [in] is the points of the coordinate axes
+	//     * @param var [in] number of solution variables. See  NSolutionVariables() method
+	//     * @param Solout [out] is the solution vector
+	//     */	
+	//	virtual void Solution(TPZVec<REAL> &Sol,TPZFMatrix &DSol,TPZFMatrix &axes, int var,TPZVec<REAL> &Solout);
+	
+	//public:
+	virtual void Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<REAL> &Solout)
+	{
+	DebugStop();
+	}
+	
+//	virtual int IntegrationRuleOrder(TPZVec<int> elPMaxOrder) const;
+	
+	/**
+	 * @brief It computes a contribution to stiffness matrix and load vector at one integration point
+	 * @param data [in]
+	 * @param weight [in]
+	 * @param ek [out] is the stiffness matrix
+	 * @param ef [out] is the load vector
+	 * @since April 16, 2007
+	 */
+	virtual void ContributeInterface(TPZMaterialData &data, REAL weight, TPZFMatrix &ek, TPZFMatrix &ef);
+	
+	/**
+	 * @brief It computes a contribution to stiffness matrix and load vector at one BC integration point
+	 * @param data [in]
+	 * @param weight [in]
+	 * @param ek [out] is the stiffness matrix
+	 * @param ef [out] is the load vector
+	 * @param bc [in] is the boundary condition object
+	 * @since April 16, 2007
+	 */
+	virtual void ContributeBCInterface(TPZMaterialData &data, REAL weight, TPZFMatrix &ek,TPZFMatrix &ef,TPZBndCond &bc);
+	
+
 	/** @name Contribute methods
 	 * @{
 	 */
@@ -172,7 +220,5 @@ public:
     virtual void ContributeBC(TPZMaterialData &data, REAL weight, TPZFMatrix &ek, TPZFMatrix &ef, TPZBndCond &bc){
 		DebugStop();
 	}
-	
-	
 };
 #endif
