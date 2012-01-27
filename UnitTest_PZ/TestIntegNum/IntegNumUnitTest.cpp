8f07fa55e1ee3a38f30fa8bab3b7e7c3e6672ad1
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-01-27 20:37:05 +0000

    Improved the use of the output_test_stream of the Boost Unit Test

--- UnitTest_PZ/TestIntegNum/IntegNumUnitTest.cpp
+++ UnitTest_PZ/TestIntegNum/IntegNumUnitTest.cpp
@@ -29,17 +29,10 @@ using namespace std;
 // Output file with the first test.
 bool first_run = false;
 int FirstOrder = 0;
+int max_order = MAXORDER;
+int expo = 3;
 
 std::string dirname = PZSOURCEDIR;
-
-// Numerical integration values obtained using Mathematica: one-dim, quadrilateral, triangular, cube, tetrahedram, pyramidal, prism
-REAL intvalue[7][MAXORDER] = { {2.000000000000000,2.606198284550454,2.659572164415588,1.499341835485549,2.367617787494460,1.825083430864047,2.169215575174691,1.617745418673051,2.480691807001154,1.347699766137747,1.417801801857337,1.935192061654517,1.429663752832786},
-	{4.,6.424793138201817,4.310074335087271,2.626129673372531,6.054379903820096,2.924805380542269,3.6230005930154684,4.556300644939264,4.577512525115554,1.6940832032465925,4.052863938018176,3.858582377588495,1.6518896896567403 },
-	{0.5,0.9320398994069123,0.54479546786258,0.5063395201360538,0.45428110865992366,0.2509209988237399,0.26457181178979317,0.4853077813019233,0.8543252469564175,0.3195852820307958,0.3282395374596146,0.49096556829476024,0.18197735362039558},
-	{10.42479313820182,15.27437941460545,11.04494180837636,7.677052484946879,14.53355294584201,8.274403899286355,9.670794324232754,11.53739442808034,11.57981818843293,5.812959544695003,10.53052101423817,10.14195789337881,5.728572517515299},
-	{0.21688291481409032598,0.31731541110893806824,0.2271127993891759,0.2227611387537444,0.2227611387537444,0.1579731476556451,0.1592039908614216,0.2114714205797953,0.3036594998229580,0.1789852244559073,0.1737775985061321,0.2150662368602839,0.1447150978897556},
-	{1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0},
-	{1.303099142275227,2.167178941089052,1.392690078000387,1.315778182547335,1.211661359595075,0.8049411399227070,0.8322427658548136,1.273714704879074,2.011749636188062,0.9422697063368188,0.9595782171944565,1.285030278864748,0.6670538495160184} };
 
 // Computing coefficient of the polinomial function a(i,p) = sin(i*p*p)
 double CoefficientX(int i, int p) {
@@ -56,16 +49,26 @@ double CoefficientZ(int i, int p) {
 // Defining a function to integrate f(x,y,z,i,p) = CoeffX(0,p) + CoeffX(1,p)x + CoeffX(2,p)x^2 + ... + CoeffX(i,p)x^i 
 //                                                             + CoeffY(1,p)y + CoeffY(2,p)y^2 + ... + CoeffY(i,p)y^i
 //                                                             + CoeffZ(1,p)z + CoeffZ(2,p)z^2 + ... + CoeffZ(i,p)z^i 
-double Funcao(TPZVec<REAL> &point, int i, int p) {
-	REAL val = 0.0;
+double Funcao(TPZVec<REAL> &point, int expo, int p) {
+	REAL val = 0.0, power;
 	REAL x = point[0];
 	REAL y = point[1];
 	REAL z = point[2];
+	int ii, jj;
 	val = CoefficientX(0,p);
-	for(int ii=1; ii<= i; ii++) {
-		val += (CoefficientX(ii,p) * pow(x,ii));
-		val += (CoefficientY(ii,p) * pow(y,ii));
-		val += (CoefficientZ(ii,p) * pow(z,ii));
+	for(ii=1; ii<= expo; ii++) {
+		power = 1.;
+		for(jj=0;jj<ii;jj++)
+			power *= x;
+		val += (CoefficientX(ii,p) * power);
+		power = 1.;
+		for(jj=0;jj<ii;jj++)
+			power *= y;
+		val += (CoefficientY(ii,p) * power);
+		power = 1.;
+		for(jj=0;jj<ii;jj++)
+			power *= z;
+		val += (CoefficientZ(ii,p) * power);
 	}
 	return val;
 }
@@ -85,12 +88,14 @@ double Funcao(TPZVec<REAL> &point, int i, int p) {
  * Para i = 2, order = 6, 2D, integral = 
  */
 template <class NumInteg>
-void TestingNumericIntegrationRule(int exp, int p, REAL intvalue,boost::test_tools::output_test_stream &out,std::ofstream &results) {
+void TestingNumericIntegrationRule(int exp, int p,boost::test_tools::output_test_stream &out) {
 	
 	TPZVec<REAL> point(3,0.);
 	REAL weight = 0.;
 	REAL integral = 0.;
+	char text[64];
 	TPZVec<int> order(3,p);
+	memset(text,0,64);
 
 	// Integration rule
 	NumInteg intrule(p);
@@ -107,14 +112,12 @@ void TestingNumericIntegrationRule(int exp, int p, REAL intvalue,boost::test_too
 		integral += weight * Funcao(point,exp,p);
 	}
 	
-	cout << "Numerical integration value = " << integral << "\tDiff = " << fabs(intvalue - integral) << std::endl << std::endl;
-	
-	BOOST_CHECK(IsZero((intvalue-integral)*.0001));
-	
+	sprintf(text,"%.7lf\n",integral);
+	cout << "Numerical integration value = " << text << std::endl;
 	// Stores the obtained integral value
-	if(!first_run) out << integral << std::endl;
-	// First tests stores the integral values for following tests
-	else results << integral << std::endl;
+	out << text;
+
+	BOOST_CHECK( out.match_pattern() );
 }
 
 /**
@@ -133,21 +136,19 @@ BOOST_AUTO_TEST_CASE(numinteg1D_tests) {
 
 	std::string filename = dirname + "/UnitTest_PZ/TestIntegNum/";
 	filename += "FirstResult1D.txt";
-	boost::test_tools::output_test_stream output(filename,false);
-	std::ofstream FirstResult(filename.c_str());
+	boost::test_tools::output_test_stream output(filename,true);
 	
-	int i = 2;
 	int order;
 	// Whether FirstResult.txt is empty then it is a first run
-	for(order=FirstOrder;order<MAXORDER;order++) {
-		if(order == 1) continue;
-		TestingNumericIntegrationRule<TPZInt1d>(i,order,intvalue[0][order],output,FirstResult);   // OK
+	for(order=FirstOrder;order<max_order;order++) {
+		if(order == 1) {
+			output << "\n";
+			BOOST_CHECK( output.match_pattern() );	
+			continue;
+		}
+		TestingNumericIntegrationRule<TPZInt1d>(expo,order,output);   // OK
 	}
 	// Conclusion: It's failed at order = 1. But the erro is proportional e-10 from order = 8
-	
-	// For next executions
-	if(!first_run) BOOST_CHECK( output.match_pattern() );
-	FirstResult.close();
 }
 
 BOOST_AUTO_TEST_CASE(numinteg2D_tests) {
@@ -155,29 +156,31 @@ BOOST_AUTO_TEST_CASE(numinteg2D_tests) {
 	std::string filename = dirname + "/UnitTest_PZ/TestIntegNum/";
 	filename += "FirstResult2D.txt";
 	boost::test_tools::output_test_stream output(filename,false);
-	std::ofstream FirstResult(filename.c_str());
 	
-	int i = 2;
 	int order;
 	// Quadrilateral parametric space
-	for(order=FirstOrder;order<MAXORDER;order++) {
-		if(order == 1) continue;
-		TestingNumericIntegrationRule<TPZIntQuad>(i,order,intvalue[1][order],output,FirstResult);   // OK
+	for(order=FirstOrder;order<max_order;order++) {
+		if(order == 1) {
+			output << "\n";
+			BOOST_CHECK( output.match_pattern() );	
+			continue;
+		}
+		TestingNumericIntegrationRule<TPZIntQuad>(expo,order,output);   // OK
 	}
 	// Conclusion: We have problem at order 1. But the erro is proportional e-10 from order = 8
-	if(first_run) FirstResult << std::endl;
-	else output << std::endl;
+	output << "\n";
+	BOOST_CHECK( output.match_pattern() );	
 	
 	// Triangular parametric space
-	for(order=FirstOrder;order<MAXORDER;order++) {
-		if(order == 1 || order == 5) continue;
-		TestingNumericIntegrationRule<TPZIntTriang>(i,order,intvalue[2][order],output,FirstResult);
+	for(order=FirstOrder;order<max_order;order++) {
+		if(order == 1 || order == 5) {
+			output << "\n";
+			BOOST_CHECK( output.match_pattern() );	
+			continue;
+		}
+		TestingNumericIntegrationRule<TPZIntTriang>(expo,order,output);
 	}
 	// Conclusion: We have problem at order 1 and 5. It's failed at order >= 10.
-	
-	// For next executions
-	if(!first_run) BOOST_CHECK( output.match_pattern() );
-	FirstResult.close();
 }
 
 BOOST_AUTO_TEST_CASE(numinteg3D_tests) {
@@ -185,46 +188,49 @@ BOOST_AUTO_TEST_CASE(numinteg3D_tests) {
 	std::string filename = dirname + "/UnitTest_PZ/TestIntegNum/";
 	filename += "FirstResult3D.txt";
 	boost::test_tools::output_test_stream output(filename,false);
-	std::ofstream FirstResult(filename.c_str());
 	
-	int i = 2;
 	int order;
 	// Cube
-	for(order=FirstOrder;order<MAXORDER;order++) {
-		if(!order || order == 1) continue;
-		TestingNumericIntegrationRule<TPZIntCube3D>(i,order,intvalue[3][order],output,FirstResult);   // OK
+	for(order=FirstOrder;order<max_order;order++) {
+		if(!order || order == 1) {
+			output << "\n";
+			BOOST_CHECK( output.match_pattern() );	
+			continue;
+		}
+		TestingNumericIntegrationRule<TPZIntCube3D>(expo,order,output);   // OK
 	}
 	// Conclusion: We have problem at order 1 and 0. But the erro is proportional e-10 from order = 8
-	if(first_run) FirstResult << std::endl;
-	else output << std::endl;
+	output << "\n";
+	BOOST_CHECK( output.match_pattern() );
 	
 	// Tetrahedram
-	for(order=FirstOrder;order<MAXORDER;order++) {
-		if(!order || order == 1 || order == 4) continue;
-		TestingNumericIntegrationRule<TPZIntTetra3D>(i,order,intvalue[4][order],output,FirstResult);
+	for(order=FirstOrder;order<max_order;order++) {
+		if(!order || order == 1 || order == 4) {
+			output << "\n";
+			BOOST_CHECK( output.match_pattern() );	
+			continue;
+		}
+		TestingNumericIntegrationRule<TPZIntTetra3D>(expo,order,output);
 	}
 	// Conclusion: It's failed at order = 1 and order = 4
-	if(first_run) FirstResult << std::endl;
-	else output << std::endl;	
+	output << "\n";	
+	BOOST_CHECK( output.match_pattern() );
 	
 	// Pyramidal
-	for(order=FirstOrder;order<MAXORDER;order++) {
-		//		TestingNumericIntegrationRule<TPZIntPyram3D>(i,order,out);  // It isn't implemented yet
+	for(order=FirstOrder;order<max_order;order++) {
+		//		TestingNumericIntegrationRule<TPZIntPyram3D>(expo,order,out);  // It isn't implemented yet
 	}
-//	if(first_run) FirstResult << std::endl;
-//	else output << std::endl;
 	
 	// Prism
-	for(order=FirstOrder;order<MAXORDER;order++) {
-		if(!order || order == 1 || order == 5) continue;
-		TestingNumericIntegrationRule<TPZIntPrism3D>(i,order,intvalue[6][order],output,FirstResult);
+	for(order=FirstOrder;order<max_order;order++) {
+		if(!order || order == 1 || order == 5) {
+			output << "\n";
+			BOOST_CHECK( output.match_pattern() );	
+			continue;
+		}
+		TestingNumericIntegrationRule<TPZIntPrism3D>(expo,order,output);
 	}
 	// Conclusion: It's failed at order = 1 and order = 0
-
-	// For next executions
-	if(!first_run) BOOST_CHECK( output.match_pattern() );
-	
-	FirstResult.close();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
