4cf4dad1085f32ef4bda6a98d3cb02b9c5039501
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-02-25 21:24:42 +0000

    Fixing nightly compilation problem. Re-adding default parameter (matColor = false) on 3 functions from Post/TPZVTKGeoMesh.h

--- Post/TPZVTKGeoMesh.h
+++ Post/TPZVTKGeoMesh.h
@@ -32,7 +32,7 @@ public:
 	~TPZVTKGeoMesh();
 	
 	/** @brief Generate an output of all geomesh to VTK */
-	static void PrintGMeshVTK(TPZGeoMesh *gmesh, std::ofstream &file, bool matColor);
+	static void PrintGMeshVTK(TPZGeoMesh *gmesh, std::ofstream &file, bool matColor = false);
 	
 	/** @brief Generate an output of all geomesh to VTK, associating to each one the given data */
 	static void PrintGMeshVTK(TPZGeoMesh *gmesh, std::ofstream &file, TPZVec<int> &elData);
@@ -44,7 +44,7 @@ public:
 	/**
 	 * @brief Based on a given geomesh, just the elements that have an neighbour with a given material id will be exported to an VTK file
 	 */
-	static void PrintGMeshVTKneighbour_material(TPZGeoMesh *gmesh, std::ofstream &file, int neighMaterial, bool matColor);
+	static void PrintGMeshVTKneighbour_material(TPZGeoMesh *gmesh, std::ofstream &file, int neighMaterial, bool matColor = false);
     
     /**
      * @brief Print the elements that surround a givel geoel
@@ -55,7 +55,7 @@ public:
 	/**
 	 * @brief Based on a given geomesh, just the elements that have the given material id will be exported to an VTK file
 	 */
-	static void PrintGMeshVTKmy_material(TPZGeoMesh *gmesh, std::ofstream &file, std::set<int> myMaterial, bool matColor);
+	static void PrintGMeshVTKmy_material(TPZGeoMesh *gmesh, std::ofstream &file, std::set<int> myMaterial, bool matColor = false);
 	
 	static int GetVTK_ElType(TPZGeoEl *gel);
 };
