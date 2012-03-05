9c784a37f4b900efa7690362b8b56c19352be787
nathan.sh@gmail.com <nathan.sh@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-03-05 21:50:41 +0000

    DebugStop se NAO foi feito o BuildConnectivity antes de gerar as condicoes de contorno

--- Mesh/pzgeoelbc.cpp
+++ Mesh/pzgeoelbc.cpp
@@ -14,12 +14,20 @@ TPZGeoElBC::TPZGeoElBC(TPZGeoEl *el,int side,int matid):fCreatedElement(NULL){
 		PZError << "Error at " << __PRETTY_FUNCTION__ << " - TPZGeoEl *el is NULL\n";
 		return;
 	}
+	if(el->SideIsUndefined(side)) //Verify if the connectivity was made before the creation of the bc
+	{
+		DebugStop();
+	}
 	this->fCreatedElement = el->CreateBCGeoEl( side, matid );
 }
 
 TPZGeoElBC::TPZGeoElBC(TPZGeoElSide &elside,int matid):fCreatedElement(NULL){
 	TPZGeoEl * el = elside.Element();
 	const int side = elside.Side();
+	if(el->SideIsUndefined(side)) //Verify if the connectivity was made before the creation of the bc
+	{
+		DebugStop();
+	}
 	if (!el || side == -1){
 		if (!el) PZError << "Error at " << __PRETTY_FUNCTION__ << " - TPZGeoEl *elside.Element() is NULL\n";
 		if (side == -1) PZError << "Error at " << __PRETTY_FUNCTION__ << " - int elside.Side() is -1\n";
