26d51fc5c1c73d4fd9442cb99fb4ebc9d54b1b5d
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-03-24 19:34:43 +0000

    Re-organizing the first project for TUTORIAL

--- Projects/PDElinear1D/main.cpp
+++ Projects/PDElinear1D/main.cpp
@@ -18,39 +18,13 @@
 #include "TPZRefPattern.h"
 #include "tpzgeoelrefpattern.h"
 
-#include "tpzautopointer.h"
-#include "pzbndcond.h"
-#include "pzanalysis.h"
-#include "TPZParSkylineStructMatrix.h"
-#include "pzstepsolver.h"
-
-#include "pzstrmatrix.h"
-#include "TPZFrontNonSym.h"
-#include "TPZFrontSym.h"
-
+#include "PZ_Process.h"
 
 #include <iostream>
 #include <string>
 #include <math.h>
 
-
-
-//Task to DO:
-
-//Usar TPZtransient Aanlysis
-//Usar TPZtransient Material
-//Do changes to contribute of Transient analysis
-
-
 using namespace std;
-
-
-const int matId = 1;
-const int bc1 = -1;
-const int bc2 = -2;
-
-const int dirichlet = 0;
-const int neumann = 1;
 
 //
 //	This program solve  a system of linear PDE - 1D I hope! 
@@ -58,15 +32,7 @@ const int neumann = 1;
 //	using uD = 0  x = 0  and  x = 1.
 //
 
-
-// Create a Geometrical Mesh
-TPZGeoMesh *GeomMesh(int h, TPZVec<REAL> &xL, TPZVec<REAL> &xR);
-
-// Create a Computational Mesh
-TPZCompMesh * CompMesh(TPZGeoMesh *gmesh, int p, REAL a, REAL b);
-
-// Assemble and Solve the generate linear System
-void SolveSist(TPZAnalysis &an, TPZCompMesh *fCmesh);
+/* RELATED TO MODEL */
 
 // Right handside term of our Linear PDE
 void ForcingFunction(const TPZVec<REAL> &pto, TPZVec<REAL> &xfloat) 
@@ -88,292 +54,165 @@ void SolExata(TPZVec<REAL> &pto, TPZVec<REAL> &u_exact,TPZFMatrix<REAL> &du_exac
 	//	du_exact(1,0) =0.;//dy
 }
 
-int main()
-//int main_firs(int argc, char *argv[])
-{	
+//int main()
+int main_firs(int argc, char *argv[])
+{
+	// Initializing generation log comments as log4cxx done
 	std::string logs("log4cxx.doubleprojection1d");
 	InitializePZLOG();
-	// gRefDBase.InitializeRefPatterns();
 	
-	std::ofstream arg1("gmesh.txt");
-	std::ofstream arg2("cmesh.txt");	
-	std::ofstream file("Solution.txt",ios::app);	
-	std::ofstream outerror("erros.txt",ios::app);
+	// Files for information (geometric mesh and computational mesh)
+	std::ofstream infoGeoMesh("gmesh.txt");
+	std::ofstream infoCompMesh("cmesh.txt");	
+	// Files for output solution as multiplier coefficients
+	std::ofstream FileSolution("Solution.txt");	
+	std::ofstream FileError("erros.txt");
 
-	outerror << "\n\n Solving with P = 1\n";
-	int p = 1;
-	int h = 2;
+	// p -> interpolation order
+	int p = 2;
+	// h -> level of uniform refinement of the initial mesh
+	int h = 4;
 	
-	// Creating a geometric mesh
+	// Creating main extremes and material for current project
 	TPZManVector<REAL> x0(3,0.), x1(3,0.);  // Corners of the mesh. Coordinates are zeros.
 	x1[0] = 1.0;
-	TPZGeoMesh *gmesh = GeomMesh(h,x0,x1);
-	// Print Geometrical Mesh	
-	gmesh->Print(arg1);
+	// id of material and boundary conditions
+	TPZVec<int> matId(1,1);
+	TPZVec<int> bc(2,-1);
+	bc[1] = -2;
+	// type of boundary conditions
+	TPZVec<int> bcType(2,0);   // 0 - dirichlet
 	
-	REAL a=1.;
-	REAL b=1.;
-			
-	TPZCompMesh * cmesh = CompMesh(gmesh,p,a,b);
-	// Print Computational mesh			
-	cmesh->Print(arg2);
+	// Coefficients of the differential equation (Linear 1D). They will be required by material constructor
+	TPZFMatrix<REAL> xkin(1,1,1.0);   //  k = 1
+	TPZFMatrix<REAL> xbin(1,1,1.0);   //  b = 1
+	TPZFMatrix<REAL> xcin(1,1,0.0);   //  c = 0
+	TPZFMatrix<REAL> xfin(1,1,0.0);
+	// Material data
+	TPZMat1dLin *material;
+	material = new TPZMat1dLin(matId[0]); 
+	material->SetMaterial(xkin,xcin,xbin,xfin);
+	// inserting function force
+	material->SetForcingFunction(new TPZDummyFunction(ForcingFunction));
+	
+	// FEM PROCESS
+	// Creating a geometric mesh and printing geometric information. Note: The coordinates of the nodes are 3D
+	TPZGeoMesh *gmesh = GeomMesh(h,matId,bc,x0,x1);
+	gmesh->Print(infoGeoMesh);	
+	
+	// Creating a computational mesh and printing computational information.
+	TPZCompMesh * cmesh = CompMesh(gmesh,p,material,bc,bcType);
+	cmesh->Print(infoCompMesh);
 
+	// Assembling and Solving linear system
 	TPZAnalysis an(cmesh);
 	SolveSist(an,cmesh);
 	
-	// Print Solution			
+	// Print Solution as multiplier coefficients
 	TPZFMatrix<REAL> toprint = an.Solution();
-	toprint.Print("Solution", file);
+	toprint.Print("Solution", FileSolution);
 
-	/// Plot erro (norms) 
+	// Plot erro (norms) 
 	an.SetExact(SolExata);
 	TPZVec<REAL> posproc;
-	an.PostProcess(posproc,outerror); // Compute the errors
-	outerror<<endl;
+	FileError << "\n\n Solving with P = 1\n";
+	an.PostProcess(posproc,FileError); // Compute the errors
+	FileError<<endl;
 	
+	// Close output files
+	infoGeoMesh.close();
+	infoCompMesh.close();
+	FileSolution.close();
+	FileError.close();
+
 	return EXIT_SUCCESS;
 }
 
-TPZGeoMesh *GeomMesh(int h, TPZVec<REAL> &xL, TPZVec<REAL> &xR){
-	
-	TPZGeoMesh * gmesh = new TPZGeoMesh;
-	int Qnodes = 2;
-	gmesh->SetMaxNodeId(Qnodes-1);
-	gmesh->NodeVec().Resize(Qnodes);
-	TPZVec<TPZGeoNode> Node(Qnodes);
-	
-	TPZVec <int> TopolLine(2);
-	TPZVec <int> TopolPoint(1);
-	
-	//indice dos nos
-	int id = 0;
-	int ndiv = 1;
-	REAL dx = fabs(xR[0] - xL[0])/ndiv;
-	REAL pointco;
-	for(int xi = 0; xi < Qnodes; xi++)
-	{
-		pointco = xL[0]+xi*dx;
-		Node[id].SetNodeId(id);
-		Node[id].SetCoord(0 ,pointco);//coord X
-		gmesh->NodeVec()[id] = Node[id];
-		id++;
-	}
-	
-	//indice dos elementos
-	id=0;
-	TopolPoint[0] = 0;
-	new TPZGeoElRefPattern< pzgeom::TPZGeoPoint > (id,TopolPoint,bc1, *gmesh);
-	id++;
-	
-	for (int eli=0; eli<ndiv; eli++) {
-		TopolLine[0] = eli;
-		TopolLine[1] = eli+1;
-		new TPZGeoElRefPattern< pzgeom::TPZGeoLinear > (id,TopolLine,matId,*gmesh);
-		id++;
-	}
-	
-	TopolPoint[0] = Qnodes-1;
-	new TPZGeoElRefPattern< pzgeom::TPZGeoPoint > (id,TopolPoint,bc2,*gmesh);
-	
-	gmesh->BuildConnectivity();
-	
-	
-	//refinamento uniforme
-	for (int ref = 0; ref < h; ref++){
-		TPZVec<TPZGeoEl *> filhos;
-		int n = gmesh->NElements();
-		for ( int i = 0; i < n; i++ ){
-			TPZGeoEl * gel = gmesh->ElementVec() [i];
-			if (gel->Dimension() == 1) gel->Divide (filhos);
-		}//for i
-	}//ref
-	
-	
-	//	ofstream arg("gmesh.txt");
-	//	gmesh->Print(arg);
-	
-	return gmesh;
-}
-
-//int main(int argc, char *argv[])
-int main_refinement(int argc, char *argv[])
+int main(int argc, char *argv[])
+//int main_refinement(int argc, char *argv[])
 {	
+	// Initializing generation log comments as log4cxx done
 	std::string logs("log4cxx.doubleprojection1d");
 	InitializePZLOG();
-	// gRefDBase.InitializeRefPatterns();
 	
-	std::ofstream arg1("gmesh.txt");
-	std::ofstream arg2("cmesh.txt");	
-	std::ofstream file("Solution.txt",ios::app);	
-	std::ofstream outerror("erros.txt",ios::app);
+	// Files for information (geometric mesh and computational mesh)
+	std::ofstream infoGeoMesh("gmesh.txt");
+	std::ofstream infoCompMesh("cmesh.txt");	
+	// Files for output solution as multiplier coefficients
+	std::ofstream FileSolution("Solution.txt");	
+	std::ofstream FileError("erros.txt");
 	
-	for(int p = 1; p < 2; p++)
+	// File for Mathematica output format
+	std::ofstream outMath("Mathematica.txt");
+	// File for VTK visualization
+	std::string outVTK("saidaT.vtk");
+
+	// Creating nodes (extremes) and material for current project
+	TPZManVector<REAL> x0(3,0.), x1(3,0.);  // Corners of the mesh. Coordinates are zeros.
+	x1[0] = 1.0;
+	// id of material and boundary conditions
+	TPZVec<int> matId(1,1);
+	TPZVec<int> bc(2,-1);
+	bc[1] = -2;
+	// type of boundary conditions
+	TPZVec<int> bcType(2,0);   // 0 - dirichlet
+
+	// Coefficients of the differential equation (Linear 1D). They will be required by material constructor
+	TPZFMatrix<REAL> xkin(1,1,1.0);   //  k = 1
+	TPZFMatrix<REAL> xbin(1,1,1.0);   //  b = 1
+	TPZFMatrix<REAL> xcin(1,1,0.0);   //  c = 0
+	TPZFMatrix<REAL> xfin(1,1,0.0);
+	// Material data
+	TPZMat1dLin *material;
+	material = new TPZMat1dLin(matId[0]); 
+	material->SetMaterial(xkin,xcin,xbin,xfin);
+	// inserting function force
+	material->SetForcingFunction(new TPZDummyFunction(ForcingFunction));
+	
+	// Loop over p -> interpolation order
+	for(int p = 1; p < 20; p++)
 	{
-		for(int h = 1; h <10 	; h++)
-		{	
-			outerror << "\n\n Solving with P = " << p << " E H = " << pow(2.,h) << "\n";
-			
-			TPZManVector<REAL> x0(3,0.), x1(3,0.);  // Corners of the mesh. Coordinates are zeros.
-			x1[0] = 1.0;
-			TPZGeoMesh * gmesh = GeomMesh(h, x0, x1);
-			//int p=1;
-			REAL a=1.;
-			REAL b=1.;
-			
-			TPZCompMesh * cmesh = CompMesh(gmesh,p,a,b);
+		// Loop over h -> level of uniform refinement
+		for(int h = 2; h <10; h++)
+		{
+			// Creating a geometric mesh and printing geometric information.  Note: The coordinates of the nodes are 3D
+			TPZGeoMesh *gmesh = GeomMesh(h,matId,bc,x0,x1);
+			gmesh->Print(infoGeoMesh);
+
+			// Creating a geometric mesh and printing computational information.
+			TPZCompMesh *cmesh = CompMesh(gmesh,p,material,bc,bcType);
+			cmesh->Print(infoCompMesh);
+
+			// Assembling and Solving linear system
 			TPZAnalysis an(cmesh);
-			
 			SolveSist(an,cmesh);
-			
-			// Print Geometrical Mesh	
-			gmesh->Print(arg1);
-			
-			// Print Computational mesh			
-			cmesh->Print(arg2);
-			
-			// Print Solution			
+
+			// Print Solution
 			TPZFMatrix<REAL> toprint = an.Solution();
-			toprint.Print("Solution", file);
-			
-			
-			//			// Saida para mathematica
-			//			std::ofstream outMath("Mathematica.txt",ios::app);
-			//			std::cout << "Saida = {";
-			//			for(int i = 0;  i< cmesh->ElementVec().NElements(); i++)
-			//			{
-			//				TPZCompEl * cel = cmesh->ElementVec()[i];
-			//				TPZInterpolationSpace * sp = dynamic_cast <TPZInterpolationSpace*>(cel);
-			//				TPZVec<REAL> qsi(1,0.), sol(1,0.), outfem(1,0.);
-			//				TPZFMatrix<REAL> axes(1,3,0.), dsol(1,1,0.);
-			//				if(i != 0) std::cout << ",";
-			//				for(int j = 0; j < 11; j++)
-			//				{
-			//					qsi[0] = -1.+2.*j/10.;
-			//					sp->Solution(qsi,1,sol);
-			//					gmesh->ElementVec()[i]->X(qsi,outfem);
-			//					std::cout << "{" << 0 << "," << 0<<"}";
-			//					if(j != 0) std::cout << ",";
-			//				}
-			//				if(i== cmesh->ElementVec().NElements()-1) std::cout<<"};"<<std::endl;
-			//			}
-			//			std::cout<<"ListPlot[Saida,Joined->True]"<<endl;
-			
-			
-			///3. Plotar as normas dos erros 
+			toprint.Print("Solution", FileSolution);
+
+			// Solution output for Mathematica
+			OutputMathematica(outMath, cmesh);
+
+			// Computing the errors (and plot)
 			an.SetExact(SolExata);
+			FileError << "\n\n Solving with P = " << p << " E H = " << pow(2.,h) << "\n";
 			TPZVec<REAL> posproc;
-			an.PostProcess(posproc,outerror); // Calcula os erros.(pzanalysis.cpp)
-			outerror<<endl;
-			
-			//saiada para VTK
-			//TPZManVector<std::string,10> scalnames(1), vecnames(1);
-			//			scalnames[0] = "Solution";
-			//			vecnames[0] = "Derivate";
-			//			
-			//			std::string plotfile("saidaT.vtk");
-			//			const int dim = 2;
-			//			int div = 0;
-			//			an.DefineGraphMesh(dim,scalnames,vecnames,plotfile);
-			//			an.PostProcess(div,dim);
-			//			std::ofstream out("malha.txt");
-			//			an.Print("nothing",out);
+			an.PostProcess(posproc,FileError); // Computing the errors.(pzanalysis.cpp)
+			FileError << endl;
+
+			// Solution output for VTK
+		//	OutputVTK(outVTK,cmesh,an);
 		}
+		outMath << endl;
 	}
 	
+	// Close output files
+	infoGeoMesh.close();
+	infoCompMesh.close();
+	FileSolution.close();
+	FileError.close();
+	
 	return EXIT_SUCCESS;
-}
-
-
-TPZCompMesh *CompMesh(TPZGeoMesh *gmesh, int p, REAL a, REAL b){
-	
-	/// criar materiais
-	int dim = 1;
-	
-	TPZMat1dLin *material;
-	material = new TPZMat1dLin(matId); 
-	
-	TPZFMatrix<REAL> xkin(1,1,1.0);
-	TPZFMatrix<REAL> xcin(1,1,0.0);
-	TPZFMatrix<REAL> xbin(1,1,1.0);
-	TPZFMatrix<REAL> xfin(1,1,0.0);
-	
-	material->SetMaterial(xkin,xcin,xbin,xfin);
-	
-	//	TPZMatPoisson3d *material;
-	//	material = new TPZMatPoisson3d(matId,dim); 
-	TPZAutoPointer<TPZMaterial> mat(material);
-
-	//	material->SetParameters(diff, conv, convdir);
-	material->SetForcingFunction(new TPZDummyFunction(ForcingFunction));
-
-	TPZCompEl::SetgOrder(p);
-	TPZCompMesh * cmesh = new TPZCompMesh(gmesh);
-	cmesh->SetDimModel(dim);
-	cmesh->SetAllCreateFunctionsContinuous();
-	cmesh->InsertMaterialObject(mat);
-
-	///Inserir condicao de contorno
-	REAL uD=0.;
-//	REAL uN=1-cosh(1.)/sinh(1.);
-	TPZFMatrix<REAL> val1(1,1,0.), val2(1,1,uD);
-	TPZAutoPointer<TPZMaterial> BCond1 = material->CreateBC(mat, bc1,dirichlet, val1, val2);
-	cmesh->InsertMaterialObject(BCond1);
-	
-	TPZFMatrix<REAL> val12(1,1,0.), val22(1,1,uD);
-	TPZAutoPointer<TPZMaterial> BCond2 = material->CreateBC(mat, bc2,dirichlet, val12, val22);
-	cmesh->InsertMaterialObject(BCond2);
-	
-	//Ajuste da estrutura de dados computacional
-	cmesh->AutoBuild();
-	cmesh->AdjustBoundaryElements(); 
-	cmesh->CleanUpUnconnectedNodes();
-
-	return cmesh;
-}
-
-void SolveSist(TPZAnalysis &an, TPZCompMesh *fCmesh)
-{
-	
-	// Caso simetrico	
-	TPZSkylineStructMatrix full(fCmesh); //caso simetrico	
-	an.SetStructuralMatrix(full);
-	TPZStepSolver<REAL> step;
-	step.SetDirect(ELDLt); //caso simetrico
-	an.SetSolver(step);
-	an.Run();
-	
-	
-	//	// Caso nao simetrico
-	//	TPZBandStructMatrix full(fCmesh); //caso nao simetrico	
-	//	an.SetStructuralMatrix(full);
-	//	TPZStepSolver step;
-	//	step.SetDirect(ELU); //caso nao simetrico
-	//	an.SetSolver(step);
-	//	an.Run();	
-	
-	
-	//	TPZAutoPointer<TPZMaterial> mat = fCmesh->FindMaterial(matId);
-	//	TPZMatPoisson3d * aximat1 = dynamic_cast<TPZMatPoisson3d*>(mat.operator->());
-	//	
-	//	ofstream file("Solution.out");
-	//	an.Solution().Print("solution", file);    //Solution visualization on Paraview (VTK)
-	//	
-	
-	
-	//  #ifdef VTK
-	//	TPZManVector<std::string,10> scalnames(1), vecnames(1);
-	//	scalnames[0] = "Solution";
-	//	vecnames[0] = "Derivate";
-	//	
-	//	std::string plotfile("saidaT.vtk");
-	//	const int dim = 2;
-	//	int div = 0;
-	//	an.DefineGraphMesh(dim,scalnames,vecnames,plotfile);
-	//	an.PostProcess(div,dim);
-	//	std::ofstream out("malha.txt");
-	//	an.Print("nothing",out);
-	//  #endif	
-	
-	
 }
 
