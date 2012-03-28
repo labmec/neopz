daf3e44c7910482534102c3fc71b916927054d17
jorge.diazcalle@gmail.com <jorge.diazcalle@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-03-28 15:56:37 +0000

    New Tutorial project

---
+++ Projects/PDElinear2D/Linear2D.cpp
@@ -0,0 +1,129 @@
+/*
+ * @file
+ * @brief Contains the main function for solving a 2D linear partial differential equation.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "pzvec.h"
+#include "pzfmatrix.h"
+#include "pzfstrmatrix.h"
+#include "pzlog.h"
+#include "pzmat2dlin.h"
+
+#include "pzgmesh.h"
+#include "pzcmesh.h"
+#include "pzcompel.h"
+
+#include "TPZGeoLinear.h"
+#include "pzgeopoint.h"
+
+#include "TPZRefPattern.h"
+#include "tpzgeoelrefpattern.h"
+
+#include "../PZ_Process.h"
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+//
+//	This program solve  a system of linear PDE - 1D I hope! 
+//	- k.[(ddu/dxdx) + (ddu/dydy)] = f(x),   0<= x <=1    --->  Now a=1 and b=1 and  f(x)=x
+//	using uD = 0  x = 0  and  x = 1.
+//
+
+/* RELATED TO MODEL */
+
+// Right handside term of our Linear PDE
+void ForcingFunction(const TPZVec<REAL> &pto, TPZVec<REAL> &xfloat) 
+{
+	int n = xfloat.NElements();
+	for(int i=0;i<n;i++)
+		xfloat[i] = 0.0;
+}
+
+// Exact Solution u(x)
+void SolExata(TPZVec<REAL> &pto, TPZVec<REAL> &u_exact,TPZFMatrix<REAL> &du_exact)
+{
+}
+
+int main()
+{
+	// Initializing generation log comments as log4cxx done
+	std::string logs("log4cxx.doubleprojection1d");
+	InitializePZLOG();
+	
+	// Files for information (geometric mesh and computational mesh)
+	std::ofstream infoGeoMesh("gmesh.txt");
+	std::ofstream infoCompMesh("cmesh.txt");	
+	// Files for output solution as multiplier coefficients
+	std::ofstream FileSolution("Solution.txt");	
+	std::ofstream FileError("erros.txt");
+
+	// File for Mathematica output format
+	std::ofstream outMath("Mathematica.txt");
+
+	// p -> interpolation order
+	int p = 1;
+	// h -> level of uniform refinement of the initial mesh
+	int h = 2;
+	
+	// Creating main extremes and material for current project
+	TPZManVector<REAL> x0(3,0.), x1(3,1.);  // Corners of the mesh.
+	x1[2] = 0.0;
+	// id of material and boundary conditions
+	TPZVec<int> matId(1,1);
+	TPZVec<int> bc(2,-1);
+	bc[1] = -2;
+	// type of boundary conditions
+	TPZVec<int> bcType(2,0);   // 0 - dirichlet
+	
+	// Coefficients of the differential equation (Linear 2D). They will be required by material constructor
+	TPZMat2dLin *material = new TPZMat2dLin(matId[0]);
+	TPZFMatrix<REAL> xkin (1,1,1.0);
+	TPZFMatrix<REAL> xcin (1,1,0.0);
+	TPZFMatrix<REAL> xfin (1,1,0.0);
+	material->SetMaterial(xkin,xcin,xfin);
+	// inserting function force
+	//material->SetForcingFunction(new TPZDummyFunction(ForcingFunction));
+	
+	// FEM PROCESS
+	// Creating a geometric mesh and printing geometric information. Note: The coordinates of the nodes are 3D
+	TPZGeoMesh *gmesh = GeomMesh2D(h,matId,bc,x0,x1);
+	gmesh->Print(infoGeoMesh);	
+	
+	// Creating a computational mesh and printing computational information.
+	TPZCompMesh * cmesh = CompMesh2D(gmesh,p,material,bc,bcType);
+	cmesh->Print(infoCompMesh);
+	
+	// Assembling and Solving linear system
+	TPZAnalysis an(cmesh);
+	SolveSist(an,cmesh);
+	
+	// Print Solution as multiplier coefficients
+	TPZFMatrix<REAL> toprint = an.Solution();
+	toprint.Print("Solution", FileSolution);
+	
+	// Solution output for Mathematica
+	OutputMathematica(outMath, cmesh);
+
+	// Plot erro (norms) 
+	//an.SetExact(SolExata);
+	//TPZVec<REAL> posproc;
+	//FileError << "\n\n Solving with P = 1\n";
+	//an.PostProcess(posproc,FileError); // Compute the errors
+	//FileError<<endl;
+	
+	// Close output files
+	infoGeoMesh.close();
+	infoCompMesh.close();
+	FileSolution.close();
+	FileError.close();
+	
+	return EXIT_SUCCESS;
+}
---
+++ Projects/PDElinear2D/PZ_Process.cpp
@@ -0,0 +1,266 @@
+/*
+ * @file
+ * @brief Contains the implementation of the functions grouping the processes to: \n
+ * - Generate one-dimensional geometric mesh (gmesh)
+ * - Generate a computational mesh (cmesh) associated with gmesh,
+ * - Assembling and solving linear system for 1D linear diff. eq. on \f$[0.0, 1.0]\f$
+ */
+
+#include "PZ_Process.h"
+
+using namespace std;
+
+TPZGeoMesh *GeomMesh1D(int h,TPZVec<int> &matId,TPZVec<int> &bc,TPZVec<REAL> &xL,TPZVec<REAL> &xR)
+{	
+	if(!matId.NElements() || bc.NElements() < 2)
+		return NULL;
+	TPZGeoMesh *gmesh = new TPZGeoMesh;
+	int Qnodes = 2;
+	gmesh->SetMaxNodeId(Qnodes-1);
+	gmesh->NodeVec().Resize(Qnodes);
+	TPZVec<TPZGeoNode> Node(Qnodes);
+	
+	TPZVec <int> TopolLine(2);
+	TPZVec <int> TopolPoint(1);
+	
+	// Creating geometric nodes
+	int id = 0;
+	int ndiv = 1;
+	REAL dx = fabs(xR[0] - xL[0])/ndiv;
+	REAL pointco;
+	for(int xi = 0; xi < Qnodes; xi++) {
+		pointco = xL[0]+xi*dx;
+		Node[id].SetNodeId(id);
+		Node[id].SetCoord(0 ,pointco);//coord X
+		gmesh->NodeVec()[id] = Node[id];
+		id++;
+	}
+
+	// Creating geometric elements (point and linear)
+	id=0;
+	TopolPoint[0] = 0;
+	new TPZGeoElRefPattern< pzgeom::TPZGeoPoint > (id,TopolPoint,bc[0], *gmesh);
+	id++;
+	for (int eli=0; eli<ndiv; eli++) {
+		TopolLine[0] = eli;
+		TopolLine[1] = eli+1;
+		new TPZGeoElRefPattern< pzgeom::TPZGeoLinear > (id,TopolLine,matId[0],*gmesh);
+		id++;
+	}
+	TopolPoint[0] = Qnodes-1;
+	new TPZGeoElRefPattern< pzgeom::TPZGeoPoint > (id,TopolPoint,bc[1],*gmesh);
+	
+	// Building the connectivity between elements as (element, side)
+	gmesh->BuildConnectivity();
+	
+	// If necessary it refines level by level initial mesh
+	UniformRefine(h,gmesh);
+	
+	return gmesh;
+}
+
+void UniformRefine(int h,TPZGeoMesh *gmesh) {
+	//uniform refinement
+	TPZVec<TPZGeoEl *> filhos;
+	for (int ref = 0; ref < h; ref++) {
+		int n = gmesh->NElements();
+		for(int i = 0; i < n; i++ ) {
+			TPZGeoEl *gel = gmesh->ElementVec()[i];
+			gel->Divide(filhos);
+		}
+	}
+}
+
+TPZCompMesh *CompMesh1D(TPZGeoMesh *gmesh,int p, TPZMaterial *material,TPZVec<int> &bc,TPZVec<int> &bcType) {
+
+	if(!material || bc.NElements()<2 || bcType.NElements() != bc.NElements()) return NULL;
+	int dim = 1;
+	
+	
+	TPZAutoPointer<TPZMaterial> mat(material);
+	
+	// related to interpolation space
+	TPZCompEl::SetgOrder(p);
+	TPZCompMesh *cmesh = new TPZCompMesh(gmesh);
+	cmesh->SetDimModel(dim);
+	cmesh->SetAllCreateFunctionsContinuous();
+	cmesh->InsertMaterialObject(mat);
+	
+	// Related to boundary conditions
+	//	REAL uN=1-cosh(1.)/sinh(1.);
+	TPZFMatrix<REAL> val1(1,1,0.), val2(1,1,0.);
+	if(!bcType[0])  // dirichlet
+		val2.PutVal(0,0,0.);
+	TPZAutoPointer<TPZMaterial> BCond1 = material->CreateBC(mat, bc[0],bcType[0], val1, val2);
+	cmesh->InsertMaterialObject(BCond1);
+	
+	if(!bcType[1])  // dirichlet
+		val2.PutVal(0,0,0.0);
+	TPZAutoPointer<TPZMaterial> BCond2 = material->CreateBC(mat, bc[1],bcType[1], val1, val2);
+	cmesh->InsertMaterialObject(BCond2);
+
+	//Adjusting data
+	cmesh->AutoBuild();
+	cmesh->AdjustBoundaryElements(); 
+	cmesh->CleanUpUnconnectedNodes();
+	
+	return cmesh;
+}
+
+void SolveSist(TPZAnalysis &an, TPZCompMesh *fCmesh)
+{
+	// Symmetric case	
+	TPZSkylineStructMatrix full(fCmesh);
+	an.SetStructuralMatrix(full);
+	TPZStepSolver<REAL> step;
+	step.SetDirect(ELDLt);
+	an.SetSolver(step);
+	an.Run();
+
+	//	Nonsymmetric case
+	//	TPZBandStructMatrix full(fCmesh);
+	//	an.SetStructuralMatrix(full);
+	//	TPZStepSolver step;
+	//	step.SetDirect(ELU);
+	//	an.SetSolver(step);
+	//	an.Run();
+
+	//	TPZAutoPointer<TPZMaterial> mat = fCmesh->FindMaterial(matId);
+	//	TPZMatPoisson3d * aximat1 = dynamic_cast<TPZMatPoisson3d*>(mat.operator->());
+	//	
+	//	ofstream file("Solution.out");
+	//	an.Solution().Print("solution", file);    //Solution visualization on Paraview (VTK)
+	//	
+}
+
+
+// CASE 2D
+
+#include "pzgengrid.h"
+#include "TPZExtendGridDimension.h"
+
+TPZGeoMesh *GeomMesh2D(int h,TPZVec<int> &matId,TPZVec<int> &bc,TPZVec<REAL> &x0,TPZVec<REAL> &x1)
+{
+	if(!matId.NElements() || bc.NElements() < 2)
+		return NULL;
+	TPZGeoMesh *gmesh = new TPZGeoMesh;
+	int Qnodes = 9;
+	gmesh->SetMaxNodeId(Qnodes-1);
+	gmesh->NodeVec().Resize(Qnodes);
+	TPZVec<TPZGeoNode> Node(Qnodes);
+	
+	TPZManVector<int> nx(2,2);   // subdivisions in X and in Y. 
+	TPZGenGrid gen(nx,x0,x1);    // mesh generator. On X we has three segments and on Y two segments. Then: hx = 0.2 and hy = 0.1  
+	gen.SetElementType(0);       // type = 0 means rectangular elements  --> type = 1 means triangular elements
+	gen.Read(gmesh);             // generating grid in gmesh
+	
+	// setting bc condition -2 (u = 0) from (0.0, 0.0) until (1.0, 0.0)
+	x1[0] = 0.0;
+	gen.SetBC(gmesh,x1,x0,-2);
+	// setting bc condition -1 from (0.0, 1.0) until (1.0, 1.0)
+	x0[0] = 1.; x0[1] = 1.;
+	gen.SetBC(gmesh,x1,x0,-1);
+	// setting bc condition -1 from (1.0, 1.0) until (0.0, 1.0)
+	x1[0] = 0.;	x1[1] = 1.;
+	gen.SetBC(gmesh,x0,x1,-1);
+	// setting bc condition -1 from (0.0, 1.0) until (0.0, 0.0)
+	x0[0] = x0[1] = 0.;
+	gen.SetBC(gmesh, x1, x0, -1);
+
+	// Building the connectivity between elements as (element, side)
+	gmesh->BuildConnectivity();
+	
+	// If necessary it refines level by level initial mesh
+	UniformRefine(h,gmesh);
+	
+	return gmesh;
+}
+TPZCompMesh *CompMesh2D(TPZGeoMesh *gmesh,int p, TPZMaterial *material,TPZVec<int> &bc,TPZVec<int> &bcType) {
+	
+	if(!material || bc.NElements()<2 || bcType.NElements() != bc.NElements()) return NULL;
+	int dim = 2;
+	
+	TPZAutoPointer<TPZMaterial> mat(material);
+	
+	// related to interpolation space
+	TPZCompEl::SetgOrder(p);
+	TPZCompMesh *cmesh = new TPZCompMesh(gmesh);
+	cmesh->SetDimModel(dim);
+	cmesh->SetAllCreateFunctionsContinuous();
+	cmesh->InsertMaterialObject(mat);
+	
+	// Related to boundary conditions
+	//	REAL uN=1-cosh(1.)/sinh(1.);
+	TPZFMatrix<REAL> val1(1,1,0.), val2(1,1,0.);
+	if(!bcType[0])  // dirichlet
+		val2.PutVal(0,0,0.);
+	TPZAutoPointer<TPZMaterial> BCond1 = material->CreateBC(mat, bc[0],bcType[0], val1, val2);
+	cmesh->InsertMaterialObject(BCond1);
+	
+	if(!bcType[1])  // dirichlet
+		val2.PutVal(0,0,0.25);
+	TPZAutoPointer<TPZMaterial> BCond2 = material->CreateBC(mat, bc[1],bcType[1], val1, val2);
+	cmesh->InsertMaterialObject(BCond2);
+	
+	//Adjusting data
+	cmesh->AutoBuild();
+	cmesh->AdjustBoundaryElements(); 
+	cmesh->CleanUpUnconnectedNodes();
+	
+	return cmesh;
+}
+
+
+// OUTPUT FORMAT
+
+// Output as Mathematica format
+void OutputMathematica(std::ofstream &outMath, TPZCompMesh *cmesh) {
+	outMath << "Saida = {";
+	int i, j, k;
+	int nelem = cmesh->ElementVec().NElements();
+	int pointsInElement = 11;
+	int dim = cmesh->Dimension();
+	for(i = 0;  i< nelem; i++)
+	{
+		TPZCompEl *cel = cmesh->ElementVec()[i];
+		if(cel->Reference()->Dimension() < cmesh->Dimension()) continue;
+		TPZInterpolationSpace * sp = dynamic_cast <TPZInterpolationSpace*>(cel);
+		TPZVec<REAL> qsi(1,0.), sol(1,0.), outfem(3,0.);
+		TPZFMatrix<REAL> axes(1,3,0.), dsol(1,1,0.);
+		for(j = 0; j < pointsInElement; j++)
+		{
+			qsi[0] = -1.+2.*j/10.;
+			sp->Solution(qsi,0,sol);
+			cel->Reference()->X(qsi,outfem);
+			outMath << "{{";
+			for(k=0;k<dim;k++) {
+				outMath << outfem[k];
+				if(k!=dim-1) outMath << ",";
+			}
+			outMath << "},{";
+			for(k=0;k<dim;k++) {
+				outMath << sol[k];
+				if(k!=dim-1) outMath << ",";
+			}
+			outMath << "}}";
+			if(j != pointsInElement-1) outMath << ",";
+		}
+		if(i == nelem-1) outMath << "};" << std::endl;
+		else outMath << ",";
+	}
+	outMath << "ListPlot[Saida,Joined->True]"<< endl;
+}
+
+// Output as VTK (Visualization Tool Kit) format
+void OutputVTK(std::string &outVTK, TPZCompMesh *cmesh,TPZAnalysis &an) {
+	TPZManVector<std::string,10> scalnames(1), vecnames(1);
+	scalnames[0] = "Solution";
+	vecnames[0] = "Derivate";
+	
+	const int dim = 2;
+	int div = 0;
+	an.DefineGraphMesh(dim,scalnames,vecnames,outVTK);
+	an.PostProcess(div,dim);
+	std::ofstream out("malha.txt");
+	an.Print("nothing",out);
+}
