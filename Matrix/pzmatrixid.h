53cb86a955e1df8443d959ff4a522f581cd86a79
philippedevloo1958@gmail.com <philippedevloo1958@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-04-26 14:59:13 +0000

    File that contains the Saveable Ids of the matrix classes

---
+++ Matrix/pzmatrixid.h
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * @brief Contains TPZMatrix<TVar>class, root matrix class.
+ */
+//
+// Author: MISAEL LUIS SANTANA MANDUJANO.
+//
+// File:   tmatrix.hh
+//
+// Class:  TPZMatrix
+//
+// Obs.:   Implements abstract matrix class.
+//
+// Versao: 04 / 1996.
+//
+
+
+#ifndef _TMATRIXIDHH_
+#define _TMATRIXIDHH_
+
+/** @brief Id of TPZMATRED matrix very sparse */
+const int TPZMATRED_VERYSPARSE_ID = 500;
+/** @brief If of TPZMATRED full matrix */
+const int TPZMATRED_FMATRIX_ID = 501;
+/** @brief Id of full matrix */
+const int TPZFMATRIXID = 100;
+/** @brief Id of the DohrMatrix */
+const int TPZDOHRMATRIXSUBSTRUCTCONDENSE = 502;
+/** @brief Id of the DohrMatrix */
+const int TPZDOHRMATRIXSUBSTRUCT = 503;
+const int TSKYLMATRIX_ID = 504;
+const int TPZDOHRPRECOND = 505;
+const int TPZDOHRPRECONDCONDENSE = 506;
+/** @ingroup solvers */
+const int TPZSTEPSOLVER_ID = 507;
+
+
+#endif
+// _TMATRIXIDHH_
+
