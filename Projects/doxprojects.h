304c976b51c0b5eb4c70d93eb26c620f06ea19c7
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-05-05 15:01:32 +0000

    Creating tutorial, application and unit test groups for projects.

---
+++ Projects/doxprojects.h
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * @brief Creates the projects group for Doxygen documentation.
+ */
+
+/**
+ * @defgroup app_projects Application Projects.
+ * @brief Groups all the projects developed using neoPZ for resolve a differential equation problem using finite element method.
+ */
+/**
+ \page pg_app_projects DESCRIPTION OF THE APPLICATION PROJECTS
+ \section app_hiperbolic The hiperbolic projects
+ \section app_elliptic The elliptic projects
+ \section app_parabolic The parabolic projects
+*/
+
+/**
+ * @defgroup tut_projects Tutorial Projects.
+ * @brief Groups all the projects developed by show how to use the neoPZ components and modules for using finite element method.
+ */
+/**
+ \page pg_tut_projects DESCRIPTION OF THE TUTORIAL PROJECTS
+ \section tut_testgeom To create geometric mesh
+ \section tut_testcomp To create computational mesh
+ \section tut_testonedim To solve one-dimensional differential equation
+ \section tut_testtwodim To solve two-dimensional differential equation
+ */
+
+/**
+ * @defgroup unit_projects Unit test Projects for validation.
+ * @brief Groups all the projects developed to validate neoPZ components.
+ */
+/**
+ \page pg_unit_projects DESCRIPTION OF THE UNIT TEST PROJECTS FOR VALIDATION
+ \section unit_integral To test numerical integration module
+ \section unit_matrix To test matrix module
+ \section unit_solvers To test linear solvers for systems module
+ \section unit_topology To test topology module
+ \section unit_linmaterial To test linear material
+ */
