fb3cf6904d9fe05c322289338b206284168fceaa
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-05-31 18:10:37 +0000

    Fixed cmake/ctest tests for VisualMatrix project

--- Projects/visualmatrix/CTestTestFile.cmake
+++ Projects/visualmatrix/CTestTestFile.cmake
@@ -3,7 +3,9 @@ ENABLE_TESTING()
 #Running the executable and test if it will run ok
 ADD_TEST (VisualMatrix VisualMatrix)
 
+#Checking generated files
 SET (filename "visualmatrix.dx")
 SET (expectedMD5 "654b8ae8045e237d00a60ccbc7032b1c")
 add_test (VisualMatrix_visualmatrix.dx "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
-set_tests_properties (VisualMatrix_visualmatrix.dx PROPERTIES DEPENDS "VisualMatrix")
+#Adding dependency
+set_tests_properties (VisualMatrix_visualmatrix.dx PROPERTIES DEPENDS "VisualMatrix" REQUIRED_FILES "${filename}")
