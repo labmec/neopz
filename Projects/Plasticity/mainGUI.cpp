d8426341032c5ece7c68776c3df44dbfe6ddec11
cecilio.diogo@gmail.com <cecilio.diogo@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-05-31 18:25:58 +0000

    New methods for elastoplastic material point.

--- Projects/Plasticity/mainGUI.cpp
+++ Projects/Plasticity/mainGUI.cpp
@@ -1,10 +1,66 @@
 #include <iostream>
 #include <cstdlib>
  using namespace std;
+void MaterialPointTests();
 
  int main(int argc, char *argv[])
  {
  	cout << "QT GUI" << endl;
+     MaterialPointTests();
+     cout << "\n End runing ! "<< endl;
 
  return EXIT_SUCCESS;
  }
+
+#include "TPZPlasticityTest.h"
+//
+void MaterialPointTests()
+{
+    
+    cout << "\nChoose Plasticity test:";
+    cout << "\n0 - Isotropic compression ";
+    cout << "\n1 - Biaxial Tests ";
+    cout << "\n2 - Uniaxial traction ";
+    cout << "\n";
+    int choice;
+  //  cin >> choice;
+    choice =0;
+    switch(choice)
+    {
+        case(0):
+            cout << "\n Choose the Plastic model tou need to run Isotropic compression: ";
+            cout << "\n0 - Lade - Kim ";
+            cout << "\n1 - Sandler Dimaggio ";
+            cout << "\n2 - Drucker Prager ";
+            cout << "\n";
+            int choice2;
+         //   cin >> choice2;
+            choice2 = 1;
+            switch(choice2)
+        {
+            case(0):
+                LKIsotropicCompression();
+                break;
+            case(1):
+                SandlerDimaggioIsotropicCompression();
+                break;
+            case(2):
+                DruckerIsotropicCompression();
+                break;
+        }
+            
+            
+            break;
+        case(1):
+            LKBiaxialTest();
+            break;
+        case(2):
+            cout << "NOT IMPLEMENTED YET";
+            //    LadeKim_ReversalTest();
+            break;
+        default:
+            cout << "Unknown Test Type. Exiting...";
+    }
+    
+}
+
