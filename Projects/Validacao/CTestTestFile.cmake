cc627718f7ce4a107ad30bae4b8b41fa0fede144
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-06-28 16:55:20 +0000

    Fixing Validacao test file

--- Projects/Validacao/CTestTestFile.cmake
+++ Projects/Validacao/CTestTestFile.cmake
@@ -4,13 +4,17 @@ ENABLE_TESTING()
 ADD_TEST (Validacao Validacao)
 
 #Checking generated files
-SET (filename "MalhaDomioTodo.vtk")
-SET (expectedMD5 "3e7ff74e99b4066c7be84f7a80fe5f56")
-add_test (Validacao_MalhaDomioTodo.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
+#SET (filename "MalhaDomioTodo.vtk")
+#SET (expectedMD5 "3e7ff74e99b4066c7be84f7a80fe5f56")
+#add_test (Validacao_${filename} "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 
 SET (filename "ErroHdivTodo.txt")
-SET (expectedMD5 "75fe6265ffc73c2f0ae5bb94a9793be7")
-add_test (Validacao_ErroHdivTodo.txt "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
+SET (expectedMD5 "85551d58936589043028c3b440b36df6")
+add_test (Validacao_${filename} "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
+  
+SET (filename "GraficoH1Todo.scal_vec.0.vtk")
+SET (expectedMD5 "743f3c285f2c9baf6d31606bf4d3fb48")
+add_test (Validacao_${filename} "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 
 #Adding dependency
 #set_tests_properties (VisualMatrix_visualmatrix.dx PROPERTIES DEPENDS "VisualMatrix" REQUIRED_FILES "${filename}")
