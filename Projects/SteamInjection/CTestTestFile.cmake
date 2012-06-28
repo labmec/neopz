ade6bd64d74350274c655a44218c3cd8d85e8c95
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-06-28 17:54:40 +0000

    Fixing SteamInjection test file

--- Projects/SteamInjection/CTestTestFile.cmake
+++ Projects/SteamInjection/CTestTestFile.cmake
@@ -4,8 +4,8 @@ ENABLE_TESTING()
 ADD_TEST (SteamInjection SteamInjection)
 
 #Checking generated files
-SET (filename "teamInjectionMATHEMATICA.nb")
+SET (filename "steamInjectionMATHEMATICA.nb")
 SET (expectedMD5 "fe9ee68844ff96272fba5186e8207c41")
-add_test (SteamInjection_teamInjectionMATHEMATICA.nb "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
+add_test (SteamInjection_${filename} "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 #Adding dependency
 #set_tests_properties (VisualMatrix_visualmatrix.dx PROPERTIES DEPENDS "VisualMatrix" REQUIRED_FILES "${filename}")
