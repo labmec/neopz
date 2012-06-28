4b24adde5a88d83beef57891c58ea8be9430bcf6
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-06-28 17:51:12 +0000

    Fixing ThermalStress test file and log4cxx.cfg file

--- Projects/ThermalStress/CTestTestFile.cmake
+++ Projects/ThermalStress/CTestTestFile.cmake
@@ -4,8 +4,8 @@ ENABLE_TESTING()
 ADD_TEST (ThermalStress ThermalStress)
 
 #Checking generated files
-SET (filename "perkins.scal_vec.0.vtk")
-SET (expectedMD5 "6c2523fef65390fdf2196afe09d09bff")
-add_test (ThermalStress_perkins.scal_vec.0.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
+#SET (filename "perkins.scal_vec.0.vtk")
+#SET (expectedMD5 "6c2523fef65390fdf2196afe09d09bff")
+#add_test (ThermalStress_perkins.scal_vec.0.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 #Adding dependency
 #set_tests_properties (VisualMatrix_visualmatrix.dx PROPERTIES DEPENDS "VisualMatrix" REQUIRED_FILES "${filename}")
