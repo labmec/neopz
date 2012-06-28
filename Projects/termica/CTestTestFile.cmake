16a59f8ebc7f10ddb6f70d3be4da47c735eee44d
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-06-28 16:47:34 +0000

    Fixing termica test file

--- Projects/termica/CTestTestFile.cmake
+++ Projects/termica/CTestTestFile.cmake
@@ -1,11 +1,11 @@
 ENABLE_TESTING()
 
 #Running the executable and test if it will run ok
-ADD_TEST (Termica termica)
+ADD_TEST (termica termica)
 
 #Checking generated files
 SET (filename matrixstruct.vtk)
-SET (expectedMD5 edd29a9dc8afee12f066590186c3cb9dd)
+SET (expectedMD5 edd29a9dc8afee12f066590186c3cb9d)
 add_test (termica_matrixstruct.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 #Adding dependency
 #set_tests_properties (termica_matrixstruct.vtk PROPERTIES DEPENDS "VisualMatrix" REQUIRED_FILES "${filename}")
