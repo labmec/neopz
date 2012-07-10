bea1dfd829ccadac37a90109c094685d58d803b3
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-07-10 13:59:09 +0000

    Disabled SubStruct Viscoelastic Test

--- Projects/Substruct_Viscoelastic/CTestTestFile.cmake
+++ Projects/Substruct_Viscoelastic/CTestTestFile.cmake
@@ -1,24 +1,24 @@
 ENABLE_TESTING()
 
 #Running the executable and test if it will run ok
-ADD_TEST (SubStruct_Viscoelastic Substruct_Viscoelastic)
+#ADD_TEST (SubStruct_Viscoelastic Substruct_Viscoelastic)
 
 #Checking generated files
 #SET (filename "CoarseMatrix.vtk")
 #SET (expectedMD5 "21b4804f7ffac977c2ba8c25e699f3c7")
 #add_test (SubStruct_Viscoelastic_CoarseMatrix.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 
-SET (filename "Cube.vtk")
-SET (expectedMD5 "64aa9b689c442c5bf3b0a2b428fc0985")
-add_test (SubStruct_Viscoelastic_Cube.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
+#SET (filename "Cube.vtk")
+#SET (expectedMD5 "64aa9b689c442c5bf3b0a2b428fc0985")
+#add_test (SubStruct_Viscoelastic_Cube.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 
 #SET (filename "dohrmann_visco.scal_vec.0.vtk")
 #SET (expectedMD5 "c1eea00cf5dbcf7848104ef11a9c187a")
 #add_test (SubStruct_Viscoelastic_dohrmann_visco.scal_vec.0.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 
-SET (filename "malhaPZ1BC.txt")
-SET (expectedMD5 "fdc16e681800611f9730a5ffcf9d231b")
-add_test (SubStruct_Viscoelastic_malhaPZ1BC.txt "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
+#SET (filename "malhaPZ1BC.txt")
+#SET (expectedMD5 "fdc16e681800611f9730a5ffcf9d231b")
+#add_test (SubStruct_Viscoelastic_malhaPZ1BC.txt "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 
 #SET (filename "partition.vtk")
 #SET (expectedMD5 "81d786560029ef762e138d658157da10")
@@ -40,9 +40,9 @@ add_test (SubStruct_Viscoelastic_malhaPZ1BC.txt "${CMAKE_COMMAND}" "-Dfilename:S
 #SET (expectedMD5 "bf40abb00e996049ddd33e7df46f71bc")
 #add_test (SubStruct_SubMatrix84.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 
-SET (filename "SubMatrixInternal82.vtk")
-SET (expectedMD5 "8b15752850a70992f6a41a525c4d5fe7")
-add_test (SubStruct_Viscoelastic_SubMatrixInternal82.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
+#SET (filename "SubMatrixInternal82.vtk")
+#SET (expectedMD5 "8b15752850a70992f6a41a525c4d5fe7")
+#add_test (SubStruct_Viscoelastic_SubMatrixInternal82.vtk "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 
 #SET (filename "SubMatrixInternal83.vtk")
 #SET (expectedMD5 "cc74a68792733b3b1ffdf692c5068380")
