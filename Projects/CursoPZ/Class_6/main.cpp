4016145bf50cc019688ebba1e1dcf60835f3258d
jorge.diazcalle <jorge.diazcalle@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-08-04 20:43:23 +0000

    Example for elasticity material -

---
+++ Projects/CursoPZ/Class_6/main.cpp
@@ -0,0 +1,184 @@
+
+#include "pzvec.h"
+#include "pzadmchunk.h"
+#include "pzcmesh.h"
+#include "pzvec_extras.h"
+#include "pzdebug.h"
+#include "pzcheckgeom.h"
+
+#include "pzgeoel.h"
+#include "pzgnode.h"
+#include "pzgeoelside.h"
+#include "pzgeoelbc.h"
+
+#include "pzintel.h"
+#include "pzcompel.h"
+
+#include "pzmatrix.h"
+
+#include "pzanalysis.h"
+#include "pzfstrmatrix.h"
+#include "pzskylstrmatrix.h"
+#include "pzstepsolver.h"
+
+#include "pzmaterial.h"
+#include "pzbndcond.h"
+#include "pzelasmat.h"
+#include "pzplaca.h"
+#include "pzmat2dlin.h"
+#include "pzmathyperelastic.h"
+#include "pzmattest3d.h"
+#include "pzmatplaca2.h"
+
+#include "pzfunction.h"
+#include "TPZVTKGeoMesh.h"
+
+#include "pzlog.h"
+
+#ifdef LOG4CXX
+static LoggerPtr logger(Logger::getLogger("pz.adaptivity"));
+static LoggerPtr loggerconv(Logger::getLogger("pz.adaptivity.conv"));
+static LoggerPtr loggerpoint(Logger::getLogger("pz.adaptivity.points"));
+#endif
+
+#include <time.h>
+#include <stdio.h>
+#include <fstream>
+
+TPZCompMesh *CreateMesh();
+
+int main() {
+	
+#ifdef LOG4CXX
+	InitializePZLOG();
+#endif
+	
+	int p = 1;
+    TPZCompEl::SetgOrder(p);
+    
+    TPZCompMesh *cmesh = CreateMesh();
+	
+	TPZAnalysis an (cmesh);
+	TPZSkylineStructMatrix strskyl(cmesh);
+	an.SetStructuralMatrix(strskyl);
+	
+	TPZStepSolver<REAL> *direct = new TPZStepSolver<REAL>;
+	direct->SetDirect(ECholesky);
+	an.SetSolver(*direct);
+	delete direct;
+	direct = 0;
+	
+	an.Run();
+	
+	// Post process
+	TPZManVector<std::string> scalarnames(3), vecnames(1);
+	scalarnames[0] = "SigmaX";
+	scalarnames[1] = "SigmaY";
+	scalarnames[2] = "Pressure";
+	vecnames[0] = "displacement";
+	//vecnames[1] = "";
+	an.DefineGraphMesh(2,scalarnames,vecnames,"ElasticitySolutions.vtk");
+
+	an.PostProcess(2);
+	
+}
+
+
+//*************************************
+//************Option 1*****************
+//*******L Shape Quadrilateral*********
+//*************************************
+TPZCompMesh *CreateMesh() {
+	
+    REAL co[8][2] = {{0.,0.},{0.,-1.},{1.,-1.},{1.,0.},{1.,1.},{0.,1.},{-1.,1.},{-1.,0.}};
+    int indices[3][4] = {{0,1,2,3},{0,3,4,5},{0,5,6,7}};
+    TPZGeoEl *elvec[3];
+    TPZGeoMesh *gmesh = new TPZGeoMesh();
+    int nnode = 8;
+    int nod;
+    for(nod=0; nod<nnode; nod++) {
+        int nodind = gmesh->NodeVec().AllocateNewElement();
+        TPZVec<REAL> coord(2);
+        coord[0] = co[nod][0];
+        coord[1] = co[nod][1];
+        gmesh->NodeVec()[nodind] = TPZGeoNode(nod,coord,*gmesh);
+    }
+    
+    int el;
+    int nelem = 3;
+    for(el=0; el<nelem; el++) {
+        TPZVec<int> nodind(4);
+        for(nod=0; nod<4; nod++) nodind[nod]=indices[el][nod];
+        //    elvec[el] = new TPZGeoElQ2d(el,nodind,1);
+        int index;
+        elvec[el] = gmesh->CreateGeoElement(EQuadrilateral,nodind,1,index);
+    }
+    
+    gmesh->BuildConnectivity();
+    
+    TPZVec<TPZGeoEl *> sub;
+	
+    // bc -1 -> Dirichlet
+    TPZGeoElBC gbc1(elvec[0],4,-1);
+    // bc -2 -> Neumann at the bottom y==-1
+    TPZGeoElBC gbc2(elvec[0],5,-2);
+    // bc -3 -> Neumann at the right x==1
+    TPZGeoElBC gbc3(elvec[0],6,-3);
+    
+    // bc -3 -> Neumann at the right x==1
+    TPZGeoElBC gbc4(elvec[1],5,-3);
+    
+    // bc -4 -> Neumann at the top y==1
+    TPZGeoElBC gbc5(elvec[1],6,-4);
+    
+    // bc -4 -> Neumann at the top y==1
+    TPZGeoElBC gbc6(elvec[2],5,-4);
+    
+    // bc -5 -> Neumann at the left x==-1
+    TPZGeoElBC gbc7(elvec[2],6,-5);
+    
+    // bc -6 -> Homogeneous Neumann
+    TPZGeoElBC gbc8(elvec[2],7,-6);
+    
+    int nel = gmesh->NElements();
+    for (int iel=0; iel<nel; iel++) {
+        TPZManVector<TPZGeoEl *> subels;
+        TPZGeoEl *gel = gmesh->ElementVec()[iel];
+        gel->Divide(subels);
+    }
+    
+    TPZCompMesh *cmesh = new TPZCompMesh(gmesh);
+    cmesh->SetDefaultOrder(2);
+
+    // Creating elasticity material
+    TPZMaterial * mat = new TPZElasticityMaterial(1,2000.,0.3,0.,0.);
+
+	// Creating four boundary condition
+    TPZFMatrix<REAL> val1(2,2,0.),val2(2,1,0.);
+	TPZMaterial *bcBottom, *bcRight;
+	val1(1,1) = 1000000.;
+    bcBottom = mat->CreateBC(mat,-2,2,val1,val2);
+	val1(1,1) = 0.;
+	val2(1,0) = 10.;
+    bcRight = mat->CreateBC(mat,-4,1,val1,val2);
+
+    cmesh->InsertMaterialObject(mat);
+	// Inserting boundary conditions into computational mesh
+	cmesh->InsertMaterialObject(bcBottom);
+	cmesh->InsertMaterialObject(bcRight);
+    
+    cmesh->AutoBuild();
+    cmesh->AdjustBoundaryElements();
+    cmesh->CleanUpUnconnectedNodes();
+    
+#ifdef LOG4CXX
+    if (logger->isDebugEnabled())
+    {
+        std::stringstream sout;
+        cmesh->Print(sout);
+        LOGPZ_DEBUG(logger, sout.str())
+    }
+#endif
+    return cmesh;
+}
+
