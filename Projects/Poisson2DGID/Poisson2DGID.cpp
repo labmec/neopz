d1d51d0ab0dad94523d269d90c90bba468ab3440
omaryesiduran@gmail.com <omaryesiduran@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-08-13 17:17:04 +0000

    Adding some examples for easy GID computational mesh generation based on Dump files. (A dump example is given). to use other examples download GID from: http://gid.cimne.upc.es/download/official-versions

---
+++ Projects/Poisson2DGID/Poisson2DGID.cpp
@@ -0,0 +1,191 @@
+#include "pzvec.h"
+#include "pzadmchunk.h"
+#include "pzcmesh.h"
+#include "pzcompel.h"
+#include "pzgnode.h"
+#include "pzmaterial.h"
+#include "pzfmatrix.h"
+#include "pzerror.h"
+#include "pzgeoel.h"
+#include "pzmatrix.h"
+#include "TPZReadGIDGrid.h"
+#include "TPZVTKGeoMesh.h"
+
+#include "pzanalysis.h"
+#include "pzfstrmatrix.h"
+#include "pzstepsolver.h"
+#include "pzintel.h"
+#include "pzbndcond.h"
+#include "pzlog.h"
+
+#include "pzskylstrmatrix.h"
+#include "TPZSpStructMatrix.h"
+#include "pzpoisson3d.h"
+#include <time.h>
+#include <stdio.h>
+
+// Using Log4cXX as logging tool
+//
+#ifdef LOG4CXX
+static LoggerPtr logger(Logger::getLogger("pz.poisson3d"));
+#endif
+
+#ifdef LOG4CXX
+static LoggerPtr logdata(Logger::getLogger("pz.material.poisson3d.data"));
+#endif
+//
+// End Using Log4cXX as logging tool
+
+
+void PosProcess(int Dimension, TPZAnalysis &an, std::string plotfile, int div);
+void RefinamentoUniforme(TPZGeoMesh  *gmesh, int nh);
+void Run(int PolynomialOrder, int  Href, std::string GeoGridFile, int div);
+
+
+
+int main()
+{
+	#ifdef LOG4CXX
+		InitializePZLOG();
+	#endif		
+
+	//	Files to read
+	std::string GeoGridFile;	
+	GeoGridFile = "PoissonGIDMeshVar.dump";
+
+	int Href = 0;
+	int PolynomialOrder = 2;
+	int div = 0;	
+
+	// run the problem
+	Run(PolynomialOrder, Href, GeoGridFile, div);
+
+	
+	return 0;
+}
+
+void Run(int PolynomialOrder, int Href, std::string GeoGridFile, int div)
+
+{
+    int pOrder = PolynomialOrder;
+	TPZReadGIDGrid myreader;
+	TPZGeoMesh * gmesh = myreader.GeometricGIDMesh(GeoGridFile);	
+	{
+		//	Print Geometrical Base Mesh
+		ofstream arg1("BaseGeoMesh.txt");
+		gmesh->Print(arg1);
+		ofstream file1("BaseGeoMesh.vtk");	
+		//	In this option true -> let you use shrink paraview filter
+		//	PrintGMeshVTK(gmesh,file1);	
+		TPZVTKGeoMesh::PrintGMeshVTK(gmesh,file1, true);	
+	}	
+	
+	//	Modifying Geometric Mesh
+
+	RefinamentoUniforme(gmesh, Href);
+	{
+		//	Print Geometrical refined Base Mesh
+		ofstream arg1("RefineGeoMesh.txt");
+		gmesh->Print(arg1);
+		ofstream file1("RefineGeoMesh.vtk");
+		TPZVTKGeoMesh::PrintGMeshVTK(gmesh,file1, true);	
+	}	
+	
+	
+	TPZCompEl::SetgOrder(pOrder);
+	TPZCompMesh * cmesh = new TPZCompMesh(gmesh);
+	cmesh->SetDimModel(myreader.fProblemDimension);
+	cmesh->SetAllCreateFunctionsContinuous();
+	TPZVec <TPZMatPoisson3d *> materialist(myreader.MatNumber,0);
+	TPZVec <TPZMaterial *> MatList(myreader.MatNumber,0);
+	
+	
+
+//	This part depends on Material parameters	
+	for (int imat = 0 ; imat < myreader.MatNumber; imat++)
+	{
+		
+		REAL conv1 = myreader.fMaterialDataVec[imat].Properties[1];
+		TPZVec<REAL> convdir1(3,0);
+		REAL flux1 = myreader.fMaterialDataVec[imat].Properties[5];	
+		
+		for (int i=0; i < 3; i++) 
+		{
+			convdir1[i]=myreader.fMaterialDataVec[imat].Properties[i+2];
+		}	
+		
+		materialist[imat] = new TPZMatPoisson3d(myreader.fMaterialDataVec[imat].MatID,myreader.fProblemDimension); 
+		materialist[imat]->SetParameters(myreader.fMaterialDataVec[imat].Properties[0], conv1, convdir1);// K
+		materialist[imat]->SetInternalFlux(flux1);
+		MatList[imat] = materialist[imat];
+		cmesh->InsertMaterialObject(MatList[imat]);	
+		
+		for (int ibc = 0; ibc < myreader.BCNumber ; ibc++) 
+		{
+			if(imat+1 == int(myreader.fBCMaterialDataVec[ibc].Properties[0])) 
+			{	
+				TPZFMatrix<REAL> val11(1,1,myreader.fBCMaterialDataVec[ibc].Properties[2]), val21(1,1,myreader.fBCMaterialDataVec[ibc].Properties[3]);	
+				TPZMaterial * BC = materialist[imat]->CreateBC(MatList[imat], myreader.fBCMaterialDataVec[ibc].MatID,int(myreader.fBCMaterialDataVec[ibc].Properties[1]), val11, val21);
+				cmesh->InsertMaterialObject(BC);	
+			}
+		}
+	}
+//	End: This part depends on Material parameters	
+
+	{	
+				
+		cmesh->AutoBuild();
+	}	
+	
+
+	TPZAnalysis MyAnalysis (cmesh);
+	
+#ifdef LOG4CXX
+	{
+		std::stringstream sout;
+		cmesh->Print(sout);
+		LOGPZ_DEBUG(logger,sout.str())
+	}
+	
+#endif
+
+	TPZSkylineStructMatrix strskyl(cmesh);
+	MyAnalysis.SetStructuralMatrix(strskyl);
+	TPZStepSolver<REAL> direct;
+	direct.SetDirect(ELDLt);
+	MyAnalysis.SetSolver(direct);
+	MyAnalysis.Run();
+	std::string plotfile("MyProblemSolution.vtk");		
+	PosProcess(myreader.fProblemDimension,MyAnalysis, plotfile, div);	
+	
+	
+}
+
+void RefinamentoUniforme(TPZGeoMesh *gMesh, int nh){
+	for ( int ref = 0; ref < nh; ref++ ){
+		TPZVec<TPZGeoEl *> filhos;
+		int n = gMesh->NElements();
+		for ( int i = 0; i < n; i++ ){
+			TPZGeoEl * gel = gMesh->ElementVec() [i];
+			int GelDimension = gel->Dimension();
+			if (GelDimension == 2 || GelDimension == 1) 
+			{
+				gel->Divide (filhos);
+			}
+		}//for i
+	}//ref
+}
+
+void PosProcess(int Dimension, TPZAnalysis &an, std::string plotfile, 	int div){
+	TPZManVector<std::string,10> scalnames(1), vecnames(1);
+	scalnames[0] = "Pressure";	
+	vecnames[0]= "MinusKGradU";
+	
+	
+	const int dim = Dimension;
+	an.DefineGraphMesh(dim,scalnames,vecnames,plotfile);
+	an.PostProcess(div,dim);
+	std::ofstream out("Mesh.txt");
+	an.Print("nothing",out);
+}
+
