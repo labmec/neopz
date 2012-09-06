8a15ca97090952c084fd04beb6a30cd44939d9d1
borinho <borinho@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-09-06 18:30:20 +0000

    Fixing pthread_vs_tbb script

--- PerfTests/RunScripts/perf02-substruct-assemble/pthread_vs_tbb-cubo_binckpt.sh
+++ PerfTests/RunScripts/perf02-substruct-assemble/pthread_vs_tbb-cubo_binckpt.sh
@@ -1,11 +1,3 @@
-# Select the time command arguments
-if /usr/bin/time -l ls &> /dev/null; then
-    TIMEARGS="-l"
-elif /usr/bin/time --verbose ls &> /dev/null; then
-    TIMEARGS="--verbose"
-fi
-echo "TIMEARGS = $TIMEARGS"
-
 function fail
 {
     echo "FAIL: $@"
@@ -40,17 +32,17 @@ function run_cfg
 
     local EXTRAARGS=$8
 
-    BASEOUT="pthread_vs_tbb.$NAME.@REAL_TYPE@.p$P.nsub$NS.nt_a.$NTA.nt_d.$NTD.nt_m.$NTM.nt_sm.$NTSM"
+    BASEOUT="pthread_vs_tbb.$NAME.double.p$P.nsub$NS.nt_a.$NTA.nt_d.$NTD.nt_m.$NTM.nt_sm.$NTSM"
     
-    IF="cubo1.p$P.nsub$NS.t.@REAL_TYPE@.bin.ckpt1"
-    OF="cubo1.p$P.nsub$NS.t.@REAL_TYPE@.bin.ckpt3"
-    GOLDEN="@PERFTEST_DATA_DIR@/SubStruct/outputs/$OF"
-    MD5F="@PERFTEST_DATA_DIR@/SubStruct/outputs/$OF.md5"
+    IF="cubo1.p$P.nsub$NS.t.double.bin.ckpt1"
+    OF="cubo1.p$P.nsub$NS.t.double.bin.ckpt3"
+    GOLDEN="/Users/borin/Desktop/neopz/neopz_trunk/PerfTests/neopz_perf_data/SubStruct/outputs/$OF"
+    MD5F="/Users/borin/Desktop/neopz/neopz_trunk/PerfTests/neopz_perf_data/SubStruct/outputs/$OF.md5"
 
     ASSRDT="$BASEOUT.ass.rdt"
     CRERDT="$BASEOUT.cre.rdt"
 
-    CMD="$APP -bc -cf1 @PERFTEST_DATA_DIR@/SubStruct/inputs/$IF -dc3 $OF -st3 -ass_rdt $ASSRDT -cre_rdt $CRERDT" 
+    CMD="$APP -bc -cf1 /Users/borin/Desktop/neopz/neopz_trunk/PerfTests/neopz_perf_data/SubStruct/inputs/$IF -dc3 $OF -st3 -ass_rdt $ASSRDT -cre_rdt $CRERDT" 
     CMD="$CMD -chk_c3_md5 $MD5F"
     CMD="$CMD -nsub $NS -nt_a $NTA -nt_d $NTD -nt_m $NTM -nt_sm $NTSM -p $P $EXTRAARGS"
     verbose 1 "cmd: $CMD"
@@ -82,15 +74,40 @@ function run_cfg
     fi        
 }
 
-VERBOSE_LEVEL=1
-APP="@PERFTEST_APPS_DIR@/SubStruct/Perf-SubStruct"
+# ==================================
+APP="/Users/borin/Desktop/neopz/neopz_trunk_build/PerfTests/SubStruct/Perf-SubStruct"
 
+# Select the time command arguments
+if /usr/bin/time -l ls &> /dev/null; then
+    TIMEARGS="-l"
+elif /usr/bin/time --verbose ls &> /dev/null; then
+    TIMEARGS="--verbose"
+fi
+echo "TIMEARGS = $TIMEARGS"
+
+# ==================================
 # Main
+# ==================================
+
+# Run configuration
+VERBOSE_LEVEL=1
+
+# Base number of threads for each step.
+NTHREADS=4
+L1NTHREADS=$NTHREADS
+L2NTHREADS=$NTHREADS
+
+NCORES=`sysctl hw.physicalcpu`
+
 verbose 1 "pthread vs tbb assemble: cubo"
-verbose 1 "app: $APP"
 
 # Check if application binary exists
 [ -f $APP ] || fail "Application $APP is not a file"
+
+verbose 1 "app: $APP"
+verbose 1 " NCORES = $NCORES"
+verbose 1 " L1 NTHREADS =  / $L1NTHREADS" 
+verbose 1 " L2 NTHREADS =  / $L2NTHREADS" 
 
 ERRORS=0
 OKS=0
@@ -106,7 +123,7 @@ for NSUB in 2 4 8 16 32 64; do
 
     # L1 Serial, L2 PThreads
     #       subm  L1(ntd ntsm) L2(nta) nt_mul plevel                 "extra" 
-    run_cfg $NSUB      0    0       4       0      2  "L1_ser_L2_pth" ""  
+    run_cfg $NSUB      0    0  $NCORES      0      2  "L1_ser_L2_pth" ""  
 
     # L1 Serial, L2 serial
     #       subm  L1(ntd ntsm) L2(nta) nt_mul plevel                 "extra" 
@@ -118,26 +135,25 @@ for NSUB in 2 4 8 16 32 64; do
 
     # L1 TBB, L2 PThread
     #       subm  L1(ntd ntsm) L2(nta) nt_mul plevel                 "extra" 
-    run_cfg $NSUB      0    0       4       0      2  "L1_TBB_L2_pth" "-dohr_tbb"  
+    run_cfg $NSUB      0    0  $NCORES      0      2  "L1_TBB_L2_pth" "-dohr_tbb"  
 
     # L1 PThread, L2 TBB
     #       subm  L1(ntd ntsm) L2(nta) nt_mul plevel                 "extra" 
-    run_cfg $NSUB      4    4       0       0      2  "L1_pth_L2_TBB" "-pair_tbb"  
+    run_cfg $NSUB $NCORES $NCORES    0      0      2  "L1_pth_L2_TBB" "-pair_tbb"  
 
     # L1 PThread, L2 PThread
     #       subm  L1(ntd ntsm) L2(nta) nt_mul plevel                 "extra" 
-    run_cfg $NSUB      4    4       4       0      2  "L1_pth_L2_pth" ""  
+    run_cfg $NSUB $NCORES $NCORES $NCORES   0      2  "L1_pth_L2_pth" ""  
 
     # L1 PThread, L2 Serial
     #       subm  L1(ntd ntsm) L2(nta) nt_mul plevel                 "extra" 
-    run_cfg $NSUB      4    4       0       0      2  "L1_pth_L2_ser" ""  
+    run_cfg $NSUB $NCORES $NCORES   0       0      2  "L1_pth_L2_ser" ""  
 
     # L1 TBB, L2 Serial
     #       subm  L1(ntd ntsm) L2(nta) nt_mul plevel                 "extra" 
-    run_cfg $NSUB      0    0       0       0      2  "L1_tbb_L2_ser" "-dohr_tbb"  
+    run_cfg $NSUB      0    0       0       0      2  "L1_TBB_L2_ser" "-dohr_tbb"  
 
 done
 
 echo "# of ERROR: $ERRORS"
 echo "# of OK   : $OKS"
-
