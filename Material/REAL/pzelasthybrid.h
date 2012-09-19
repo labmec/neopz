0d104a74b998cef8905041f88b1428650a05021d
joaolg82@gmail.com <joaolg82@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-09-19 13:41:01 +0000

    now its derivate from TPZElaticityMaterial, this way we do not need to re-implement a lot of methods

--- Material/REAL/pzelasthybrid.h
+++ Material/REAL/pzelasthybrid.h
@@ -19,26 +19,18 @@
 
 #include "pzmaterial.h"
 #include "pzdiscgal.h"
+#include "pzelasmat.h" 
 
 /**
  * @ingroup material
- * @brief This class implements a two dimensional elastic material to hybrid method.
+ * @brief This class implements a two dimensional elastic material to hybrid method. It is derivate from the class TPZElasticityMaterial.
  */
-class TPZElasticityHybridMaterial : public TPZDiscontinuousGalerkin {
+class TPZElasticityHybridMaterial : public TPZElasticityMaterial {
 	
 	public :
     
 	/** @brief Default constructor */
 	TPZElasticityHybridMaterial();
-	/**
-	 * @brief Creates an elastic material with:
-	 * @param num material id
-	 * @param E elasticity modulus
-	 * @param nu poisson coefficient
-	 * @param fx forcing function \f$ -x = fx \f$
-	 * @param fy forcing function \f$ -y = fy \f$
-	 * @param plainstress \f$ plainstress = 1 \f$ indicates use of plainstress
-	 */
 	TPZElasticityHybridMaterial(int num, REAL E, REAL nu, REAL fx, REAL fy, int plainstress = 1);
 	
 	/** @brief Copies the data of one TPZElasticityHybridMaterial object to another */
@@ -55,144 +47,34 @@ class TPZElasticityHybridMaterial : public TPZDiscontinuousGalerkin {
 	
 	/** @brief Returns the number of state variables associated with the material */
 	virtual  int NStateVariables();
-	
-	/** @brief Print the material data*/
-	virtual void Print(std::ostream & out = std::cout);
-	
+		
 	/** @brief Returns the material name*/
 	std::string Name() { return "TPZElasticityHybridMaterial"; }
 	
-	/** @brief Returns the number of components which form the flux function */
-	virtual short NumberOfFluxes(){return 3;}
-	
-	/** @brief Returns the number of components which form the flux function */
-	virtual int NFluxes(){ return 3;}
     
 	/** @name Contribute methods */
 	/** @{ */
 	
 	/** @brief Calculates the element stiffness matrix */
 	virtual void Contribute(TPZMaterialData &data, REAL weight,TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef);
-    
-    void ContributeVecShape(TPZMaterialData &data,REAL weight,TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef);
+		
+	virtual void ContributeInterface(TPZMaterialData &data, TPZMaterialData &dataleft, TPZMaterialData &dataright, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef);
 	
-	/** @brief Calculates the element stiffness matrix */
-	virtual void Contribute(TPZMaterialData &data, REAL weight,TPZFMatrix<STATE> &ef)
-	{
-		TPZDiscontinuousGalerkin::Contribute(data,weight,ef);
-	}
+	virtual void ContributeBCInterface(TPZMaterialData &data, TPZMaterialData &dataleft, REAL weight, TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc);
 	
-	/** @brief Applies the element boundary conditions */
-	virtual void ContributeBC(TPZMaterialData &data,REAL weight,
-							  TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc);
-    
-    void ContributeVecShapeBC(TPZMaterialData &data,REAL weight,
-                              TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc);
+//	virtual void ContributeInterface(TPZMaterialData &data, TPZMaterialData &dataleft, TPZMaterialData &dataright, REAL weight, TPZFMatrix<STATE> &ef);
 	
-	/** @brief Applies the element boundary conditions */
-	virtual void ContributeBC(TPZMaterialData &data,REAL weight,
-							  TPZFMatrix<STATE> &ef,TPZBndCond &bc)
-	{
-        std::cout<< "Passou ContributeBC"<< std::endl;
-		TPZDiscontinuousGalerkin::ContributeBC(data,weight,ef,bc);
-	}
+//	virtual void ContributeBCInterface(TPZMaterialData &data, TPZMaterialData &left, REAL weight, TPZFMatrix<STATE> &ef,TPZBndCond &bc);
+	    
 	
-	virtual void ContributeInterface(TPZMaterialData &data, TPZMaterialData &dataleft, TPZMaterialData &dataright, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef){
-        std::cout<< "Passou ContributeInterface"<< std::endl;
-		PZError << "\nFATAL ERROR - Method not implemented: " << __PRETTY_FUNCTION__ << "\n";
-	}
-	
-	virtual void ContributeBCInterface(TPZMaterialData &data, TPZMaterialData &dataleft, REAL weight, TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc){
-        std::cout<< "Passou ContributeBCInterface"<< std::endl;
-		PZError << "\nFATAL ERROR - Method not implemented: " << __PRETTY_FUNCTION__ << "\n";
-	}
-	
-	virtual void ContributeInterface(TPZMaterialData &data, TPZMaterialData &dataleft, TPZMaterialData &dataright, REAL weight, TPZFMatrix<STATE> &ef){
-        std::cout<< "Passou ContributeInterface2"<< std::endl;
-		PZError << "\nFATAL ERROR - Method not implemented: " << __PRETTY_FUNCTION__ << "\n";
-	}
-	
-	virtual void ContributeBCInterface(TPZMaterialData &data, TPZMaterialData &left, REAL weight, TPZFMatrix<STATE> &ef,TPZBndCond &bc){
-        std::cout<< "Passou ContributeBCInterface2"<< std::endl;
-		PZError << "\nFATAL ERROR - Method not implemented: " << __PRETTY_FUNCTION__ << "\n";
-	}
-	
-	/** @} */
-	
-	/** @brief Returns the variable index associated with the name */
-	virtual int VariableIndex(const std::string &name);
-	
-	/**
-	 * @brief Returns the number of variables associated with the variable indexed by var.
-	 */
-	virtual int NSolutionVariables(int var);
-    
-    STATE GetLambda()
-    {
-        STATE lambda = (fnu*fE)/((1.+fnu)*(1.-2.*fnu));
-        return lambda;
-    }
-    
-    STATE GetMU()
-    {
-        STATE mu = fE/(2.*(1.+fnu));
-        return mu;
-    }
-	
-protected:
-	virtual void Solution(TPZVec<STATE> &Sol,TPZFMatrix<STATE> &DSol,TPZFMatrix<REAL> &axes,int var,TPZVec<REAL> &Solout);
 public:
-    
-    /** @brief Returns the solution associated with the var index based on the finite element approximation */
-    virtual void Solution(TPZMaterialData &data, int var, TPZVec<REAL> &Solout);
-	
-    
-    /** @brief Returns the solution associated with the var index based on the finite element approximation */
-	virtual void SolutionDisc(TPZMaterialData &data, TPZMaterialData &dataleft, TPZMaterialData &dataright, int var, TPZVec<REAL> &Solout)
-	{
-		TPZDiscontinuousGalerkin::SolutionDisc(data,dataleft,dataright,var,Solout);
-	}
-    
-	/** @brief Computes the value of the flux function to be used by ZZ error estimator */
-	virtual void Flux(TPZVec<REAL> &x, TPZVec<STATE> &Sol, TPZFMatrix<STATE> &DSol, TPZFMatrix<REAL> &axes, TPZVec<STATE> &flux);
-	
-	/**
-	 * @brief Computes the error due to the difference between the interpolated flux \n
-	 * and the flux computed based on the derivative of the solution
-	 */
-	void Errors(TPZVec<REAL> &x,TPZVec<STATE> &u,
-				TPZFMatrix<STATE> &dudx, TPZFMatrix<REAL> &axes, TPZVec<STATE> &flux,
-				TPZVec<STATE> &u_exact,TPZFMatrix<STATE> &du_exact,TPZVec<REAL> &values);//Cedric
-	
-	/** @brief Returns the elasticity modulus E */
-	REAL E() {return fE;}
-	
-	/** @brief Returns the poison coefficient modulus E */
-	REAL Nu() {return fnu;}
-	
+    	
 	/** @brief Set PresStress Tensor */
 	void SetPreStress(REAL Sigxx, REAL Sigyy, REAL Sigxy);
     
-    /** @brief indicates which variable should be post processed */
-    void SetPostProcessIndex(int index)
-    {
-#ifdef DEBUG
-        if (index < 0)
-        {
-            DebugStop();
-        }
-#endif
-        fPostProcIndex = index;
-    }
-	
 	virtual int ClassId() const;
 	
-	virtual void Read(TPZStream &buf, void *context);
-	
-	virtual void Write(TPZStream &buf, int withclassid);
-	
-	
-	
+
 private:
 	/** @brief Elasticity modulus */
 	REAL fE;
@@ -223,6 +105,7 @@ private:
     
     /** @brief indicates which solution should be used for post processing */
     int fPostProcIndex;
+
 };
 
 #endif /* defined(__PZ__pzelasthybrid__) */
