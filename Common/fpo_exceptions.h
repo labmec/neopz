a67889497381603184bff39cffc31aa1c3d54c78
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-09-26 17:26:27 +0000

    Added FP functions to help with invalid arithmetic operations.

---
+++ Common/fpo_exceptions.h
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * @brief Defines functions to help with invalid arithmetic operations
+ */
+#ifndef __FPO_EXCEPTIONS_H
+#define __FPO_EXCEPTIONS_H
+
+#ifdef DEBUG
+#include <iostream>
+#include <stdlib.h>
+#include <xmmintrin.h>
+#include <signal.h>
+#include <fenv.h>
+
+/** 
+ * @ingroup common
+ * @brief Declares a handler function to deal with SIGFPE exception
+ */
+void InvalidFPOHandler(int signo) {
+    switch(signo) {
+        case SIGFPE: std::cout << "ERROR : Invalid Arithmetic operation." << std::endl; break;
+    }
+    exit(signo);
+}
+
+/** 
+ * @ingroup common
+ * @brief This macro enable exceptions during invalid FP operations. (Use this macro inside the main function)
+ */
+#define ENABLE_FPO_EXCEPTIONS _MM_SET_EXCEPTION_MASK(_MM_GET_EXCEPTION_MASK() & ~_MM_MASK_INVALID);
+
+/** 
+ * @ingroup common
+ * @brief This macro link the handler function to the SIGFPE. (Use this macro inside the main function)
+ */
+#define ATTACH_FPO_SIGNAL struct sigaction act = {};\
+    act.sa_handler = InvalidFPOHandler;\
+    sigaction(SIGFPE, &act, NULL);
+
+#endif //DEBUG
+
+#endif //__FPO_EXCEPTIONS_H
