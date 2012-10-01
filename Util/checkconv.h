640db9688873f7df58a30a1b6370889594eb8ba4
philippedevloo1958@gmail.com <philippedevloo1958@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-10-01 16:34:20 +0000

    Added an improved interface

--- Util/checkconv.h
+++ Util/checkconv.h
@@ -107,139 +107,99 @@ void CheckConvergence(TConv &obj, TPZFMatrix<STATE> &state, TPZFMatrix<STATE> &r
 	log.flush();
 }
 
-//
-//#include <stdio.h>
-//
-//#include <stdlib.h>
-//
-//#include <fstream>
-//
-//
-////#include "pztempmat.h"
-//
-//
-//
-//template <class TConv>
-//
-///**
-// * implements a general procedure to check whether the class TConv implements a consistente tangent matrix for the
-// * object obj.\n
-// * The following methods need to be implemented for TConv
-// * -# LoadState : to load the state variables in local storage
-// * -# ComputeTangent : which will compute a tangent matrix
-// * -# Residual : which will compute a residual vector
-// */
-//void CheckConvergence(TConv &obj, TPZFMatrix<REAL> &state, TPZFMatrix<REAL> &range, TPZVec<REAL> &coefs){
-//    
-//    
-//    
-//    TPZFMatrix<REAL> incval(range);
-//    
-//    int i,j,numrows;
-//    
-//    numrows = state.Rows();
-//    
-//    for(i=0; i<numrows; i++) {
-//        
-//        REAL randnum = (rand()&1000)/999.;
-//        
-//        incval(i,0) = range(i,0)*randnum;
-//        
-//    }
-//    
-//    std::ofstream log("conv.log");
-//    
-//    int icase;
-//    
-//    int numcases = obj.NumCases();
-//    
-//    int ncoefs = coefs.NElements();
-//    
-//    for(icase = 0; icase < numcases; icase++) {
-//        
-//        obj.LoadState(state);
-//        
-//        TPZFMatrix<REAL> ReferenceResidual, Tangent, EstimateRes;
-//        
-//        obj.ComputeTangent(Tangent,coefs,icase);
-//        
-//        //            Tangent.Print("tangent matrix");
-//        
-//        obj.Residual(ReferenceResidual,icase);
-//        
-//        //            ReferenceResidual.Print("reference residual");
-//        
-//        EstimateRes.Redim(ReferenceResidual.Rows(),ReferenceResidual.Cols());
-//        
-//        Tangent.Multiply(incval,EstimateRes);
-//        
-//        int interval;
-//        
-//        REAL difnorm[10] = {0.};
-//        
-//        //        REAL resnorm[10] ={0.};
-//        
-//        for(interval = 1; interval < 10; interval++) {
-//            
-//            TPZFMatrix<REAL> actualstate(state);
-//            
-//            TPZFMatrix<REAL> residual;
-//            
-//            for(i=0; i<numrows; i++) {
-//                
-//                for(j=0; j<ncoefs; j++) {
-//                    
-//                    actualstate(i,j) += (interval/10.)*incval(i,0)*coefs[j];
-//                    
-//                }
-//                
-//            }
-//            
-//            obj.LoadState(actualstate);
-//            
-//            obj.Residual(residual,icase);
-//            
-//            //               residual.Print("residual");
-//            
-//            residual -= ReferenceResidual;
-//            
-//            //               resnorm[interval] = Norm(residual);
-//            
-//            residual = residual - EstimateRes*(REAL(interval/10.));
-//            
-//            //       residual.Print("residual adaptado");
-//            difnorm[interval] = Norm(residual);
-//            //       cout << "difnorm = " << difnorm[interval];
-//            
-//        }
-//        
-//        std::cout << "icase = " << icase << std::endl;
-//        
-//        log << "icase = " << icase << std::endl;
-//        
-//        for(interval = 2; interval<10; interval++) {
-//            
-//            if(fabs(difnorm[interval]) < REAL(1.e-12) || fabs(difnorm[interval-1]) <REAL(1.e-12)) {
-//                
-//                std::cout << "residual too small\n";
-//                
-//                log << "residual too small\n";
-//                
-//                break;
-//                
-//            }
-//            std::cout << (log10(difnorm[interval])-log10(difnorm[interval-1]))/
-//            
-//            (log10((float)interval)-log10(interval-1.0)) << std::endl;
-//            
-//            log << (log10(difnorm[interval])-log10(difnorm[interval-1]))/
-//            
-//            (log10((float)interval)-log10(interval-1.0)) << std::endl;
-//            
-//        }
-//        
-//    }
-//    
-//    log.flush();
-//    
-//}
+/*
+ 
+ templates for the interface a TPZCheckConvergence class must specify
+ /// Number of test cases implemented by this class
+ int NumCasesCheckConv() const;
+ /// Compute the tangent matrix for a particular case
+ void TangentCheckConv(TPZFMatrix<STATE> &state, TPZFMatrix<STATE> &tangent, int icase);
+ /// Compute the residual for a particular state
+ void ResidualCheckConv(TPZFMatrix<STATE> &state, TPZFMatrix<STATE> &residual, int icase);
+ */
+
+/**
+ * The following methods need to be implemented for TConv 
+ * \li LoadStateCheckConv : to load the state variables in local storage \n
+ * \li TangentCheckConv : which will compute a tangent matrix \n
+ * \li ResidualCheckConv : which will compute a residual vector \n
+ * \li NumCasesCheckConv() : number of test cases implemented
+ */
+template<class TConv>
+void TPZCheckConvergence(TConv &obj, TPZFMatrix<STATE> &state, TPZFMatrix<STATE> &range, bool direction_range = false) {
+    
+	TPZFMatrix<STATE> incval(range);
+	int numrows;
+	
+	numrows = state.Rows();
+    REAL randnum = (rand()&1000)/999.;
+	for(int i=0; i<numrows; i++) {
+		incval(i,0) = range(i,0)*(STATE)randnum;
+		if(!direction_range) randnum = (rand()&1000)/999.;
+	}
+	
+	std::ofstream log("conv.log");
+	
+	int icase;
+	
+	int numcases = obj.NumCasesCheckConv();
+	
+	
+	
+	for(icase = 0; icase < numcases; icase++) {
+		
+		TPZFMatrix<STATE> ReferenceResidual, Tangent, EstimateRes;
+		
+		obj.TangentCheckConv(state, Tangent,icase);
+		
+		// Tangent.Print("tangent matrix");
+		obj.ResidualCheckConv(state, ReferenceResidual,icase);
+		
+		// ReferenceResidual.Print("reference residual");
+		EstimateRes.Redim(ReferenceResidual.Rows(),ReferenceResidual.Cols());
+		Tangent.Multiply(incval,EstimateRes);
+				
+		REAL difnorm[10] = {0.};
+		
+		for(int interval = 1; interval < 10; interval++) 
+        {
+			
+			TPZFMatrix<STATE> actualstate(state);
+			TPZFMatrix<STATE> residual;
+			
+			for(int i=0; i<numrows; i++) 
+            {
+                actualstate(i) += (STATE)(interval/10.)*(STATE)incval(i,0);
+			}
+			
+			obj.ResidualCheckConv(actualstate, residual,icase);
+			// residual.Print("residual");
+			residual -= ReferenceResidual;
+			// resnorm[interval] = Norm(residual);
+			residual = residual - EstimateRes*(STATE(interval/10.));
+			// residual.Print("residual adaptado");
+			difnorm[interval] = Norm(residual);
+            std::cout << "difnorm = " << difnorm[interval];
+			
+		}
+        
+		std::cout << "icase = " << icase << std::endl;
+		log << "icase = " << icase << std::endl;
+		
+		for(int interval = 2; interval<10; interval++) {
+			
+			if(fabs(difnorm[interval]) < REAL(1.e-12) || fabs(difnorm[interval-1]) <REAL(1.e-12)) {
+				std::cout << "residual too small\n";
+				log << "residual too small\n";
+				break;
+			}
+			std::cout << (log10(difnorm[interval])-log10(difnorm[interval-1]))/
+			
+			(log10((REAL)interval)-log10(interval-1.0)) << std::endl;
+			log << (log10(difnorm[interval])-log10(difnorm[interval-1]))/
+			(log10((REAL)interval)-log10(interval-1.0)) << std::endl;
+		}
+		
+	}	
+	log.flush();
+}
