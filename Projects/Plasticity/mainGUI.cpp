2ef6115461cea9176a8e79cae98e92aa455fb7f8
cecilio.diogo@gmail.com <cecilio.diogo@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2012-11-01 16:36:08 +0000

    Modelos de carregamento. Uniaxial(UCS) Confinamento.

--- Projects/Plasticity/mainGUI.cpp
+++ Projects/Plasticity/mainGUI.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <cstdlib>
+
+
+
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+
+
+
  using namespace std;
 void MaterialPointTests();
 
- int main(int argc, char *argv[])
+ int main()
  {
  	cout << "QT GUI" << endl;
      MaterialPointTests();
@@ -17,50 +29,109 @@ void MaterialPointTests();
 void MaterialPointTests()
 {
     
-    cout << "\nChoose Plasticity test:";
-    cout << "\n0 - Isotropic compression ";
-    cout << "\n1 - Biaxial Tests ";
-    cout << "\n2 - Uniaxial traction ";
-    cout << "\n";
-    int choice;
-  //  cin >> choice;
-    choice =0;
-    switch(choice)
+//    cout << "\nChoose Plasticity test:";
+//    cout << "\n0 - Isotropic compression ";
+//    cout << "\n1 - Biaxial Tests ";
+//    cout << "\n2 - Uniaxial traction ";
+//    cout << "\n";
+//    int choice;
+//  //  cin >> choice;
+//    choice =0;
+//    switch(choice)
+//    {
+//        case(0):
+//            cout << "\n Choose the Plastic model tou need to run Isotropic compression: ";
+//            cout << "\n0 - Lade - Kim ";
+//            cout << "\n1 - Sandler Dimaggio ";
+//            cout << "\n2 - Drucker Prager ";
+//            cout << "\n";
+//            int choice2;
+//         //   cin >> choice2;
+//            choice2 = 1;
+//            switch(choice2)
+//        {
+//            case(0):
+//                LKIsotropicCompression();
+//                break;
+//            case(1):
+//                SandlerDimaggioIsotropicCompression();
+//                break;
+//            case(2):
+//                DruckerIsotropicCompression();
+//                break;
+//        }
+//            
+//            
+//            break;
+//        case(1):
+//            LKBiaxialTest();
+//            break;
+//        case(2):
+//            cout << "NOT IMPLEMENTED YET";
+//            //    LadeKim_ReversalTest();
+//            break;
+//        default:
+//            cout << "Unknown Test Type. Exiting...";
+//    }
+    TPZSandlerDimaggio sandler;
+    sandler.McCormicRanchSand(sandler);
+    ofstream outfiletxty("SandlerDimaggioApplyStrain.txt");
+    ofstream outfiletxtS("SandlerApplyLoad.txt");
+    TPZTensor<REAL> deltaeps,eps,sigma,deltasigma;
+    
+    sandler.fIntegrTol = 1.e-12;
+    sandler.SetIntegrTol(1.e-4);
+    
+    deltaeps.XX()= -0.0013;
+    deltaeps.YY()=0;
+    deltaeps.ZZ()=0;
+    eps=deltaeps;
+    
+    
+    for(int i=0;i<100;i++)
     {
-        case(0):
-            cout << "\n Choose the Plastic model tou need to run Isotropic compression: ";
-            cout << "\n0 - Lade - Kim ";
-            cout << "\n1 - Sandler Dimaggio ";
-            cout << "\n2 - Drucker Prager ";
-            cout << "\n";
-            int choice2;
-         //   cin >> choice2;
-            choice2 = 1;
-            switch(choice2)
+        
+        sandler.ApplyStrainComputeSigma(eps, sigma);//UCS
+        // sandler.ApplyLoad(sigma, eps);
+        
+        TPZPlasticState<REAL> state = sandler.GetState();
+        //state.Print(cout);
+        if(i==49)
         {
-            case(0):
-                LKIsotropicCompression();
-                break;
-            case(1):
-                SandlerDimaggioIsotropicCompression();
-                break;
-            case(2):
-                DruckerIsotropicCompression();
-                break;
+            deltaeps*=-1;
         }
-            
-            
-            break;
-        case(1):
-            LKBiaxialTest();
-            break;
-        case(2):
-            cout << "NOT IMPLEMENTED YET";
-            //    LadeKim_ReversalTest();
-            break;
-        default:
-            cout << "Unknown Test Type. Exiting...";
+        
+        outfiletxty << fabs(eps.XX()) << " " << fabs(sigma.XX()) << "\n";
+        eps+=deltaeps;
     }
     
+    TPZSandlerDimaggio sandler2;
+    sandler2.McCormicRanchSand(sandler2);
+    sandler2.SetIntegrTol(0.0000001);
+    deltaeps.XX()= 1.5*-0.065;
+    deltaeps.YY()=0;
+    deltaeps.ZZ()=0;
+    eps=deltaeps;
+    sandler2.ApplyStrainComputeSigma(eps, sigma);
+    //outfiletxty << fabs(eps.XX()) << " " << fabs(sigma.XX()) << "\n";
+    
+    ///TESTE DE COMPRESSAO SIMPLES
+    deltasigma.XX()=-0.004;
+    deltasigma.YY()=deltasigma.XX()*0.4;
+    deltasigma.ZZ()=deltasigma.YY();
+    sigma=deltasigma;
+    
+    TPZSandlerDimaggio sandler3;
+    sandler3.McCormicRanchSand(sandler3);
+    
+    for(int i=0;i<100;i++)
+    {
+        sandler3.ApplyLoad(sigma,eps);
+        outfiletxtS << fabs(eps.XX()) << " " << fabs(sigma.XX()) << "\n";
+        sigma+=deltasigma;
+    }
+    //VisualizeSandlerDimaggio(fileName,pSD);
+
+  
 }
 
