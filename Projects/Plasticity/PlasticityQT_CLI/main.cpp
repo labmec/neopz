a6305a4381e6fc805d23189d27a0351bd71e7128
raulbaldin@gmail.com <raulbaldin@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2013-11-04 20:28:26 +0000

    Included the reading of one input file on PlasticityQT_CLI

--- Projects/Plasticity/PlasticityQT_CLI/main.cpp
+++ Projects/Plasticity/PlasticityQT_CLI/main.cpp
@@ -3,11 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    DADOS.OpenLabFile("/home/felps/ensaio_all_columns.txt");
-    std::cout << "Medicoes.NElements: " << DADOS.Medicoes.NElements();
+  
+    string FileName, dirname = PZSOURCEDIR;
+    FileName = dirname + "/Projects/Plasticity/";
+    FileName += "ensaio_all_columns.txt";
+  
+    DADOS.OpenLabFile(FileName);
+    std::cout << "Medicoes.NElements: " << DADOS.Medicoes.NElements() << std::endl;
 
-    std::cout << DADOS.Medicoes[0].Get_start_idx();
+    std::cout << DADOS.Medicoes[0].Get_start_idx()  << std::endl;
 
-    std::cout << "END";
+    std::cout << "END" << std::endl;
 
 }
