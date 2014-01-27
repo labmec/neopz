98444f8395de1740b4776623f06c39a7ace0a9e2
cesar.lucci@mac.com <cesar.lucci@mac.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-01-27 18:26:32 +0000

    Swx stuffs (Do not distribute!!!)

---
+++ Projects/HydraulicFracturePropagation/SWXGraph/TSWXVector.cpp
@@ -0,0 +1,30 @@
+#include "TSWXVector.h"
+#include <fstream>
+
+template class swx::vector<int>;
+
+template class swx::vector<double>;
+
+template class swx::vector< swx::vector<int> >;
+
+template class swx::vector< swx::vector<double> >;
+
+template class swx::vector< std::vector<double> >;
+
+template class std::vector< swx::vector<double> >;
+#ifndef __GNUC__
+template class swx::vector< System::UnicodeString >;
+
+int teste(){
+  swx::vector< System::UnicodeString > vec;
+  std::ifstream file("c:\\Temp\\teste.txt");
+  vec.Read(file);
+  _di_IXMLNode myNode;
+  vec.Write(myNode);
+  vec.Read(myNode);
+  return 1;
+}
+#endif
+
+
+
