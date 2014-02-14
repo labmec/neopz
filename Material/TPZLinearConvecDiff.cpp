b79bfd53ae6024c604698889f62fac8d23a5be50
fortiago@gmail.com <fortiago@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-02-14 11:27:13 +0000

    conveccao difusao linear com MEF H1 com SUPG

---
+++ Material/TPZLinearConvecDiff.cpp
@@ -0,0 +1,183 @@
+
+
+#include "TPZLinearConvecDiff.h"
+
+
+TPZLinearConvecDiff::TPZLinearConvecDiff(int nummat, REAL k, const TPZVec<REAL> &conv, REAL f, REAL SD)
+                         :TPZMaterial(numat){
+  fK = k;
+  fConvDir[0] = conv[0];
+  fConvDir[1] = conv[1];
+  fXf = f;
+  fSD = SD;
+}
+
+TPZLinearConvecDiff::TPZLinearConvecDiff(int matid)
+            : TPZMaterial(matid), fXf(0.), fK(0.), fSD(0.){
+  fConvDir[0] = 0.;
+  fConvDir[1] = 0.;
+}
+
+TPZLinearConvecDiff::TPZLinearConvecDiff(): TPZMaterial(), fXf(0.), fK(0.), fSD(0.){
+  fConvDir[0] = 0.;
+  fConvDir[1] = 0.;
+}
+
+TPZLinearConvecDiff::TPZLinearConvecDiff(const TPZMatPoisson3d &c)
+                  :TPZMaterial(c){
+  fK = c.fK;
+  fConvDir[0] = c.fConvDir[0];
+  fConvDir[1] = c.fConvDir[1];
+  fXf = f;
+  fSD = SD;
+}
+
+TPZLinearConvecDiff::~TPZLinearConvecDiff(){
+///nothing here
+}
+
+void TPZLinearConvecDiff::Print(std::ostream & out){
+	out << "name of material : " << Name() << "\n";
+	out << "fK "<< fK << std::endl;
+	out << "Convection vector " << fConvDir[0] << "\t" << fConvDir[1] << std::endl;
+	out << "fXf " << fXf << std::endl;
+  out << "fSD " << fSD << std::endl;
+	out << "Base Class properties :";
+	TPZMaterial::Print(out);
+	out << "\n";
+}
+
+void TPZLinearConvecDiff::Contribute(TPZMaterialData &data,REAL weight,TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef){
+
+  TPZFMatrix<REAL>  &phi = data.phi;
+  TPZFMatrix<REAL> &dphidaxes = data.dphix;
+  TPZFNMatrix<REAL,200> dphi(dphidaxes.Rows(),dphidaxes.Cols(),0.);
+  TPZAxesTools<STATE>::Axes2XYZ(dphidaxes, dphi, axes);
+
+  TPZVec<REAL>  &x = data.x;
+  TPZFMatrix<REAL> &axes = data.axes;
+  TPZFMatrix<REAL> &jacinv = data.jacinv;
+  const int nshape = phi.Rows();
+
+  STATE FVal = this->fXf;
+  if(fForcingFunction) {
+      TPZManVector<STATE,1> res(1);
+      TPZFMatrix<STATE> dres(Dimension(),1);
+      fForcingFunction->Execute(x,res,dres);
+      FVal = res[0];
+  }
+  const REAL normaConveccao = sqrt(fConvDir[0]*fConvDir[0]+fConvDir[1]*fConvDir[1]);
+
+  const REAL h = data.HSize;
+  for( int in = 0; in < nshape; in++ ) {
+    const REAL gradVBeta = this->fConvDir[0] * dphi(0,in) + this->fConvDir[1] * dphi(1,in);
+    ef(in, 0) += weight * FVal * ( phi(in,0) + this->fSD*(0.5*h/normaConveccao)*gradVBeta );
+    for( int jn = 0; jn < phr; jn++ ) {
+      ek(in,jn) += weight * (
+      +fK * ( dphi(0,in) * dphi(0,jn) + dphi(1,in) * dphi(1,jn) )
+      - ( (dphi(0,in) * phi(jn)) * fConvDir[0] + (dphi(1,in) * phi(jn)) * fConvDir[1] )
+      + this->fSD*(0.5*h/normaConveccao)*(dphi(0,jn)*dphi(0,in) + dphi(1,jn)*dphi(1,in) )
+      );
+    }
+  }
+}///void
+
+void TPZLinearConvecDiff::ContributeBC(TPZMaterialData &data,REAL weight,
+							              TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc){
+	TPZFMatrix<REAL>  &phi = data.phi;
+	TPZFMatrix<REAL> &axes = data.axes;
+	const int nshape = phi.Rows();
+	STATE v2 = bc.Val2()(0,0);
+
+	if(bc.HasForcingFunction()) {
+		TPZManVector<STATE> res(1);
+		bc.ForcingFunction()->Execute(data.x,res);
+		v2 = res[0];
+	}
+
+	switch (bc.Type()) {
+		case 0 :			// Dirichlet condition
+			for(in = 0 ; in < nshape; in++) {
+				ef(in,0) += weight * gBigNumber* phi(in,0) * v2;
+				for (jn = 0 ; jn < nshape; jn++) {
+					ek(in,jn) += weight * gBigNumber * phi(in,0) * phi(jn,0);
+				}
+			}
+			break;
+		case 1 :			// Neumann condition
+			for(in = 0 ; in < nshape; in++) {
+				ef(in,0) += weight * v2 * phi(in,0);
+			}
+			break;
+		case 3: // outflow condition
+			REAL normal[2];
+			normal[0] = axes(0,1);
+			normal[1] = axes(1,1);
+
+			REAL ConvNormal = 0.;
+			for(int id = 0; id < 2; id++) ConvNormal += fConvDir[id]*normal[id];
+			if(ConvNormal > 0.){
+				for(int il = 0; il < nshape; il++){
+					for(int jl = 0; jl < nshape; jl++){
+						ek(il,jl) += weight * ConvNormal * phi(il)*phi(jl);
+					}
+				}
+			}
+			else{
+				if (ConvNormal < 0.) std::cout << "Boundary condition error: inflow detected in outflow boundary condition: ConvNormal = " << ConvNormal << "\n";
+			}
+			break;
+	}
+
+}
+
+int TPZLinearConvecDiff::VariableIndex(const std::string &name){
+	if(!strcmp("Solution",name.c_str()))        return  1;
+	if(!strcmp("Derivative",name.c_str()))      return  2;
+	return TPZMaterial::VariableIndex(name);
+}
+
+int TPZLinearConvecDiff::NSolutionVariables(int var){
+	if(var == 1) return 1;
+	if(var == 2) return 2;
+	return TPZMaterial::NSolutionVariables(var);
+}
+
+void TPZLinearConvecDiff::Solution(TPZMaterialData &data, int var, TPZVec<STATE> &Solout){
+
+	Solout.Resize( this->NSolutionVariables( var ) );
+
+	if(var == 1){
+		Solout[0] = data.sol[0][0];//solution - escalar
+		return;
+	}
+	if(var == 2) {
+    TPZFNMatrix<9,STATE> dsoldx;
+    TPZAxesTools<STATE>::Axes2XYZ(data.dsol[0], dsoldx, data.axes);
+		for(int id = 0 ; id < 2; id++) {
+			Solout[id] = dsoldx(id,0);//derivative - vetorial
+		}
+		return;
+	}//var == 2
+
+  return TPZMaterial::Solution(data,var,Solout);
+}
+
+void TPZLinearConvecDiff::Errors(TPZVec<REAL> &x,TPZVec<STATE> &u,
+				TPZFMatrix<STATE> &dudx, TPZFMatrix<REAL> &axes, TPZVec<STATE> &flux,
+				TPZVec<STATE> &u_exact,TPZFMatrix<STATE> &du_exact,TPZVec<REAL> &values){
+
+	values.Resize(3);
+	///L2 norm
+	values[1] = (u[0] - u_exact[0])*(u[0] - u_exact[0]);
+	///semi norma de H1
+	values[2] = 0.;
+	for(int i = 0; i < this->fDim; i++){
+		values[2] += (dudx(i,0) - du_exact(i,0))*(dudx(i,0) - du_exact(i,0));
+	}
+	///H1 norm
+	values[0] = values[1]+values[2];
+
+}
+
+
---
+++ Material/TPZLinearConvecDiff.h
@@ -0,0 +1,113 @@
+//---------------------------------------------------------------------------
+
+#ifndef TPZLinearConvecDiffH
+#define TPZLinearConvecDiffH
+#include <iostream>
+#include "pzfmatrix.h"
+
+
+/**
+ * @ingroup material
+ * @brief Convecção-difusão linear 2D
+ */
+/**
+ * \f$ -fK Laplac(u) + div(fConv*u) = fXf  \f$
+ */
+class TPZLinearConvecDiff : public TPZMaterial {
+
+	protected :
+
+	/** @brief Forcing function value */
+	STATE fXf;
+
+	/** @brief Coeficient which multiplies the Laplacian operator. */
+	STATE fK;
+
+	/** @brief Convection vector */
+	REAL fConvDir[2];
+
+  /** @brief Multiplication value for the streamline diffusion term */
+	STATE fSD;
+
+public:
+
+	TPZLinearConvecDiff(int nummat, REAL k, const TPZVec<REAL> &conv, REAL f, REAL SD):TPZMaterial(numat){
+    fK = k;
+    fConvDir[0] = conv[0];
+    fConvDir[1] = conv[1];
+    fXf = f;
+    fSD = SD;
+  }
+
+  TPZLinearConvecDiff(int matid) : TPZMaterial(matid), fXf(0.), fK(0.), fSD(0.)
+  {
+    fConvDir[0] = 0.;
+    fConvDir[1] = 0.;
+  }
+
+	TPZLinearConvecDiff(): TPZMaterial(), fXf(0.), fK(0.), fSD(0.)
+  {
+    fConvDir[0] = 0.;
+    fConvDir[1] = 0.;
+  }
+
+	TPZLinearConvecDiff(const TPZMatPoisson3d &c):TPZMaterial(c){
+    fK = c.fK;
+    fConvDir[0] = c.fConvDir[0];
+    fConvDir[1] = c.fConvDir[1];
+    fXf = f;
+    fSD = SD;
+  }
+
+	virtual ~TPZLinearConvecDiff(){
+
+  }
+
+	virtual TPZMaterial * NewMaterial(){
+		return new TPZLinearConvecDiff(*this);
+	}
+
+	int Dimension() { return 2;}
+
+	int NStateVariables(){ return 1; }
+
+	virtual void Print(std::ostream & out);
+
+	virtual std::string Name() { return "TPZLinearConvecDiff"; }
+
+	virtual void Contribute(TPZMaterialData &data,REAL weight,TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef);
+
+	virtual void ContributeBC(TPZMaterialData &data,REAL weight,
+							              TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc);
+
+	virtual int VariableIndex(const std::string &name);
+
+	virtual int NSolutionVariables(int var);
+
+	virtual int NFluxes(){ return 2;}
+
+	virtual void Solution(TPZMaterialData &data, int var, TPZVec<STATE> &Solout);
+
+	void Errors(TPZVec<REAL> &x,TPZVec<STATE> &u,
+				TPZFMatrix<STATE> &dudx, TPZFMatrix<REAL> &axes, TPZVec<STATE> &flux,
+				TPZVec<STATE> &u_exact,TPZFMatrix<STATE> &du_exact,TPZVec<REAL> &values);
+
+	virtual int NEvalErrors() {return 3;}
+
+  virtual int ClassId() const {
+    DebugStop();///implementar
+    return -1;
+  }
+
+	virtual void Write(TPZStream &buf, int withclassid){
+    DebugStop();///implementar
+  }
+
+	virtual void Read(TPZStream &buf, void *context){
+    DebugStop();///implementar
+  }
+
+};
+
+
+#endif
