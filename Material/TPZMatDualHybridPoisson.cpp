15db3e89558255849d18c6ccaa62c2d05df18c6e
fortiago@gmail.com <fortiago@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-02-19 14:24:42 +0000

    MAterial que implementa formulacao hibrida de Abimael

---
+++ Material/TPZMatDualHybridPoisson.cpp
@@ -0,0 +1,228 @@
+
+#include "TPZMatDualHybridPoisson.h"
+
+
+TPZMatDualHybridPoisson::TPZMatDualHybridPoisson(int nummat, REAL f, REAL betaZero)
+ :TPZDiscontinuousGalerkin(matid),fXf(f), fBetaZero(betaZero){
+
+}
+
+TPZMatDualHybridPoisson::TPZMatDualHybridPoisson(int matid) : TPZDiscontinuousGalerkin(matid),
+  fXf(0.), fBetaZero(0.)
+{
+
+}
+
+TPZMatDualHybridPoisson::TPZMatDualHybridPoisson(): TPZDiscontinuosGalerkin(){
+
+}
+
+TPZMatDualHybridPoisson::TPZMatDualHybridPoisson(const TPZMatDualHybridPoisson &copy)
+ : TPZDiscontinuosGalerkin(copy){
+ fXf = copy.fXf;
+ fBetaZero = copy.fBetaZero;
+}
+
+TPZMatDualHybridPoisson::~TPZMatDualHybridPoisson(){
+
+}
+
+void TPZMatDualHybridPoisson::Print(std::ostream & out){
+  out << "\n" << this->Name() << "\n";
+  out << "fXf = " << fXf << "\n";
+  out << "fBetaZero = " << fBetaZero << "\n";
+  TPZDiscontinuosGalerkin::Print(out);
+}
+
+void TPZMatDualHybridPoisson::Contribute(TPZMaterialData &data,
+                                         REAL weight,
+                                         TPZFMatrix<STATE> &ek,
+                                         TPZFMatrix<STATE> &ef){
+  TPZFMatrix<REAL>  &phi = data.phi;
+  TPZFMatrix<REAL> &dphi = data.dphix;
+  TPZVec<REAL>  &x = data.x;
+  TPZFMatrix<REAL> &axes = data.axes;
+  TPZFMatrix<REAL> &jacinv = data.jacinv;
+  const int nshape = phi.Rows();
+
+  if(dphi.Rows() == 1){///estou no elemento 1D do multiplicador de Lagrange
+
+    for( int in = 0; in < nshape; in++ ) {
+      for( int jn = 0; jn < nshape; jn++ ) {
+        ek(i,j) += weight * (this->fBetaZero/data.HSize) * phi(i,0) * phi(j,0);
+      }
+    }
+    return;
+
+  }///1D
+  else if(dphi.Rows() == 2){///estou no elemento 2D
+
+    STATE Fval = fXf;
+    if(this->HasForcingFunction()){
+      TPZManVector<STATE,1> res(1);
+      TPZFMatrix<STATE> dres(Dimension(),1);
+      fForcingFunction->Execute(x,res,dres);
+      Fval = res[0];
+    }
+
+    //Equacao de Poisson
+    for( int in = 0; in < nshape; in++ ) {
+      ef(in, 0) +=  weight * Fval * phi(in,0);
+      for( int jn = 0; jn < nshape; jn++ ) {
+        ek(in,jn) += weight * ( dphi(0,in) * dphi(0,jn) + dphi(1,in) * dphi(1,jn)  );
+      }
+    }
+
+    return;
+
+  }///2D
+  else DebugStop();
+
+}///void
+
+void TPZMatDualHybridPoisson::ContributeBC(TPZMaterialData &data,
+                                           REAL weight,
+                                           TPZFMatrix<STATE> &ek,
+                                           TPZFMatrix<STATE> &ef,
+                                           TPZBndCond &bc){
+
+	TPZFMatrix<REAL>  &phi = data.phi;
+	const int nshape = phi.Rows();
+	STATE valBC;
+	valBC = bc.Val2()(0,0);
+
+	if(bc.HasForcingFunction()) {
+		TPZManVector<STATE> res(1);
+		bc.ForcingFunction()->Execute(data.x,res);
+		valBC = res[0];
+	}
+
+	if(bc.Type() == 0){ // Dirichlet condition
+    for(int i = 0; i < nshape; i++){
+      ef(i,0) += weight * gBigNumber * phi(in,0) * valBC;
+      for (int j = 0; j < nshape; j++){
+        ek(i,j) += weight * gBigNumber * phi(i,0) * phi(j,0);
+      }
+    }
+  }
+  else{
+    PZError << "TPZMatDualHybridPoisson::ContributeBC error. BC type not implemented\n";
+    DebugStop();
+  }
+
+}///void
+
+void TPZMatDualHybridPoisson::ContributeInterface(TPZMaterialData &data,
+                                                  TPZMaterialData &dataleft,
+                                                  TPZMaterialData &dataright,
+                                                  REAL weight,
+                                                  TPZFMatrix<STATE> &ek,
+                                                  TPZFMatrix<STATE> &ef){
+	TPZFMatrix<REAL> &dphiL = dataleft.dphix;
+	TPZFMatrix<REAL> &dphiR = dataright.dphix;
+	TPZFMatrix<REAL> &phiL = dataleft.phi;
+	TPZFMatrix<REAL> &phiR = dataright.phi;
+	TPZManVector<REAL,3> &normal = data.normal;
+	const REAL faceSize = data.HSize;
+  const REAL beta = this->fBetaZero/faceSize;
+
+	const int nshapeL = phiL.Rows();
+	const int nshapeR = phiR.Rows();
+
+  if(dphiR.Rows() > 1) DebugStop(); //o multiplicador de Lagrange foi assumido como direito
+  ///u, v : left
+  ///lambda, mu : right
+
+  ///-gradU.n v
+  for(int il = 0; il < nshapeL; il++){
+    for(int jl = 0; jl < nshapeL; jl++){
+      ek(il,jl) += weight * (-1.)* (dphiL(0,jl)*normal[0]+dphiL(1,jl)*normal[1]) * phiL(il,0);
+    }
+  }
+
+  ///-gradV.n u
+  for(int il = 0; il < nshapeL; il++){
+    for(int jl = 0; jl < nshapeL; jl++){
+      ek(il,jl) += weight * (-1.)* (dphiL(0,il)*normal[0]+dphiL(1,il)*normal[1]) * phiL(jl,0);
+    }
+  }
+
+  ///+gradV.n lambda
+  for(int il = 0; il < nshapeL; il++){
+    for(int jr = 0; jr < nshapeR; jr++){
+      ek(il,nshapeL+jr) += weight * (+1.)* (dphiL(0,il)*normal[0]+dphiL(1,il)*normal[1]) * phiR(jr,0);
+    }
+  }
+
+  ///beta u v
+  for(int il = 0; il < nshapeL; il++){
+    for(int jl = 0; jl < nshapeL; jl++){
+      ek(il,jl) += weight * beta * phiL(il,0) * phiL(jl,0);
+    }
+  }
+
+  ///-beta lambda v
+  for(int il = 0; il < nshapeL; il++){
+    for(int jr = 0; jr < nshapeR; jr++){
+      ek(il,nshapeL+jr) += weight * (-1.) * beta * phiL(il,0) * phiR(jr,0);
+    }
+  }
+
+  ///gradU.n mu - beta u mu
+  for(int ir = 0; ir < nshapeR; ir++){
+    for(int jl = 0; jl < nshapeL; jl++){
+      ek(nshapeL+ir,jl) += weight * ( dphiL(0,jl)*normal[0] + dphiL(1,jl)*normal[1] ) * phiR(ir,0);
+      ek(nshapeL+ir,jl) += weight * (-beta) * phiL(jl,0) * phiR(ir,0);
+    }
+  }
+
+}//void
+
+void TPZMatDualHybridPoisson::ContributeBCInterface(TPZMaterialData &data,
+                                                    TPZMaterialData &dataleft,
+                                                    REAL weight,
+                                                    TPZFMatrix<STATE> &ek,
+                                                    TPZFMatrix<STATE> &ef,
+                                                    TPZBndCond &bc){
+  PZError << "TPZMatDualHybridPoisson::ContributeBCInterface should never be called in this formulation\n";
+  DebugStop();
+}
+
+int TPZMatDualHybridPoisson::VariableIndex(const std::string &name){
+	if(!strcmp("Solution",name.c_str())) return  1;
+	return TPZMaterial::VariableIndex(name);
+}
+
+int TPZMatDualHybridPoisson::NSolutionVariables(int var){
+	if(var == 1) return 1;
+	return TPZMaterial::NSolutionVariables(var);
+}
+
+void TPZMatDualHybridPoisson::Solution(TPZMaterialData &data, int var, TPZVec<STATE> &Solout){
+	Solout.Resize( this->NSolutionVariables( var ) );
+
+	if(var == 1){
+		Solout[0] = Sol[0];//solution - escalar
+		return;
+	}
+
+  TPZMaterial::Solution(data,var,Solout);
+}
+
+void Errors(TPZVec<REAL> &x,TPZVec<STATE> &u,
+      TPZFMatrix<STATE> &dudx, TPZFMatrix<REAL> &axes, TPZVec<STATE> &flux,
+      TPZVec<STATE> &u_exact,TPZFMatrix<STATE> &du_exact,TPZVec<REAL> &values){
+	values.Resize(3);
+	///L2 norm
+	values[1] = (u[0] - u_exact[0])*(u[0] - u_exact[0]);
+	///semi norma de H1
+	values[2] = 0.;
+	for(int i = 0; i < this->fDim; i++){
+		values[2] += (dudx(i,0) - du_exact(i,0))*(dudx(i,0) - du_exact(i,0));
+	}
+	///H1 norm
+	values[0] = values[1]+values[2];
+}
+
+
+
---
+++ Material/TPZMatDualHybridPoisson.h
@@ -0,0 +1,109 @@
+//---------------------------------------------------------------------------
+
+/**
+ * @file
+ * @brief Contains the TPZMatLaplacian class.
+ */
+#ifndef TPZMatDualHybridPoissonH
+#define TPZMatDualHybridPoissonH
+
+
+#include <iostream>
+#include "pzdiscgal.h"
+#include "pzfmatrix.h"
+
+/**
+ * @ingroup material
+ * @brief \f$ -Laplac(u) = fXf  \f$
+ */
+/**
+ * \f$ -Laplac(u) = fXf  \f$
+ */
+class TPZMatDualHybridPoisson : public TPZDiscontinuousGalerkin {
+
+	protected :
+
+	/** @brief Forcing function value */
+	STATE fXf;
+
+  STATE fBetaZero;
+
+public:
+
+	TPZMatDualHybridPoisson(int nummat, REAL f, REAL betaZero);
+
+  TPZMatDualHybridPoisson(int matid);
+
+	TPZMatDualHybridPoisson();
+
+	TPZMatDualHybridPoisson(const TPZMatDualHybridPoisson &copy);
+
+	virtual ~TPZMatDualHybridPoisson();
+
+	virtual TPZMaterial * NewMaterial(){
+		return new TPZMatDualHybridPoisson(*this);
+	}
+
+	int Dimension() { return 2;}
+
+	int NStateVariables(){
+    return 1;
+  }
+
+	virtual void Print(std::ostream & out);
+
+	virtual std::string Name() { return "TPZMatDualHybridPoisson"; }
+
+	/**
+	 * @name Contribute methods (weak formulation)
+	 * @{
+	 */
+
+  virtual void Contribute(TPZMaterialData &data,REAL weight,TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef);
+
+	virtual void ContributeBC(TPZMaterialData &data,REAL weight,
+							  TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc);
+
+	virtual void ContributeInterface(TPZMaterialData &data, TPZMaterialData &dataleft, TPZMaterialData &dataright, REAL weight,
+									 TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef);
+
+	virtual void ContributeBCInterface(TPZMaterialData &data, TPZMaterialData &dataleft, REAL weight,TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc);
+
+	virtual int VariableIndex(const std::string &name);
+
+	virtual int NSolutionVariables(int var);
+
+	virtual int NFluxes(){ return 2;}
+
+  public:
+
+	virtual void Solution(TPZMaterialData &data, int var, TPZVec<STATE> &Solout);
+
+	virtual void Flux(TPZVec<REAL> &x, TPZVec<STATE> &Sol, TPZFMatrix<STATE> &DSol, TPZFMatrix<REAL> &axes, TPZVec<STATE> &flux)
+  {
+    DebugStop();
+  }
+
+	void Errors(TPZVec<REAL> &x,TPZVec<STATE> &u,
+				TPZFMatrix<STATE> &dudx, TPZFMatrix<REAL> &axes, TPZVec<STATE> &flux,
+				TPZVec<STATE> &u_exact,TPZFMatrix<STATE> &du_exact,TPZVec<REAL> &values);
+
+	virtual int NEvalErrors() {return 3;}
+
+  virtual int ClassId() const {
+    DebugStop();
+    return -1;
+  }
+
+	virtual void Write(TPZStream &buf, int withclassid){
+    DebugStop();
+  }
+
+	virtual void Read(TPZStream &buf, void *context){
+    DebugStop();
+  }
+
+};
+
+#endif
+
