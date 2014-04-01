8b989db38befda43f2b41f789bec591cae18281a
nathan.sh@gmail.com <nathan.sh@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-04-01 22:12:26 +0000

    Ongoing work on cohesive BC

--- Projects/ReducedSpacePlasticity/TPZCohesiveBC.cpp
+++ Projects/ReducedSpacePlasticity/TPZCohesiveBC.cpp
@@ -6,12 +6,17 @@
 #include "TPZCohesiveBC.h"
 
 
-TPZCohesiveBC::TPZCohesiveBC() : TPZMaterial(), fSigmaT(0.), fDeltaC(0.)
+TPZCohesiveBC::TPZCohesiveBC() : TPZMatWithMem<TPZFMatrix<REAL> >(), fSigmaT(0.), fDeltaC(0.), fDeltaT(0.)
+{
+	 
+}
+
+TPZCohesiveBC::TPZCohesiveBC(int id) : TPZMatWithMem<TPZFMatrix<REAL> >(id), fSigmaT(0.), fDeltaC(0.), fDeltaT(0.)
 {
 	
 }
 
-TPZCohesiveBC::TPZCohesiveBC(int id) : TPZMaterial(id), fSigmaT(0.), fDeltaC(0.)
+TPZCohesiveBC::TPZCohesiveBC(const TPZCohesiveBC &cp) : TPZMatWithMem<TPZFMatrix<REAL> >(cp), fSigmaT(cp.fSigmaT), fDeltaC(cp.fDeltaC), fDeltaT(cp.fDeltaT)
 {
 	
 }
@@ -21,51 +26,85 @@ TPZCohesiveBC::~TPZCohesiveBC()
 	
 }
 
-void TPZCohesiveBC::ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef, TPZBndCond &bc)
+void TPZCohesiveBC::SetCohesiveData(const REAL &SigmaT, const REAL &DeltaC, const REAL &DeltaT)
 {
+	fSigmaT = SigmaT;
+	fDeltaC = DeltaC;
+	fDeltaT = DeltaT;
+	TPZFNMatrix<2,REAL> DTST(2,1,0.);
+	DTST(0,0) = DeltaT;
+	DTST(1,0) = SigmaT;
+	this->SetDefaultMem(DTST);
+}
+
+void TPZCohesiveBC::CalculateSigma(TPZVec<TPZMaterialData> &datavec, REAL &sigma) const 
+{
+	TPZManVector<REAL,3> sol_u = datavec[0].sol[0];
+	const REAL uy = sol_u[1];
+	const REAL w = 2.*uy;
 	
+	const int index = datavec[0].intGlobPtIndex; 
+	TPZFMatrix<REAL> DTST = this->MemItem(index);
+	if (DTST.Rows() != 2 || DTST.Cols() != 1) {
+		PZError << "The DTST memory number of Rows must be 2 and Cols 1\n";
+		DebugStop();
+	}
+	const REAL DeltaT = DTST(0,0);
+	const REAL SigmaT = DTST(1,0);
+	
+	// Calculating the function
+	if (w <= DeltaT) {
+		sigma = w * SigmaT/DeltaT;
+	}
+	else {
+		sigma = SigmaT * (1. - (w - DeltaT)/(fDeltaC - DeltaT) );
+	}
+
+}
+
+void TPZCohesiveBC::UpdateCohesiveCurve(TPZVec<TPZMaterialData> &datavec)
+{
+	TPZManVector<REAL,3> sol_u = datavec[0].sol[0];
+	const REAL w = 2.*sol_u[1];
+	REAL sigma = -6378.;
+	CalculateSigma(datavec,sigma);
+	
+	TPZFMatrix<REAL> DTST(2,1,0.);
+	DTST(0,0) = w;
+	DTST(1,0) = sigma;
+	
+	int index = datavec[0].intGlobPtIndex;
+	this->MemItem(index) = DTST;
 }
 
 void TPZCohesiveBC::Contribute(TPZMaterialData &data, REAL weight, TPZFMatrix<STATE> &ef)
 {
+	/*
+	TPZFMatrix<REAL> &phi = data[0].phi; // 0 is the elastic mesh
+	TPZFMatrix<REAL> &sol = data[0].sol[0];
+
 	
+	for(in=0 ; in<numnod ; ++in){
+		for(idf = 0;idf<r;idf++) {
+			(ef)(in*r+idf,0) += (STATE)phi(in,0)*bc.Val2()(idf,0)*(STATE)weight;
+		}
+	}	
+	 */
 }
 
-void TPZCohesiveBC::ContributeBC(TPZMaterialData &data, REAL weight, TPZFMatrix<STATE> &ef, TPZBndCond &bc)
+void TPZCohesiveBC::Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef)
 {
 	
 }
 
 void TPZCohesiveBC::Print(std::ostream &out)
 {
-	
+	out << "TPZMatWithMem::Print:" << std::endl;
+	TPZMatWithMem<TPZFMatrix<REAL> >::Print(out);
+	out << "\nfSigmaT = " << fSigmaT << std::endl;
+	out << "fDeltaT = " << fDeltaT << std::endl;
+	out << "fDeltaC = " << fDeltaC << std::endl;
+	out << std::endl;
 }
 
-void TPZCohesiveBC::Read(TPZStream &buf, void *context)
-{
-	PZError << "METHOD NOT IMPLEMENTED!!!\n";
-	DebugStop();
-}
-
-void TPZCohesiveBC::Write(TPZStream &buf, int withclassid)
-{
-	PZError << "METHOD NOT IMPLEMENTED!!!\n";
-	DebugStop();	
-}
-
-int TPZCohesiveBC::VariableIndex(const std::string &name)
-{
-	
-}
-
-int TPZCohesiveBC::NSolutionVariables(int var)
-{
-	
-}
-
-
-void TPZCohesiveBC::Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<STATE> &Solout)
-{
-	
-}
 
--- Projects/ReducedSpacePlasticity/TPZCohesiveBC.h
+++ Projects/ReducedSpacePlasticity/TPZCohesiveBC.h
@@ -9,22 +9,26 @@
 #include <iostream>
 #include "pzmaterial.h"
 #include "pzbndcond.h"
+#include "pzmatwithmem.h"
 
 const int TPZCohesiveBCID = 400;
 
 /**
  * @ingroup material
- * @brief This class implements a cohesive bc, which has the stress dependent of the displacement
- *        
+ * @brief This class implements a cohesive bc, which has the stress dependent of the displacement.
+ * Remembering that the std::pair memory holds the pair (DeltaT,SigmaT) at each time step.
+ * @author Nathan Shauer in 01/04/2014
  */
-class  TPZCohesiveBC : public TPZMaterial
+class  TPZCohesiveBC : public TPZMatWithMem<TPZFMatrix<REAL> >
 {
 private:
 
-	/// SigmaT (Traction) of the cohesive equation Sigma = SigmaT(1 - Delta/DeltaC) whete Delta is the displacement opening 
+	/// SigmaT (Maximum Traction) of the cohesive equation
 	REAL fSigmaT;
-	/// DeltaC (Critical) of the cohesive equation Sigma = SigmaT(1 - Delta/DeltaC) whete SigmaT is the maximum traction 
+	/// DeltaC (Critical Displacement) of the cohesive equation
 	REAL fDeltaC;
+	/// DeltaT (diplacement for maximum traction). The point (DeltaT,SigmaT)
+	REAL fDeltaT;
 	
 public:
 	
@@ -37,9 +41,19 @@ public:
 	
 	/** @brief Creates a material object based on the referred object and inserts it in the vector of material pointers of the mesh. */
 	/** Upon return vectorindex contains the index of the material object within the vector */
-	TPZCohesiveBC(const TPZMaterial &mat);
+	TPZCohesiveBC(const TPZCohesiveBC &mat);
+	
 	/** @brief Default destructor */
 	virtual ~TPZCohesiveBC();
+	
+	/** @brief Sets the Cohesive Data */
+	void SetCohesiveData(const REAL &SigmaT, const REAL &DeltaC, const REAL &DeltaT);
+	
+	/** @brief Calculates Sigma for determined solution */
+	void CalculateSigma(TPZVec<TPZMaterialData> &datavec, REAL &sigma) const; 
+
+	/** @brief Updates the cohesive curve acording to the calculated w of the time step */
+	void UpdateCohesiveCurve(TPZVec<TPZMaterialData> &datavec);
 	
 	/** 
 	 * @brief Fill material data parameter with necessary requirements for the
@@ -49,6 +63,7 @@ public:
 	virtual void FillDataRequirements(TPZVec<TPZMaterialData > &datavec)
 	{
 		datavec[0].SetAllRequirements(false);
+		datavec[0].fNeedsSol = true;
 	}
 	
 	/** @brief Returns the name of the material */
@@ -58,42 +73,12 @@ public:
 	virtual int Dimension() {
 		return 1;
 	}
-	
-
-	/** @brief Returns the number of state variables associated with the material */
-	virtual int NStateVariables() {
-		return 1;
-	}
 		
 	/** @brief Prints out the data associated with the material */
 	virtual void Print(std::ostream &out = std::cout);
 	
-	/** @brief Returns the variable index associated with the name */
-	virtual int VariableIndex(const std::string &name);
-	
-	/** 
-	 * @brief Returns the number of variables associated with the variable indexed by var. 
-	 * @param var Index variable into the solution, is obtained by calling VariableIndex
-	 */
-	virtual int NSolutionVariables(int var);
-	
-	/** @brief Returns the solution associated with the var index based on the finite element approximation */
-	virtual void Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<STATE> &Solout);
-	
 public:
 
-	
-	/**
-	 * @brief It computes a contribution to the stiffness matrix and load vector at one BC integration point
-	 * to multiphysics simulation.
-	 * @param datavec [in]  stores all input data
-	 * @param weight [in] is the weight of the integration rule
-	 * @param ek [out] is the stiffness matrix
-	 * @param ef [out] is the load vector
-	 * @param bc [in] is the boundary condition material
-	 * @since October 18, 2011
-	 */
-	virtual void ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef, TPZBndCond &bc);
 	
 	/**
 	 * @brief It computes a contribution to the residual vector at one integration point.
@@ -105,14 +90,13 @@ public:
 	virtual void Contribute(TPZMaterialData &data, REAL weight, TPZFMatrix<STATE> &ef);
 	
 	/**
-	 * @brief It computes a contribution to the stiffness matrix and load vector at one BC integration point.
-	 * @param data [in] stores all input data
+	 * @brief It computes a contribution to the stiffness matrix and load vector at one integration point to multiphysics simulation.
+	 * @param datavec [in] stores all input data
 	 * @param weight [in] is the weight of the integration rule
+	 * @param ek [out] is the stiffness matrix
 	 * @param ef [out] is the load vector
-	 * @param bc [in] is the boundary condition material
-	 * @since April 16, 2007
 	 */
-	virtual void ContributeBC(TPZMaterialData &data, REAL weight, TPZFMatrix<STATE> &ef, TPZBndCond &bc);
+	virtual void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef);
 	
 	
 	/** @brief Unique identifier for serialization purposes */
@@ -120,13 +104,6 @@ public:
 	{
 		return TPZCohesiveBCID;
 	}
-	
-	/** @brief Saves the element data to a stream */
-	virtual void Write(TPZStream &buf, int withclassid);
-	
-	/** @brief Reads the element data from a stream */
-	virtual void Read(TPZStream &buf, void *context);
-	
 	
 };
 
