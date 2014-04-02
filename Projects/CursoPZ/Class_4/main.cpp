3ff585188261b0ba27cea2e0a98dbc0d2a21559c
philippedevloo1958@gmail.com <philippedevloo1958@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-04-02 02:29:18 +0000

    Included a function which illustrates directional refinement

--- Projects/CursoPZ/Class_4/main.cpp
+++ Projects/CursoPZ/Class_4/main.cpp
@@ -13,6 +13,9 @@
 #include "TPZReadGIDGrid.h"
 #include "TPZVTKGeoMesh.h"
 
+#include "TPZRefPatternDataBase.h"
+
+#include "pzlog.h"
 //IO
 #include <iostream>
 using namespace std;
@@ -21,9 +24,18 @@ using namespace std;
 // ny = number of nodes in y direction
 TPZGeoMesh * GetMesh(int nx,int ny);
 
+void RefineTowards(TPZGeoElSide gelside, int numtimes);
+
+void RefineTowardsCenter(TPZGeoMesh &gmesh, int sidedim, int numtimes);
+
+void RefineTowards(TPZGeoMesh &gmesh, TPZVec<REAL> &x, int sidedim, int numtimes);
+
 std::string DirectorioCorriente = PZSOURCEDIR;
 
 int main() {
+
+    InitializePZLOG();
+    gRefDBase.InitializeRefPatterns();
 
 	DirectorioCorriente += "/Projects/CursoPZ/Class_4/";
 /*
@@ -167,9 +179,15 @@ int main() {
 	TPZGeoMesh *meshgrid2D1 = grid.GeometricGIDMesh(nombre);
 	if(!meshgrid2D1->NElements())
 		return 1;
-	ofstream meshoutvtk2D1("MeshGIDToVTK2D1.vtk");
+	ofstream meshoutvtk2D1("MeshGIDToVTK2D1.1.vtk");
 	TPZVTKGeoMesh::PrintGMeshVTK(meshgrid2D1,meshoutvtk2D1,true);
-	meshoutvtk2D1.close();
+
+	RefineTowardsCenter(*meshgrid2D1, 1, 3);
+	ofstream meshoutvtk2D1b("MeshGIDToVTK2D1.2.vtk");
+	TPZVTKGeoMesh::PrintGMeshVTK(meshgrid2D1,meshoutvtk2D1b,true);
+    
+    meshoutvtk2D1.close();
+    meshoutvtk2D1b.close();
 
 	// Segundo ejemplo bi-dimensional para la generacion de una malla para un reservatorio con las bordas superior e inferior
 	nombre = DirectorioCorriente;
@@ -253,3 +271,95 @@ TPZGeoMesh *GetMesh (int nx,int ny) {
 	gmesh->BuildConnectivity();
 	return gmesh;
 }
+
+#include "pzgeoelbc.h"
+#include "TPZRefPatternTools.h"
+
+void RefineTowards(TPZGeoElSide gelside, int numtimes)
+{
+    TPZGeoElBC gbc(gelside,10);
+    TPZGeoEl *gel = gelside.Element();
+    TPZGeoEl *gelbc = gbc.CreatedElement();
+    int nsides = gel->NSides();
+    std::set<int> matids;
+    matids.insert(10);
+    
+    for (int iref=0; iref<numtimes; iref++)
+    {
+        std::set<TPZGeoEl *> elementstodivide;
+        elementstodivide.insert(gel);
+        int ncorners = gel->NSideNodes(gelside.Side());
+        for (int corner=0; corner<ncorners; corner++) {
+            int side = gel->SideNodeLocIndex(gelside.Side(), corner);
+            TPZGeoElSide start(gel,side);
+            TPZGeoElSide neighbour = start.Neighbour();
+            while (neighbour != start) {
+                elementstodivide.insert(neighbour.Element());
+                neighbour = neighbour.Neighbour();
+            }
+        }
+        std::set<TPZGeoEl *>::iterator it;
+        for (it=elementstodivide.begin(); it != elementstodivide.end(); it++) {
+            TPZRefPatternTools::RefineDirectional(*it, matids, 11+iref);
+        }
+    }
+}
+
+void RefineTowardsCenter(TPZGeoMesh &gmesh, int sidedim, int numtimes)
+{
+    if (gmesh.NElements()==0) {
+        return;
+    }
+    TPZManVector<REAL,3> xmin(3), xmax(3),xmid(3);
+    
+    
+    gmesh.NodeVec()[0].GetCoordinates(xmin);
+    xmax = xmin;
+    int nnodes = gmesh.NNodes();
+    for (int in=0; in<nnodes; in++) {
+        TPZManVector<REAL,3> xco(3);
+        gmesh.NodeVec()[in].GetCoordinates(xco);
+        for (int i=0; i<3; i++) {
+            if (xmin[i] > xco[i]) {
+                xmin[i] = xco[i];
+            }
+            if (xmax[i] < xco[i]) {
+                xmax[i] = xco[i];
+            }
+        }
+    }
+    for (int i=0; i<3; i++) {
+        xmid[i] = (xmin[i]+xmax[i])/2.;
+    }
+    RefineTowards(gmesh, xmid, sidedim, numtimes);
+}
+
+void RefineTowards(TPZGeoMesh &gmesh, TPZVec<REAL> &x, int sidedim, int numtimes)
+{
+    if (gmesh.NElements()==0) {
+        return;
+    }
+    int dim = gmesh.ElementVec()[0]->Dimension();
+    
+    for (int el=0; el<gmesh.NElements(); el++) {
+        TPZGeoEl *gel = gmesh.ElementVec()[el];
+        if (!gel) {
+            continue;
+        }
+        dim = max(dim,gel->Dimension());
+    }
+
+    TPZManVector<REAL,3> qsi(dim,0.);
+    long elindex = 0;
+    TPZGeoEl *gel = gmesh.FindElement(x, qsi, elindex, dim);
+    
+    sidedim = min(sidedim,gel->Dimension());
+    int side = 0;
+    while (gel->SideDimension(side) != sidedim) {
+        side++;
+    }
+    
+    TPZGeoElSide targetside(gel,side);
+    RefineTowards(targetside, numtimes);
+}
+
