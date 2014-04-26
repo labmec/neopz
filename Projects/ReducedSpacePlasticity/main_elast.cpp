62c252c338f5ca1a499d265fc3fbbf6c7058e2fd
nathan.sh@gmail.com <nathan.sh@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-04-26 05:25:04 +0000

    non linear elasticity material validation

--- Projects/ReducedSpacePlasticity/main_elast.cpp
+++ Projects/ReducedSpacePlasticity/main_elast.cpp
@@ -17,89 +17,92 @@
 //static LoggerPtr logger(Logger::getLogger("pz.reducedspace.data"));
 //#endif
 
-
 int main(int argc, char *argv[])
 {
-    //Propagation criterion
-    REAL Lx = 1000.;
-    REAL Ly = 600.;
-    REAL Lf = 50.;
-    REAL Hf = 1.;
-    REAL Lmax_edge = 10.;
-    REAL Young1 = 3.9E4;
-    REAL Poiss1 = 0.25;
-    REAL Young2 = 3.9E5;
-    REAL Poiss2 = 0.25;
-    REAL Xinterface = 120.;
-    REAL Fx = 0.;
-    REAL Fy = 0.;
-    REAL preStressXX = 0.;
-    REAL preStressXY = 0.;
-    REAL preStressYY = -50.; //(negativo : estado de compressao)
-    
-    int NStripes = 1;
-    REAL Visc = 0.001E-6;
-    
-    REAL SigN = 10.;
-    
-    /**
-     * Lembre-se que a divisao por 2 (1 asa) e por Hf (na secao de 1 asa) eh feita no kernel.
-     * Aqui vai Qinj total mesmo (no poco)!!!
-     */
-    REAL QinjTot  = -0.5;
-
-    REAL Ttot = 20.; /** em segundos */
-    REAL maxDeltaT = 4.; /** em segundos */
-    int nTimes = 1; /** quantidade de divisao do maxDeltaT para definir minDeltaT (minDeltaT = maxDeltaT/nTimes) */
-    
-    REAL Cl = 0.005;
-    REAL Pe = 10.;
-    REAL SigmaConf = -preStressYY;
-    REAL Pref = 60000.;
-    REAL vsp = 0.001;
-    REAL KIc = 25.;
-    REAL Jradius = 0.5;
-    
-    int p = 1;
-    
-    globFractInputData.SetData(Lx, Ly, Lf, Hf, Lmax_edge, Young1, Poiss1, Young2, Poiss2, Xinterface,
-                               Fx, Fy, preStressXX, preStressXY, preStressYY, NStripes, Visc, SigN,
-                               QinjTot, Ttot, maxDeltaT, nTimes, Cl, Pe, SigmaConf, Pref, vsp, KIc, Jradius);
-    ToolsTransient ToolTrans(p);
-    
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "****\n";
-    std::cout << "*****\n";
-    std::cout << "******\n";
-    std::cout << "*******\n";
-    std::cout << "********\n";
-    std::cout << "**********\n";
-    std::cout << "*************\n";
-    std::cout << "*******************\n";
-    std::cout << "*************************\n";
-    std::cout << "*************************************\n";
-    std::cout << "*************************************************\n";
-    std::cout << "*******************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "**********************************************************************\n";
-    std::cout << "Lembre-se de deixar os minT, maxT, actT etc como inteiros!!!\n";
-    std::cout << "*******************************************************\n";
-    std::cout << "*******************************************************\n";
-    std::cout << "*******************************************************\n";
-    std::cout << "*******************************************************\n";
-    std::cout << "*******************************************************\n";
-//    LEIA ACIMA!!!
+  //Propagation criterion
+  REAL Lx = 1000.;
+  REAL Ly = 600.;
+  REAL Lf = 50.;
+  REAL Hf = 1.;
+  REAL Lmax_edge = 10.;
+  REAL Young1 = 3.9E4;
+  REAL Poiss1 = 0.25;
+  REAL Young2 = 3.9E5;
+  REAL Poiss2 = 0.25;
+  REAL Xinterface = 120.;
+  REAL Fx = 0.;
+  REAL Fy = 0.;
+  REAL preStressXX = 0.;
+  REAL preStressXY = 0.;
+  REAL preStressYY = -50.; //(negativo : estado de compressao)
+  
+  int NStripes = 1;
+  REAL Visc = 0.001E-6;
+  
+  REAL SigN = 10.;
+  
+  /**
+   * Lembre-se que a divisao por 2 (1 asa) e por Hf (na secao de 1 asa) eh feita no kernel.
+   * Aqui vai Qinj total mesmo (no poco)!!!
+   */
+  REAL QinjTot  = -0.5;
+  
+  REAL Ttot = 20.; /** em segundos */
+  REAL maxDeltaT = 4.; /** em segundos */
+  int nTimes = 1; /** quantidade de divisao do maxDeltaT para definir minDeltaT (minDeltaT = maxDeltaT/nTimes) */
+  
+  REAL Cl = 0.005;
+  REAL Pe = 10.;
+  REAL SigmaConf = -preStressYY;
+  REAL Pref = 60000.;
+  REAL vsp = 0.001;
+  REAL KIc = 25.;
+  REAL Jradius = 0.5;
+  
+  int p = 1;
+  
+  globFractInputData.SetData(Lx, Ly, Lf, Hf, Lmax_edge, Young1, Poiss1, Young2, Poiss2, Xinterface,
+                             Fx, Fy, preStressXX, preStressXY, preStressYY, NStripes, Visc, SigN,
+                             QinjTot, Ttot, maxDeltaT, nTimes, Cl, Pe, SigmaConf, Pref, vsp, KIc, Jradius);
+  ToolsTransient ToolTrans(p);
+  
+  ToolTrans.ElastNLTest();
+  
+  
+  
+  std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+  std::cout << "****\n";
+  std::cout << "*****\n";
+  std::cout << "******\n";
+  std::cout << "*******\n";
+  std::cout << "********\n"; 
+  std::cout << "**********\n";
+  std::cout << "*************\n";
+  std::cout << "*******************\n";
+  std::cout << "*************************\n";
+  std::cout << "*************************************\n";
+  std::cout << "*************************************************\n";
+  std::cout << "*******************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "**********************************************************************\n";
+  std::cout << "Lembre-se de deixar os minT, maxT, actT etc como inteiros!!!\n";
+  std::cout << "*******************************************************\n";
+  std::cout << "*******************************************************\n";
+  std::cout << "*******************************************************\n";
+  std::cout << "*******************************************************\n";
+  std::cout << "*******************************************************\n";
+  //    LEIA ACIMA!!!
 	
 	ToolTrans.RunPlasticity();
-    
-    return 0;
+  
+  return 0;
 }
 
 
--- Projects/ReducedSpacePlasticity/toolstransienttime.h
+++ Projects/ReducedSpacePlasticity/toolstransienttime.h
@@ -20,67 +20,77 @@ class TPZElastoPlasticAnalysis;
 
 class ToolsTransient
 {
-    public:
-    
-    ToolsTransient();
-    ToolsTransient(int pOrder);
-    
-    ~ToolsTransient();
-    
-    //---------------------------------------------------------------
-    
-    void Run();
-    void RunPlasticity();
-    
-    void InitializeUncoupledMeshesAttributes();
-    TPZCompMesh * ElastCMeshReferenceProcessed();
-    void Mesh2D();
-    TPZCompMesh * CMeshElastic();
-    void SetSigmaNStripeNum(TPZCompMesh * cmeshref, int actStripe);
-    void SolveInitialElasticity(TPZAnalysis &an, TPZCompMesh *Cmesh);
-    TPZCompMeshReferred * CMeshReduced(TPZCompMesh * cmeshref);
-    TPZCompMesh * CMeshPressure();
-    void CMeshMultiphysics();
-    
-    bool SolveSistTransient(TPZAnalysis *an, bool initialElasticKickIsNeeded);
-    
-    void TransferSolutions(TPZCompMesh * lastMPhysicsCMesh, TPZCompMesh * lastElastReferredCMesh);
-    void TransferElasticSolution(TPZCompMesh * cmeshFrom);
-    REAL IntegrateSolution(TPZCompMesh * cmesh, int variable);//0 = meshvec[0] ; 1 = meshvec[1]
-    void TransferLeakoff(TPZCompMesh * oldMphysicsCMesh);
-    
-    //---------------------------------------------------------------
-    
-    void MassMatrix(TPZFMatrix<REAL> & Un);
-    
-    void StiffMatrixLoadVec(TPZAnalysis *an,
-                            TPZAutoPointer< TPZMatrix<REAL> > & matK1, TPZFMatrix<REAL> &fvec);
-    
-    void PostprocessPressure();
-    void PostProcessAcumVolW();
-    void PostProcessVolLeakoff();
-    
-    REAL ComputeKIPlaneStrain();
-    
-    void CheckConv();
-    
-    void SolveInitialElastoPlasticity(TPZElastoPlasticAnalysis &analysis, TPZCompMesh *Cmesh);
-    
-    TPZCompMesh * CMeshElastoPlastic(TPZGeoMesh *gmesh, REAL SigmaN);    
-    
-    //---------------------------------------------------------------
-    
-    int fpOrder;
-    bool fMustStop;
-    
-    TPZNLFluidStructure2d * fCouplingMaterial1;
-    TPZNLFluidStructure2d * fCouplingMaterial2;
-    TPZGeoMesh * fgmesh;
-    
-    /** fmeshvec[0] = Malha computacional elastica do tipo referred */
-    /** fmeshvec[1] = Malha computacional de fluxo 1D */
-    TPZVec<TPZCompMesh *> fmeshvec;
-    TPZCompMesh * fmphysics;
+public:
+  
+  ToolsTransient();
+  ToolsTransient(int pOrder);
+  
+  ~ToolsTransient();
+  
+  //---------------------------------------------------------------
+  
+  void Run();
+  void RunPlasticity();
+  
+  void InitializeUncoupledMeshesAttributes();
+  TPZCompMesh * ElastCMeshReferenceProcessed();
+  void Mesh2D();
+  TPZCompMesh * CMeshElastic();
+  void SetSigmaNStripeNum(TPZCompMesh * cmeshref, int actStripe);
+  void SolveInitialElasticity(TPZAnalysis &an, TPZCompMesh *Cmesh);
+  TPZCompMeshReferred * CMeshReduced(TPZCompMesh * cmeshref);
+  TPZCompMesh * CMeshPressure();
+  void CMeshMultiphysics();
+  
+  bool SolveSistTransient(TPZAnalysis *an, bool initialElasticKickIsNeeded);
+  
+  void TransferSolutions(TPZCompMesh * lastMPhysicsCMesh, TPZCompMesh * lastElastReferredCMesh);
+  void TransferElasticSolution(TPZCompMesh * cmeshFrom);
+  REAL IntegrateSolution(TPZCompMesh * cmesh, int variable);//0 = meshvec[0] ; 1 = meshvec[1]
+  void TransferLeakoff(TPZCompMesh * oldMphysicsCMesh);
+  
+  //---------------------------------------------------------------
+  
+  void MassMatrix(TPZFMatrix<REAL> & Un);
+  
+  void StiffMatrixLoadVec(TPZAnalysis *an,
+                          TPZAutoPointer< TPZMatrix<REAL> > & matK1, TPZFMatrix<REAL> &fvec);
+  
+  void PostprocessPressure();
+  void PostProcessAcumVolW();
+  void PostProcessVolLeakoff();
+  
+  REAL ComputeKIPlaneStrain();
+  
+  void CheckConv();
+  
+  void SolveInitialElastoPlasticity(TPZElastoPlasticAnalysis &analysis, TPZCompMesh *Cmesh);
+  
+  TPZCompMesh * CMeshElastoPlastic(TPZGeoMesh *gmesh, REAL SigmaN);
+  
+  
+  //---------------------------------------------------------------
+  
+  void ElastTest();
+  void ElastNLTest();
+  TPZGeoMesh* CreateGeoMesh();
+  TPZCompMesh* CreateCMesh(TPZGeoMesh *gmesh);
+  void SolveLinearElasticity(TPZCompMesh *cmesh);
+  void SolveNLElasticity(TPZCompMesh *cmesh);
+  
+  //---------------------------------------------------------------
+  
+  int fpOrder;
+  bool fMustStop;
+  
+  TPZNLFluidStructure2d * fCouplingMaterial1;
+  TPZNLFluidStructure2d * fCouplingMaterial2;
+  TPZGeoMesh * fgmesh;
+  
+  /** fmeshvec[0] = Malha computacional elastica do tipo referred */
+  /** fmeshvec[1] = Malha computacional de fluxo 1D */
+  TPZVec<TPZCompMesh *> fmeshvec;
+  TPZCompMesh * fmphysics;
 };
 
 
@@ -88,68 +98,68 @@ class ToolsTransient
 template<class TVar>
 class TElastSolFunction : public TPZFunction<TVar>
 {
-    public:
-    
-    /**
-     * Class constructor
-     */
-    TElastSolFunction();
-    
-    TElastSolFunction(TPZCompMesh * cmesh);
-    
-    /**
-     * Class destructor
-     */
-    ~TElastSolFunction();
-    
-    virtual void Execute(const TPZVec<REAL> &x, TPZVec<TVar> &f);
-    virtual void Execute(const TPZVec<REAL> &x, TPZVec<TVar> &f, TPZFMatrix<TVar> &df);
-    
-    /** Returns number of functions.
-     */
-    virtual int NFunctions();
-    
-    /** Polynomial order of this function. In case of non-polynomial
-     * function it can be a reasonable approximation order.
-     */
-    virtual int PolynomialOrder();
-    
-    TPZCompMesh * fcmesh;
-    long fIniElIndex;
-    
+public:
+  
+  /**
+   * Class constructor
+   */
+  TElastSolFunction();
+  
+  TElastSolFunction(TPZCompMesh * cmesh);
+  
+  /**
+   * Class destructor
+   */
+  ~TElastSolFunction();
+  
+  virtual void Execute(const TPZVec<REAL> &x, TPZVec<TVar> &f);
+  virtual void Execute(const TPZVec<REAL> &x, TPZVec<TVar> &f, TPZFMatrix<TVar> &df);
+  
+  /** Returns number of functions.
+   */
+  virtual int NFunctions();
+  
+  /** Polynomial order of this function. In case of non-polynomial
+   * function it can be a reasonable approximation order.
+   */
+  virtual int PolynomialOrder();
+  
+  TPZCompMesh * fcmesh;
+  long fIniElIndex;
+  
 };
 
 template<class TVar>
 class TLeakoffFunction : public TPZFunction<TVar>
 {
 public:
-    
-    /**
-     * Class constructor
-     */
-    TLeakoffFunction();
-    
-    TLeakoffFunction(TPZCompMesh * cmesh);
-    
-    /**
-     * Class destructor
-     */
-    ~TLeakoffFunction();
-    
-    virtual void Execute(const TPZVec<REAL> &x, TPZVec<TVar> &f);
-    virtual void Execute(const TPZVec<REAL> &x, TPZVec<TVar> &f, TPZFMatrix<TVar> &df);
-    
-    /** Returns number of functions.
-     */
-    virtual int NFunctions();
-    
-    /** Polynomial order of this function. In case of non-polynomial
-     * function it can be a reasonable approximation order.
-     */
-    virtual int PolynomialOrder();
-    
-    TPZCompMesh * fcmesh;
-    long fIniElIndex;
+  
+  /**
+   * Class constructor
+   */
+  TLeakoffFunction();
+  
+  TLeakoffFunction(TPZCompMesh * cmesh);
+  
+  /**
+   * Class destructor
+   */
+  ~TLeakoffFunction();
+  
+  virtual void Execute(const TPZVec<REAL> &x, TPZVec<TVar> &f);
+  virtual void Execute(const TPZVec<REAL> &x, TPZVec<TVar> &f, TPZFMatrix<TVar> &df);
+  
+  /** Returns number of functions.
+   */
+  virtual int NFunctions();
+  
+  /** Polynomial order of this function. In case of non-polynomial
+   * function it can be a reasonable approximation order.
+   */
+  virtual int PolynomialOrder();
+  
+  TPZCompMesh * fcmesh;
+  long fIniElIndex;
 };
 
 #endif
