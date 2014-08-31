b4b2c8449cda988ec4a3ded81c4306820840b0f8
nathan.sh@gmail.com <nathan.sh@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-08-31 23:44:56 +0000

    ContributeBC and some cleaning

--- Projects/Frac1DHdiv/TPZMatfrac1dhdiv.cpp
+++ Projects/Frac1DHdiv/TPZMatfrac1dhdiv.cpp
@@ -1,11 +1,3 @@
-//
-//  pzmultiphase.h
-//  PZ
-//
-//  Created by Omar Duran on 19/08/2013.
-//  Copyright (c) 2012 LabMec-Unicamp. All rights reserved.
-//
-
 #include "pzlog.h"
 #include "TPZMatfrac1dhdiv.h"
 #include "pzbndcond.h"
@@ -22,26 +14,20 @@ static LoggerPtr logger(Logger::getLogger("pz.multiphase"));
 static LoggerPtr logdata(Logger::getLogger("pz.material.multiphase.data"));
 #endif
 
-TPZMatfrac1dhdiv::TPZMatfrac1dhdiv(): TPZDiscontinuousGalerkin()
+TPZMatfrac1dhdiv::TPZMatfrac1dhdiv(): TPZMaterial()
 {
-  fDim = -1;
+  fDim = 1;
   fTheta = 0.0;
   fDeltaT = 0.0;
   fmu = 0.;
 }
 
-TPZMatfrac1dhdiv::TPZMatfrac1dhdiv(int matid, int dim, REAL viscosity): TPZDiscontinuousGalerkin(matid)
+TPZMatfrac1dhdiv::TPZMatfrac1dhdiv(int matid): TPZMaterial(matid)
 {
-  // Two-dimensional analysis
-  
-  if(dim != 1){
-    DebugStop();
-  }
-  
-  fDim = dim;
+  fDim = 1;
   fTheta = 0.0;
   fDeltaT = 0.0;
-  fmu = viscosity;
+  fmu = 0.;
 }
 
 
@@ -92,7 +78,6 @@ void TPZMatfrac1dhdiv::Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight,
   const REAL Q = datavec[0].sol[0][0];
   const REAL dQ = datavec[0].dsol[0](0,0);
   const REAL p = datavec[1].sol[0][0];
-  const REAL dp = datavec[1].dsol[0](0,0);
   
   // Abertura
   const REAL w = 1.; // sera uma funcao de p
@@ -142,201 +127,89 @@ void TPZMatfrac1dhdiv::Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight,
       ef(FirstP+ip,0) += weight * res;
     }
   }
+  
 }
 
-
-void TPZMatfrac1dhdiv::ContributeInterface(TPZMaterialData &data, TPZVec<TPZMaterialData> &dataleft, TPZVec<TPZMaterialData> &dataright, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef)
+void TPZMatfrac1dhdiv::ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef, TPZBndCond &bc)
 {
-  DebugStop(); // should not be called because there are no interfaces
-  
-  /*
-  if(gState == ELastState)
-  {
+
+  if(gState == ELastState){
     return;
   }
   
-  TPZFMatrix<REAL> &phiQL = dataleft[0].phi;
-  TPZFMatrix<REAL> &phiQR = dataright[0].phi;
-  TPZFMatrix<REAL> &phiPL = dataleft[1].phi;
-  TPZFMatrix<REAL> &phiPR = dataright[1].phi;
+#ifdef DEBUG
+  int nref =  datavec.size();
+  if (nref != 2 )
+  {
+    std::cout << " Erro. The size of the datavec is different from 2 \n";
+    DebugStop();
+  }
+#endif
+  
+  //BigNumber
+  const REAL BIGNUMBER = TPZMaterial::gBigNumber;
+  
+  // Getting shape functions
+  TPZFMatrix<REAL>  &phiQ = datavec[0].phi;
+  //TPZFMatrix<REAL>  &phiP = datavec[1].phi;
   
   // number of test functions for each state variable
-  int phrQL, phrQR, phrPL, phrPR;
-  phrQL = phiQL.Rows();
-  phrPL = phiPL.Rows();
-  phrQR = phiQR.Rows();
-  phrPR = phiPR.Rows();
-  
-  TPZManVector<REAL,3> &normal = data.normal;
-  
-  // Derivadas na ordem = {QLeft,PLeft,DPDXLeft,QRight,PRight,DPDXRight}
-  MFad sol_qL(dataleft[0].sol[0][0],0);
-  MFad sol_pL(dataleft[1].sol[0][0],1);
-  MFad dsol_pL(dataleft[1].dsol[0](0,0),2);
-  MFad sol_qR(dataright[0].sol[0][0],3);
-  MFad sol_pR(dataright[1].sol[0][0],4);
-  MFad dsol_pR(dataright[1].dsol[0](0,0),5);
+  const int phrQ = phiQ.Rows();
   
   // blocks
-  int FirstQL  = 0;
-  int FirstPL  = phrQL+FirstQL;
-  
-  int FirstQR  = 0;
-  int FirstPR  = phrQR+FirstQR;
-  
-  int ileftblock  = FirstPL + phrPL;
-  int jrightblock = FirstPR + phrPR;
-  
-  //  Contribution of contour integrals for Jacobian matrix and Residual vector
-  
-  for (int iql = 0; iql < phrQL; iql++) {
-    MFad res01 = (sol_pR-sol_pL) * phiQL(iql,0);
-    ef(FirstQL+iql,0) += weight * (res01.val());
-    for (int jpl = 0; jpl < phrPL; jpl++)
-    {
-      ek(FirstQL+iql,FirstPL+jpl) += weight * (res01.dx(1) * phiPL(jpl,0));
-    }
-    for (int jpr = 0; jpr < phrPR; jpr++)
-    {
-      ek(FirstQL+iql,jrightblock + FirstPR+jpr) += weight * (res01.dx(3) * phiPR(jpr,0));
-    }
-  }
-  
-  
-  for (int jpl = 0; jpl < phrPL; jpl++) {
-    MFad res10 = (phiPL(jpl,0) * sol_qL);
-    for (int iql = 0; iql < phrQL; iql++) {
-      ek(jpl,iql) = 0.0; //Complete the transpose term;
-    }
-  }
-  
-  
-  
-  // End of contribution of contour integrals for Jacobian matrix and Residual vector
-  */
-  
-  
-  
-}
+  const int FirstQ  = 0;
 
-void TPZMatfrac1dhdiv::ContributeBCInterface(TPZMaterialData &data, TPZVec<TPZMaterialData> &dataleft, REAL weight, TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc)
-{
+  // Solutions and derivate of solutions
+  const REAL Q = datavec[0].sol[0][0];
+  //const REAL p = datavec[1].sol[0][0];
   
-  DebugStop(); // should not be called because there are no interfaces
+  // Abertura
+  const REAL w = 1.; // sera uma funcao de p
+  //const REAL dwdp = 0.;
   
-  /*
+  // Valores impostos
+  REAL v2 = bc.Val2()(0,0);
   
-  // n time step
-  if(gState == ELastState)
-  {
-    return;
-  }
-  
-  int nref =  dataleft.size();
-  if (nref != 2) {
-    std::cout << " Error:: datavec size must to be equal to 2 \n" << std::endl;
-    DebugStop();
-  }
-  if (bc.Val2().Rows() != 2)
-  {
-    std::cout << " Error:: This material need boundary conditions for qn, p (fluid pressure).\n" << std::endl;
-    std::cout << " give me one matrix with this form Val2(2,1).\n" << std::endl;
-    DebugStop();
-  }
-  
-  if (bc.Val1().Rows() != 2)
-  {
-    std::cout << " Error:: This material need boundary conditions for qn, p (fluid pressure) .\n" << std::endl;
-    DebugStop();
-  }
-  
-  
-  // n+1 time step
-  if(gState == ECurrentState)
-  {
-    
-    
-    
-  }
-  
-  
-  switch (bc.Type()) {
-    case 0 :    // Flux bc: qn = (+ or -)g (Dirichlet)
+	switch (bc.Type()) {
+		case 0: // Flux strong imposition (v2 = imposed flux)
+		{
+      const REAL difQ = Q - v2;
+			for(int iq = 0 ; iq < phrQ ; iq++) {
+				ef(FirstQ+iq,0) += BIGNUMBER * difQ * phiQ(iq,0) * weight; // forced v2 flux
+				for (int jq = 0 ; jq < phrQ ; jq++)
+        {
+					ek(FirstQ+iq,FirstQ+jq) += BIGNUMBER * phiQ(iq,0) * phiQ(jq,0) * weight;
+				}
+			}
+		}
+    break;
+    case 1: // Pressure weak imposition (v2 = imposed pressure)
     {
-      ApplyQnD(data, dataleft, weight, ek, ef, bc);
+      for (int iq = 0; iq < phrQ; iq++) {
+        ef(FirstQ+iq,0) += w*w*w/(12.*fmu) * v2 * phiQ(iq,0);
+      }
+      DebugStop(); // Tenho derivada aqui???
     }
-    case 1 :    // Pressure bc: PN = (+)k (Neumann)
-    {
-      ApplyPN(data, dataleft, weight, ek, ef, bc);
-    }
-      break;
+    break;
       
-    default: std::cout << "This BC doesn't exist." << std::endl;
-    {
-      
-      DebugStop();
-    }
-      break;
   }
-  
-  
-  */
-  
-}
-
-void TPZMatfrac1dhdiv::ApplyQnD(TPZMaterialData &data, TPZVec<TPZMaterialData> &dataleft, REAL weight, TPZFMatrix<> &ek,TPZFMatrix<> &ef,TPZBndCond &bc)
-{
-  
-  TPZFMatrix<REAL> &phiQL     = dataleft[1].phi;
-  TPZFMatrix<REAL> &phiPL     = dataleft[2].phi;
-  
-  int QRowsleft = dataleft[0].fVecShapeIndex.NElements();
-  int PRowsleft = phiPL.Rows();
-  
-  int iRightInterfaceBlock = QRowsleft + PRowsleft;
-  int jRightInterfaceBlock = QRowsleft + PRowsleft;
-  
-  int FirstQL = 0;
-  int FirstPL = QRowsleft + FirstQL;
-  
-  TPZManVector<REAL,3> &normal = data.normal;
-  REAL n1 = normal[0];
-  REAL n2 = normal[1];
-  
-}
-
-
-void TPZMatfrac1dhdiv::ApplyPN        (TPZMaterialData &data, TPZVec<TPZMaterialData> &dataleft, REAL weight, TPZFMatrix<> &ek,TPZFMatrix<> &ef,TPZBndCond &bc)
-{
-  
-  TPZFMatrix<REAL> &phiQL     = dataleft[0].phi;
-  TPZFMatrix<REAL> &phiPL     = dataleft[1].phi;
-  
-  int QRowsleft = dataleft[0].fVecShapeIndex.NElements();
-  int PRowsleft = phiPL.Rows();
-  
-  int iRightInterfaceBlock = QRowsleft + PRowsleft;
-  int jRightInterfaceBlock = QRowsleft + PRowsleft;
-  
-  int FirstQL = 0;
-  int FirstPL = QRowsleft + FirstQL;
-  
-  TPZManVector<REAL,3> &normal = data.normal;
-  REAL n1 = normal[0];
-  REAL n2 = normal[1];
   
 }
 
 /** Returns the variable index associated with the name */
 int TPZMatfrac1dhdiv::VariableIndex(const std::string &name){
-  if(!strcmp("BulkVelocity",name.c_str()))        return  1;
-  if(!strcmp("Pressure",name.c_str()))    return  2;
+  if(!strcmp("Pressure",name.c_str())) return  0;
+  if(!strcmp("State",name.c_str())) return  0;
+  if(!strcmp("Flow",name.c_str())) return  1;
+  if(!strcmp("Flux",name.c_str())) return  1;
+  if(!strcmp("Opening",name.c_str())) return  2;
   
   return TPZMaterial::VariableIndex(name);
 }
 
 int TPZMatfrac1dhdiv::NSolutionVariables(int var){
-  if(var == 1) return 2;
+  if(var == 0) return 1;
+  if(var == 1) return 1;
   if(var == 2) return 1;
   
   return TPZMaterial::NSolutionVariables(var);
@@ -344,20 +217,23 @@ int TPZMatfrac1dhdiv::NSolutionVariables(int var){
 
 void TPZMatfrac1dhdiv::Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<STATE> &Solout){
   
-  Solout.Resize( this->NSolutionVariables(var));
-  TPZVec<STATE> SolQ, SolP;
-  SolQ = datavec[0].sol[0];
-  SolP = datavec[1].sol[0];
+  Solout.Resize(this->NSolutionVariables(var));
+  const REAL SolQ = datavec[0].sol[0][0];
+  const REAL SolP = datavec[1].sol[0][0];
   
-  if(var == 1){ //function (state variable Q)
-    Solout[0] = SolQ[0];
-    Solout[1] = SolQ[1];
+  if(var == 0){ //function (state variable Q)
+    Solout[0] = SolQ;
     return;
   }
   
-  if(var == 2){
-    Solout[0] = SolP[0];//function (state variable p)
+  if(var == 1){
+    Solout[0] = SolP;//function (state variable p)
     return;
+  }
+  
+  if (var == 2) {
+    const REAL w = 1.; // Trocar pela formula do w
+    Solout[0] = w;
   }
 }
 
@@ -369,9 +245,6 @@ void TPZMatfrac1dhdiv::FillDataRequirements(TPZVec<TPZMaterialData > &datavec)
   {
     datavec[i].SetAllRequirements(false);
     datavec[i].fNeedsSol = true;
-    //datavec[i].fNeedsNeighborSol = true;
-    //datavec[i].fNeedsNeighborCenter = false;
-    //datavec[i].fNeedsNormal = true;
   }
   
 }
@@ -382,7 +255,5 @@ void TPZMatfrac1dhdiv::FillBoundaryConditionDataRequirement(int type,TPZVec<TPZM
   {
     datavec[i].SetAllRequirements(false);
     datavec[i].fNeedsSol = true;
-    //datavec[i].fNeedsNormal = true;
-    //datavec[i].fNeedsNeighborSol = true;
   }
 }
--- Projects/Frac1DHdiv/TPZMatfrac1dhdiv.h
+++ Projects/Frac1DHdiv/TPZMatfrac1dhdiv.h
@@ -2,15 +2,14 @@
 //  pzmultiphase.h
 //  PZ
 //
-//  Created by Omar Duran on 19/08/2013.
-//  Copyright (c) 2012 LabMec-Unicamp. All rights reserved.
+//  Created by Omar Duran and Nathan Shauer on 19/08/2014.
+//  Copyright (c) 2014 LabMec-Unicamp. All rights reserved.
 //
 
 #ifndef PZ_pzmultiphase_h
 #define PZ_pzmultiphase_h
 
 #include "pzmaterial.h"
-#include "pzdiscgal.h"
 #ifdef _AUTODIFF
 #include "fad.h"
 #endif
@@ -20,126 +19,120 @@
 
 /**
  * @ingroup material
- * @author Omar Duran
+ * @author Omar Duran and Nathan Shauer
  * @since 19/08/2013
- * @brief Material to solve a 2d multiphase transport problem by multiphysics simulation
- * @brief Here is used L, Hdiv ... spaces and first order upwind scheme
+ * @brief Material to solve a 1d mixed formulation for fracture opening
+ * @brief Here is used H1 for flux and L2 for pressure
  */
-
-
-class TPZMatfrac1dhdiv : public TPZDiscontinuousGalerkin {
-    
-protected:
-    
-    /** @brief Problem dimension */
-    int fDim;
-    
-    /** @brief State: Stiffness or Mass Matrix Calculations */
-    enum EState { ELastState = 0, ECurrentState = 1 };
-    EState gState;
+class TPZMatfrac1dhdiv : public TPZMaterial {
   
-    
+protected:
+  
+  /** @brief Problem dimension */
+  int fDim;
+  
+  /** @brief State: Stiffness or Mass Matrix Calculations */
+  enum EState { ELastState = 0, ECurrentState = 1 };
+  EState gState;
+  
+  
 public:
-    
-    TPZMatfrac1dhdiv();
-    
-    TPZMatfrac1dhdiv(int matid, int dim, REAL viscosity);
-    
-    virtual ~TPZMatfrac1dhdiv();
-    
-    /** @brief copy constructor */
-    TPZMatfrac1dhdiv(const TPZMatfrac1dhdiv &copy);
-    
-    TPZMatfrac1dhdiv &operator=(const TPZMatfrac1dhdiv &copy);
-    
-    virtual void Print(std::ostream & out);
-    
-    virtual std::string Name() { return "TPZMatfrac1dhdiv"; }
-    
-    virtual int Dimension() const;
-    
-    virtual int NStateVariables();
-    
-    virtual void Contribute(TPZMaterialData &data, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef)
-    {
-        DebugStop();
-    }
-    
-    virtual void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef);
-    
-    virtual void ContributeBC(TPZMaterialData &data, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef, TPZBndCond &bc)
-    {
-        DebugStop(); // Should never be called for this material
-    }
-    
-    virtual void ContributeInterface(TPZMaterialData &data, TPZVec<TPZMaterialData> &dataleft, TPZVec<TPZMaterialData> &dataright, REAL weight, TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef);
-    
-    virtual void ContributeInterface(TPZMaterialData &data, TPZMaterialData &dataleft, REAL weight, TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc)
-    {
-        DebugStop();
-    }
-    
-    virtual void ContributeBCInterface(TPZMaterialData &data, TPZVec<TPZMaterialData> &dataleftvec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef, TPZBndCond &bc);
-    
-	virtual void ContributeBCInterface(TPZMaterialData &data, TPZMaterialData &dataleft, REAL weight, TPZFMatrix<STATE> &ek,TPZFMatrix<STATE> &ef,TPZBndCond &bc)
-    {
-        DebugStop();
-    }
-    
-    
-    virtual void FillDataRequirements(TPZVec<TPZMaterialData > &datavec);
-    
-    virtual void FillBoundaryConditionDataRequirement(int type,TPZVec<TPZMaterialData > &datavec);
-    
-    
-    virtual int VariableIndex(const std::string &name);
-    
-    virtual int NSolutionVariables(int var);
-    
-    virtual void Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<STATE> &Solout);
-    
-    
+  
+  /** @brief Default Constructor */
+  TPZMatfrac1dhdiv();
+  
+  /** @brief Constructor with matid */
+  TPZMatfrac1dhdiv(int matid);
+
+  /** @brief Destructor */  
+  virtual ~TPZMatfrac1dhdiv();
+  
+  /** @brief copy constructor */
+  TPZMatfrac1dhdiv(const TPZMatfrac1dhdiv &copy);
+
+  /** @brief operator equal */
+  TPZMatfrac1dhdiv &operator=(const TPZMatfrac1dhdiv &copy);
+  
+  /** @brief Print Method */
+  virtual void Print(std::ostream & out);
+  
+  /** @brief Name of the material */
+  virtual std::string Name() { return "TPZMatfrac1dhdiv"; }
+  
+  /** @brief Returns the integrable dimension */
+  virtual int Dimension() const;
+  
+  /** @brief Return the number of state variables */
+  virtual int NStateVariables();
+  
+  /** @brief Contribute method for not multiphysics materials */
+  virtual void Contribute(TPZMaterialData &data, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef)
+  {
+    DebugStop();
+  }
+  
+  /** @brief Contribute method beeing used */
+  virtual void Contribute(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef);
+  
+  /** @brief ContributeBC method for not multiphysics materials */
+  virtual void ContributeBC(TPZMaterialData &data, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef, TPZBndCond &bc)
+  {
+    DebugStop();
+  }
+
+  /** @brief ContributeBC method beeing used */
+  virtual void ContributeBC(TPZVec<TPZMaterialData> &datavec, REAL weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef, TPZBndCond &bc);
+  
+  /** @brief Fill material data parameter with necessary requirements for the Contribute method*/
+  virtual void FillDataRequirements(TPZVec<TPZMaterialData > &datavec);
+
+  /** @brief Fill material data parameter with necessary requirements for the ContributeBC method*/
+  virtual void FillBoundaryConditionDataRequirement(int type,TPZVec<TPZMaterialData > &datavec);
+  
+  /** @brief Returns the variable index associated with the name */
+  virtual int VariableIndex(const std::string &name);
+  
+  /** @brief Returns the number of variables associated with the variable indexed by var */
+  virtual int NSolutionVariables(int var);
+  
+  /** @brief Calculates a solution given datavec*/
+  virtual void Solution(TPZVec<TPZMaterialData> &datavec, int var, TPZVec<STATE> &Solout);
+  
+  
 private:
-    
-    /** @brief Fluid Viscosity - Pa.s */
-    REAL fmu;
-    
-    /** @brief Simulation time step */
-    REAL fDeltaT;
-    
-    /** @brief Simulation current time */
-    REAL fTime;
-    
-    /** @brief Parameter representing temporal scheme for transport equation */
-    REAL fTheta;
-    
+  
+  /** @brief Fluid Viscosity - Pa.s */
+  REAL fmu;
+  
+  /** @brief Simulation time step */
+  REAL fDeltaT;
+  
+  /** @brief Simulation current time */
+  REAL fTime;
+  
+  /** @brief Parameter representing temporal scheme for transport equation */
+  REAL fTheta;
+  
 public:
-    
-    /** @brief Set fluid viscosity. */
-    void SetViscosity(REAL mu){this->fmu = mu;}
-    
-    /** @brief Defines simulation time step. */
-    void SetTimeStep(REAL timestep){ this->fDeltaT = timestep;}
-    
-    /** @brief Defines simulation time step. */
-    void SetTime(REAL time){ this->fTime = time;}
-    
-    /** @brief Defines stemporal scheme. */
-    void SetTScheme(REAL timetheta){ this->fTheta = timetheta;}
-
-    /** @brief Set evaluating step n */
-    void SetLastState(){ gState = ELastState;}
-
-    /** @brief Set evaluating step n + 1 */
-    void SetCurrentState(){ gState = ECurrentState;}
-    
-    /** @brief Apply dirichlet condition on flux strongly */
-    void ApplyQnD(TPZMaterialData &data, TPZVec<TPZMaterialData> &dataleft, REAL weight, TPZFMatrix<> &ek,TPZFMatrix<> &ef,TPZBndCond &bc);
-    
-    /** @brief Apply neumann condition on pressure weakly */
-    void ApplyPN(TPZMaterialData &data, TPZVec<TPZMaterialData> &dataleft, REAL weight, TPZFMatrix<> &ek,TPZFMatrix<> &ef,TPZBndCond &bc);
-    
-    
+  
+  /** @brief Set fluid viscosity. */
+  void SetViscosity(REAL mu){this->fmu = mu;}
+  
+  /** @brief Defines simulation time step. */
+  void SetTimeStep(REAL timestep){ this->fDeltaT = timestep;}
+  
+  /** @brief Defines simulation time step. */
+  void SetTime(REAL time){ this->fTime = time;}
+  
+  /** @brief Defines stemporal scheme. */
+  void SetTScheme(REAL timetheta){ this->fTheta = timetheta;}
+  
+  /** @brief Set evaluating step n */
+  void SetLastState(){ gState = ELastState;}
+  
+  /** @brief Set evaluating step n + 1 */
+  void SetCurrentState(){ gState = ECurrentState;}
+  
 };
 
 #endif
