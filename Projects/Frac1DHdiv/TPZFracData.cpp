8cb2352e1506fbb33e09ef9292fc725cd006be36
nathan.sh@gmail.com <nathan.sh@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-09-15 21:01:34 +0000

    Leak-off methods implemented. Next step - Run tests

--- Projects/Frac1DHdiv/TPZFracData.cpp
+++ Projects/Frac1DHdiv/TPZFracData.cpp
@@ -22,7 +22,7 @@ TPZFracData::~TPZFracData()
 }
 
 
-void TPZFracData::Porosity(REAL p, REAL &porosity, REAL &dPorosityDp)
+void TPZFracData::Porosity(REAL p, REAL &porosity, REAL &dPorosityDp) const
 {
   const REAL Rockcomp = (1.0e-8);
   const REAL pref = (10.0e6);
@@ -30,7 +30,7 @@ void TPZFracData::Porosity(REAL p, REAL &porosity, REAL &dPorosityDp)
   dPorosityDp = fPhi*Rockcomp*exp(Rockcomp*(p-pref));
 }
 
-void TPZFracData::Density(REAL p, REAL &RhoFluid, REAL &dRhoDp)
+void TPZFracData::Density(REAL p, REAL &RhoFluid, REAL &dRhoDp) const
 {
   const REAL Oilcomp = (1.0e-8);
   const REAL pref = (10.0e6);
@@ -39,35 +39,37 @@ void TPZFracData::Density(REAL p, REAL &RhoFluid, REAL &dRhoDp)
 }
 
 
-void TPZFracData::Viscosity(REAL p, REAL &FluidViscosity, REAL &dFluidViscosityDp)
+void TPZFracData::Viscosity(REAL p, REAL &FluidViscosity, REAL &dFluidViscosityDp) const
 {
   // Constant for a while.
   FluidViscosity = fmu;
   dFluidViscosityDp = 0;
 }
 
-TPZFMatrix<STATE> TPZFracData::K(){
+TPZFMatrix<STATE> TPZFracData::K() const{
   return fKab;
 }
 
-TPZFMatrix<STATE> TPZFracData::Kinv(){
+TPZFMatrix<STATE> TPZFracData::Kinv() const{
   TPZFMatrix<STATE> Kinverse(2,2,0.0);
-  REAL Constant = (-1.0*fKab(0,1)*fKab(1,0)+fKab(0,0)*fKab(1,1));
+  
+  TPZFMatrix<STATE> Kab = fKab;
+  REAL Constant = (-1.0*Kab(0,1)*Kab(1,0)+Kab(0,0)*Kab(1,1));
   
   if (fKab.Rows() != 2) {
     std::cout << "Absolute permeability must to be 2 Rows 2 Cols Matrix" << std::endl;
     DebugStop();
   }
   
-  Kinverse(0,0) =     1.0*fKab(1,1)/(Constant);
-  Kinverse(0,1) = -   1.0*fKab(0,1)/(Constant);
-  Kinverse(1,0) = -   1.0*fKab(1,0)/(Constant);
-  Kinverse(1,1) =     1.0*fKab(0,0)/(Constant);
+  Kinverse(0,0) =     1.0*Kab(1,1)/(Constant);
+  Kinverse(0,1) = -   1.0*Kab(0,1)/(Constant);
+  Kinverse(1,0) = -   1.0*Kab(1,0)/(Constant);
+  Kinverse(1,1) =     1.0*Kab(0,0)/(Constant);
   
   return Kinverse;
 }
 
-REAL TPZFracData::G()
+REAL TPZFracData::G() const
 {
 #ifdef DEBUG
   if (fE < 0. || fnu < 0.) {
@@ -96,4 +98,96 @@ void TPZFracData::SetPostProcessFileName(std::string &postProcessFileName)
 std::string TPZFracData::PostProcessFileName()
 {
   return fpostProcessFileName;
+}
+
+REAL TPZFracData::VlFtau(REAL pfrac, REAL tau) const
+{
+  const REAL Cl = this->Cl();
+  REAL sigmaConf = this->SigmaConf();
+  REAL Pe = this->Pe();
+  REAL Pref = this->Pref();
+  REAL vsp = this->Vsp();
+  
+  REAL Pef = pfrac + sigmaConf;
+  REAL Pcalc = (Pef - Pe)/Pref;
+  if(Pcalc < 0.)
+  {
+    Pcalc = 0.;
+  }
+  
+  REAL Clcorr = Cl * sqrt(Pcalc);
+  REAL Vl = 2. * Clcorr * sqrt(tau) + vsp;
+  
+  return Vl;
+}
+
+REAL TPZFracData::FictitiousTime(REAL VlAcum, REAL pfrac) const
+{
+  REAL Cl = this->Cl();
+  REAL sigmaConf = this->SigmaConf();
+  REAL Pe = this->Pe();
+  REAL Pref = this->Pref();
+  REAL vsp = this->Vsp();
+  
+  REAL tStar = 0.;
+  if(VlAcum > vsp)
+  {
+    REAL Pef = pfrac + sigmaConf;
+    REAL Pcalc = (Pef - Pe)/Pref;
+    if(Pcalc < 0.)
+    {
+      Pcalc = 0.;
+    }
+    REAL Clcorr = Cl * sqrt(Pcalc);
+    tStar = (VlAcum - vsp)*(VlAcum - vsp)/( (2. * Clcorr) * (2. * Clcorr) );
+  }
+  
+  return tStar;
+}
+
+REAL TPZFracData::QlFVl(REAL VlAcum, REAL pfrac) const
+{
+  REAL deltaT = this->TimeStep();
+  
+  REAL tStar = FictitiousTime(VlAcum, pfrac);
+  REAL Vlnext = VlFtau(pfrac, tStar + deltaT);
+  REAL Ql = (Vlnext - VlAcum)/deltaT;
+  
+  return Ql;
+  
+}
+
+// AQUINATHAN - depois trocar essa diferencas finitas da derivada do leak off pela analitica
+REAL TPZFracData::dQlFVl(REAL VlAcum, REAL pfrac) const
+{
+  
+  REAL deltaPfrac = fabs(pfrac/10000.);
+  if(deltaPfrac < 1.E-10)
+  {
+    deltaPfrac = 1.E-10;
+  }
+  else if(deltaPfrac > 1.E-3)
+  {
+    deltaPfrac = 1.E-3;
+  }
+  
+  REAL deltaT = this->TimeStep();
+  /////////////////////////////////////////////////Ql maior
+  REAL pfracUP = pfrac + deltaPfrac;
+  REAL tStar1 = FictitiousTime(VlAcum, pfracUP);
+  REAL Vlnext1 = VlFtau(pfracUP, tStar1 + deltaT);
+  REAL Ql1 = (Vlnext1 - VlAcum )/deltaT;
+  //...
+  
+  /////////////////////////////////////////////////Ql menor
+  REAL pfracDOWN = pfrac - deltaPfrac;
+  REAL tStar0 = FictitiousTime(VlAcum, pfracDOWN);
+  REAL Vlnext0 = VlFtau(pfracDOWN, tStar0 + deltaT);
+  REAL Ql0 = (Vlnext0 - VlAcum)/deltaT;
+  //...
+  
+  REAL dQldpfrac = (Ql1-Ql0)/(2.*deltaPfrac);
+  
+  return dQldpfrac;
+
 }
--- Projects/Frac1DHdiv/TPZFracData.h
+++ Projects/Frac1DHdiv/TPZFracData.h
@@ -113,7 +113,7 @@ public:
   void SetViscosity(REAL mu){this->fmu = mu;}
   
   /** @brief Returns fluid viscosity. */
-  REAL Viscosity(){return this->fmu;}
+  REAL Viscosity() const {return this->fmu;}
   
   // Nathan: Our indentation setting are different
   
@@ -121,104 +121,104 @@ public:
   void SetDensity(REAL rho){this->fRho = rho;}
   
   /** @brief Returns fluid density. */
-  REAL Density(){return this->fRho;}
+  REAL Density() const {return this->fRho;}
   
   /** @brief Set absolute permeability. */
   void SetK(TPZFMatrix<STATE> &Kab){ this->fKab = Kab;}
   
   /** @brief Returns absolute permeability. */
-  TPZFMatrix<STATE> K();
+  TPZFMatrix<STATE> K() const;
   
   /** @brief Returns absolute permeability inverse. */
-  TPZFMatrix<STATE>  Kinv();
+  TPZFMatrix<STATE>  Kinv() const;
   
   /** @brief Set rock porosity. */
   void SetPorosity(REAL phi){this->fPhi = phi;}
   
   /** @brief Returns rock porosity. */
-  REAL Porosity(){return this->fPhi;}
+  REAL Porosity() const {return this->fPhi;}
   
   /**
    * @brief \f$ Rock porosity function. \f$ Phi = Phi( p ) \f$
    * @param p pressure
    */
-  void Porosity(REAL p, REAL &porosity, REAL &dPorosityDp);
+  void Porosity(REAL p, REAL &porosity, REAL &dPorosityDp) const;
   
   /**
    * @brief \f$ Fluid density function. \f$ RhoFluid = RhoFluid( p ) \f$
    * @param p pressure
    */
-  void Density(REAL p, REAL &RhoFluid, REAL &dRhoDp);
+  void Density(REAL p, REAL &RhoFluid, REAL &dRhoDp) const;
   
   /**
    * @brief Fluid viscosity function. \f$ FluidViscosity = Visc( p ) \f$
    * @param p pressure
    */
-  void Viscosity(REAL p, REAL &FluidViscosity, REAL &dFluidViscosityDp);
+  void Viscosity(REAL p, REAL &FluidViscosity, REAL &dFluidViscosityDp) const;
   
   
   /** @brief Set fluid viscosity. */
   void SetTotalTime(REAL Ttot){this->fTtot = Ttot;}
   
   /** @brief Returns fluid viscosity. */
-  REAL TotalTime(){return this->fTtot;}
+  REAL TotalTime() const {return this->fTtot;}
   
   /** @brief Defines simulation time step. */
   void SetTimeStep(REAL timestep){ this->fDeltaT = timestep;}
   
   /** @brief Returns simulation time step. */
-  REAL TimeStep(){return this->fDeltaT;}
+  REAL TimeStep() const {return this->fDeltaT;}
   
   /** @brief Defines simulation time */
   void SetTime(REAL time){ this->fTime = time;}
   
   /** @brief Returns simulation time */
-  REAL Time(){return this->fTime;}
+  REAL Time() const {return this->fTime;}
   
   /** @brief Defines simulation temporal scheme  */
   void SetTheta(REAL theta){ this->fTheta = theta;}
   
   /** @brief Returns simulation temporal scheme  */
-  REAL Theta(){return this->fTheta;}
+  REAL Theta() const {return this->fTheta;}
   
   /** @brief Defines simulation fracture length */
   void SetLfrac(REAL Lfrac){ this->fLfrac = Lfrac;}
   
   /** @brief Returns simulation fracture length */
-  REAL Lfrac(){return this->fLfrac;}
+  REAL Lfrac() const {return this->fLfrac;}
   
   /** @brief Defines simulation fracture height */
   void SetHf(REAL Hf){ this->fHf = Hf;}
   
   /** @brief Returns simulation fracture height */
-  REAL Hf(){return this->fHf;}
+  REAL Hf() const {return this->fHf;}
   
   /** @brief Defines simulation elasticity */
   void SetE(REAL E){ this->fE = E;}
   
   /** @brief Returns simulation elasticity */
-  REAL E(){return this->fE;}
+  REAL E() const {return this->fE;}
   
   /** @brief Defines simulation poisson */
   void SetPoisson(REAL nu){ this->fnu = nu;}
   
   /** @brief Returns simulation poisson */
-  REAL Poisson(){return this->fnu;}
+  REAL Poisson() const {return this->fnu;}
   
   /** @brief Defines simulation Flow BC */
   void SetQ(REAL Q){ this->fQ = Q;}
   
   /** @brief Returns simulation Flow BC */
-  REAL Q(){return this->fQ;}
+  REAL Q() const {return this->fQ;}
   
   /** @brief Defines simulation Confinement stress */
   void SetSigmaConf(REAL SigmaConf){ this->fSigmaConf = SigmaConf;}
   
   /** @brief Returns simulation confinement stress */
-  REAL SigmaConf(){return this->fSigmaConf;}
+  REAL SigmaConf() const {return this->fSigmaConf;}
   
   /** @brief Returns simulation G */
-  REAL G();
+  REAL G() const;
   
   /** @brief Defines p order of the pressure in L2 space */
   void SetPorderPressure(int PorderPressure){ this->fPorderPressure = PorderPressure;}
@@ -230,13 +230,13 @@ public:
   void SetPorderFlow(int PorderFlow){ this->fPorderFlow = PorderFlow;}
   
   /** @brief Returns p order of the flow */
-  int PorderFlow(){return this->fPorderFlow;}
+  int PorderFlow() const {return this->fPorderFlow;}
   
   /** @brief Defines p order of the flow in H1 space */
   void SetNelFrac(int NelFrac){ this->fnelFrac = NelFrac;}
   
   /** @brief Returns p order of the flow */
-  int NelFrac(){return this->fnelFrac;}
+  int NelFrac() const {return this->fnelFrac;}
   
   /** @brief Set evaluating step n */
   void SetLastState(){ State = n;}
@@ -245,32 +245,41 @@ public:
   void SetCurrentState(){ State = nplusone;}
 
   /** @brief Returns true if is last state */
-  bool IsLastState() { return State == n;}
+  bool IsLastState() const { return State == n;}
   
   /** @brief Sets Carter coefficient */
   void SetCl(REAL Cl) {fCl = Cl;}
 
   /** @brief Return Carter coefficient */
-  REAL Cl(){return fCl;}
+  REAL Cl() const {return fCl;}
   
   /** @brief Sets static pressure */
   void SetPe(REAL Pe) {fPe = Pe;}
   
   /** @brief Returns static pressure */
-  REAL Pe(){return fPe;}
+  REAL Pe() const {return fPe;}
   
   /** @brief Sets reference pressure for leak off */
   void SetPref(REAL Pref){fPref = Pref;}
   
   /** @brief Returns reference pressure for leak off */
-  REAL Pref(){return fPref;}
+  REAL Pref() const {return fPref;}
   
   /** @brief Sets spurt loss */
   void SetVsp(REAL vsp){fvsp = vsp;}
 
   /** @brief Returns spurt loss */
-  REAL Vsp(){return fvsp;}
+  REAL Vsp() const {return fvsp;}
 
+  /** @brief Return vl based on exposition time and pressure */
+  REAL VlFtau(REAL pfrac, REAL tau) const;
+
+  /** @brief Return the "ficticious time" based on acumulated volume and pressure */
+  REAL FictitiousTime(REAL VlAcum, REAL pfrac) const;
+  
+  REAL QlFVl(REAL VlAcum, REAL pfrac) const;
+  
+  REAL dQlFVl(REAL VlAcum, REAL pfrac) const;
 };
 
 #endif
