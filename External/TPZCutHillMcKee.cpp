e586004b7ce46cad7710f4e6054e1468c59376e7
fortiago@gmail.com <fortiago@gmail.com@a693bd38-532a-e730-8659-88a8c11ee2f8>
2014-10-15 17:27:01 +0000

    Implementação da reordenação de CutHill-McKee para redução de banda de matriz

---
+++ External/TPZCutHillMcKee.cpp
@@ -0,0 +1,99 @@
+
+#include "TPZCutHillMcKee.h"
+
+
+TPZCutHillMcKee::TPZCutHillMcKee():TPZRenumbering(){
+  fReverse = true;
+}
+
+TPZCutHillMcKee::TPZCutHillMcKee(int NElements, int NNodes, bool Reverse):TPZRenumbering(NElements,NNodes){
+  fReverse = Reverse;
+}
+
+TPZCutHillMcKee::~TPZCutHillMcKee(){
+
+}
+
+void TPZCutHillMcKee::Resequence(TPZVec<int> &perm, TPZVec<int> &iperm){
+
+  std::cout << "TPZCutHillMcKee ConvertGraph...";std::cout.flush();
+  SGraph graph;
+  ConvertGraph(fElementGraph, fElementGraphIndex, graph.fnodegraph, graph.fnodegraphindex);
+  graph.fnodegraph.Shrink();
+  graph.fnodegraphindex.Shrink();
+  std::cout << " done!\n";std::cout.flush();
+
+        //CutHillMcKee
+  std::queue<int> Q;
+  TPZStack<int> R;
+  TPZManVector<int> adjNodes;
+  //reservando memoria em R
+  const unsigned int nnodes = graph.NNodes();
+  R.Resize(nnodes);
+  R.Resize(0);
+  TPZVec<int> ExploredNodes(nnodes,0);
+  std::cout << "TPZCutHillMcKee process...\n";std::cout.flush();
+  while(R.NElements() != graph.NNodes()){
+    const int Parent = graph.SmallestDegree(ExploredNodes);
+    if(Parent == -1){
+      if(R.NElements() == graph.NNodes()) return;
+      else DebugStop();
+    }
+
+    this->ProcessParentNode(Parent, graph, ExploredNodes, R, Q, adjNodes);
+    while(Q.size()){
+      const int Child = Q.front();
+      Q.pop();
+      this->ProcessParentNode(Child, graph, ExploredNodes, R, Q, adjNodes);
+    }
+
+  }//loop completo
+  std::cout << " done!\n";std::cout.flush();
+
+  const unsigned int n = R.NElements();
+  if(n != nnodes) DebugStop();
+
+#ifdef DEBUG
+{//verificando se ha duplicados
+  std::set<int> check;
+  for(unsigned int i = 0; i < n; i++) check.insert(R[i]);
+  if(check.size() != n) DebugStop();
+}
+  #endif
+
+  std::cout << "TPZCutHillMcKee Filling perm and iperm vectors...";std::cout.flush();
+  if(fReverse){
+    iperm = R;
+    perm.Resize(n);
+    for(unsigned int i = 0; i < n; i++) perm[ iperm[i] ] = i;
+  }
+  else{
+    perm = R;
+    iperm.Resize(n);
+    for(unsigned int i = 0; i < n; i++) iperm[ perm[i] ] = i;
+  }
+  std::cout << " done!\n";std::cout.flush();
+
+}///void
+
+void TPZCutHillMcKee::ProcessParentNode(int Parent,
+                                        SGraph &graph,
+                                        TPZVec<int> &ExploredNodes,
+                                        TPZStack<int> &R,
+                                        std::queue<int> &Q,
+                                        TPZVec<int> &adjNodes){
+
+    R.Push( Parent );
+    ExploredNodes[Parent] = 1;
+    graph.AdjacentNodesOrdered(Parent, adjNodes);
+    const int nadj = adjNodes.NElements();
+    for(int i = 0; i < nadj; i++){
+      if(ExploredNodes[adjNodes[i]] == 0){//then a new element
+        Q.push(adjNodes[i]);
+        ExploredNodes[adjNodes[i]] = 1;
+      }
+    }//for
+
+
+}//void
+
---
+++ External/TPZCutHillMcKee.h
@@ -0,0 +1,92 @@
+//---------------------------------------------------------------------------
+
+#ifndef TPZCutHillMcKeeH
+#define TPZCutHillMcKeeH
+
+#include "pzrenumbering.h"
+#include <map>
+#include "pzmanvector.h"
+#include <queue>
+#include "pzstack.h"
+
+class TPZCutHillMcKee : public TPZRenumbering {
+
+  private:
+
+  struct SGraph{
+
+    TPZManVector<int> fnodegraph;
+
+    TPZManVector<int> fnodegraphindex;
+
+    int NNodes(){
+      return fnodegraphindex.NElements()-1;
+    }
+
+    int Degree(int node){
+      return fnodegraphindex[node+1]-fnodegraphindex[node];
+    }
+
+    int SmallestDegree(TPZVec<int> &ExploredNodes){
+      std::cout << "TPZCutHillMcKee::SGraph::SmallestDegree\n";std::cout.flush();
+      int mindegree = 1000000;
+      int found = -1;
+      const int n = this->NNodes();
+      for(int i = 0; i < n; i++){
+        if(ExploredNodes[i] == 1) continue;
+        int degree = this->Degree(i);
+        if(degree < mindegree){
+          mindegree = degree;
+          found = i;
+        }
+      }//for
+      return found;
+    }
+
+    void AdjacentNodesOrdered(int parent, TPZVec<int> &adjNodes){
+      std::multimap<int,int> order;
+
+      const unsigned int n = this->Degree(parent);
+      for(int i = 0; i < n; i++){
+        const int adj = fnodegraph[fnodegraphindex[parent]+i];
+        const int adjDegree = this->Degree(adj);
+        order.insert(std::make_pair(adjDegree,adj));
+      }//for
+      adjNodes.Resize(n);
+#ifdef DEBUG
+      if(n!=order.size()){
+        DebugStop();
+      }
+#endif
+      std::multimap<int,int>::const_iterator mapIt;
+      int i;
+      for(i = 0, mapIt = order.begin(); mapIt != order.end(); i++, mapIt++){
+        adjNodes[i] = mapIt->second;
+      }
+    }//void
+
+  };//SGraph
+
+
+  void ProcessParentNode(int Parent,
+                         SGraph &graph,
+                         TPZVec<int> &ExploredNodes,
+                         TPZStack<int> &R,
+                         std::queue<int> &Q,
+                         TPZVec<int> &adjNodes);
+
+  bool fReverse;
+
+  public:
+
+    virtual void Resequence(TPZVec<int> &perm, TPZVec<int> &iperm);
+
+    TPZCutHillMcKee();
+
+    TPZCutHillMcKee(int NElements, int NNodes, bool Reverse = true);
+
+    virtual ~TPZCutHillMcKee();
+
+};
+//---------------------------------------------------------------------------
+#endif
