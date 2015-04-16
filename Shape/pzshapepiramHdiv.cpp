0fd2d018337d9c18e6beb8c9ddbf1c1e78857082
Nathan Shauer <nathan.sh@gmail.com>
2015-04-16 18:38:12 -0300

    new class to construct pyramidhdiv pressure space

---
+++ Shape/pzshapepiramHdiv.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file
+ * @brief Contains the implementation of the TPZShapePiram methods.
+ */
+
+#include "pzshapepiramHdiv.h"
+
+
+using namespace std;
+
+namespace pzshape {
+	
+    void TPZShapePiramHdiv::Shape(TPZVec<REAL> &pt, TPZVec<long> &id, TPZVec<int> &order, TPZFMatrix<REAL> &phi,TPZFMatrix<REAL> &dphi) {
+
+        TPZShapePiram::Shape(pt, id, order, phi, dphi);
+
+        
+        TPZFMatrix<REAL> phicp(phi), dphicp(dphi);
+        for (int i = 4; i > 0; i--) {
+            phicp(i) = phicp(i-1);
+            for (int j=0; j<3; j++) {
+                dphicp(j,i) = dphicp(j,i-1);
+            }
+        }
+        
+        // I decided to put the four new functions (without * (1-z)) in the begining of the vector
+        const int stride = 4;
+        for (int i = 0 ; i < phicp.Rows() - 5; i++) {
+            phi(i+stride,0) = phicp(i+1,0);
+            for (int j = 0; j < 3; j++) {
+                dphi(j,i+stride) = dphicp(j,i+1);
+            }
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            phi(i,0) = phi(i+stride,0)/(1.-pt[2]);
+            for (int j = 0; j < 3; j++) {
+                dphi(j,i) = dphi(j,i+stride)/(1.-pt[2]);
+            }
+            dphi(2,i) += phi(i,0)/(1-pt[2])/(1.-pt[2]);
+        }
+    }
+	
+    int TPZShapePiramHdiv::NConnectShapeF(int side, int order) {
+        if (side == 0){ // the first connect has now 4 shapefunctions
+            return 4;
+        } // the other connects remain the same
+        else
+        {
+            return TPZShapePiram::NConnectShapeF(side, order);
+        }
+    }
+
+    
+};
---
+++ Shape/pzshapepiramHdiv.h
@@ -0,0 +1,52 @@
+/**
+ * @file
+ * @brief Contains TPZShapePiram class which implements the shape functions of a pyramid element.
+ * author: Nathan Shauer
+ * date: 01/04/2015
+ */
+
+#ifndef SHAPEPIRAMHDIV_H
+#define SHAPEPIRAMHDIV_H
+
+#include "pzshapepiram.h"
+
+/// groups all classes dedicated to the computation of shape functions
+namespace pzshape {
+
+	/** 
+	 * @brief Implements the shape functions of a pyramid (3D) element for Hdiv space. \ref shape "Shape"
+	 * @ingroup shape
+	 */
+	/**
+	 * The range of the master element is -1,1 for the base and [0,1] for the height
+	 */
+	class TPZShapePiramHdiv  : public TPZShapePiram{
+
+    public:
+		
+		/**
+		 * @brief Computes the values of the shape functions and their derivatives for a pyramid element
+		 * @param pt (input) point where the shape functions are computed
+		 * @param id (input) indexes of the corner points which determine the orientation of the shape functions
+		 * @param order (input) order of the side connects different from the corner connects (5 connects in this case)
+		 * @param phi (output) values of the shape functions
+		 * @param dphi (output) values of the derivatives of the shapefunctions   
+		 */
+		/**
+		 * These values depend on the point, the order of interpolation and ids of the corner points
+		 * The shapefunction computation uses the shape functions of the linear and quadrilateral element for its implementation
+		 */
+		static void Shape(TPZVec<REAL> &pt, TPZVec<long> &id, TPZVec<int> &order, TPZFMatrix<REAL> &phi,TPZFMatrix<REAL> &dphi);
+        
+        /**
+         * @brief Number of shapefunctions of the connect associated with the side, considering the order
+         * of interpolation of the element
+         * @param side associated side
+         * @param order vector of integers indicating the interpolation order of the element
+         * @return number of shape functions
+         */
+        static int NConnectShapeF(int side, int order);
+	};
+};
+
+#endif
