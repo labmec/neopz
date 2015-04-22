a20e2fc62471a83fd781df384e8edaac62fed8a1
Nathan Shauer <nathan.sh@gmail.com>
2015-04-22 18:38:49 -0300

    omar proj

---
+++ Projects/iRMS/TPZSimulationControl.h
@@ -0,0 +1,21 @@
+#ifndef TPZSimulationControlH
+#define TPZSimulationControlH
+/**
+ * @file   TPZSimulationControl.h
+ * @Author Omar Duran Triana. omaryesiduran@gmail.com
+ * @date   March, 2015
+ * @brief  Brief this file implements a iRMS simulation contol services.
+ *
+ */
+
+#include "tpzautopointer.h"
+#include "pzmanvector.h"
+#include <math.h>
+
+class TPZiRMSAnalysis {
+    
+    
+};
+
+
+#endif
