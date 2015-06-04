3a7f1122553078193f38c34f57dbab0e1182872c
philippedevloo <philippedevloo1958@gmail.com>
2015-06-04 10:05:26 -0300

    Classes which integrate elements with quarter point geometry
    Integration rule adapted to integrate functions with singularity

---
+++ Integral/TPZIntQuadQuarterPoint.cpp
@@ -0,0 +1,62 @@
+//
+//  TPZIntQuadQuarterPoint.cpp
+//  PZ
+//
+//  Created by Philippe Devloo on 6/1/15.
+//
+//
+
+#include "TPZIntQuadQuarterPoint.h"
+
+
+/// return the weight and position of the integration point
+void TPZIntQuadQuarterPoint::Point(int ip, TPZVec<REAL> &pos, REAL &w) const
+{
+    TPZManVector<REAL,2> ptr(2);
+    TPZIntQuad::Point(ip/2, ptr, w);
+    REAL theta = (ptr[0]+1.)*M_PI/8.;
+    REAL rrange = 1./cos(theta);
+    REAL r = (ptr[1]+1.)*rrange/2.;
+    w *= r*rrange*M_PI/4.;
+    std::cout << "ip " << ip << " ptr " << ptr << " theta " << theta << " rrange " << rrange << " r " << r << std::endl;
+    
+    REAL xi,eta;
+    xi = r*cos(theta);
+    eta = r*sin(theta);
+    std::cout << "xi " << xi << " eta " << eta << std::endl;
+    if (ip%2) {
+        pos[0] = -1.+2.*xi;
+        pos[1] = -1.+2.*eta;
+    }
+    else
+    {
+        pos[0] = -1.+2.*eta;
+        pos[1] = -1.+2.*xi;
+    }
+    switch (fCorner) {
+        case 0:
+            break;
+        case 1:
+            pos[0] = -pos[0];
+            break;
+        case 2:
+            pos[0] = -pos[0];
+            pos[1] = -pos[1];
+            break;
+        case 3:
+            pos[1] = -pos[1];
+            break;
+        default:
+            DebugStop();
+            break;
+    }
+    std::cout << "pos " << pos << std::endl;
+}
+
+/// set the integration order
+void TPZIntQuadQuarterPoint::SetOrder(TPZVec<int> &ord,int type)
+{
+    TPZIntQuad::SetOrder(ord, type);
+    fNPoints = TPZIntQuad::NPoints()*2;
+}
+
---
+++ Integral/TPZIntQuadQuarterPoint.h
@@ -0,0 +1,59 @@
+//
+//  TPZIntQuadQuarterPoint.h
+//  PZ
+//
+//  Created by Philippe Devloo on 6/1/15.
+//
+//
+
+#ifndef __PZ__TPZIntQuadQuarterPoint__
+#define __PZ__TPZIntQuadQuarterPoint__
+
+#include "pzquad.h"
+#include <stdio.h>
+
+class TPZIntQuadQuarterPoint : public TPZIntQuad
+{
+    /// corner associated with the integration rule
+    int fCorner;
+    /// number of integration points
+    int fNPoints;
+    
+public:
+    
+    TPZIntQuadQuarterPoint(int ordksi) : TPZIntQuad(ordksi), fCorner(0)
+    {
+        fNPoints = TPZIntQuad::NPoints()*2;
+    }
+    
+    TPZIntQuadQuarterPoint(const TPZIntQuadQuarterPoint &copy) : TPZIntQuad(copy), fCorner(copy.fCorner), fNPoints(copy.fNPoints)
+    {
+        
+    }
+    
+    virtual int NPoints() const
+    {
+        return fNPoints;
+    }
+    
+    /// return the weight and position of the integration point
+    virtual void Point(int ip, TPZVec<REAL> &pos, REAL &w) const;
+    
+    /// set the integration order
+    virtual void SetOrder(TPZVec<int> &ord,int type = 0);
+    
+    /// set the corner where the singularity resides
+    void SetCorner(int corner)
+    {
+#ifdef DEBUG
+        if (corner < 0 || corner > 3) {
+            DebugStop();
+        }
+#endif
+        fCorner = corner;
+    }
+
+
+};
+
+#endif /* defined(__PZ__TPZIntQuadQuarterPoint__) */
