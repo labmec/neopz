35097d1d7399cfdb971dca4526b1524a795f318d
Nathan Shauer <nathan.sh@gmail.com>
2015-06-15 18:25:06 -0300

    modified main file to create geomesh

--- Projects/iRMS/iRMS.cpp
+++ Projects/iRMS/iRMS.cpp
@@ -1,9 +1,9 @@
 #include "pzlog.h"
-#include "tpzautopointer.h"
-#include <time.h>
+#include "pzgmesh.h"
+#include "TPZRefPatternTools.h"
 
-#include "TRMRockProperties.h"
-#include "TRMBlackOilProperties.h"
+#include "TRMRawData.h"
+#include "TRMSimworxMeshGenerator.h"
 
 
 // iRMS (i rise for / innovatory Reservoir Muli-scale Simulator)
@@ -14,16 +14,32 @@ static LoggerPtr logdata(Logger::getLogger("pz.iRMS"));
 
 void LinearTracer();
 
+void CreateExampleRawData(TRMRawData &data)
+{
+    data.fLw = 500.;
+    data.fHasLiner = true;
+    data.fHasCasing = true;
+    
+    data.fReservoirWidth = 500.;
+    data.fReservoirLength = 1000.;
+    data.fReservoirHeight = 50.;
+    data.fProdVertPosition = 25;
+}
+
 int main()
 {
     
     // This code use normalized piola contravariant mapping for nonlinear mappings
     // HDivPiola = 0;
     
+    gRefDBase.ReadRefPatternDBase("../RefPatterns.rpt");
     
+    TRMRawData rawdata;
     
+    CreateExampleRawData(rawdata);
     
-    
+    TRMSimworxMeshGenerator meshGen;
+    TPZGeoMesh *gmesh = meshGen.CreateSimworxGeoMesh(rawdata);
     
     std::cout << " Process complete normally." << std::endl;
     return 0;
