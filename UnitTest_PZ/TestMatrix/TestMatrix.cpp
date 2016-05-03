092c9e28d00391c7ba46d3e87f38bf9012a47d60
orlandini <francisco.orlandini@gmail.com>
2016-05-03 09:40:10 -0300

    revised indentation while understanding the code

--- UnitTest_PZ/TestMatrix/TestMatrix.cpp
+++ UnitTest_PZ/TestMatrix/TestMatrix.cpp
@@ -55,6 +55,8 @@ int TestingGeneratingDiagonalDominantMatrix(matx &matr) {
 /**
 * @brief Tests the Inverse method of the matrix to any matrix types. It uses the AutoFill method to create a square matrix with 
 * @param dim Dimension of the square matrix to be build.
+* @param symmetric Whether to build a symmetric matrix
+* @param dec Decomposition method to be used (See enum DecomposeType at pzmatrix.h)
 * @note Process: build square matrix with randomic values, compute its inverse and the inverse of the inverse.
 * Then, checks whether the first and last matrices are identical.
 */
@@ -73,30 +75,30 @@ void TestingInverseWithAutoFill(int dim, int symmetric, DecomposeType dec) {
 //    ma.Print("skyl =",std::cout,EMathematicaInput);
 	BOOST_CHECK_EQUAL(TestingGeneratingDiagonalDominantMatrix<matx>(ma),1);
 	// Making ma copy because ma is modified by Inverse method (it's decomposed)
-	matx cpy(ma);
+	matx cpma(ma);
 	TPZFMatrix<TVar> inv(dim,dim), invkeep;
 	TPZFMatrix<TVar> res(inv);
 	// getting inverse twice
-	cpy.Inverse(inv,dec);
-    invkeep = inv;
+	ma.Inverse(inv,dec);
+  invkeep = inv;
 	inv.Inverse(res,dec);
-//    ma.Print("skyl2 =",std::cout,EMathematicaInput);
-    bool check = true;
+  //    ma.Print("skyl2 =",std::cout,EMathematicaInput);
+  bool check = true;
 	/// Checking whether the res matrix is identical to m1 matrix
 	for(i=0;i<dim;i++)
     {
-		for(j=0;j<dim;j++)
-        {
-            TVar diff = ma.GetVal(i,j) - res.GetVal(i,j);
-            bool loccheck = IsZero(TVar(diff/10.));
-            if (loccheck == false) {
-                std::cout << "diff " << diff << std::endl;
-            }
-            check &= loccheck;
-        }
+      for(j=0;j<dim;j++)
+      {
+          TVar diff = cpma.GetVal(i,j) - res.GetVal(i,j);
+          bool loccheck = IsZero(TVar(diff/10.));
+          if (loccheck == false) {
+              std::cout << "diff " << diff << std::endl;
+          }
+          check &= loccheck;
+      }
     }
     if (!check) {
-        ma.Print("Matrix = ",std::cout,EMathematicaInput);
+        cpma.Print("Matrix = ",std::cout,EMathematicaInput);
         invkeep.Print("Inv = ",std::cout,EMathematicaInput);
     }
     BOOST_CHECK(check);
@@ -105,7 +107,8 @@ void TestingInverseWithAutoFill(int dim, int symmetric, DecomposeType dec) {
 /**
 * @brief Tests the operator * of the matrix, using AutoFill to build a square matrix of dimension dim (user defined)
 * @param dim Dimension of the square matrix to be build.
-* @note Process: build square matrix with randomic values, compute the product using * operator and then checks whether it is a identity matrix.
+* @param symmetric Whether to build a symmetric matrix
+* @note Process: build square matrix with randomic values, compute its square twice using * operator with two different copies of itself and checks whether the result is the same
 */
 template <class matx, class TVar>
 void TestingMultiplyOperatorWithAutoFill(int dim, int symmetric) {
@@ -119,22 +122,23 @@ void TestingMultiplyOperatorWithAutoFill(int dim, int symmetric) {
 	square2 = duplicate*duplicate;
 	square = ma*duplicate;
 	// Checking whether both matrices are equal
-    bool check = true;
+  bool check = true;
 	for(int i=0;i<dim;i++) {
 		for(int j=0;j<dim;j++) {
-            TVar diff = fabs(square.GetVal(i, j)- square2.GetVal(i, j));
-            if (!IsZero(diff)) {
-                check = false;
-            }
+      TVar diff = fabs(square.GetVal(i, j)- square2.GetVal(i, j));
+      if (!IsZero(diff)) {
+          check = false;
+      }
 		}
 	}
-    BOOST_CHECK(check);
+  BOOST_CHECK(check);
 }
 
 /**
 * @brief Tests the Multiply method of the matrix, using AutoFill to build a square matrix of dimension dim (user defined)
 * @param dim Dimension of the square matrix to be build.
-* @note Process: build square matrix with randomic values, compute its inverse then calculate the product of the first and inverse.
+* @param symmetric Whether to build a symmetric matrix
+* @note Process: build square matrix with randomic values, compute its square twice using Multiply function with two different copies of itself and checks whether the result is the same
 */
 /**
  * Check if the result is a identity matrix.
@@ -150,25 +154,27 @@ void TestingMultiplyWithAutoFill(int dim, int symmetric) {
 //    ma.Print("SkylineNS");
 //    duplicate.Print("FullMat");
 	ma.Multiply(duplicate,square);
-    duplicate.Multiply(duplicate, square2);
+  duplicate.Multiply(duplicate, square2);
 	// Checking whether result matrix is the identity matrix
     bool check = true;
 	for(int i=0;i<dim;i++) {
 		for(int j=0;j<dim;j++) {
-            if(!IsZero(fabs(square(i,j)-square2(i,j))))
-            {
-                check = false;
-            }
+      if(!IsZero(fabs(square(i,j)-square2(i,j))))
+      {
+          check = false;
+      }
 		}
 	}
-    BOOST_CHECK(check);
+  BOOST_CHECK(check);
 
 }
 
 /**
-* @brief Tests the Transpose method of the matrix, using AutoFill to build a square matrix of dimension dim (user defined)
-* @param dim Dimension of the square matrix to be build.
-* @note Process: build square matrix with randomic values, compute its transpose and transpose again then compare the first and last matrices.
+* @brief Tests the Transpose method of the matrix, using AutoFill to build a matrix of dimension row x cols (user defined)
+* @param rows Number of rows
+* @param cols Number of columns
+* @param symmetric Whether to build a symmetric matrix
+* @note Process: build matrix with randomic values, compute its transpose and transpose again then compare the first and last matrices.
 */
 template <class matx, class TVar>
 void TestingTransposeWithAutoFill(int rows,int cols, int symmetric) {
@@ -191,9 +197,11 @@ void TestingTransposeWithAutoFill(int rows,int cols, int symmetric) {
 }
 
 /**
- * @brief Tests the Transpose method of the matrix, using AutoFill to build a square matrix of dimension dim (user defined)
+ * @brief Tests the MultAdd method of the matrix, that calculates z = beta * y + alpha * A * x , using AutoFill to build a square matrix of dimension dim (user defined)
  * @param dim Dimension of the square matrix to be build.
- * @note Process: build square matrix with randomic values, compute its transpose and transpose again then compare the first and last matrices.
+ * @param symmetric Whether to build a symmetric matrix
+ * @param dec Decomposition method to be used (See enum DecomposeType at pzmatrix.h)
+ * @note Process: build square matrix with randomic values, compute its inverse and uses MultAdd for calculating I - A*A^-1 and check whether you have any non-zero entries on the result
  */
 template <class matx, class TVar>
 void TestingMultAdd(int dim, int symmetric, DecomposeType dec) {
@@ -260,21 +268,21 @@ BOOST_AUTO_TEST_CASE(inverse_tests)
 {
 	int dim;
 	for(dim = 9;dim < 10; dim+=5) {
-        TestingInverseWithAutoFill<TPZSBMatrix<float>, float >(dim,1,ELDLt);
-        TestingInverseWithAutoFill<TPZSBMatrix<float>, float >(dim,1,ECholesky);
-        TestingInverseWithAutoFill<TPZFBMatrix<double>,double >(dim,0,ELU);
-        TestingInverseWithAutoFill<TPZFBMatrix<float>,float >(dim,0,ELU);
-        TestingInverseWithAutoFill<TPZFMatrix<float>, float>(dim,1,ECholesky);
-        TestingInverseWithAutoFill<TPZFMatrix<float>, float>(dim,0,ELU);
-        TestingInverseWithAutoFill<TPZFMatrix<double>, double>(dim,1,ECholesky);
-        TestingInverseWithAutoFill<TPZFMatrix<float>, float>(dim,1,ELDLt);
-        TestingInverseWithAutoFill<TPZFMatrix<double>, double>(dim,1,ELDLt);
-		TestingInverseWithAutoFill<TPZBlockDiagonal<float>,float >(dim,0,ELU);
-		TestingInverseWithAutoFill<TPZSkylMatrix<float>, float >(dim,1,ELDLt);
-		TestingInverseWithAutoFill<TPZFNMatrix<9,float>, float >(dim,0,ELU);
-		TestingInverseWithAutoFill<TPZSFMatrix<float>, float >(dim,1,ELDLt);
-        TestingInverseWithAutoFill<TPZSkylNSymMatrix<float>, float >(dim,0,ELU);
-        
+    TestingInverseWithAutoFill<TPZSBMatrix<float>, float >(dim,1,ELDLt);
+    TestingInverseWithAutoFill<TPZSBMatrix<float>, float >(dim,1,ECholesky);
+    TestingInverseWithAutoFill<TPZFBMatrix<double>,double >(dim,0,ELU);
+    TestingInverseWithAutoFill<TPZFBMatrix<float>,float >(dim,0,ELU);
+    TestingInverseWithAutoFill<TPZFMatrix<float>, float>(dim,1,ECholesky);
+    TestingInverseWithAutoFill<TPZFMatrix<float>, float>(dim,0,ELU);
+    TestingInverseWithAutoFill<TPZFMatrix<double>, double>(dim,1,ECholesky);
+    TestingInverseWithAutoFill<TPZFMatrix<float>, float>(dim,1,ELDLt);
+    TestingInverseWithAutoFill<TPZFMatrix<double>, double>(dim,1,ELDLt);
+    TestingInverseWithAutoFill<TPZBlockDiagonal<float>,float >(dim,0,ELU);
+    TestingInverseWithAutoFill<TPZSkylMatrix<float>, float >(dim,1,ELDLt);
+    TestingInverseWithAutoFill<TPZFNMatrix<9,float>, float >(dim,0,ELU);
+    TestingInverseWithAutoFill<TPZSFMatrix<float>, float >(dim,1,ELDLt);
+    TestingInverseWithAutoFill<TPZSkylNSymMatrix<float>, float >(dim,0,ELU);
+    
 	}
 }
 
@@ -290,8 +298,8 @@ BOOST_AUTO_TEST_CASE(multiplyoperator_tests)
 {
 	int dim;
 	for(dim = 3;dim < 100; dim+=5) {
-		TestingMultiplyOperatorWithAutoFill<TPZFMatrix<REAL>, REAL >(dim,0);
-		//TestingMultiplyOperatorWithAutoFill<TPZSkylNSymMatrix<REAL> >(dim);
+		TestingMultiplyOperatorWithAutoFill<TPZFMatrix<float>, float >(dim,0);
+    TestingMultiplyOperatorWithAutoFill<TPZFMatrix<double>, double >(dim,0);
 	}
 }
 BOOST_AUTO_TEST_CASE(transpose_tests)
@@ -299,8 +307,8 @@ BOOST_AUTO_TEST_CASE(transpose_tests)
 	int rows, cols;
 	for(rows = 3;rows < 4; rows+=5) {
 		for(cols = 3;cols < 100;cols+=5) {
-//			TestingTransposeWithAutoFill<TPZSkylNSymMatrix<REAL> >(rows,cols);
-			TestingTransposeWithAutoFill<TPZFMatrix<REAL>, REAL >(rows,cols,0);
+			TestingTransposeWithAutoFill<TPZFMatrix<float>, float >(rows,cols,0);
+      TestingTransposeWithAutoFill<TPZFMatrix<double>, double >(rows,cols,0);
 		}
 	}
 }
@@ -308,7 +316,12 @@ BOOST_AUTO_TEST_CASE(transpose_tests)
 BOOST_AUTO_TEST_CASE(multadd_tests)
 {
     for (int dim = 5; dim < 6; dim += 10) {
-        TestingMultAdd<TPZFMatrix<float>, float>(dim, 1, ECholesky);
+      TestingMultAdd<TPZFMatrix<float>, float>(dim, 1, ECholesky);
+      TestingMultAdd<TPZFMatrix<double>, double>(dim, 1, ECholesky);
+      TestingMultAdd<TPZSkylMatrix<float>, float>(dim, 1, ECholesky);
+      TestingMultAdd<TPZSkylMatrix<double>, double>(dim, 1, ECholesky);
+      TestingMultAdd<TPZSkylNSymMatrix<float>, float>(dim, 1, ECholesky);
+      TestingMultAdd<TPZSkylNSymMatrix<double>, double>(dim, 1, ECholesky);
     }
 }
 BOOST_AUTO_TEST_SUITE_END()
