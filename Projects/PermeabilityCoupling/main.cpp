4763fda43c7a13ba3f66afbecfb2daeb1b64d0d5
manouchehr <manouchehr.sanei@gmail.com>
2016-08-13 18:40:22 -0300

    Adding a new project
    Purpose:
    Implement coupling of permeability and poro-elasticity deformation with
    continuous formulations.

---
+++ Projects/PermeabilityCoupling/main.cpp
@@ -0,0 +1,35 @@
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <time.h>
+#include <stdio.h>
+#include <fstream>
+#include <cmath>
+#include <iostream>
+#include <math.h>
+
+#include "pzgmesh.h"
+#include "pzcmesh.h"
+#include "pzcompel.h"
+
+
+#include "pzlog.h"
+
+
+
+#ifdef LOG4CXX
+static LoggerPtr logdata(Logger::getLogger("pz.material"));
+#endif
+
+int main(int argc, char *argv[])
+{
+//#ifdef LOG4CXX
+//    InitializePZLOG();
+//#endif
+    
+    std::cout << " welcome to the library ... " << std::endl;
+    
+	return EXIT_SUCCESS;
+}
