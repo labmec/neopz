92c4a4403e870f911f124333127faaa6fa4ab734
Omar Duran <omaryesiduran@gmail.com>
2017-01-15 20:27:09 -0200

    Adding class for reading gmsh file

---
+++ Projects/iRMS/TRMGmshReader.cpp
@@ -0,0 +1,9 @@
+//
+//  TRMGmshReader.cpp
+//  PZ
+//
+//  Created by Omar on 1/15/17.
+//
+//
+
+#include "TRMGmshReader.h"
---
+++ Projects/iRMS/TRMGmshReader.h
@@ -0,0 +1,14 @@
+//
+//  TRMGmshReader.hpp
+//  PZ
+//
+//  Created by Omar on 1/15/17.
+//
+//
+
+#ifndef TRMGmshReader_h
+#define TRMGmshReader_h
+
+#include <stdio.h>
+
+#endif /* TRMGmshReader_h */
