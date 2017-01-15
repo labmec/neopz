fbc8298e4f58063ef99303d9eedcbe6774b51029
Omar Duran <omaryesiduran@gmail.com>
2017-01-15 21:23:17 -0200

    ongoing with gmsh reader

--- Projects/iRMS/TRMGmshReader.h
+++ Projects/iRMS/TRMGmshReader.h
@@ -10,5 +10,42 @@
 #define TRMGmshReader_h
 
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include "pzgmesh.h"
+
+class TPZGeoMesh;
+
+
+struct MaterialDataS {
+
+    int fMatID;
+    int fMatID;    
+    std::string fMaterial;
+    TPZStack<REAL> fProperties;
+    
+    MaterialDataV() : fMatID(-1), fMaterial(), fProperties()
+    {
+        
+    }
+    MaterialDataV(int num) : fMatID(-1), fMaterial(), fProperties()
+    {
+        
+    }
+    MaterialDataV(const MaterialDataV &copy) : fMatID(copy.fMatID),
+    fMaterial(copy.fMaterial), fProperties(copy.fProperties)
+    {
+    }
+    MaterialDataV &operator=(const MaterialDataV &copy)
+    {
+        fMatID = copy.fMatID;
+        fMaterial = copy.fMaterial;
+        fProperties = copy.fProperties;
+        return *this;
+    }
+};
+
 
 #endif /* TRMGmshReader_h */
