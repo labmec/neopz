885c3117ad41a1259682c4059479ea9e7add7c3f
Omar Duran <omaryesiduran@gmail.com>
2017-06-17 17:44:44 -0300

    ...

--- Projects/iRMS/TRMRawData.cpp
+++ Projects/iRMS/TRMRawData.cpp
@@ -919,9 +919,9 @@ void TRMRawData::TwoPhaseWaterOilReservoir(bool Is3DGeometryQ){
     fIsQuasiNewtonQ = true; // Deprecated fixed due to secant method
     fIsAdataptedQ = false;
     fEnhancedPressureQ = false;
-    fMHMResolutionQ.first = true;
+    fMHMResolutionQ.first = false;
     fMHMResolutionQ.second.first = 0; // level
-    fMHMResolutionQ.second.second = 1; // fine
+    fMHMResolutionQ.second.second = 0; // fine
     fIncreaseTransporResolutionQ.first = true;
     fIncreaseTransporResolutionQ.second = 0;
     
--- Projects/iRMS/iRMS.cpp
+++ Projects/iRMS/iRMS.cpp
@@ -61,7 +61,7 @@ void MultiScaleSimulation()
     TPZAutoPointer<TRMRawData> RawData  = new TRMRawData;
     
     //  Dimension on gmsh reservoir    
-    bool Is3DGeometry = false;
+    bool Is3DGeometry = true;
     
     bool IsSinglePhaseQ = false;
     if(IsSinglePhaseQ){
