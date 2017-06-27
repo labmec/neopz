4f07da681eb9d1f8040190156726d68382127fe0
Marcio Ivan <marciofilho96@yahoo.com.br>
2017-06-27 18:47:31 -0300

    Fixed a setting in CTestConfig

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,5 +10,6 @@ set(CTEST_PROJECT_NAME "PZ")
 set(CTEST_NIGHTLY_START_TIME "03:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "labmec.org.br")
+set(CTEST_DROP_SITE "www.labmec.org.br")
 set(CTEST_DROP_LOCATION "/pz/CDash/submit.php?project=PZ")
+set(CTEST_DROP_SITE_CDASH TRUE)
