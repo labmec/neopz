798ce30ceb20c0b9ddb9ac2ce61859baf6cddaa3
Francisco Orlandini <francisco.orlandini@gmail.com>
2017-07-19 17:05:38 -0300

    [ONGOING]trying to refactor previous commit

--- Save/TPZFileStream.h
+++ Save/TPZFileStream.h
@@ -1,162 +1,82 @@
 #ifndef TPZFILESTREAM_H
 #define TPZFILESTREAM_H
 
-#include "TPZStream.h"
 #include <fstream>
 #include <string>
+#include "TPZStream.h"
 /**
  * @ingroup save
- * @brief Implements reading from and writing to an ascii file. \ref save "Persistency"
+ * @brief Implements reading from and writing to an ascii file. \ref save
+ * "Persistency"
  */
 class TPZFileStream : public TPZStream {
-  private:
-    std::ifstream fIn;
-    std::ofstream fOut;
+ private:
+  std::ifstream fIn;
+  std::ofstream fOut;
 
-	template<class T>
-	void ReadData(T *p, int size);
-	
-    template<class T>
-	void WriteData(const T *p, int size);
+  template <class T>
+  void ReadData(T *p, int howMany);
 
-  public:
-    TPZFileStream();
-    virtual ~TPZFileStream();
+  template <class T>
+  void WriteData(const T *p, int howMany);
 
-    void OpenRead(const std::string &fileName);
-    void OpenWrite(const std::string &fileName);
+ public:
+  TPZFileStream();
+  virtual ~TPZFileStream();
 
-    void CloseRead();
-    void CloseWrite();
+  void OpenRead(const std::string &fileName);
+  void OpenWrite(const std::string &fileName);
 
-	bool eof() {
-		return fIn.eof();
-	}
-	
-	virtual void Write(const int *p, int howMany) {
-		WriteData<int>(p,howMany);
-	}
-	
-	virtual void Write(const unsigned int *p, int howMany) {
-		WriteData<unsigned int>(p,howMany);
-	}
-	
-	virtual void Write(const long *p, int howMany) {
-		WriteData<long>(p,howMany);
-	}
-	
-	virtual void Write(const float *p, int howMany) {
-		WriteData<float>(p,howMany);
-	}
-	
-	virtual void Write(const double *p, int howMany) {
-		WriteData<double>(p,howMany);
-	}
-	
-	virtual void Write(const long double *p, int howMany) {
-		WriteData<long double>(p,howMany);
-	}
-	
-	virtual void Write(const char *p, int howMany) {
-		WriteData<char>(p,howMany);
-	}
-	
-	virtual void Write(const std::string *p, int howMany) {
-		WriteData<std::string>(p,howMany);
-	}
-	
-	virtual void Write(const std::complex <float> *p, int howMany) {
-		WriteData< std::complex <float> >(p,howMany);
-	}
-	
-	virtual void Write(const std::complex <double> *p, int howMany) {
-		WriteData< std::complex <double> >(p,howMany);
-	}
-	
-	virtual void Write(const std::complex <long double> *p, int howMany) {
-		WriteData< std::complex <long double> >(p,howMany);
-	}
-	
-#ifdef _AUTODIFF
-	
-	virtual void Write(const Fad <float> *p, int size) {
-		WriteData< Fad <float> >(p,size);
-	}
-	
-	virtual void Write(const Fad <double> *p, int size) {
-		WriteData< Fad <double> >(p,size);
-	}
-	
-	virtual void Write(const Fad <long double> *p, int size) {
-		WriteData< Fad <long double> >(p,size);
-	}
-	
-#endif
-	
-	virtual void Read(int *p, int size) {
-		ReadData<int>(p,size);
-	}
-	
-	virtual void Read(unsigned int *p, int size) {
-		ReadData<unsigned int>(p,size);
-	}
-	
-	virtual void Read(long *p, int size) {
-		ReadData<long>(p,size);
-	}
-	
-	virtual void Read(float *p, int size) {
-		ReadData<float>(p,size);
-	}
-	
-	virtual void Read(double *p, int size) {
-		ReadData<double>(p,size);
-	}
-	
-	virtual void Read(long double *p, int size) {
-		ReadData<long double>(p,size);
-	}
-	
-	virtual void Read(char *p, int size) {
-		ReadData<char>(p,size);
-	}
-	
-	virtual void Read(std::string *p, int size) {
-		int c;
-		char buf[2560];
-		for(c=0; c<size; c++)
-		{
-			fIn.getline(buf,2560);
-			p[c] = buf;
-		}
-	}
-	
-	virtual void Read(std::complex <float> *p, int size) {
-		ReadData< std::complex <float> >(p,size);
-	}
-	
-	virtual void Read(std::complex <double> *p, int size) {
-		ReadData< std::complex <double> >(p,size);
-	}
-	
-	virtual void Read(std::complex <long double> *p, int size) {
-		ReadData< std::complex <long double> >(p,size);
-	}
-	
-#ifdef _AUTODIFF
-	
-	virtual void Read(Fad <float> *p, int size) {
-		ReadData< Fad <float> >(p,size);
-	}
-	
-	virtual void Read(Fad <double> *p, int size) {
-		ReadData< Fad <double> >(p,size);
-	}
-	
-	virtual void Read(Fad <long double> *p, int size) {
-		ReadData< Fad <long double> >(p,size);
-	}
-	
-#endif
+  void CloseRead();
+  void CloseWrite();
+
+  bool eof() { return fIn.eof(); }
+    using TPZStream::Read;
+    using TPZStream::Write;
+  virtual void Read(std::string *p, int howMany) {
+    int c;
+    char buf[2560];
+    for (c = 0; c < howMany; c++) {
+      fIn.getline(buf, 2560);
+      p[c] = buf;
+    }
+  }
+
+  virtual void Write(const std::string *p, int howMany) {
+    WriteData<std::string>(p, howMany);
+  }
 };
-#endif// TPZFILESTREAM_H
+
+template<> void TPZFileStream::ReadData<int>(int *p, int howMany);
+template<> void TPZFileStream::ReadData<unsigned int>(unsigned int *p, int howMany);
+template<> void TPZFileStream::ReadData<long>(long *p, int howMany);
+template<> void TPZFileStream::ReadData<float>(float *p, int howMany);
+template<> void TPZFileStream::ReadData<double>(double *p, int howMany);
+template<> void TPZFileStream::ReadData<long double>(long double *p, int howMany);
+template<> void TPZFileStream::ReadData<char>(char *p, int howMany);
+template<> void TPZFileStream::ReadData<std::complex<float> >(std::complex<float> *p, int howMany);
+template<> void TPZFileStream::ReadData<std::complex<double> >(std::complex<double> *p, int howMany);
+template<> void TPZFileStream::ReadData<std::complex<long double> >(std::complex<long double>  *p, int howMany);
+#ifdef _AUTODIFF
+template<> void TPZFileStream::ReadData<Fad <float> >(Fad <float>  *p, int howMany);
+template<> void TPZFileStream::ReadData<Fad <double> >(Fad <double>  *p, int howMany);
+template<> void TPZFileStream::ReadData<Fad<long double> >(Fad<long double>  *p, int howMany);
+#endif
+
+template<> void TPZFileStream::WriteData<int>(const int *p, int howMany);
+template<> void TPZFileStream::WriteData<unsigned int>(const unsigned int *p, int howMany);
+template<> void TPZFileStream::WriteData<long>(const long *p, int howMany);
+template<> void TPZFileStream::WriteData<float>(const float *p, int howMany);
+template<> void TPZFileStream::WriteData<double>(const double *p, int howMany);
+template<> void TPZFileStream::WriteData<long double>(const long double *p, int howMany);
+template<> void TPZFileStream::WriteData<char>(const char *p, int howMany);
+template<> void TPZFileStream::WriteData<std::complex<float> >(const std::complex<float> *p, int howMany);
+template<> void TPZFileStream::WriteData<std::complex<double> >(const std::complex<double> *p, int howMany);
+template<> void TPZFileStream::WriteData<std::complex<long double> >(const std::complex<long double>  *p, int howMany);
+#ifdef _AUTODIFF
+template<> void TPZFileStream::WriteData<Fad <float> >(Fad <float>  *p, int howMany);
+template<> void TPZFileStream::WriteData<Fad <double> >(Fad <double>  *p, int howMany);
+template<> void TPZFileStream::WriteData<Fad<long double> >(Fad<long double>  *p, int howMany);
+#endif
+
+#endif  // TPZFILESTREAM_H
