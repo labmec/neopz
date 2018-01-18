e8a540e8dc7bb492dc85f7d582d44cdcdb49e721
luiz <lccmarques@gmail.com>
2018-01-18 11:31:08 -0200

    lapack header files used the same way now for linux and windows. MKL's lapack implementation now has precedence in all platforms.

--- Common/TPZLapack.h
+++ Common/TPZLapack.h
@@ -2,18 +2,17 @@
 #define TPZLapackH
 
 #ifdef USING_LAPACK
-#ifdef MACOSX
-#include <Accelerate/Accelerate.h>
-typedef __CLPK_doublecomplex vardoublecomplex;
-typedef __CLPK_complex varfloatcomplex;
-
-#elif USING_MKL
+#ifdef USING_MKL
 #include <mkl.h>
 typedef MKL_Complex16 vardoublecomplex;
 typedef MKL_Complex8 varfloatcomplex;
 
+#elif MACOSX
+#include <Accelerate/Accelerate.h>
+typedef __CLPK_doublecomplex vardoublecomplex;
+typedef __CLPK_complex varfloatcomplex;
 
-#elif WIN32
+#else
 #include <complex>
 
 #define lapack_complex_float std::complex<float>
@@ -24,9 +23,6 @@ typedef lapack_complex_float varfloatcomplex;
 
 #include "cblas.h"
 #include "lapacke.h"
-
-#else
-#include <clapack.h>
 
 #endif
 #endif
