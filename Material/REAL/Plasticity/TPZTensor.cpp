f257e0618e5dff020b8692b424d15ade8f41cbe9
Thiago de Oliveira Quinelato <thiago.quinelato@gmail.com>
2018-02-28 14:18:42 -0300

    Fixing TPZTensor.

--- Material/REAL/Plasticity/TPZTensor.cpp
+++ Material/REAL/Plasticity/TPZTensor.cpp
@@ -3,6 +3,11 @@
 #include "TPZTensor.h"
 
 template<class T>
-STATE TPZTensor<T>::TPZDecomposed::gEigval[3] = {T(0.)};
+STATE TPZTensor<T>::TPZDecomposed::gEigval[3] = {0.};
 
 template class TPZTensor<STATE>;
+
+#ifdef _AUTODIFF
+#include "tfad.h"
+template class TPZTensor<TFad<6,double>>;
+#endif
