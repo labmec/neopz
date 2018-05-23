60c3a7d25ace0a73c5cd39361c80175cea25356d
Thiago de Oliveira Quinelato <thiago.quinelato@gmail.com>
2018-05-23 11:36:02 -0300

    Bug fix in the PostProcessing of the error.

--- Analysis/pzanalysis.cpp
+++ Analysis/pzanalysis.cpp
@@ -746,7 +746,7 @@ void TPZAnalysis::PostProcessErrorSerial(TPZVec<REAL> &ervec, bool store_error, 
         if(el) {
             TPZMaterial *mat = el->Material();
             TPZBndCond *bc = dynamic_cast<TPZBndCond *>(mat);
-            if(!bc)
+            if(!bc && mat->Dimension() == fCompMesh->Dimension())
             {
                 errors.Fill(0.0);
                 el->EvaluateError(fExact, errors, store_error);
