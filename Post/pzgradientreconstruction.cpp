e266e8f51452b52b1ba4d5ef4292f66c2536b173
luiz <lccmarques@gmail.com>
2018-05-25 16:00:13 -0300

    Suppressing more warning in Linux

--- Post/pzgradientreconstruction.cpp
+++ Post/pzgradientreconstruction.cpp
@@ -309,7 +309,10 @@ void TPZGradientReconstruction::TPZGradientData::SetCel(TPZCompEl * cel, bool us
     
     ComputeSlopeLimiter3();
 }
-
+#ifdef linux
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat"
+#endif
 void TPZGradientReconstruction::TPZGradientData::Print(std::ostream &out) const
 {
     const char *name1 = "Cell average";
@@ -372,7 +375,6 @@ void TPZGradientReconstruction::TPZGradientData::Print(std::ostream &out) const
         }
         out <<")\n";
     }
-    
     //Interface
     out <<"\n\n" << name7 <<"\n";
     for (i=0; i<fCenterPointInterface.size(); i++)
@@ -388,6 +390,9 @@ void TPZGradientReconstruction::TPZGradientData::Print(std::ostream &out) const
     
 }
 
+#ifdef linux
+#pragma GCC diagnostic pop    
+#endif
 
 void TPZGradientReconstruction::TPZGradientData::GetCenterPointAndCellAveraged(TPZCompEl *cel, TPZManVector<REAL,3> &xcenter, STATE &solcel)
 {
