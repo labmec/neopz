fa49b2b1e8810209e0dac80da47e838d72107ae2
luiz <lccmarques@gmail.com>
2018-10-26 14:42:36 -0200

    Fix MacOS warnings (II)

--- Post/pzcompelpostproc.h
+++ Post/pzcompelpostproc.h
@@ -291,7 +291,7 @@ inline void TPZCompElPostProc<TCOMPEL>::CalcResidual(TPZElementMatrix &ef)
     REAL weight = 0.;
     REAL weightRef = 0;
   
-    TPZIntPoints* pFIntegrationRule = &this->GetIntegrationRule();
+    void* pFIntegrationRule = &this->GetIntegrationRule();
     if (!pFIntegrationRule) {
         PZError << "Error at " << __PRETTY_FUNCTION__ << " Integration rule must be used from TPZCompEl. \n";
         DebugStop();
