aee73333bcd81a0f06b7e37fc772d0af05746320
luiz <lccmarques@gmail.com>
2018-10-30 09:20:23 -0300

    Fix mistype of DebugStop()

--- Material/pzmat1dlin.cpp
+++ Material/pzmat1dlin.cpp
@@ -32,7 +32,7 @@ void TPZMat1dLin::Contribute(TPZMaterialData &data,
         << phi.Cols() << " dphi.Cols + " << dphi.Cols() <<
         " phi.Rows = " << phi.Rows() << " dphi.Rows = " <<
         dphi.Rows() << "\n";
-        StopError();
+        DebugStop();
     }
 
     if(fForcingFunction)
