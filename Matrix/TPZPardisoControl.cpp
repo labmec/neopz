70672ade715c0e4ba104621dfaf8f2507c48838b
Omar Duran <omaryesiduran@gmail.com>
2019-04-14 08:21:10 -0300

    Update TPZPardisoControl.cpp

--- Matrix/TPZPardisoControl.cpp
+++ Matrix/TPZPardisoControl.cpp
@@ -209,7 +209,7 @@ void TPZPardisoControl<TVar>::Decompose()
     }
     perm = &fPermutation[0];
     
-    fParam[4 ] = 1; // user permutation PERM
+//    fParam[4 ] = 1; // user permutation PERM
     
     /// analyse and factor the equations
     // LU preconditioned CGS (10*L+K) where K={1:CGS,2:CG} and L=10^-L stopping threshold
