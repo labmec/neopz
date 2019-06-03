1e9ec9d841828132bf7ae331521ae84c0ea47203
philippedevloo <philippedevloo1958@gmail.com>
2019-06-03 10:46:35 -0300

    The graph generation based on material ids is not operational yet

--- Mesh/pzcmesh.cpp
+++ Mesh/pzcmesh.cpp
@@ -1108,7 +1108,8 @@ void TPZCompMesh::ComputeElGraph(TPZStack<int64_t> &elgraph, TPZVec<int64_t> &el
     for(i=0; i<nelem; i++) {
         TPZCompEl *el = fElementVec[i];
         /// Apply the filter by material identifiers
-        bool has_material_Q = el->HasMaterial(mat_ids);
+        bool has_material_Q = true;
+//        if(el) has_material_Q = el->HasMaterial(mat_ids);
         if(!el || !has_material_Q){
             elgraphindex[curel+1]=elgraph.NElements();
             curel++;
