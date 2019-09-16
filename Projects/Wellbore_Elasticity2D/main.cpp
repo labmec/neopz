3740798c6ba084750c12c1d3131403106d597b88
NathaliaBatalha <nathalia_batalha@hotmail.com>
2019-09-16 09:17:31 -0300

    start .cpp pzrandomfield again
    started .cpp of pzrandomfield needs to be fixed.

--- Projects/Wellbore_Elasticity2D/main.cpp
+++ Projects/Wellbore_Elasticity2D/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char *argv[])
     int projection = 0;
     
     // define disposicao do poco - inclined == 1
-    int inclinedwellbore = 1;
+    int inclinedwellbore = 0;
     
     // pressao da lama de perfuracao - MPa
     REAL Pwb = -19.5;//-19.5; //-10.52
@@ -101,8 +101,8 @@ int main(int argc, char *argv[])
     REAL drdcirc = 0.5;
     
     // Define Posicao do Poco
-    REAL direction   = 30.;//30.; // Azimuth em graus
-    REAL inclination = 45.;//45.; // Polar Inclination em graus / wellbore inclination
+    REAL direction   = 0.;//30.; // Azimuth em graus
+    REAL inclination = 0.;//45.; // Polar Inclination em graus / wellbore inclination
     
     // Tensoes in Situ, horizontais e vertical em MPa
     REAL SigmaV = -48.2; //-48.053;  // tensao vertical
