51a2f06d63a7555ceacaa1f240cb6d5bc1b7eddd
luiz <lccmarques@gmail.com>
2020-02-12 18:59:58 -0200

    Fix duplicity during merge with RefactorGeom

--- Mesh/TPZMultiphysicsCompMesh.cpp
+++ Mesh/TPZMultiphysicsCompMesh.cpp
@@ -143,39 +143,6 @@ void TPZMultiphysicsCompMesh::BuildMultiphysicsSpace(TPZVec<TPZCompMesh * > & me
     LoadSolutionFromMeshes();
 }
 
-void TPZMultiphysicsCompMesh::BuildMultiphysicsSpaceWithMemory(TPZVec<int> & active_approx_spaces, TPZVec<TPZCompMesh * > & mesh_vector)
-{
-    m_active_approx_spaces = active_approx_spaces;
-    m_mesh_vector          = mesh_vector;
-    if (m_mesh_vector.size() != m_active_approx_spaces.size()) {
-        std::cout<< "TPZMultiphysicsCompMesh:: The vector provided should have the same size." << std::endl;
-        DebugStop();
-    }
-    
-    int n_approx_spaces = m_mesh_vector.size();
-    
-    SetNMeshes(n_approx_spaces);
-    Reference()->ResetReference();
-    SetAllCreateFunctionsMultiphysicElemWithMem();
-    ApproxSpace().CreateWithMemory(true);
-    // delete all elements and connects in the mesh
-    CleanElementsConnects();
-    TPZCompMesh::AutoBuild();
-    AddElements();
-    AddConnects();
-    LoadSolutionFromMeshes();
-    
-    int nel_res = NElements();
-    for (long el = 0; el < nel_res; el++) {
-        TPZCompEl *cel = Element(el);
-        TPZMultiphysicsElement *mfcel = dynamic_cast<TPZMultiphysicsElement *>(cel);
-        if (!mfcel) {
-            continue;
-        }
-        mfcel->PrepareIntPtIndices();
-    }
-    
-}
 
 void TPZMultiphysicsCompMesh::AutoBuild(){
     
