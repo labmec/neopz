a2b293eca92ac00ac2314089bbc2712fe022434b
luiz <lccmarques@gmail.com>
2020-06-19 17:15:32 -0300

    Fix wrong INTERFACE path in all CMakeLists.txt

--- Projects/visualmatrix/CTestTestFile.cmake
+++ Projects/visualmatrix/CTestTestFile.cmake
@@ -8,4 +8,4 @@ SET (filename "visualmatrix.dx")
 SET (expectedMD5 "654b8ae8045e237d00a60ccbc7032b1c")
 add_test (VisualMatrix_visualmatrix.dx "${CMAKE_COMMAND}" "-Dfilename:STRING=${filename}" "-DexpectedMD5:STRING=${expectedMD5}" "-P" "${CMAKE_SOURCE_DIR}/Tests/MD5FileTest.cmake")
 #Adding dependency
-#set_tests_properties (VisualMatrix_visualmatrix.dx PROPERTIES DEPENDS "VisualMatrix" REQUIRED_FILES "${filename}")
+set_tests_properties (VisualMatrix_visualmatrix.dx PROPERTIES DEPENDS "VisualMatrix" REQUIRED_FILES "${filename}")
