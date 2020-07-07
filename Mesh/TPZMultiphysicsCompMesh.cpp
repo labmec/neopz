57ab51ce508aa44a436cf564ee098b2e64210858
philippedevloo <philippedevloo1958@gmail.com>
2020-07-07 10:07:15 -0300

    The BuildMultiphysicsSpace does not alter the approximation space style if it is already of type multiphysics space

--- Mesh/TPZMultiphysicsCompMesh.cpp
+++ Mesh/TPZMultiphysicsCompMesh.cpp
@@ -132,7 +132,12 @@ void TPZMultiphysicsCompMesh::BuildMultiphysicsSpace(TPZVec<TPZCompMesh * > & me
     int n_approx_spaces = m_mesh_vector.size();
     SetNMeshes(n_approx_spaces);
     Reference()->ResetReference();
-    SetAllCreateFunctionsMultiphysicElem();
+    if(ApproxSpace().Style() != TPZCreateApproximationSpace::EMultiphysics)
+    {
+        std::cout << __PRETTY_FUNCTION__ << " Modifying the style of approximation space "
+        " to multiphysics\n";
+        SetAllCreateFunctionsMultiphysicElem();
+    }
     // delete all elements and connects in the mesh
     CleanElementsConnects();
     TPZCompMesh::AutoBuild(gelindexes);
