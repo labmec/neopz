2f2706bdee1e102df8fbfbc19ef2be1c4e8423fb
PabloGSCarvalho <pablogscarvalho@hotmail.com>
2021-01-18 11:48:14 -0300

    Add a new exact solution for Navier-Stokes simulation

--- Pre/TPZAnalyticSolution.h
+++ Pre/TPZAnalyticSolution.h
@@ -455,7 +455,7 @@ struct TStokesAnalytic : public TPZAnalyticSolution
     
     enum MProblemType {EStokes, ENavierStokes, EOseen, ENavierStokesCDG, EOseenCDG, EBrinkman};
     
-    enum EExactSol {ENone, ECavity,  EKovasznay, EKovasznayCDG, ESinCos, ESinCos3D, EPconst, EObstacles, EOneCurve , ESinCosBDS, ESinCosBDS3D, EGatica3D, ECouplingSD, ECouplingNSD, EVugs2D, EVugs3D};
+    enum EExactSol {ENone, ECavity,  EKovasznay, EKovasznayCDG, ESinCos, ESinCos3D, EPconst, EObstacles, EOneCurve , ESinCosBDS, ESinCosBDS3D, EGatica3D, ECouplingSD, ECouplingNSD, EVugs2D, EVugs3D, EInfiltrationNS};
     
     int fDimension = 2;
     
