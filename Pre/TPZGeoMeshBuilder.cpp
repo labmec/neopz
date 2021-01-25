388445b1001ff493c4cd5fabc6f5b14b5f90d72c
Pedro Lima <pedrolima.ec@outlook.com>
2021-01-25 19:41:06 -0300

    Return element pointer in TPZGeoMeshBuilder

--- Pre/TPZGeoMeshBuilder.cpp
+++ Pre/TPZGeoMeshBuilder.cpp
@@ -35,7 +35,7 @@ void TPZGeoMeshBuilder::InsertNodes(TPZGeoMesh * gmesh, std::vector<std::size_t>
     }
 }
 
-void TPZGeoMeshBuilder::InsertElement(TPZGeoMesh * gmesh, int & physical_identifier, int & el_type, int & el_identifier, std::vector<int> & node_identifiers){
+TPZGeoEl* TPZGeoMeshBuilder::InsertElement(TPZGeoMesh * gmesh, int & physical_identifier, int & el_type, int & el_identifier, std::vector<int> & node_identifiers){
     
     TPZManVector <int64_t,15> Topology;
     int n_nodes = node_identifiers.size();
@@ -43,93 +43,93 @@ void TPZGeoMeshBuilder::InsertElement(TPZGeoMesh * gmesh, int & physical_identif
     for (int k_node = 0; k_node<n_nodes; k_node++) {
         Topology[k_node] = node_identifiers[k_node]-1;
     }
-    
+    TPZGeoEl* gel = nullptr;
     el_identifier--;
     switch (el_type) {
         case 1:
         {   // Line
-            new TPZGeoElRefPattern< pzgeom::TPZGeoLinear> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZGeoLinear> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 2:
         {
             // Triangle
-            new TPZGeoElRefPattern< pzgeom::TPZGeoTriangle> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZGeoTriangle> (el_identifier, Topology, physical_identifier, *gmesh);
             
         }
             break;
         case 3:
         {
             // Quadrilateral
-            new TPZGeoElRefPattern< pzgeom::TPZGeoQuad> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZGeoQuad> (el_identifier, Topology, physical_identifier, *gmesh);
             
         }
             break;
         case 4:
         {
             // Tetrahedron
-            new TPZGeoElRefPattern< pzgeom::TPZGeoTetrahedra> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZGeoTetrahedra> (el_identifier, Topology, physical_identifier, *gmesh);
             
         }
             break;
         case 5:
         {
             // Hexahedra
-            new TPZGeoElRefPattern< pzgeom::TPZGeoCube> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZGeoCube> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 6:
         {
             // Prism
-            new TPZGeoElRefPattern< pzgeom::TPZGeoPrism> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZGeoPrism> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 7:
         {
             // Pyramid
-            new TPZGeoElRefPattern< pzgeom::TPZGeoPyramid> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZGeoPyramid> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 8:
         {
             // Quadratic Line
-            new TPZGeoElRefPattern< pzgeom::TPZQuadraticLine> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZQuadraticLine> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 9:
         {
             // Triangle
-            new TPZGeoElRefPattern< pzgeom::TPZQuadraticTrig> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZQuadraticTrig> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 10:
         {
             // Quadrilateral
-            new TPZGeoElRefPattern< pzgeom::TPZQuadraticQuad> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZQuadraticQuad> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 11:
         {
             // Tetrahedron
-            new TPZGeoElRefPattern< pzgeom::TPZQuadraticTetra> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZQuadraticTetra> (el_identifier, Topology, physical_identifier, *gmesh);
             
         }
             break;
         case 12:
         {
             // Hexahedra
-            new TPZGeoElRefPattern< pzgeom::TPZQuadraticCube> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZQuadraticCube> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 13:
         {
             // Prism
-            new TPZGeoElRefPattern< pzgeom::TPZQuadraticPrism> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElRefPattern< pzgeom::TPZQuadraticPrism> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         case 15:{
             // Point
-            new TPZGeoElement< pzgeom::TPZGeoPoint, pzrefine::TPZRefPoint> (el_identifier, Topology, physical_identifier, *gmesh);
+            gel = new TPZGeoElement< pzgeom::TPZGeoPoint, pzrefine::TPZRefPoint> (el_identifier, Topology, physical_identifier, *gmesh);
         }
             break;
         default:
@@ -139,7 +139,7 @@ void TPZGeoMeshBuilder::InsertElement(TPZGeoMesh * gmesh, int & physical_identif
         }
             break;
     }
-    
+    return gel;
 }
 
 int TPZGeoMeshBuilder::GetNumberofNodes(int & el_type){
