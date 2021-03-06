292f9ba31e8104ebd0a02067b5f605215dd98b12
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-03-05 21:53:56 -0300

    Deleted Deprecated Projects

--- Projects/Deprecated/Conductivity/voidflux_test.h
+++
@@ -1,7 +0,0 @@
-//
-//  voidflux_test.h
-//  PZ
-//
-//  Created by Philippe Devloo on 5/5/11.
-//  Copyright 2011 UNICAMP. All rights reserved.
-//
--- Projects/Deprecated/SantosProjects/Stokes/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "TPZStokesMaterial.h"
-
-int main(){
-    
-    
-    return 0;
-}
