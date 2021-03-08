95ba65b0a5d7ea8f22f3f252e390ee100fda4192
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-03-08 16:27:58 -0300

    Fixed linking issue w.r.t. <atomic>

---
+++ cmake/check_for_atomic.cmake
@@ -0,0 +1,36 @@
+#
+# This function will check if any linking is needed when
+# using functions from <atomic>
+# Usage:
+#     check_for_atomic(target)
+#
+include(CMakePushCheckState)
+include(CheckCXXSourceCompiles)
+
+function(check_for_atomic target)
+cmake_push_check_state()
+
+if(CMAKE_CXX11_STANDARD_COMPILE_OPTION)
+  set(CMAKE_REQUIRED_FLAGS ${CMAKE_CXX11_STANDARD_COMPILE_OPTION})
+endif()
+
+check_cxx_source_compiles("
+#include <atomic>
+std::atomic<int64_t> x;
+
+int main() {
+  x.load(std::memory_order_relaxed);
+  return std::atomic_is_lock_free(&x);
+}"
+  atomic)
+if(NOT atomic)
+  find_library(lib_atomic NAMES atomic libatomic)
+  if(lib_atomic)
+    message("Found lib atomic: ${lib_atomic}")
+    target_link_libraries(${target} PRIVATE ${lib_atomic})
+  else()
+    message(FATAL_ERROR "Failed to compile dummy program with std::atomic")
+  endif()
+endif()
+cmake_pop_check_state()
+endfunction()
