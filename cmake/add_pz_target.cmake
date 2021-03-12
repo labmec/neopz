38ac57396f3a9c7aed070369650e0851a5bf2eb0
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-03-12 16:43:32 -0300

    created add_pz_target cmake function for external projs

--- cmake/add_pz_target.cmake
+++ cmake/add_pz_target.cmake
@@ -1,24 +1,22 @@
 #
 # Create a target dependent on NeoPZ with possible linkage to extra libraries, copied files to binary dir variable number of sources.
-# This function is meant to be used for targets IN THIS PROJECT.
-# For linking external projects, use add_target_with_pz
-#
 # Usage:
-#     add_pz_internal_target(
+#     add_pz_target(
 #       NAME myTarget
 #       SOURCES source.cpp header.h
+#       EXTRA_LIBS [optional] extra libraries to be LINKED, not found
 #       FILES [optional] files that will be copied to target binary dir
 #
 
 
 include (CMakeParseArguments)
 
-function(add_pz_internal_target)
+function(add_pz_target)
   cmake_parse_arguments(
     PARSED_ARGS # prefix of output variables
     "" # list of names of the boolean arguments (only defined ones will be true)
     "NAME" # list of names of mono-valued arguments
-    "SOURCES;FILES" # list of names of multi-valued arguments (output variables are lists)
+    "SOURCES;EXTRA_LIBS;FILES" # list of names of multi-valued arguments (output variables are lists)
     ${ARGN} # arguments of the function to parse, here we take the all original ones
     )
   # note: if it remains unparsed arguments, here, they can be found in variable PARSED_ARGS_UNPARSED_ARGUMENTS
@@ -31,8 +29,14 @@ function(add_pz_internal_target)
   endif(N_SOURCES LESS 1)
   add_executable(${PARSED_ARGS_NAME} "")
   target_sources(${PARSED_ARGS_NAME} PRIVATE ${PARSED_ARGS_SOURCES})
-  target_link_libraries(${PARSED_ARGS_NAME} PRIVATE pz)
+
+  find_package(NeoPZ REQUIRED)
+  
+  target_link_libraries(${PARSED_ARGS_NAME} PRIVATE NeoPZ::pz)
+  foreach(extlib ${PARSED_ARGS_EXTRA_LIBS})
+    target_link_libraries(${PARSED_ARGS_NAME} PRIVATE ${extlib})
+  endforeach(file)  
   foreach(file ${PARSED_ARGS_FILES})
     configure_file(${file} ${file} COPYONLY)
   endforeach(file)  
-endfunction(add_pz_target)
+endfunction(add_target_with_pz)
