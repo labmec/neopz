labmec/neopz

commit 60b5584334d5de1a942f0384d7a05ea48fb4c2a0

created interfaces for (TPZF|TPZ(F|S)B)Matrix when not using lapack
PZ_USING_(LAPACK|BLAS|MKL) are now INTERFACE compile definitions.
This commit fixed an issue where the unit tests would break
if USING_MKL=ON or USING_LAPACK=ON due to the presence
of USING_LAPACK on headers and the fact that USING_LAPACK is now
a PRIVATE compile definition

Matrix/pzbndmat.cpp
@@ -617,7 +617,26 @@ int TPZFBMatrix<double>::Substitution( TPZFMatrix<double> *B ) const{
     return( 1 );
 }
 
+#else
+#define NON_LAPACK \
+  PZError<<__PRETTY_FUNCTION__<<" requires Lapack\n";\
+  PZError<<" Set either USING_LAPACK=ON or USING_MKL=ON on CMake ";\
+  PZError<<" when configuring NeoPZ library"<<std::endl;\
+  DebugStop();
+template<class TVar>
+int TPZFBMatrix<TVar>::Decompose_LU(std::list<int64_t> &singular)
+{
+    NON_LAPACK
+}
 
+
+template<>
+int
+TPZFBMatrix<float>::Decompose_LU()
+{
+  NON_LAPACK
+}
+#undef NON_LAPACK
 #endif
 
 template<class TVar>

Matrix/pzbndmat.h
@@ -57,13 +57,11 @@ public:
         for (int64_t el=0; el<nel; el++) {
             fElem[el] = orig.fElem[el];
         }
-#ifdef USING_LAPACK
         fPivot = orig.fPivot;
         int64_t nwork = orig.fWork.size();
         for (int64_t el=0; el<nwork; el++) {
             fWork[el] = orig.fWork[el];
         }
-#endif
         
     }
     
@@ -134,10 +132,8 @@ public:
 	
 	void Transpose(TPZMatrix<TVar> *const T) const override;
     
-#ifdef USING_LAPACK
 	int       Decompose_LU(std::list<int64_t> &singular) override;
 	int       Decompose_LU() override;
-#endif
 	
     public:
 int ClassId() const override;
@@ -153,11 +149,10 @@ private:
 	
 	TPZVec<TVar> fElem;
 	int64_t  fBandLower, fBandUpper;
-#ifdef USING_LAPACK
+
     TPZManVector<int,5> fPivot;
     
     TPZVec<TVar> fWork;
-#endif
 
 };
 

Matrix/pzfmatrix.cpp
@@ -39,7 +39,7 @@ static TPZLogger loggerCheck("pz.checkconsistency");
 #endif
 
 #ifdef USING_LAPACK
-/** CBlas Math Library */
+/** Maybe some calls just need BLAS. We need to check it. */
 #include "TPZLapack.h"
 #define BLAS_MULT
 #endif
@@ -514,7 +514,6 @@ void TPZFMatrix<TVar>::DeterminantInverse(TVar &determinant, TPZFMatrix<TVar> &i
     for(r=0; r<this->Rows(); r++) determinant *= copy(r,r);
 }
 
-#ifdef USING_LAPACK
 
 template <class TVar>
 /** @brief Initialize pivot with i = i  */
@@ -525,8 +524,6 @@ void TPZFMatrix<TVar>::InitializePivot()
         fPivot[i] = i+1; // Fortran based indexing
     }
 }
-
-#endif
 
 template <class TVar>
 void TPZFMatrix<TVar>::MultAdd(const TVar *ptr, int64_t rows, int64_t cols, const TPZFMatrix<TVar> &x,const TPZFMatrix<TVar> &y, TPZFMatrix<TVar> &z,
@@ -3138,7 +3135,33 @@ Fad<REAL> Norm(const TPZFMatrix<Fad<REAL> > &A)
     return res;
 }
 
+#ifndef USING_LAPACK    
+#define NON_LAPACK \
+  PZError<<__PRETTY_FUNCTION__<<" requires Lapack\n";\
+  PZError<<" Set either USING_LAPACK=ON or USING_MKL=ON on CMake ";\
+  PZError<<" when configuring NeoPZ library"<<std::endl;\
+  DebugStop();
 
+    int TPZFMatrix<TVar>::Subst_Forward( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
+
+    int TPZFMatrix<TVar>::Subst_Backward( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
+
+    int TPZFMatrix<TVar>::Subst_LForward( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
+
+    int TPZFMatrix<TVar>::Subst_LBackward( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
+
+    int TPZFMatrix<TVar>::Subst_Diag( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
+
+    int TPZFMatrix<TVar>::SolveEigenProblem(TPZVec < std::complex<double> > &w, TPZFMatrix < std::complex<double> > &eigenVectors){NON_LAPACK}
+
+    int TPZFMatrix<TVar>::SolveEigenProblem(TPZVec < std::complex<double> > &w){NON_LAPACK}
+
+    int TPZFMatrix<TVar>::SolveGeneralisedEigenProblem(TPZFMatrix< TVar > &B , TPZVec < std::complex<double> > &w, TPZFMatrix < std::complex<double> > &eigenVectors){NON_LAPACK}
+
+    int SolveGeneralisedEigenProblem(TPZFMatrix< TVar > &B , TPZVec < std::complex<double> > &w){NON_LAPACK}    
+    /** @} */
+#undef NON_LAPACK
+#endif
 #include <complex>
 
 template class TPZFMatrix<int >;

Matrix/pzfmatrix.h
@@ -299,11 +299,8 @@ public:
     /** @brief Makes Zero all the elements */
     int Zero() override;
     
-#ifdef USING_LAPACK
     /** @brief Initialize pivot with i = i  */
     void InitializePivot();
-#endif
-
     
     /**
      * @brief This method implements a Gram Schimidt method. \n this = Orthog.TransfToOrthog
@@ -353,7 +350,7 @@ public:
     /** @brief LU substitution using pivot. Static version. */
     static int Substitution(const TVar *ptr, int64_t rows,  TPZFMatrix<TVar> *B, const TPZVec<int> &index );
     
-#ifdef USING_LAPACK
+
     /**
      * @brief Computes B = Y, where A*Y = B, A is lower triangular.
      * @param b right hand side and result after all
@@ -383,11 +380,9 @@ public:
      * @param b right hand side and result after all
      */
     virtual int Subst_Diag( TPZFMatrix<TVar>* b ) const override;
-#endif
     
     /** @} */
     
-#ifdef USING_LAPACK
     /*** @name Solve eigenvalues ***/
     /** @{ */
     /** @brief Solves the Ax=w*x eigenvalue problem and calculates the eigenvectors
@@ -410,7 +405,7 @@ public:
      */
     virtual int SolveGeneralisedEigenProblem(TPZFMatrix< TVar > &B , TPZVec < std::complex<double> > &w);    
     /** @} */
-#endif
+
     
     /** @brief Routines to send and receive messages */
     public:
@@ -447,11 +442,9 @@ private:
     TVar *fGiven;
     int64_t fSize;
     
-#ifdef USING_LAPACK
     TPZManVector<int,5> fPivot;
     
     TPZVec<TVar> fWork;
-#endif
 };
 
 /** @} */

Matrix/pzsbndmat.cpp
@@ -1984,7 +1984,32 @@ TPZSBMatrix<complex<double> >::SolveGeneralisedEigenProblem(TPZSBMatrix<complex<
 }
 
 /** @} */
+#else
+#define NON_LAPACK \
+  PZError<<__PRETTY_FUNCTION__<<" requires Lapack\n";\
+  PZError<<" Set either USING_LAPACK=ON or USING_MKL=ON on CMake ";\
+  PZError<<" when configuring NeoPZ library"<<std::endl;\
+  DebugStop();
+template<class TVar>
+int TPZSBMatrix<TVar>::Decompose_Cholesky(){NON_LAPACK}
+
+template<class TVar>
+int TPZSBMatrix<TVar>::Decompose_Cholesky(std::list<int64_t> &singular){NON_LAPACK}
+
+template<class TVar>
+int TPZSBMatrix<TVar>::SolveEigenProblem(TPZVec < std::complex<double> > &w, TPZFMatrix < std::complex<double> > &eigenVectors){NON_LAPACK}
+
+template<class TVar>
+int TPZSBMatrix<TVar>::SolveEigenProblem(TPZVec < std::complex<double> > &w){NON_LAPACK}
+
+template<class TVar>
+int TPZSBMatrix<TVar>::SolveGeneralisedEigenProblem(TPZSBMatrix< TVar > &B , TPZVec < std::complex<double> > &w, TPZFMatrix < std::complex<double> > &eigenVectors){NON_LAPACK}
+
+template<class TVar>
+int TPZSBMatrix<TVar>::SolveGeneralisedEigenProblem(TPZSBMatrix< TVar > &B , TPZVec < std::complex<double> > &w){NON_LAPACK}
+#undef NON_LAPACK
 #endif
+
 template<class TVar>
 int TPZSBMatrix<TVar>::ClassId() const{
     return Hash("TPZSBMatrix") ^ TPZMatrix<TVar>::ClassId() << 1;

Matrix/pzsbndmat.h
@@ -102,10 +102,8 @@ public:
     
     /// To solve linear systems
     // @{
-#ifdef USING_LAPACK
     int Decompose_Cholesky() override;  // Faz A = GGt.
     int Decompose_Cholesky(std::list<int64_t> &singular) override;
-#endif
     
     int Subst_Forward( TPZFMatrix<TVar>*B ) const override;
     int Subst_Backward ( TPZFMatrix<TVar> *b ) const override;
@@ -120,7 +118,6 @@ public:
     
     // @}
     
-#ifdef USING_LAPACK
     /*** @name Solve eigenvalues ***/
     /** @{ */
     
@@ -146,7 +143,7 @@ public:
     int SolveGeneralisedEigenProblem(TPZSBMatrix< TVar > &B , TPZVec < std::complex<double> > &w);
     
     /** @} */
-#endif
+
     public:
 int ClassId() const override;
 

UnitTest_PZ/TestMatrix/TestMatrix.cpp
@@ -280,7 +280,7 @@ void TestingMultAdd(int dim, int symmetric, DecomposeType dec) {
     BOOST_CHECK(check);
 }
 
-#ifdef USING_LAPACK
+#ifdef PZ_USING_LAPACK
 
 /**
  * @brief Tests the Eigenvalues/eigenvectors of the generalised eigenproblem Av=wBv to any matrix types. It uses the AutoFill method to create a square matrix with
@@ -412,7 +412,7 @@ void TestingEigenDecompositionAutoFill(int dim, int symmetric) {
 
 BOOST_AUTO_TEST_SUITE(matrix_tests)
 
-#ifdef USING_LAPACK
+#ifdef PZ_USING_LAPACK
 
 
 BOOST_AUTO_TEST_CASE(eigenvalue_tests) {

cmake/EnableBLAS.cmake
@@ -7,6 +7,7 @@ function(enable_blas target)
     #target_link_libraries(${target} BLAS::BLAS) this is ok in cmake 3.18 onwards
     target_link_libraries(${target} PRIVATE ${BLAS_LIBRARIES})
     target_compile_definitions(${target} PRIVATE USING_BLAS)
+    target_compile_definitions(${target} INTERFACE PZ_USING_BLAS)
     mark_as_advanced(BLAS_LIBRARIES)
     foreach(BL_LIB ${BLAS_LIBRARIES})
         if(${BL_LIB} MATCHES ".*mkl.*")

commit 961ba064d66570dd669c2d390d7e398b3c438857

Some TPZ(F|FB|SB)Matrix methods were broken since 60b5584
The mentioned commit prevented to fallback to the TPZMatrix<TVar>
implementation of some methods if the library was not configured with
USING_LAPACK=ON.

Matrix/pzbndmat.cpp
@@ -492,7 +492,6 @@ TPZFBMatrix<TVar>::Transpose (TPZMatrix<TVar> *const T) const
     }
 }
 
-#ifdef USING_LAPACK
 
 template<class TVar>
 int TPZFBMatrix<TVar>::Decompose_LU(std::list<int64_t> &singular)
@@ -501,7 +500,18 @@ int TPZFBMatrix<TVar>::Decompose_LU(std::list<int64_t> &singular)
 	return ELU;
 }
 
+template<class TVar>
+int TPZFBMatrix<TVar>::Decompose_LU()
+{
+    if (  this->fDecomposed && this->fDecomposed == ELU) {
+        return ELU;
+    } else if(this->fDecomposed) {
+        TPZMatrix<TVar>::Error(__PRETTY_FUNCTION__,"TPZFBMatrix::Decompose_LU is already decomposed with other scheme");
+    }
+    return TPZMatrix<TVar>::Decompose_LU();
+}
 
+#ifdef USING_LAPACK
 template<>
 int
 TPZFBMatrix<float>::Decompose_LU()
@@ -565,18 +575,6 @@ TPZFBMatrix<double>::Decompose_LU()
 }
 
 
-
-template<class TVar>
-int TPZFBMatrix<TVar>::Decompose_LU()
-{
-    if (  this->fDecomposed && this->fDecomposed == ELU) {
-        return ELU;
-    } else if(this->fDecomposed) {
-        TPZMatrix<TVar>::Error(__PRETTY_FUNCTION__,"TPZFBMatrix::Decompose_LU is already decomposed with other scheme");
-    }
-    return TPZMatrix<TVar>::Decompose_LU();
-}
-
 template<>
 int TPZFBMatrix<float>::Substitution( TPZFMatrix<float> *B ) const{
     
@@ -617,26 +615,6 @@ int TPZFBMatrix<double>::Substitution( TPZFMatrix<double> *B ) const{
     return( 1 );
 }
 
-#else
-#define NON_LAPACK \
-  PZError<<__PRETTY_FUNCTION__<<" requires Lapack\n";\
-  PZError<<" Set either USING_LAPACK=ON or USING_MKL=ON on CMake ";\
-  PZError<<" when configuring NeoPZ library"<<std::endl;\
-  DebugStop();\
-  return -1;
-template<class TVar>
-int TPZFBMatrix<TVar>::Decompose_LU(std::list<int64_t> &singular)
-{
-    NON_LAPACK
-}
-
-
-template<class TVar>
-int TPZFBMatrix<TVar>::Decompose_LU()
-{
-  NON_LAPACK
-}
-#undef NON_LAPACK
 #endif
 
 template<class TVar>

Matrix/pzbndmat.h
@@ -63,10 +63,6 @@ public:
             fElem[el] = orig.fElem[el];
         }
         fPivot = orig.fPivot;
-        int64_t nwork = orig.fWork.size();
-        for (int64_t el=0; el<nwork; el++) {
-            fWork[el] = orig.fWork[el];
-        }
         
     }
     
@@ -136,9 +132,9 @@ public:
 	int Zero() override;
 	
 	void Transpose(TPZMatrix<TVar> *const T) const override;
-    //Depends on LAPACK
+    //If LAPACK is available, it will use its implementation.
 	int       Decompose_LU(std::list<int64_t> &singular) override;
-    //Depends on LAPACK
+    //If LAPACK is available, it will use its implementation.
 	int       Decompose_LU() override;
 	
     public:
@@ -157,8 +153,6 @@ private:
 	int64_t  fBandLower, fBandUpper;
 
     TPZManVector<int,5> fPivot;
-    
-    TPZVec<TVar> fWork;
 
 };
 

Matrix/pzfmatrix.cpp
@@ -1853,7 +1853,7 @@ int TPZFMatrix<double>::Subst_Forward( TPZFMatrix<double>* b ) const
         return TPZMatrix<double>::Subst_Forward(b);
     }
 }
-
+#endif 
 /**
  * @brief Computes B = Y, where A*Y = B, A is lower triangular.
  * @param b right hand side and result after all
@@ -1874,7 +1874,7 @@ int TPZFMatrix<TVar>::Subst_Backward( TPZFMatrix<TVar>* b ) const
     return TPZMatrix<TVar>::Subst_Backward(b);
 }
 
-
+#ifdef USING_LAPACK
 template<>
 int TPZFMatrix<float>::Subst_Backward( TPZFMatrix<float>* b ) const
 {
@@ -1985,18 +1985,6 @@ int TPZFMatrix<double>::Subst_LForward( TPZFMatrix<double>* b ) const
     //    return TPZMatrix<TVar>::Subst_LForward(b);
 }
 
-
-/**
- * @brief Computes B = Y, where A*Y = B, A is lower triangular with A(i,i)=1.
- * @param b right hand side and result after all
- */
-template<class TVar>
-int TPZFMatrix<TVar>::Subst_LForward( TPZFMatrix<TVar>* b ) const
-{
-    //    ssytrs2
-    return TPZMatrix<TVar>::Subst_LForward(b);
-}
-
 /**
  * @brief Computes B = Y, where A*Y = B, A is upper triangular with A(i,i)=1.
  * @param b right hand side and result after all
@@ -2015,16 +2003,6 @@ template<>
 int TPZFMatrix<double>::Subst_LBackward( TPZFMatrix<double>* b ) const
 {
     return 1;
-}
-
-/**
- * @brief Computes B = Y, where A*Y = B, A is upper triangular with A(i,i)=1.
- * @param b right hand side and result after all
- */
-template<class TVar>
-int TPZFMatrix<TVar>::Subst_LBackward( TPZFMatrix<TVar>* b ) const
-{
-    return TPZMatrix<TVar>::Subst_LBackward(b);
 }
 
 /**
@@ -2047,6 +2025,28 @@ int TPZFMatrix<double>::Subst_Diag( TPZFMatrix<double>* b ) const
     return 1;
 }
 
+#endif
+/**
+ * @brief Computes B = Y, where A*Y = B, A is lower triangular with A(i,i)=1.
+ * @param b right hand side and result after all
+ */
+template<class TVar>
+int TPZFMatrix<TVar>::Subst_LForward( TPZFMatrix<TVar>* b ) const
+{
+    //    ssytrs2
+    return TPZMatrix<TVar>::Subst_LForward(b);
+}
+
+/**
+ * @brief Computes B = Y, where A*Y = B, A is upper triangular with A(i,i)=1.
+ * @param b right hand side and result after all
+ */
+template<class TVar>
+int TPZFMatrix<TVar>::Subst_LBackward( TPZFMatrix<TVar>* b ) const
+{
+    return TPZMatrix<TVar>::Subst_LBackward(b);
+}
+
 /**
  * @brief Computes B = Y, where A*Y = B, A is diagonal matrix.
  * @param b right hand side and result after all
@@ -2056,7 +2056,6 @@ int TPZFMatrix<TVar>::Subst_Diag( TPZFMatrix<TVar>* b ) const
 {
     return TPZMatrix<TVar>::Subst_Diag(b);
 }
-#endif //USING_LAPACK
 
 /** @brief Implement dot product for matrices */
 template<class TVar>
@@ -3133,22 +3132,6 @@ Fad<REAL> Norm(const TPZFMatrix<Fad<REAL> > &A)
     PZError<<" when configuring NeoPZ library"<<std::endl;              \
     DebugStop();\
     return -1;
-
-
-template<class TVar>
-int TPZFMatrix<TVar>::Subst_Forward( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
-
-template<class TVar>
-int TPZFMatrix<TVar>::Subst_Backward( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
-
-template<class TVar>
-int TPZFMatrix<TVar>::Subst_LForward( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
-
-template<class TVar>
-int TPZFMatrix<TVar>::Subst_LBackward( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
-
-template<class TVar>
-int TPZFMatrix<TVar>::Subst_Diag( TPZFMatrix<TVar>* b ) const{NON_LAPACK}
 
 template<class TVar>
 int TPZFMatrix<TVar>::SolveEigenProblem(TPZVec < std::complex<double> > &w, TPZFMatrix < std::complex<double> > &eigenVectors){NON_LAPACK}

Matrix/pzfmatrix.h
@@ -357,31 +357,31 @@ public:
     
 
     /**
-     * @brief Computes B = Y, where A*Y = B, A is lower triangular. DEPENDS ON LAPACK.
+     * @brief Computes B = Y, where A*Y = B, A is lower triangular. If LAPACK is available, it will use its implementation.
      * @param b right hand side and result after all
      */
     virtual int Subst_Forward( TPZFMatrix<TVar>* b ) const override;
     
     /**
-     * @brief Computes B = Y, where A*Y = B, A is upper triangular. DEPENDS ON LAPACK.
+     * @brief Computes B = Y, where A*Y = B, A is upper triangular. If LAPACK is available, it will use its implementation.
      * @param b right hand side and result after all
      */
     virtual int Subst_Backward( TPZFMatrix<TVar>* b ) const override;
     
     /**
-     * @brief Computes B = Y, where A*Y = B, A is lower triangular with A(i,i)=1. DEPENDS ON LAPACK.
+     * @brief Computes B = Y, where A*Y = B, A is lower triangular with A(i,i)=1. If LAPACK is available, it will use its implementation.
      * @param b right hand side and result after all
      */
     virtual int Subst_LForward( TPZFMatrix<TVar>* b ) const override;
     
     /**
-     * @brief Computes B = Y, where A*Y = B, A is upper triangular with A(i,i)=1. DEPENDS ON LAPACK.
+     * @brief Computes B = Y, where A*Y = B, A is upper triangular with A(i,i)=1. If LAPACK is available, it will use its implementation.
      * @param b right hand side and result after all
      */
     virtual int Subst_LBackward( TPZFMatrix<TVar>* b ) const override;
     
     /**
-     * @brief Computes B = Y, where A*Y = B, A is diagonal matrix. DEPENDS ON LAPACK.
+     * @brief Computes B = Y, where A*Y = B, A is diagonal matrix. If LAPACK is available, it will use its implementation.
      * @param b right hand side and result after all
      */
     virtual int Subst_Diag( TPZFMatrix<TVar>* b ) const override;

Matrix/pzsbndmat.cpp
@@ -624,7 +624,7 @@ TPZSBMatrix<TVar>::SetBand(int64_t newBand )
 /********************* Resolucao de sistemas *********************/
 
 
-#ifdef USING_LAPACK
+
 /**************************/
 /*** Decompose Cholesky ***/
 template<class TVar>
@@ -634,6 +634,14 @@ TPZSBMatrix<TVar>::Decompose_Cholesky(std::list<int64_t> &singular)
     return Decompose_Cholesky();
 }
 
+template<class TVar>
+int
+TPZSBMatrix<TVar>::Decompose_Cholesky()
+{
+    return TPZMatrix<TVar>::Decompose_Cholesky();
+}
+
+#ifdef USING_LAPACK
 template<>
 int
 TPZSBMatrix<std::complex< float > >::Decompose_Cholesky()
@@ -748,13 +756,6 @@ int TPZSBMatrix<double>::Decompose_Cholesky()
     }
     fDecomposed = ECholesky;
     return 1;
-}
-
-template<class TVar>
-int
-TPZSBMatrix<TVar>::Decompose_Cholesky()
-{
-    return TPZMatrix<TVar>::Decompose_Cholesky();
 }
 #endif
 
@@ -1991,11 +1992,7 @@ TPZSBMatrix<complex<double> >::SolveGeneralisedEigenProblem(TPZSBMatrix<complex<
   PZError<<" when configuring NeoPZ library"<<std::endl;\
   DebugStop();\
   return -1;
-template<class TVar>
-int TPZSBMatrix<TVar>::Decompose_Cholesky(){NON_LAPACK}
 
-template<class TVar>
-int TPZSBMatrix<TVar>::Decompose_Cholesky(std::list<int64_t> &singular){NON_LAPACK}
 
 template<class TVar>
 int TPZSBMatrix<TVar>::SolveEigenProblem(TPZVec < std::complex<double> > &w, TPZFMatrix < std::complex<double> > &eigenVectors){NON_LAPACK}

Matrix/pzsbndmat.h
@@ -107,9 +107,9 @@ public:
     
     /// To solve linear systems
     // @{
-    //Depends on LAPACK
+    //If LAPACK is available, it will use its implementation.
     int Decompose_Cholesky() override;  // Faz A = GGt.
-    //Depends on LAPACK
+    //If LAPACK is available, it will use its implementation.
     int Decompose_Cholesky(std::list<int64_t> &singular) override;
     
     int Subst_Forward( TPZFMatrix<TVar>*B ) const override;