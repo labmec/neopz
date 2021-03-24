341d73bf4d4000c983beeb4b77c98a1135c42ec1
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-03-24 16:24:50 -0300

    small doxygen cmake fixes

--- cmake/EnableDoxygen.cmake
+++ cmake/EnableDoxygen.cmake
@@ -2,7 +2,9 @@ function(enable_doxygen target)
     if (NOT "$ENV{DOXYGEN_ROOT}" STREQUAL "")
         message(STATUS "Looking for doxygen in $ENV{BOOST_ROOT}")
     endif()
-    find_package(Doxygen)
+    find_package(Doxygen
+        REQUIRED dot
+        REQUIRED)
     # set input and output files
     set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs_doxygen/Doxyfile.in)
     set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile.out)
