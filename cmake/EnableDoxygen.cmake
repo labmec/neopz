5502892c97dd5a0221a8dce8e967883ae431799a
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-03-24 16:10:26 -0300

    Integrated doxygen with cmake

---
+++ cmake/EnableDoxygen.cmake
@@ -0,0 +1,18 @@
+function(enable_doxygen target)
+    if (NOT "$ENV{DOXYGEN_ROOT}" STREQUAL "")
+        message(STATUS "Looking for doxygen in $ENV{BOOST_ROOT}")
+    endif()
+    find_package(Doxygen)
+    # set input and output files
+    set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs_doxygen/Doxyfile.in)
+    set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile.out)
+
+    # request to configure the file
+    configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT})
+
+    add_custom_target(docs
+        COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        COMMENT "Generating API documentation with Doxygen"
+        VERBATIM )
+endfunction()
