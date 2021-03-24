aae3cc2db098155d228d9648fc626d6b80c98520
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-03-24 18:33:56 -0300

    small changes in cmake for doxygen

--- cmake/EnableDoxygen.cmake
+++ cmake/EnableDoxygen.cmake
@@ -8,7 +8,7 @@ function(enable_doxygen target)
     # set input and output files
     set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs_doxygen/Doxyfile.in)
     set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile.out)
-
+    set(DOXYGEN_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/docs_doxygen)
     # request to configure the file
     configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT})
 
