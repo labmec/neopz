9de9c063bcdde194baac2cd5d76d0de1ca0b3f64
Gustavo Batistela <gustavoabatistela@gmail.com>
2021-03-25 16:55:05 -0300

    Fix project compilation when log is disabled

--- Projects/Steam/tpbrsteammesh.cpp
+++ Projects/Steam/tpbrsteammesh.cpp
@@ -49,12 +49,14 @@ TPBrSteamMesh::TPBrSteamMesh(int numcells, REAL temperature, REAL pressure, REAL
 	for (ic=0; ic<numcells; ic++) {
 		TPZManVector<int> equation,state;
 		CellDestination(ic, equation, state);
+#ifdef PZ_LOG
         if(logger.isDebugEnabled())
         {
             std::stringstream sout;
             sout << state[TPBrSteamFlux::EDarcyVelocityWater];
             LOGPZ_DEBUG(logger, sout.str());
         }
+#endif
         fPrevState(state[TPBrSteamFlux::EDarcyVelocityWater],0) = 1.;
 		fPrevState(state[TPBrSteamFlux::NumFluxEq+TPBrCellConservation::ESaturationWater],0) = 1.-oilsaturation;
 		fPrevState(state[TPBrSteamFlux::NumFluxEq+TPBrCellConservation::ESaturationOil],0) = oilsaturation;
