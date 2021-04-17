c45b48a07b0f68f17d6d373a8f5b3792bff639f0
philippedevloo <philippedevloo1958@gmail.com>
2021-04-16 22:58:56 -0300

    Resolved critical bug in the loadsolution method

--- Mesh/pzcmesh.cpp
+++ Mesh/pzcmesh.cpp
@@ -480,7 +480,24 @@ void TPZCompMesh::ExpandSolutionInternal(TPZFMatrix<TVar> &sol) {
 }
 
 void TPZCompMesh::LoadSolution(const TPZSolutionMatrix &mat){
-    fSolution = mat;
+    int64_t nrow = mat.Rows();
+    int64_t ncol = mat.Cols();
+    int64_t solrow = fSolution.Rows();
+    fSolution.Resize(solrow, ncol);
+    int64_t i,j;
+    const TPZFMatrix<STATE> &matst = mat;
+    TPZFMatrix<STATE> &solst = fSolution;
+    STATE val;
+    for(j=0;j<ncol;j++)
+    {
+        for(i=0;i<nrow;i++)
+        {
+            val = (matst.GetVal(i,j));
+            solst(i,j) =  val;
+        }
+    }
+
+    
     const auto nelem = NElements();
 	for(auto i=0; i<nelem; i++) {
 		TPZCompEl *cel = fElementVec[i];
