43214fe6db38124d351e6c2836e3e4ac3618ef9c
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-04-22 09:40:40 -0300

    Created TPZSFStructMatrix

---
+++ StrMatrix/pzsfstrmatrix.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file
+ * @brief Contains the implementation of the TPZSFStructMatrix methods. 
+ */
+
+#include "pzsfstrmatrix.h"
+#include "pzsfulmat.h"
+#include "pzsubcmesh.h"
+#include <sstream>
+#include "pzlog.h"
+
+#ifdef PZ_LOG
+static TPZLogger logger("pz.strmatrix.tpzfstructmatrix");
+static TPZLogger loggerel("pz.strmatrix.element");
+#endif
+
+
+using namespace std;
+template<class TVar, class TPar>
+TPZMatrix<TVar> * TPZSFStructMatrix<TVar,TPar>::Create(){
+	int64_t neq = this->fEquationFilter.NActiveEquations();
+    
+	return new TPZSFMatrix<TVar>(neq);
+}
+
+template<class TVar, class TPar>
+TPZStructMatrix * TPZSFStructMatrix<TVar,TPar>::Clone(){
+    return new TPZSFStructMatrix(*this);
+}
+
+
+template<class TVar, class TPar>
+int TPZSFStructMatrix<TVar,TPar>::ClassId() const{
+    return Hash("TPZSFStructMatrix") ^
+        TPZStructMatrixT<TVar>::ClassId() << 1 ^
+        TPar::ClassId() << 2;
+}
+
+template<class TVar, class TPar>
+void TPZSFStructMatrix<TVar,TPar>::Read(TPZStream& buf, void* context){
+    TPZStructMatrix::Read(buf,context);
+    TPar::Read(buf,context);
+}
+
+template<class TVar, class TPar>
+void TPZSFStructMatrix<TVar,TPar>::Write(TPZStream& buf, int withclassid) const{
+    TPZStructMatrix::Write(buf,withclassid);
+    TPar::Write(buf,withclassid);
+}
+
+#include "pzstrmatrixot.h"
+#include "pzstrmatrixflowtbb.h"
+
+template class TPZSFStructMatrix<STATE,TPZStructMatrixOR<STATE>>;
+template class TPZSFStructMatrix<STATE,TPZStructMatrixOT<STATE>>;
+template class TPZSFStructMatrix<STATE,TPZStructMatrixTBBFlow<STATE>>;
---
+++ StrMatrix/pzsfstrmatrix.h
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * @brief Contains the TPZSFStructMatrix class which implements Full Symmetric Structural Matrices.
+ */
+
+#ifndef TPZSFSTRUCTMATRIX_H
+#define TPZSFSTRUCTMATRIX_H
+
+#include "TPZStructMatrixT.h"
+#include "pzstrmatrixor.h"
+
+/**
+ * @brief Implements Full Structural Matrices. \ref structural "Structural Matrix"
+ * @ingroup structural
+ */
+template<class TVar=STATE, class TPar=TPZStructMatrixOR<TVar>>
+class TPZSFStructMatrix : public TPZStructMatrixT<TVar>,
+                                  public TPar
+{
+public:    	
+    using TPZStructMatrixT<TVar>::TPZStructMatrixT;
+    TPZMatrix<TVar>* Create() override;
+	
+    TPZStructMatrix * Clone() override;
+    //@{
+    //!Read and Write methods
+    int ClassId() const override;
+
+    void Read(TPZStream& buf, void* context) override;
+
+    void Write(TPZStream& buf, int withclassid) const override;
+    //@}
+    
+    friend TPZPersistenceManager;
+};
+
+#endif //TPZFSTRUCTMATRIX_H
