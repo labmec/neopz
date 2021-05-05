6117a5c3f618b6c4a2fede0d969f5f5a1c1404ad
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-05-05 18:15:46 -0300

    Created TPZL2, HDiv and HCurl Projection materials
    Added TPZHDivProjection
    Added TPZHCurlProjection
    adjusted tpzhcurlproj
    adapting projection mats to new big number name
    adjusting projectoin mats to new bndcondval2
    Modified projection to new hierarchy

---
+++ Material/Projection/TPZHDivProjection.cpp
@@ -0,0 +1,247 @@
+#include "TPZHDivProjection.h"
+#include "TPZMaterialDataT.h"
+#include "TPZBndCondT.h"
+#include "pzaxestools.h"
+
+template<class TVar>
+TPZHDivProjection<TVar>::TPZHDivProjection(int id, int dim) :
+    TPZRegisterClassId(&TPZHDivProjection::ClassId),
+    TBase(id), fDim(dim)
+{
+}
+
+template<class TVar>
+TPZMaterial * TPZHDivProjection<TVar>::NewMaterial() const{
+	return new TPZHDivProjection(*this);
+}
+
+template<class TVar>
+void TPZHDivProjection<TVar>::Contribute(const TPZMaterialDataT<TVar> &data,
+                                       REAL weight,
+                                       TPZFMatrix<TVar> &ek, TPZFMatrix<TVar> &ef){
+	
+	const int nshape = data.phi.Rows();
+    //last position of solLoc is the divergence
+    TPZManVector<TVar,4> solLoc(4);
+    if(!this->HasForcingFunction()){
+        PZError<<__PRETTY_FUNCTION__;
+        PZError<<" a forcing function (solution to be projected)\n";
+        PZError<<"has not been set! Aborting...\n";
+        DebugStop();
+    }
+    this->fForcingFunction(data.x,solLoc);
+    const TVar divSol = solLoc[3];
+
+    /*
+     * HDiv approximation spaces in NeoPZ are created 
+     * from the combination of scalar functions with 
+     * constant vector fields
+     */
+    
+    // Setting the scalar phis
+    const TPZFMatrix<REAL> &phiQ = data.phi;
+    const TPZFMatrix<REAL> &dphiQ = data.dphix;
+    
+    const int phrq = data.fVecShapeIndex.NElements();
+    
+    //Calculate the matrix contribution for flux. Matrix A
+    TPZFNMatrix<3,REAL> ivec(3,1,0.);
+    TPZFNMatrix<3,REAL> jvec(3,1,0.);
+    for(int iq=0; iq<phrq; iq++)
+    {
+        //ef(iq, 0) += 0.;
+        const int ivecind = data.fVecShapeIndex[iq].first;
+        const int ishapeind = data.fVecShapeIndex[iq].second;
+        
+        for(int id=0; id<3; id++){
+            ivec(id,0) = data.fDeformedDirections.GetVal(id,ivecind);
+        }
+        TVar ff = 0.;
+        
+        for (int i=0; i<3; i++) {
+            ff += ivec(i,0)*solLoc[i];
+        }
+        
+        ef(iq,0) += weight*ff*phiQ.GetVal(ishapeind,0);
+        REAL divqi = 0.;
+        TPZFNMatrix<3,REAL> axesvec(3,1,0.);
+        data.axes.Multiply(ivec,axesvec);
+        //computin div(qj)
+        for(int iloc=0; iloc<fDim; iloc++){
+            divqi += axesvec(iloc,0)*dphiQ.GetVal(iloc,ishapeind);
+        }
+        for (int jq=0; jq<phrq; jq++)
+        {
+            int jvecind = data.fVecShapeIndex[jq].first;
+            int jshapeind = data.fVecShapeIndex[jq].second;
+            
+            for(int id=0; id<3; id++){
+                jvec(id,0) = data.fDeformedDirections.GetVal(id,jvecind);
+            }
+            
+            //jvecZ.Print("mat1 = ");
+            const REAL prod1 = ivec(0,0)*jvec(0,0) + ivec(1,0)*jvec(1,0) + ivec(2,0)*jvec(2,0);
+            ek(iq,jq) +=
+                    weight*phiQ.GetVal(ishapeind,0)*phiQ.GetVal(jshapeind,0)*prod1;
+
+            REAL divqj = 0.;
+            TPZFNMatrix<3,REAL> axesvec(3,1,0.);
+            data.axes.Multiply(jvec,axesvec);
+            //computin div(qj)
+            for(int jloc=0; jloc<fDim; jloc++){
+                divqj += axesvec(jloc,0)*dphiQ.GetVal(jloc,jshapeind);
+            }
+            ek(iq,jq) += weight*divqi*divqj;
+        }
+    }
+}
+
+template<class TVar>
+void TPZHDivProjection<TVar>::ContributeBC(const TPZMaterialDataT<TVar> &data,
+                                         REAL weight,
+                                         TPZFMatrix<TVar> &ek, TPZFMatrix<TVar> &ef,
+                                         TPZBndCondT<TVar> &bc)
+{
+	const auto &phi = data.phi;
+	const int phr = phi.Rows();
+
+    const TVar v2 = [&bc = std::as_const(bc),
+                     &data = std::as_const(data)](){
+        if(bc.HasForcingFunctionBC()){
+            TPZManVector<TVar> res(3);
+            TPZFNMatrix<9,TVar> dummy(3,3,0.);
+            bc.ForcingFunctionBC()(data.x,res,dummy);
+            return res[0];
+        }else {
+            return bc.Val2()[0];
+        }
+    }();
+    
+	switch (bc.Type()){
+			// Dirichlet condition
+		case 0 : 
+            for(auto in = 0; in < phr; in++) {
+                ef(in,0) +=
+                    (TVar)TPZMaterial::fBigNumber *
+                    v2 *phi.GetVal(in,0) * (TVar)weight;
+					for (auto jn = 0 ; jn < phr; jn++) {
+						ek(in,jn) +=
+                            TPZMaterial::fBigNumber *
+                            phi.GetVal(in,0) * phi.GetVal(jn,0) * weight;
+					}//jn
+				}//in
+			break;
+			// Neumann condition
+		case 1 :
+            for(auto in = 0 ; in < phr; in++) {
+                ef(in,0) +=
+                    v2 * (TVar)fScale * (TVar)phi.GetVal(in,0) * (TVar)weight;
+            }//in
+			break;
+        case 2 :			// mixed condition
+            for(int iq = 0; iq < phr; iq++) {
+                ef(iq,0) += v2*phi.GetVal(iq,0)*weight;
+                for (int jq = 0; jq < phr; jq++) {
+                    ek(iq,jq) += weight*bc.Val1().GetVal(0,0)*phi.GetVal(iq,0)*phi.GetVal(jq,0);
+                }
+            }
+		default:{
+			std::cout << __PRETTY_FUNCTION__ << " at line " << __LINE__ << " not implemented\n";
+		}
+	}//switch
+	
+}
+template<class TVar>
+void TPZHDivProjection<TVar>::GetSolDimensions(uint64_t &u_len,
+                                             uint64_t &du_row,
+                                             uint64_t &du_col) const
+{
+    u_len=3;
+    du_row=1;
+    du_col=1;
+}
+
+
+template<class TVar>
+int TPZHDivProjection<TVar>::VariableIndex(const std::string &name) const{
+	if(!strcmp("Solution",name.c_str())) return ESolution;
+    if(!strcmp("Divergence",name.c_str())) return EDivergence;
+	return TPZMaterial::VariableIndex(name);
+}
+
+template<class TVar>
+int TPZHDivProjection<TVar>::NSolutionVariables(int var) const{
+	if(var == ESolution) return fDim;
+    if (var == EDivergence) {
+        return 1;
+    }
+	
+    return TPZMaterial::NSolutionVariables(var);
+}
+
+template<class TVar>
+void TPZHDivProjection<TVar>::Solution(const TPZMaterialDataT<TVar> &data,
+                                     int var, TPZVec<TVar> &solOut)
+{
+    const auto &sol = data.sol[0];
+    const auto &dsol = data.dsol[0];
+	if (var == ESolution){
+        solOut.Resize(sol.size());
+        for (int i=0; i<sol.size(); i++) {
+            solOut[i] = sol[i];
+        }
+		return;
+	}
+    if (var == EDivergence) {
+        solOut.Resize(fDim);
+        for (int i=0; i<fDim; i++) {
+            solOut[i] = dsol.GetVal(i,0);
+        }
+        return;
+    }
+}
+
+template<class TVar>
+void TPZHDivProjection<TVar>::Errors(const TPZVec<REAL> &x,
+                                     const TPZVec<TVar> &u,
+                                     const TPZFMatrix<TVar> &dudx,
+                                     const TPZFMatrix<REAL> &axes,
+                                     TPZVec<REAL> &values)
+{
+
+    TPZManVector<TVar,3> u_exact={0.,0.,0.};
+    TPZFNMatrix<3,TVar> du_exact(1,1,0.);
+    this->ExactSol()(x,u_exact,du_exact);
+    values.Resize(this->NEvalErrors());
+    values.Fill(0.0);
+    TPZManVector<TVar> sol(1),dsol(3,0.);
+    TPZFNMatrix<3,TVar> gradu(3,1);
+    TPZAxesTools<TVar>::Axes2XYZ(dudx,gradu,axes);
+    
+    //values[0] : error in HDiv norm
+    //values[1] : error in L2 norm
+    //values[2] : error in HDiv semi-norm
+    TVar diff = (u[0] - u_exact[0]);
+    if constexpr (is_complex<TVar>::value){
+        values[1]  = std::real((diff*std::conj(diff)));
+    }else{
+        values[1]  = diff*diff;
+    }
+
+    if constexpr (is_complex<TVar>::value){
+        values[2] = std::real(std::conj(du_exact(0,0)) * dudx.GetVal(0,0));
+    }else{
+        values[2] = du_exact(0,0) * dudx.GetVal(0,0);
+    }
+    
+    values[0]  = values[1]+values[2];
+}
+
+template<class TVar>
+int TPZHDivProjection<TVar>::ClassId() const{
+    return Hash("TPZHDivProjection") ^ TBase::ClassId() << 1;
+}
+
+
+template class TPZHDivProjection<STATE>;
+template class TPZHDivProjection<CSTATE>;
---
+++ Material/Projection/TPZHDivProjection.h
@@ -0,0 +1,99 @@
+/**
+ * @file TPZHDivProjection.h
+ * @brief Header file for TPZHDivProjection.
+ * It implements a projection of a given solution using the HDiv norm
+ */
+
+#ifndef TPZHDIVPROJECTION_H
+#define TPZHDIVPROJECTION_H
+
+#include "TPZMatBase.h"
+#include "TPZMatSingleSpace.h"
+#include "TPZMatErrorSingleSpace.h"
+
+/**
+ * @brief Implements a projection of a given solution on a HDiv approximation space.
+ * This material uses a HDiv approximation space in a geometric domain of
+ * dimension defined by the user (2 or 3D) and projects a given solution using
+ * the HDiv norm.
+ * The solution to be projected is set by the Forcing Function of the 
+ * TPZMaterialT.
+ * It is expected to be a TPZVec<TVar> of size 4, as the solution is always in
+ * a 3D space and the last position is the divergence.
+ * For the error analysis, the solution is set through the ExactSol of
+ * TPZMatErrorSingleSpace.
+ */
+template<class TVar=STATE>
+class  TPZHDivProjection :
+    public TPZMatBase<TVar,TPZMatSingleSpaceT<TVar>,TPZMatErrorSingleSpace<TVar>>
+{
+	using TBase = TPZMatBase<TVar,TPZMatSingleSpaceT<TVar>,TPZMatErrorSingleSpace<TVar>>;
+public:
+    //! Default constructor
+    TPZHDivProjection();
+    /** @brief Creates the TPZHDivProjection material.
+     * @param id material identifier
+     * @param dim dimension
+     */
+    TPZHDivProjection(int id, int dim);
+    
+    std::string Name() const override { return "TPZHDivProjection"; }
+
+    /** @brief Solution indices of post-processing */
+	enum ESolutionVars { ENone = 0, ESolution = 1 , EDivergence = 2};
+    /**
+     * @brief Set a scale factor for the stiffness matrix and right hand side
+     * the default value of the scale factor is 1
+     */
+    void SetScaleFactor(REAL scale)
+    {fScale = scale;}
+    
+    [[nodiscard]] REAL ScaleFactor() const
+    {return fScale;}
+
+	int Dimension() const  override { return this->fDim; }
+    
+    /** @brief Sets problem dimension */
+    virtual void SetDimension(int dim) { this->fDim = dim; }
+	
+    int NStateVariables() const override { return 1; }
+    void Contribute(const TPZMaterialDataT<TVar> &data,
+                    REAL weight,
+                    TPZFMatrix<TVar> &ek, TPZFMatrix<TVar> &ef) override;
+
+    void ContributeBC(const TPZMaterialDataT<TVar> &data, REAL weight,
+                      TPZFMatrix<TVar> &ek, TPZFMatrix<TVar> &ef,
+                      TPZBndCondT<TVar> &bc) override;
+    /** @brief To create another material of the same type */
+	TPZMaterial * NewMaterial() const override;
+	
+	/** @brief It returns the variable index associated with the name */
+	int VariableIndex(const std::string &name) const override;
+	
+	int NSolutionVariables(int var) const override;
+
+    void Solution(const TPZMaterialDataT<TVar> &data,
+                  int var, TPZVec<TVar> &solOut) override;
+    
+    void GetSolDimensions(uint64_t &u_len,
+                          uint64_t &du_row,
+                          uint64_t &du_col) const override;
+
+    void Errors(const TPZVec<REAL> &x, const TPZVec<TVar> &sol,
+                const TPZFMatrix<TVar> &dsol,
+                const TPZFMatrix<REAL> &axes,
+                TPZVec<REAL> &errors) override;
+    
+    virtual int ClassId() const override;
+protected:
+    /** @brief Problem dimension */
+	int fDim;
+    
+    /** @brief Scale factor applied to the stiffness matrix and right hand side */
+    REAL fScale{1};
+};
+
+extern template class TPZHDivProjection<STATE>;
+extern template class TPZHDivProjection<CSTATE>;
+#endif
+
