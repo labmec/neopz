6808493dfca9d15aebb0e37125f9720b0ed23272
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-05-10 15:41:10 -0300

    Added Common/TPZElectromagneticConstants.h

---
+++ Common/TPZElectromagneticConstants.h
@@ -0,0 +1,7 @@
+#include "pzreal.h"
+
+namespace pzeletromag{
+  constexpr STATE cZero{299792458}; //freespace speed of light
+  constexpr STATE muZero{1.256637061435917e-6};//freespace permeability
+  constexpr STATE epsilonZero{8.85418781762039e-12};//freespace permittivity
+};
