a235c37ef8bac629226a66450921b0b6b4a7bb74
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-05-10 20:03:38 -0300

    Added Latex References support to Sphinx

--- cmake/EnableSphinx.cmake
+++ cmake/EnableSphinx.cmake
@@ -5,8 +5,11 @@ function(enable_sphinx target)
 
     set(SPHINX_IN ${CMAKE_CURRENT_SOURCE_DIR}/conf.py.in)
     set(SPHINX_OUT ${CMAKE_CURRENT_BINARY_DIR}/conf.py)
-    
+
+    set(REFS_IN ${CMAKE_CURRENT_SOURCE_DIR}/references.bib)
+    set(REFS_OUT ${CMAKE_CURRENT_BINARY_DIR}/references.bib)
     configure_file(${SPHINX_IN} ${SPHINX_OUT} @ONLY)
+    configure_file(${REFS_IN} ${REFS_OUT} @ONLY)
     add_custom_target(run-sphinx
         COMMAND
         ${SPHINX_EXECUTABLE} -E -b html -c ${CMAKE_CURRENT_BINARY_DIR}
