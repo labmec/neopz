8e101b09cdc953f79a1ae9f1368b8cd80918592c
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-05-25 18:39:50 -0300

    working on TPZKrylovEigenSolver::SolveEigenProblem

--- Solvers/EigenSolvers/TPZKrylovEigenSolver.cpp
+++ Solvers/EigenSolvers/TPZKrylovEigenSolver.cpp
@@ -5,10 +5,67 @@
 template<class TVar>
 int TPZKrylovEigenSolver<TVar>::SolveEigenProblem(TPZVec<CTVar> &w,TPZFMatrix<CTVar> &eigenVectors)
 {
+#ifndef USING_LAPACK
   PZError<<__PRETTY_FUNCTION__;
-  PZError<<"\nERROR: Not yet implemented.\nAborting...\n";
+  PZError<<"\nERROR: NeoPZ was not linked against LAPACK. Aborting...\n";
   DebugStop();
-  return -1;
+#endif
+  TPZSimpleTimer total("ArnoldiSolver");
+  auto &matA = this->fMatrixA.operator*();
+  const int nRows = matA.Rows();
+  TPZAutoPointer<TPZMatrix<TVar>> invMat = matA.NewMatrix();
+  TPZAutoPointer<TPZMatrix<TVar>> shiftedMat = matA.Clone();
+  //calculating A-sigmaB
+  const auto &shift = Shift();
+  for(int i = 0; i < nRows; i++) shiftedMat->PutVal(i,i,shiftedMat->GetVal(i,i)-shift);
+  // if (shiftedMat->IsSymmetric()) shiftedMat->Decompose_LDLt();
+  // else shiftedMat->Decompose_LU();
+  
+  const int &n = NEigenpairs();
+  const int &krylovDim = KrylovDim();
+  TPZManVector<TPZAutoPointer<TPZFMatrix<TVar>>,20> qVecs;
+  TPZFNMatrix<400,TVar> h(krylovDim,krylovDim,0.);
+
+  
+  auto success = ArnoldiIteration(*shiftedMat,qVecs,h);
+  if(!success){
+    return -1;
+  }
+  TPZFNMatrix<400,CTVar> lapackEV(n,n,0.);
+  
+
+  auto lapackres = [&h,&w,&lapackEV]()
+  {
+    TPZSimpleTimer lapacktimer("Hessenberg EVP");
+    TPZLapackEigenSolver<TVar> lapack;
+    return lapack.SolveHessenbergEigenProblem(h, w, lapackEV);
+  }();
+  if(lapackres) return lapackres;
+  // for (int i = 0; i < n; i++)
+  //   w[i] = (TVar)1.0/w[i] + shift;
+
+    
+  eigenVectors.Redim(nRows,n);
+  {
+    TPZSimpleTimer evTimer("Computing eigenvectors");
+    for (int i = 0; i< n; i++){//which eigenvector from A
+      for (int j = 0; j < krylovDim; j++){//which vector from Q
+        CTVar *ev = &eigenVectors.g(0,i);
+        const auto lev = lapackEV(j,i);
+        TVar *q = &qVecs[j]->g(0,0);
+        /*
+          The following loop computes
+          eigenVectors(k,i) += lev * qvec.GetVal(k,0),
+          with const auto qvec = *qVecs[j],
+          but twice as fast
+        */
+        for(int k = 0; k < nRows; k++)
+          *ev++ += lev * *q++;
+      }
+    }
+  }
+  
+  return lapackres;
 }
 
 template<class TVar>
