8e763a634844e0b1d5c22987f99ff7769d8726db
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-05-30 11:04:24 -0300

    NeoPZExamples contents are now unpacked in $neopzsrc/Projects

--- cmake/EnableNeoPZExamples.cmake
+++ cmake/EnableNeoPZExamples.cmake
@@ -3,7 +3,10 @@ function(enable_neopzexamples)
     FetchContent_Declare(
         NeoPZExamples
         GIT_REPOSITORY https://github.com/labmec/NeoPZExamples.git
-        GIT_TAG        main)
+        GIT_TAG        main
+        DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}/Projects
+        SOURCE_DIR ${PROJECT_SOURCE_DIR}/Projects
+        BINARY_DIR ${PROJECT_BINARY_DIR}/Projects)
     message(STATUS "Checking NeoPZExamples...")
     FetchContent_MakeAvailable(NeoPZExamples)
     message(STATUS "NeoPZExamples found at ${NeoPZExamples_SOURCE_DIR}")
