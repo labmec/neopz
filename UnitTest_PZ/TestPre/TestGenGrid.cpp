554e125878a437819b7256f3a91de02070033a82
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-06-21 16:34:10 -0300

    added jacobian test for gengrid2d

--- UnitTest_PZ/TestPre/TestGenGrid.cpp
+++ UnitTest_PZ/TestPre/TestGenGrid.cpp
@@ -7,6 +7,7 @@
 
 #include "pzgmesh.h"
 #include "TPZGenGrid3D.h"
+#include "TPZGenGrid2D.h"
 
 #include <stdio.h>
 #include <iostream>
@@ -17,6 +18,8 @@
 namespace gengridtest{
     /** @brief Testing aspects of TPZGenGrid3D*/
     void TestGenGrid3D(MMeshType meshtype);
+    /** @brief Testing aspects of TPZGenGrid2D*/
+    void TestGenGrid2D(MMeshType meshtype);
     /** @brief Require positive detjacobian of all elements in the mesh*/
     void TestJacobian(TPZGeoMesh* gmesh);
 }
@@ -33,6 +36,18 @@ namespace gengridtest{
         gengridtest::TestGenGrid3D(MMeshType::EHexaPyrMixed);   /*For 3D meshes where alternating cubes are divided into six pyramids*/
         REQUIRE_THROWS(gengridtest::TestGenGrid3D(MMeshType::ENoType));             
     }
+/** @brief Test case to verify consistency of TPZGenGrid2D*/
+    TEST_CASE("GenGrid2D_tests_1","[GenGrid2D_tests]")
+    {
+        gengridtest::TestGenGrid2D(MMeshType::EQuadrilateral);  /*For 2D meshes consisting of quadrilaterals*/
+        gengridtest::TestGenGrid2D(MMeshType::ETriangular);     /*For 2D meshes where each quadrilateral is split into two triangles*/
+        REQUIRE_THROWS(gengridtest::TestGenGrid2D(MMeshType::EHexahedral));     /*For 2D meshes consisting of hexahedra*/
+        REQUIRE_THROWS(gengridtest::TestGenGrid2D(MMeshType::ETetrahedral));    /*For 2D meshes where each hexahedron is divided into five tetrahedra*/
+        REQUIRE_THROWS(gengridtest::TestGenGrid2D(MMeshType::EPyramidal));      /*For 2D meshes where each hexahedron is divided into six pyramids*/
+        REQUIRE_THROWS(gengridtest::TestGenGrid2D(MMeshType::EPrismatic));      /*For 2D meshes where each hexahedron is divided into two prisms*/
+        REQUIRE_THROWS(gengridtest::TestGenGrid2D(MMeshType::EHexaPyrMixed));   /*For 2D meshes where alternating cubes are divided into six pyramids*/
+        REQUIRE_THROWS(gengridtest::TestGenGrid2D(MMeshType::ENoType));             
+    }
     /** Suggestions for future tests to add:
      * - Initialize and test ALL grid generating tools available in PZ
     */
@@ -46,6 +61,19 @@ namespace gengridtest{
         // Create GenGrid
         TPZGenGrid3D gengrid({-6,-6,-6},{6,6,6},{3,3,3},meshtype);
         TPZGeoMesh* gmesh = gengrid.BuildVolumetricElements(1);
+        // Check determinant of jacobian matrix for all elements in the mesh
+        TestJacobian(gmesh);
+
+        delete gmesh;
+    }
+
+    /** @brief Testing aspects of TPZGenGrid3D*/
+    void TestGenGrid2D(MMeshType meshtype){
+        // Create GenGrid
+        TPZGenGrid2D gengrid({3,3},{-2.,-2.},{2.,2.});
+        gengrid.SetElementType(meshtype);
+        TPZGeoMesh* gmesh = new TPZGeoMesh;
+        gengrid.Read(gmesh,1);
         // Check determinant of jacobian matrix for all elements in the mesh
         TestJacobian(gmesh);
 
