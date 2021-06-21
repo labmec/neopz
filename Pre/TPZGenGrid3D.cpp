1ae2313885f1dd6f999ee95b72c9e1284c19cae1
Pedro Lima <pedrolima.ec@outlook.com>
2021-06-21 10:06:42 -0300

    [BugFix] HexIntoPyramids RefPattern

--- Pre/TPZGenGrid3D.cpp
+++ Pre/TPZGenGrid3D.cpp
@@ -180,11 +180,11 @@ TPZGeoMesh* TPZGenGrid3D::BuildVolumetricElements(const int matIdDomain){
                 " 0.     0.     0.  "
                 "7     8     0     1     2     3     4     5     6     7  "
                 "5     5     0     1     2     3     8 "
-                "5     5     0     1     5     4     8 "
-                "5     5     1     2     6     5     8 "
-                "5     5     2     3     7     6     8 "
-                "5     5     3     0     4     7     8 "
-                "5     5     4     5     6     7     8 "
+                "5     5     0     4     5     1     8 "
+                "5     5     1     5     6     2     8 "
+                "5     5     2     6     7     3     8 "
+                "5     5     3     7     4     0     8 "
+                "5     5     7     6     5     4     8 "
         ;
         std::istringstream str(buf);
         TPZAutoPointer<TPZRefPattern> refpat = new TPZRefPattern(str);
