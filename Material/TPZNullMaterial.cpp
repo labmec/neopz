9ecdb83e1117447b6013a24feec43e7f4287230f
Gustavo Batistela <gustavoabatistela@gmail.com>
2021-06-25 12:31:23 -0300

    Add Solution and NSolutionVariables methods to TPZNullMaterial

--- Material/TPZNullMaterial.cpp
+++ Material/TPZNullMaterial.cpp
@@ -4,7 +4,7 @@
 
 #include "TPZNullMaterial.h"
 #include "TPZStream.h"
-#include "TPZMaterialData.h"
+#include "TPZMaterialDataT.h"
 
 template<class TVar>
 int TPZNullMaterial<TVar>::ClassId() const {
@@ -61,6 +61,33 @@ void TPZNullMaterial<TVar>::Print(std::ostream &out) const {
 template<class TVar>
 TPZMaterial *TPZNullMaterial<TVar>::NewMaterial() const {
     return new TPZNullMaterial<TVar>(*this);
+}
+
+template<class TVar>
+void TPZNullMaterial<TVar>::Solution(const TPZMaterialDataT<TVar> &data, int var, TPZVec<TVar> &sol) {
+    if (var == 0) {
+        sol = data.sol[0];
+    } else if (var == 1) {
+        TVar val = 0.;
+        for (int i = 0; i < 3; i++) {
+            val += data.dsol[0](i, i);
+        }
+        sol[0] = val;
+    } else {
+        DebugStop();
+    }
+}
+
+template<class TVar>
+int TPZNullMaterial<TVar>::NSolutionVariables(int var) const {
+    if (var == 0) {
+        return 1;
+    } else if (var == 1) {
+        return 3;
+    } else {
+        DebugStop();
+        return 0;
+    }
 }
 
 template class TPZNullMaterial<STATE>;
