59a3dda1e28369793bec8047e816e4c80c89b67b
gustavobat <gustavoabatistela@gmail.com>
2021-08-17 17:32:42 -0300

    Add doc to TPZDarcyFlow methods

--- Material/DarcyFlow/TPZDarcyFlow.h
+++ Material/DarcyFlow/TPZDarcyFlow.h
@@ -59,11 +59,24 @@ public:
      */
     virtual void SetDimension(int dim);
 
-    // TODO document method
+    /**
+     * @brief It computes a contribution to the stiffness matrix and load vector at one integration point
+     * @param[in] data stores all input data
+     * @param[in] weight is the weight of the integration rule
+     * @param[out] ek is the element matrix
+     * @param[out] ef is the rhs vector
+     */
     void Contribute(const TPZMaterialDataT<STATE> &data, STATE weight, TPZFMatrix<STATE> &ek,
                     TPZFMatrix<STATE> &ef) override;
 
-    // TODO document method
+    /**
+     * @brief It computes a contribution to the stiffness matrix and load vector at one BC integration point
+     * @param[in] data stores all input data
+     * @param[in] weight is the weight of the integration rule
+     * @param[out] ek is the element matrix
+     * @param[out] ef is the rhs vector
+     * @param[in] bc is the boundary condition material
+     */
     void ContributeBC(const TPZMaterialDataT<STATE> &data, STATE weight, TPZFMatrix<STATE> &ek, TPZFMatrix<STATE> &ef,
                       TPZBndCondT<STATE> &bc) override;
 
@@ -79,19 +92,40 @@ public:
      */
     [[nodiscard]] int NSolutionVariables(int var) const override;
 
-    // TODO document method
+    /**
+     * @brief Returns the solution associated with the var index based on the
+     * finite element approximation at a point
+     * @param [in] data material data associated with a given integration point
+     * @param [in] var index of the variable to be calculated
+     * @param [out] solOut vector to store the solution
+     */
     void Solution(const TPZMaterialDataT<STATE> &data, int var, TPZVec<STATE> &solOut) override;
 
-    // TODO document method
+    /**
+     * @brief Get the dimensions of the solution for each state variable
+     *
+     * This will be used for initializing the corresponding TPZMaterialData
+     * @param [out] u_len solution dimension
+     * @param [out] du_row number of rows of the derivative
+     * @param [out] du_col number of columns of the derivative
+     */
     void GetSolDimensions(uint64_t &u_len, uint64_t &du_row, uint64_t &du_col) const override;
 
-    // TODO document method
+    /**
+     * @brief Calculates the approximation error at a point
+     * @param [in] data material data of the integration point
+     * @param [out] errors calculated errors
+     */
     void Errors(const TPZMaterialDataT<STATE> &data, TPZVec<REAL> &errors) override;
 
-    // TODO add doc
+    /*
+     * @brief fill requirements for volumetric contribute
+     */
     void FillDataRequirements(TPZMaterialData &data) const override;
 
-    // TODO add doc
+    /*
+     * @brief fill requirements for boundary contribute
+     */
     void FillBoundaryConditionDataRequirements(int type, TPZMaterialData &data) const override;
 
     /**
