8f203c319cec90ebde983ba7232cb65a1da24837
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-09-30 10:01:57 -0300

    Introduced TPZShapeH1 and TPZShapeHDiv
    Co-authored-by: philippedevloo <philippedevloo1958@gmail.com>
    Co-authored-by: Nathan Shauer <nathan.sh@gmail.com>

---
+++ Shape/TPZShapeHDiv.h
@@ -0,0 +1,39 @@
+#ifndef TPZSHAPEHDIV_H
+#define TPZSHAPEHDIV_H
+
+#include "pzreal.h"
+#include "pzvec.h"
+#include "pztrnsform.h"
+template<class T>
+class TPZFMatrix;
+
+class TPZShapeData;
+
+/// Traditional HDiv spaces, data structures that do not depend on the geometric map
+template <class TSHAPE>
+struct TPZShapeHDiv : public TSHAPE
+{
+    
+    TPZShapeHDiv();
+    
+    static void Initialize(TPZVec<int64_t> &ids,
+                    TPZVec<int> &connectorders,                    
+                    TPZVec<int>& sideorient, TPZShapeData &data);
+    
+    static void ComputeMasterDirections(TPZShapeData &data);
+    
+    static void ComputeVecandShape(TPZShapeData &data);
+    
+    static void IndexShapeToVec(TPZShapeData &data);
+    
+    static void FirstShapeIndex(TPZVec<int64_t> &Index, TPZVec<int> &scalarorders);
+    
+    static void FillOrderScalarShapeFunctions(const TPZVec<int> &connectorders, TPZVec<int> &scalarOrder);
+    
+    static void HDivPermutation(int side, TPZShapeData &data, TPZVec<int> &permutegather);
+    
+    static void Shape(TPZVec<REAL> &pt, TPZShapeData &data, TPZFMatrix<REAL> &phi, TPZFMatrix<REAL> &dphi);
+
+};
+
+#endif
