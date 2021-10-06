f37b7be933eb0a52ed9cfc23c74fcd88d19ed2ce
philippedevloo <philippedevloo1958@gmail.com>
2021-10-06 08:14:28 -0300

    Including the study on singular modes of HCurl spaces

--- Shape/TPZShapeHDivKernel.cpp
+++ Shape/TPZShapeHDivKernel.cpp
@@ -35,8 +35,26 @@ void TPZShapeHDivKernel<TSHAPE>::ComputeVecandShape(TPZShapeData &data) {
     std::map<orderpair ,std::set<int>> ShapeRemove;
     ShapeRemove[orderpair(ETriangle,2)] = {0};
     ShapeRemove[orderpair(ETriangle,3)] = {0,1,7};
-    ShapeRemove[orderpair(ETetraedro,3)] ={0};
-
+    ShapeRemove[orderpair(ETriangle,4)] = {6,9,10,11,13,14};
+    ShapeRemove[orderpair(ETriangle,5)] = {8,12,13,14,16,17,20,21,22,23};
+    ShapeRemove[orderpair(ETetraedro,3)] = {0};
+    ShapeRemove[orderpair(ETetraedro,4)] = {9,12,13,14};
+    ShapeRemove[orderpair(ETetraedro,5)] = {18,24,25,26,29,31,32,33,34,35};
+    ShapeRemove[orderpair(EQuadrilateral,1)] = {3};
+    ShapeRemove[orderpair(EQuadrilateral,2)] = {6,8,9,11};
+    ShapeRemove[orderpair(EQuadrilateral,3)] = {9,12,13,15,17,18,20,22,23};
+    ShapeRemove[orderpair(EQuadrilateral,4)] = {12,16,17,19,21,23,24,26,28,30,31,33,35,37,38,39};
+    ShapeRemove[orderpair(EQuadrilateral,5)] = {15,20,21,23,25,27,29,30,32,34,36,38,39,41,43,45,47,48,50,52,54,56,57,58,59};
+    ShapeRemove[orderpair(ECube,1)] = {5};
+    ShapeRemove[orderpair(ECube,2)] = {11,17,19,20,21,24,25,26,27,28,29,30,31,32,33,34,35};
+    ShapeRemove[orderpair(ECube,3)] = {22,23,24,25,26,37,38,40,41,42,43,44,45,46,47,50,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,
+                                       71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107};
+    ShapeRemove[orderpair(ECube,4)] = {19,23,27,31,37,38,40,41,42,43,46,47,51,55,59,63,65,66,67,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,
+                                       86,87,90,91,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,
+                                       120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,
+                                       148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,
+                                       176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,
+                                       204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239};
     for (int iedge = 0; iedge < nEdges; iedge++) {
         data.fHDivNumConnectShape[iedge] = 1;
         data.fHDivConnectOrders[iedge] = 0;
@@ -100,9 +118,25 @@ void TPZShapeHDivKernel<TSHAPE>::Shape(TPZVec<REAL> &pt, TPZShapeData &data, TPZ
     divphi.Zero();
     const auto nEdges = TSHAPE::NumSides(1);
 
+    int curldim = dim < 3 ? 1 : dim;
     // AQUI TEM QUE CALCULAR AS FUNCOES HCURL CONSTANTES
-    // Jeferson deve ter colocado em outro branch
-    DebugStop();
+    TPZFNMatrix<12,REAL> vecDiv(dim,nEdges), curl(curldim,nEdges);
+    TSHAPE::ComputeConstantHCurl(pt, vecDiv, curl);
+
+    for (int connect = 0; connect < nEdges; connect++) {
+        if(dim == 3)
+        {
+            for(int a=0; a<3; a++)
+            {
+                phi(a,connect) = curl(a,connect);
+            }
+        } else
+        {
+            DebugStop();
+        }
+
+    }
+
     
     for(int i = 0; i< data.fVecShapeIndex.size(); i++)
     {
@@ -122,6 +156,7 @@ void TPZShapeHDivKernel<TSHAPE>::Shape(TPZVec<REAL> &pt, TPZShapeData &data, TPZ
             DebugStop();
         }
     }
+    divphi.Zero();
 }
 
 
@@ -132,11 +167,6 @@ int TPZShapeHDivKernel<TSHAPE>::NConnectShapeF(int icon, TPZShapeData &data)
 }
 
 
-template
-struct TPZShapeHDivKernel<pzshape::TPZShapeTriang>;
-
-template
-struct TPZShapeHDivKernel<pzshape::TPZShapeQuad>;
 
 template
 struct TPZShapeHDivKernel<pzshape::TPZShapeTetra>;
