997bf5813076451c0491f4edd9c9f2111a677110
philippedevloo <philippedevloo1958@gmail.com>
2021-10-15 10:42:26 -0300

    DebugStop() now prints the call stack. Does it work on linux and windows?

--- Common/pzerror.cpp
+++ Common/pzerror.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 
 #include <stdexcept>
+
+#include <execinfo.h>
+#include <stdio.h>
 void pzinternal::DebugStopImpl(const char *fileName, const std::size_t lineN)
 {
 #ifdef WIN32
@@ -10,6 +13,16 @@ void pzinternal::DebugStopImpl(const char *fileName, const std::size_t lineN)
     //#ifdef PZDEBUG
 	PZError << "\n\nYour chance to put a breakpoint at " << fileName<< ":"<< lineN <<  "\n";
     //#endif
-                                                                            throw std::bad_exception();
+
+    void* callstack[128];
+    int i, frames = backtrace(callstack, 128);
+    char** strs = backtrace_symbols(callstack, frames);
+    for (i = 0; i < frames; ++i) {
+        printf("%s\n", strs[i]);
+    }
+    free(strs);
+
+
+    throw std::bad_exception();
 	
 }
