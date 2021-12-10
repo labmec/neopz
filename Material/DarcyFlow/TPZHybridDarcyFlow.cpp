c580ba5a4cc92d92e2b5fc28cde85ecd16db2dcd
philippedevloo <philippedevloo1958@gmail.com>
2021-12-10 12:03:09 -0300

    Implements hybrid darcy flow. Exemplifies how a material class for multiphysics spaces can be derived from a material class associated with and atomic mesh

--- Material/DarcyFlow/TPZHybridDarcyFlow.cpp
+++ Material/DarcyFlow/TPZHybridDarcyFlow.cpp
@@ -74,3 +74,54 @@ void TPZHybridDarcyFlow::Print(std::ostream &out) const {
     out << "Dimension: " << TPZDarcyFlow::Dimension() << "\n\n";
 }
 
+/** @name Contribute */
+/** @{ */
+/**
+ * @brief It computes a contribution to the stiffness matrix
+ * and load vector at one integration point.
+ * @param[in] datavec stores all input data
+ * @param[in] weight is the weight of the integration rule
+ * @param[out] ek is the element matrix
+ * @param[out] ef is the rhs vector
+ */
+void TPZHybridDarcyFlow::Contribute(const TPZVec<TPZMaterialDataT<STATE>> &datavec,
+                        REAL weight,TPZFMatrix<STATE> &ek,
+                        TPZFMatrix<STATE> &ef)
+{
+    const TPZFMatrix<REAL> &phi = datavec[0].fPhi;
+    const TPZFMatrix<REAL> &dphi = datavec[0].dphix;
+    const TPZVec<REAL> &x = datavec[0].x;
+    const TPZFMatrix<REAL> &axes = datavec[0].axes;
+    const TPZFMatrix<REAL> &jacinv = datavec[0].jacinv;
+    auto phr = dphi.Cols();
+
+    const STATE perm = GetPermeability(datavec[0].x);
+
+    STATE source_term = 0;
+    if (this->HasForcingFunction()) {
+        TPZManVector<STATE, 1> res(1);
+        fForcingFunction(x, res);
+        source_term = -res[0];
+    }
+
+    // Darcy's equation
+    for (int in = 0; in < phr; in++) {
+        ef(in, 0) -= weight * source_term * (phi(in, 0));
+        for (int jn = 0; jn < phr; jn++) {
+            for (int kd = 0; kd < fDim; kd++) {
+                ek(in, jn) += weight * (dphi(kd, in) * perm * dphi(kd, jn));
+            }
+        }
+    }
+    auto nspaces = datavec.size();
+    for(int ispace = 1; ispace < nspaces; ispace+= 2)
+    {
+        for (int in = 0; in <phr; in++) {
+            ek(in,phr+(ispace-1)*2) += weight*phi(in,0);
+            ek(phr+(ispace-1)*2,in) += weight*phi(in,0);
+        }
+        ek(phr+(ispace-1)*2,phr+(ispace-1)*2+1) -= weight;
+        ek(phr+(ispace-1)*2+1,phr+(ispace-1)*2) -= weight;
+    }
+}
+/**@}*/
