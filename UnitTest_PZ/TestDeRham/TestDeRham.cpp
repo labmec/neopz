e07ecd2b5fe49253d68b6d3a0b51f74c131ce190
Francisco Orlandini <francisco.orlandini@gmail.com>
2022-01-14 16:23:45 -0300

    Creates function CreateMeshes in TestDeRham to avoid code repetition

--- UnitTest_PZ/TestDeRham/TestDeRham.cpp
+++ UnitTest_PZ/TestDeRham/TestDeRham.cpp
@@ -28,6 +28,13 @@ static const std::map<ESpace, const char *> names({{ESpace::H1, "H1"},
                                                    {ESpace::L2, "L2"}});
 
 
+
+/** @brief Creates appropriate meshes to compare approximation spaces*/
+template <ESpace leftSpace, ESpace rightSpace, int dim>
+void CreateMeshes(int kRight, MMeshType elType,
+                  int &kLeft, TPZAutoPointer<TPZGeoMesh> &gmesh,
+                  TPZAutoPointer<TPZCompMesh> &cmeshL,
+                  TPZAutoPointer<TPZCompMesh> &cmeshR);
 /** @brief Compares the dimensions of the span of the operator associated
     with the left space against the kernel of the right space*/
 template <ESpace leftSpace, ESpace rightSpace, int dim>
@@ -124,280 +131,276 @@ int CalcRank(const TPZFMatrix<STATE> & S, const STATE tol);
 ***********************************TEST FUNCS***********************************
 *******************************************************************************/
 
+
+template <ESpace leftSpace, ESpace rightSpace, int dim>
+void CreateMeshes(int kRight, MMeshType elType,
+                  int &kLeft, TPZAutoPointer<TPZGeoMesh> &gmesh,
+                  TPZAutoPointer<TPZCompMesh> &cmeshL,
+                  TPZAutoPointer<TPZCompMesh> &cmeshR){
+  constexpr int matId = 1;
+  
+  
+  const auto elDim = MMeshType_Dimension(elType);
+
+  auto *matLeft = [&kLeft,kRight,elType]()
+    -> TPZMaterial* {
+    if constexpr (leftSpace == ESpace::H1) {
+      kLeft = kRight + 1;
+      return new TPZMatDeRhamH1(matId, dim);
+    } else if constexpr (leftSpace == ESpace::HCurl) {
+      if(elType == MMeshType::EQuadrilateral){
+        kLeft = kRight;
+      }else{
+        kLeft = kRight + 1;
+      }
+      return new TPZMatDeRhamHCurl(matId,dim);
+    } else if constexpr (leftSpace == ESpace::HDiv) {
+      kLeft = kRight;
+      return new TPZMatDeRhamHDiv(matId,dim);
+    } else if constexpr (leftSpace == ESpace::L2) {
+      return nullptr;
+    }
+  }();
+    
+  auto *matRight = []()
+    -> TPZMaterial * {
+    if constexpr (rightSpace == ESpace::H1) {
+      return nullptr;
+    } else if constexpr (rightSpace == ESpace::HCurl) {
+      return new TPZMatDeRhamHCurl(matId,dim);
+    } else if constexpr (rightSpace == ESpace::HDiv) {
+      return new TPZMatDeRhamHDiv(matId,dim);
+    } else if constexpr (rightSpace == ESpace::L2) {
+      return new TPZMatDeRhamL2(matId,dim);
+    }
+  }();
+
+  REQUIRE((matLeft != nullptr && matRight != nullptr));
+  //for debugging
+  constexpr bool singleElement{true};
+
+  if constexpr(singleElement){
+    constexpr bool createBoundEls{false};
+    gmesh = TPZGeoMeshTools::CreateGeoMeshSingleEl(elType, matId, createBoundEls);
+  }else{
+    gmesh = CreateGMesh(dim,elType,matId);
+  }
+  cmeshL = CreateCMesh(gmesh, matLeft, matId, kLeft, leftSpace);
+  cmeshR = CreateCMesh(gmesh, matRight, matId, kRight, rightSpace);
+
+}
+
 template <ESpace leftSpace, ESpace rightSpace, int dim>
 void CheckCompatibilityUniformMesh(int kRight) {
-  const auto nameLeft = names.at(leftSpace);
-  const auto nameRight = names.at(rightSpace);
-  constexpr int matId = 1;
+
   auto elType = GENERATE(MMeshType::ETriangular,
                          MMeshType::EQuadrilateral,
                          MMeshType::ETetrahedral,
                          MMeshType::EHexahedral,
                          MMeshType::EPrismatic);
-  const auto elName = MMeshType_Name(elType);
+
   const auto elDim = MMeshType_Dimension(elType);
+  if(elDim != dim) return;
 
-  if(elDim == dim){
+  
+  int kLeft;
+  TPZAutoPointer<TPZGeoMesh> gmesh{nullptr};
+  TPZAutoPointer<TPZCompMesh> cmeshL{nullptr}, cmeshR{nullptr};
+  CreateMeshes<leftSpace, rightSpace, dim>(kRight, elType,
+                                           kLeft, gmesh, cmeshL, cmeshR);
+  const auto nameLeft = names.at(leftSpace);
+  const auto nameRight = names.at(rightSpace);
+  const auto elName = MMeshType_Name(elType);
+  CAPTURE(nameLeft,nameRight,elName);
+  TPZAutoPointer<TPZMatrix<STATE>> matPtrL = nullptr;
+  TPZAutoPointer<TPZMatrix<STATE>> matPtrR = nullptr;
 
-    int kLeft;
-    auto *matLeft = [&kLeft,kRight,elType]()
-      -> TPZMaterial* {
-      if constexpr (leftSpace == ESpace::H1) {
-        kLeft = kRight + 1;
-        return new TPZMatDeRhamH1(matId, dim);
-      } else if constexpr (leftSpace == ESpace::HCurl) {
-        if(elType == MMeshType::EQuadrilateral){
-          kLeft = kRight;
-        }else{
-          kLeft = kRight + 1;
-        }
-        return new TPZMatDeRhamHCurl(matId,dim);
-      } else if constexpr (leftSpace == ESpace::HDiv) {
-        kLeft = kRight;
-        return new TPZMatDeRhamHDiv(matId,dim);
-      } else if constexpr (leftSpace == ESpace::L2) {
-        return nullptr;
-      }
-    }();
+  {
     
-    auto *matRight = []()
-      -> TPZMaterial * {
-      if constexpr (rightSpace == ESpace::H1) {
-        return nullptr;
-      } else if constexpr (rightSpace == ESpace::HCurl) {
-        return new TPZMatDeRhamHCurl(matId,dim);
-      } else if constexpr (rightSpace == ESpace::HDiv) {
-        return new TPZMatDeRhamHDiv(matId,dim);
-      } else if constexpr (rightSpace == ESpace::L2) {
-        return new TPZMatDeRhamL2(matId,dim);
-      }
-    }();
-
-    CAPTURE(nameLeft,nameRight,elName);
-    REQUIRE((matLeft != nullptr && matRight != nullptr));
-    TPZAutoPointer<TPZGeoMesh> gmesh{nullptr};
-    //for debugging
-    constexpr bool singleElement{true};
-
-    if constexpr(singleElement){
-      constexpr bool createBoundEls{false};
-      gmesh = TPZGeoMeshTools::CreateGeoMeshSingleEl(elType, matId, createBoundEls);
-    }else{
-      gmesh = CreateGMesh(dim,elType,matId);
-    }
-    auto cmeshL = CreateCMesh(gmesh, matLeft, matId, kLeft, leftSpace);
-    auto cmeshR = CreateCMesh(gmesh, matRight, matId, kRight, rightSpace);
-
-    TPZAutoPointer<TPZMatrix<STATE>> matPtrL = nullptr;
-    TPZAutoPointer<TPZMatrix<STATE>> matPtrR = nullptr;
-
-    {
-    
-      constexpr bool reorderEqs{false};
-      constexpr int nThreads{4};
-      TPZLinearAnalysis anL(cmeshL,reorderEqs);
-      TPZLinearAnalysis anR(cmeshR, reorderEqs);
+    constexpr bool reorderEqs{false};
+    constexpr int nThreads{4};
+    TPZLinearAnalysis anL(cmeshL,reorderEqs);
+    TPZLinearAnalysis anR(cmeshR, reorderEqs);
       
-      TPZFStructMatrix<STATE> strmtrxL(cmeshL);
-      strmtrxL.SetNumThreads(nThreads);
-      anL.SetStructuralMatrix(strmtrxL);
+    TPZFStructMatrix<STATE> strmtrxL(cmeshL);
+    strmtrxL.SetNumThreads(nThreads);
+    anL.SetStructuralMatrix(strmtrxL);
 
-      TPZFStructMatrix<STATE> strmtrxR(cmeshR);
-      strmtrxR.SetNumThreads(nThreads);
-      anR.SetStructuralMatrix(strmtrxR);
+    TPZFStructMatrix<STATE> strmtrxR(cmeshR);
+    strmtrxR.SetNumThreads(nThreads);
+    anR.SetStructuralMatrix(strmtrxR);
       
-      TPZStepSolver<STATE> step;
-      step.SetDirect(ELU);
+    TPZStepSolver<STATE> step;
+    step.SetDirect(ELU);
 
-      anL.SetSolver(step);
-      anR.SetSolver(step);
+    anL.SetSolver(step);
+    anR.SetSolver(step);
       
-      anL.Assemble();
-      anR.Assemble();
+    anL.Assemble();
+    anR.Assemble();
 
-      matPtrL = anL.MatrixSolver<STATE>().Matrix();
-      matPtrR = anR.MatrixSolver<STATE>().Matrix();
+    matPtrL = anL.MatrixSolver<STATE>().Matrix();
+    matPtrR = anR.MatrixSolver<STATE>().Matrix();
 
-    }
-    
-    TPZFMatrix<STATE> matL(*matPtrL);
-    TPZFMatrix<STATE> matR(*matPtrR);
-
-    const int dimL = matL.Rows();
-    const int dimR = matR.Rows();
-    
-    TPZFMatrix<STATE> SL, SR;
-    
-    {
-      TPZFMatrix<STATE> Udummy, VTdummy;
-      matL.SVD(Udummy, SL, VTdummy, 'N', 'N');
-      matR.SVD(Udummy, SR, VTdummy, 'N', 'N');
-    }
-    static constexpr auto tol = std::numeric_limits<STATE>::epsilon()*100000;
-    const int rankL = CalcRank(SL,tol);
-    const int rankR = CalcRank(SR,tol);
-    const int kerR = dimR-rankR;
-      
-    CAPTURE(kLeft,kRight,dimL,rankL,dimR,kerR,rankR);
-    CAPTURE(SL,SR);
-    if constexpr (rightSpace==ESpace::L2)//for L2 the operator is -> 0
-      REQUIRE(rankL == rankR);
-    else
-      REQUIRE(rankL == kerR);
-    
-    delete matLeft;
-    delete matRight;
   }
+    
+  TPZFMatrix<STATE> matL(*matPtrL);
+  TPZFMatrix<STATE> matR(*matPtrR);
+
+  const int dimL = matL.Rows();
+  const int dimR = matR.Rows();
+    
+  TPZFMatrix<STATE> SL, SR;
+    
+  {
+    TPZFMatrix<STATE> Udummy, VTdummy;
+    matL.SVD(Udummy, SL, VTdummy, 'N', 'N');
+    matR.SVD(Udummy, SR, VTdummy, 'N', 'N');
+  }
+  static constexpr auto tol = std::numeric_limits<STATE>::epsilon()*100000;
+  const int rankL = CalcRank(SL,tol);
+  const int rankR = CalcRank(SR,tol);
+  const int kerR = dimR-rankR;
+      
+  CAPTURE(kLeft,kRight,dimL,rankL,dimR,kerR,rankR);
+  CAPTURE(SL,SR);
+  if constexpr (rightSpace==ESpace::L2)//for L2 the operator is -> 0
+                 REQUIRE(rankL == rankR);
+  else
+    REQUIRE(rankL == kerR);
 }
 
 template <ESpace leftSpace, ESpace rightSpace, int dim>
 void CheckExactSequence(int kRight) {
-  const auto nameLeft = names.at(leftSpace);
-  const auto nameRight = names.at(rightSpace);
   constexpr int matId = 1;
   auto elType = GENERATE(MMeshType::ETriangular, MMeshType::EQuadrilateral,
                          MMeshType::ETetrahedral, MMeshType::EHexahedral,
                          MMeshType::EPrismatic);
-  const auto elName = MMeshType_Name(elType);
   const auto elDim = MMeshType_Dimension(elType);
 
-  if (elDim == dim) {
+  if (elDim != dim) return;
+  
+  int kLeft;
+  TPZAutoPointer<TPZGeoMesh> gmesh{nullptr};
+  TPZAutoPointer<TPZCompMesh> cmeshL{nullptr}, cmeshR{nullptr};
 
-    const int kLeft = [kRight,elType]() {
-      if constexpr (leftSpace == ESpace::H1) {
-        return kRight + 1;
-      } else if constexpr (leftSpace == ESpace::HCurl) {
-        if(elType == MMeshType::EQuadrilateral)
-          {return kRight;}
-        else
-          {return kRight + 1;}
-      } else if constexpr (leftSpace == ESpace::HDiv) {
-        return kRight;
-      } else if constexpr (leftSpace == ESpace::L2) {
-        return -1;
-      }
-    }();
-    auto *matLeft = new TPZNullMaterial<STATE>(matId,dim,1);
-    auto *matRight = new TPZNullMaterial<STATE>(matId,dim,1);
+  const auto nameLeft = names.at(leftSpace);
+  const auto nameRight = names.at(rightSpace);
+  const auto elName = MMeshType_Name(elType);
+  CAPTURE(nameLeft,nameRight,elName);
+  CreateMeshes<leftSpace, rightSpace, dim>(kRight, elType, kLeft,
+                                           gmesh, cmeshL, cmeshR);
+  
 
-    CAPTURE(nameLeft, nameRight, elName);
-    constexpr bool createBoundEls{false};
-    TPZAutoPointer<TPZGeoMesh> gmesh =
-        TPZGeoMeshTools::CreateGeoMeshSingleEl(elType, matId, createBoundEls);
-    
-    auto cmeshL = CreateCMesh(gmesh, matLeft, matId, kLeft, leftSpace);
-    auto cmeshR = CreateCMesh(gmesh, matRight, matId, kRight, rightSpace);
+  TPZAutoPointer<TPZCompMesh> cmeshMF = new TPZCompMesh(gmesh);
 
-    TPZAutoPointer<TPZCompMesh> cmeshMF = new TPZCompMesh(gmesh);
-
-    auto *matMF = []() -> TPZMaterial * {
-      if constexpr (rightSpace == ESpace::H1) {
-        return nullptr;
-      } else if constexpr (rightSpace == ESpace::HCurl) {
-        return new TPZMatDeRhamH1HCurl(matId, dim);
-      } else if constexpr (rightSpace == ESpace::HDiv) {
-        return nullptr;
-      } else if constexpr (rightSpace == ESpace::L2) {
-        return nullptr;
-      }
-    }();
-    if (!matMF)
-      return;
-
-    cmeshMF->InsertMaterialObject(matMF);
-    cmeshMF->SetDimModel(dim);
-
-    cmeshMF->SetAllCreateFunctionsMultiphysicElem();
-
-    cmeshMF->AutoBuild();
-    cmeshMF->CleanUpUnconnectedNodes();
-
-    TPZManVector<TPZCompMesh *, 2> meshVecIn = {cmeshL.operator->(),
-                                                cmeshR.operator->()};
-
-    TPZBuildMultiphysicsMesh::AddElements(meshVecIn, cmeshMF.operator->());
-    TPZBuildMultiphysicsMesh::AddConnects(meshVecIn, cmeshMF.operator->());
-    TPZBuildMultiphysicsMesh::TransferFromMeshes(meshVecIn,
-                                                 cmeshMF.operator->());
-
-    cmeshMF->ExpandSolution();
-    cmeshMF->ComputeNodElCon();
-    cmeshMF->CleanUpUnconnectedNodes();
-    constexpr bool optimizeBandwidth{false};
-    TPZLinearAnalysis analysis(cmeshMF, optimizeBandwidth);
-    TPZFStructMatrix<STATE> strmtrx(cmeshMF);
-    analysis.SetStructuralMatrix(strmtrx);
-    TPZStepSolver<STATE> step;
-    step.SetDirect(ELU);
-    analysis.SetSolver(step);
-    
-    analysis.Assemble();
-    //it is a full matrix
-    auto mfMatrix =
-      dynamic_cast<TPZFMatrix<STATE>&> (
-          analysis.MatrixSolver<STATE>().Matrix().operator*());
-
-    const int rLeftEqs = cmeshR->NEquations();
-    const int lLeftEqs = cmeshL->NEquations();
-    TPZFMatrix<STATE> rightMatrix(rLeftEqs,rLeftEqs);
-    mfMatrix.GetSub(lLeftEqs, lLeftEqs, rLeftEqs, rLeftEqs, rightMatrix);
-
-
-    TPZFMatrix<STATE> Sfull, Sright;
-    
-    {
-      TPZFMatrix<STATE> Udummy, VTdummy;
-      mfMatrix.SVD(Udummy, Sfull, VTdummy, 'N', 'N');
-      rightMatrix.SVD(Udummy, Sright, VTdummy, 'N', 'N');
+  auto *matMF = []() -> TPZMaterial * {
+    if constexpr (rightSpace == ESpace::H1) {
+      return nullptr;
+    } else if constexpr (rightSpace == ESpace::HCurl) {
+      return new TPZMatDeRhamH1HCurl(matId, dim);
+    } else if constexpr (rightSpace == ESpace::HDiv) {
+      return nullptr;
+    } else if constexpr (rightSpace == ESpace::L2) {
+      return nullptr;
     }
+  }();
+  if (!matMF)
+    return;
+
+  cmeshMF->InsertMaterialObject(matMF);
+  cmeshMF->SetDimModel(dim);
+
+  cmeshMF->SetAllCreateFunctionsMultiphysicElem();
+
+  cmeshMF->AutoBuild();
+  cmeshMF->CleanUpUnconnectedNodes();
+
+  TPZManVector<TPZCompMesh *, 2> meshVecIn = {cmeshL.operator->(),
+    cmeshR.operator->()};
+
+  TPZBuildMultiphysicsMesh::AddElements(meshVecIn, cmeshMF.operator->());
+  TPZBuildMultiphysicsMesh::AddConnects(meshVecIn, cmeshMF.operator->());
+  TPZBuildMultiphysicsMesh::TransferFromMeshes(meshVecIn,
+                                               cmeshMF.operator->());
+
+  cmeshMF->ExpandSolution();
+  cmeshMF->ComputeNodElCon();
+  cmeshMF->CleanUpUnconnectedNodes();
+  constexpr bool optimizeBandwidth{false};
+  TPZLinearAnalysis analysis(cmeshMF, optimizeBandwidth);
+  TPZFStructMatrix<STATE> strmtrx(cmeshMF);
+  analysis.SetStructuralMatrix(strmtrx);
+  TPZStepSolver<STATE> step;
+  step.SetDirect(ELU);
+  analysis.SetSolver(step);
     
-    static constexpr auto tol = std::numeric_limits<STATE>::epsilon()*100;
-    auto fullDim = Sfull.Rows();
-    auto fullRank = CalcRank(Sfull,tol);
-    auto rightDim = Sright.Rows();
-    auto rightRank = CalcRank(Sright,tol);
+  analysis.Assemble();
+  //it is a full matrix
+  auto mfMatrix =
+    dynamic_cast<TPZFMatrix<STATE>&> (
+      analysis.MatrixSolver<STATE>().Matrix().operator*());
+
+  const int rLeftEqs = cmeshR->NEquations();
+  const int lLeftEqs = cmeshL->NEquations();
+  TPZFMatrix<STATE> rightMatrix(rLeftEqs,rLeftEqs);
+  mfMatrix.GetSub(lLeftEqs, lLeftEqs, rLeftEqs, rLeftEqs, rightMatrix);
 
 
-    // if(elType == MMeshType::ETriangular){
-    //   const int postProcessResolution = 3;
-    //   const std::string executionInfo = [&]() {
-    //     std::string name("");
-    //     name.append(elName);
-    //     name.append(std::to_string(kRight));
-    //     return name;
-    //   }();
-
-    //   const std::string plotfile =
-    //       "solution" + executionInfo + ".vtk"; // where to print the vtk files
-    //   TPZStack<std::string> scalnames, vecnames;
-    //   if constexpr (leftSpace == ESpace::HDiv || leftSpace == ESpace::HCurl) {
-    //     vecnames.Push("SolutionLeft"); // print the state variable
-    //   } else {
-    //     scalnames.Push("SolutionLeft"); // print the state variable
-    //   }
-
-    //   if constexpr (rightSpace == ESpace::HDiv || rightSpace == ESpace::HCurl) {
-    //     vecnames.Push("SolutionRight"); // print the state variable
-    //   } else {
-    //     scalnames.Push("SolutionRight"); // print the state variable
-    //   }
-
-    //   analysis.DefineGraphMesh(dim, scalnames, vecnames, plotfile);
-    //   TPZFMatrix<STATE> sol(cmeshMF->NEquations(), 1);
-    //   sol.Zero();
-    //   for (int i = 0; i < sol.Rows(); i++) {
-    //     sol(i - 1 < 0 ? 0 : i - 1, 0) = 0;
-    //     sol(i, 0) = 1;
-    //     analysis.LoadSolution(sol);
-    //     TPZBuildMultiphysicsMesh::TransferFromMultiPhysics(
-    //         meshVecIn, cmeshMF.operator->());
-    //     analysis.PostProcess(postProcessResolution);
-    //   }
-    // }
-
-    CAPTURE(kLeft,kRight,fullDim, fullRank,rightDim, rightRank);
-    REQUIRE(fullRank == rightRank);
+  TPZFMatrix<STATE> Sfull, Sright;
+    
+  {
+    TPZFMatrix<STATE> Udummy, VTdummy;
+    mfMatrix.SVD(Udummy, Sfull, VTdummy, 'N', 'N');
+    rightMatrix.SVD(Udummy, Sright, VTdummy, 'N', 'N');
   }
+    
+  static constexpr auto tol = std::numeric_limits<STATE>::epsilon()*100;
+  auto fullDim = Sfull.Rows();
+  auto fullRank = CalcRank(Sfull,tol);
+  auto rightDim = Sright.Rows();
+  auto rightRank = CalcRank(Sright,tol);
+
+
+  // if(elType == MMeshType::ETriangular){
+  //   const int postProcessResolution = 3;
+  //   const std::string executionInfo = [&]() {
+  //     std::string name("");
+  //     name.append(elName);
+  //     name.append(std::to_string(kRight));
+  //     return name;
+  //   }();
+
+  //   const std::string plotfile =
+  //       "solution" + executionInfo + ".vtk"; // where to print the vtk files
+  //   TPZStack<std::string> scalnames, vecnames;
+  //   if constexpr (leftSpace == ESpace::HDiv || leftSpace == ESpace::HCurl) {
+  //     vecnames.Push("SolutionLeft"); // print the state variable
+  //   } else {
+  //     scalnames.Push("SolutionLeft"); // print the state variable
+  //   }
+
+  //   if constexpr (rightSpace == ESpace::HDiv || rightSpace == ESpace::HCurl) {
+  //     vecnames.Push("SolutionRight"); // print the state variable
+  //   } else {
+  //     scalnames.Push("SolutionRight"); // print the state variable
+  //   }
+
+  //   analysis.DefineGraphMesh(dim, scalnames, vecnames, plotfile);
+  //   TPZFMatrix<STATE> sol(cmeshMF->NEquations(), 1);
+  //   sol.Zero();
+  //   for (int i = 0; i < sol.Rows(); i++) {
+  //     sol(i - 1 < 0 ? 0 : i - 1, 0) = 0;
+  //     sol(i, 0) = 1;
+  //     analysis.LoadSolution(sol);
+  //     TPZBuildMultiphysicsMesh::TransferFromMultiPhysics(
+  //         meshVecIn, cmeshMF.operator->());
+  //     analysis.PostProcess(postProcessResolution);
+  //   }
+  // }
+
+  CAPTURE(kLeft,kRight,fullDim, fullRank,rightDim, rightRank);
+  REQUIRE(fullRank == rightRank);
 }
 
 
