17b5e75420701213394f2c815e92d90e72358b36
RicardoJavierH <rhanccoan@unsa.edu.pe>
2022-04-21 18:34:48 -0500

    Assemble parallelization with OMP and TBB, withcoloring and without coloring

---
+++ cmake/EnableOMP.cmake
@@ -0,0 +1,15 @@
+function(enable_omp target)
+    find_package(OpenMP REQUIRED)
+    include_directories(/usr/local/include)
+    link_directories(/usr/local/lib)
+    if (NOT OpenMP_FOUND)
+        message(FATAL_ERROR "Could not find OpenMP. If OpenMP is not needed, "
+                "configure the project with -DUSING_OMP=OFF")
+    endif()
+    target_link_libraries(${target} PRIVATE OpenMP::OpenMP_CXX /usr/local/lib/libomp.a)
+    target_include_directories(${target} PRIVATE ${OpenMP_INCLUDE_DIRS})
+    target_compile_definitions(${target} PRIVATE USING_OMP)
+endfunction()
+
+
+
