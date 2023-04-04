e4eaa3a8b0d0828dbd99895f3d0a0d9b39a27340
Francisco Orlandini <francisco.orlandini@gmail.com>
2023-04-04 16:53:48 -0300

    adapts krylov solver for new symmetric definitions

--- Solvers/EigenSolvers/TPZSpectralTransform.cpp
+++ Solvers/EigenSolvers/TPZSpectralTransform.cpp
@@ -19,8 +19,10 @@ template<class TVar>
 TPZAutoPointer<TPZMatrix<TVar>>
 TPZSTShiftOrigin<TVar>::CalcMatrix(TPZAutoPointer<TPZMatrix<TVar>>A, TPZAutoPointer<TPZMatrix<TVar>>B) const
 {
-  if (B->IsSymmetric()) B->Decompose(ELDLt);
-  else B->Decompose(ELU);
+  const auto sp= B->IsSymmetric();
+  const bool use_lu = sp == SymProp::Herm || (!std::is_same_v<TVar,RTVar> && sp == SymProp::Sym);
+  if (use_lu) B->Decompose(ELU);
+  else B->Decompose(ELDLt);
   TPZAutoPointer<TPZMatrix<TVar>> shiftedMat = A;
   //b-1 * shiftedA will be computed at the arnoldi iteration
   const auto &shift = Shift();
@@ -82,8 +84,10 @@ TPZSTShiftAndInvert<TVar>::CalcMatrix(TPZAutoPointer<TPZMatrix<TVar>>A, TPZAutoP
   }
   shiftedMat->Storage() -= B->Storage() * shift;
 
-  if (shiftedMat->IsSymmetric()) shiftedMat->Decompose(ELDLt);
-  else shiftedMat->Decompose(ELU);
+  const auto sp= shiftedMat->IsSymmetric();
+  const bool use_lu = sp == SymProp::Herm || (!std::is_same_v<TVar,RTVar> && sp == SymProp::Sym);
+  if (use_lu) shiftedMat->Decompose(ELU);
+  else shiftedMat->Decompose(ELDLt);
   return shiftedMat;
 }
 
@@ -95,8 +99,10 @@ TPZSTShiftAndInvert<TVar>::CalcMatrix(TPZAutoPointer<TPZMatrix<TVar>>A) const
   const auto &shift = this->Shift();
   const auto nRows = A->Rows();
   for(int i = 0; i < nRows; i++) shiftedMat->PutVal(i,i,A->GetVal(i,i)-shift);
-  if (shiftedMat->IsSymmetric()) shiftedMat->Decompose(ELDLt);
-  else shiftedMat->Decompose(ELU);
+  const auto sp= shiftedMat->IsSymmetric();
+  const bool use_lu = sp == SymProp::Herm || (!std::is_same_v<TVar,RTVar> && sp == SymProp::Sym);
+  if (use_lu) shiftedMat->Decompose(ELU);
+  else shiftedMat->Decompose(ELDLt);
   return shiftedMat;
 }
 
