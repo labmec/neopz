1a2b6f6e9dc8044677072551d48decbb0bea2f5a
Francisco Orlandini <francisco.orlandini@gmail.com>
2023-04-28 09:42:00 -0300

    feat(testiterative): adds 1d (disabled) test for ease of  debugging

--- UnitTest_PZ/TestIterative/TestIterative.cpp
+++ UnitTest_PZ/TestIterative/TestIterative.cpp
@@ -21,6 +21,10 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators_all.hpp>
 
+
+//useful for debugging
+// #define TEST_1D
+
 template<>
 struct Catch::StringMaker<long double> {
     static std::string convert(long double ref);
@@ -39,6 +43,8 @@ std::string Catch::StringMaker<long double>::convert(long double value) {
 namespace testiterative{
   //! Creates computational mesh for 2d poisson problem
   TPZCompMesh *CreateCMesh();
+  //! Creates computational mesh for 1d poisson problem
+  TPZCompMesh *CreateCMesh1D();
   //! Creates vector of active equations by removing all dirichlet bc eqs
   void
   FilterBoundaryEquations(TPZCompMesh * cmesh,
@@ -199,11 +205,72 @@ TEST_CASE("Equation filter and precond","[iterative_testss]") {
   Catch::StringMaker<STATE>::precision = oldPrecision;
 }
 
+
+#ifdef TEST_1D
+/*
+  The following test case is mostly for debugging.
+  No need to run it every time
+ */
+TEST_CASE("1D H1 problem","[iterative_tests]") {
+  auto oldPrecision = Catch::StringMaker<STATE>::precision;
+  Catch::StringMaker<STATE>::precision = std::numeric_limits<STATE>::max_digits10;
+  TPZAutoPointer<TPZCompMesh> cmesh = testiterative::CreateCMesh1D();
+  constexpr bool optimizeBandwidth{false};
+  TPZLinearAnalysis an(cmesh,optimizeBandwidth);
+  {
+    TPZStepSolver<STATE> solv;
+    solv.SetDirect(ECholesky);
+    an.SetSolver(solv);
+  }
+#if defined (PZ_USING_MKL) || (PZ_USING_EIGEN)
+  TPZSSpStructMatrix<STATE> str(cmesh);
+#else
+  TPZSkylineStructMatrix<STATE>str(cmesh);
+#endif
+  TPZVec<int64_t> active_eqs;
+  testiterative::FilterBoundaryEquations(cmesh.operator->(), active_eqs);
+  str.EquationFilter().SetActiveEquations(active_eqs);
+  an.SetStructuralMatrix(str);
+  an.Assemble();
+
+  auto *solver = dynamic_cast<TPZStepSolver<STATE>*>(an.Solver());
+
+  solver->Matrix()->SetSymmetry(SymProp::Herm);
+  solver->Matrix()->SetDefPositive(true);
+
+
+  constexpr int64_t fromCurrent{0};
+
+  auto pre_type = GENERATE(Precond::Jacobi,
+                           Precond::BlockJacobi,
+                           Precond::Element,
+                           Precond::NodeCentered);
+  
+  const bool overlap = GENERATE(true,false);
+  const auto precondname = std::string(Precond::Name(pre_type)) +
+                                       (overlap ? " with overlap" : " without overlap");
+  SECTION(precondname){
+    std::cout<<"1d: testing "<<precondname<<std::endl;
+    TPZAutoPointer<TPZMatrixSolver<STATE>> pre =
+      an.BuildPreconditioner<STATE>(pre_type, overlap);
+    const bool created_precond = pre;
+    REQUIRE(created_precond);
+    solver->SetGMRES(testiterative::niter, testiterative::niter,
+                     *pre, testiterative::tol, fromCurrent);
+    an.Solve();
+    const auto tol_res = solver->GetTolerance();
+    REQUIRE(tol_res <= testiterative::tol);
+  }
+  Catch::StringMaker<STATE>::precision = oldPrecision;
+}
+
+#endif
+
 namespace testiterative{
 
   TPZCompMesh *CreateCMesh()
   {
-    constexpr int rhsPOrder{2};
+    constexpr int rhsPOrder{3};
     const auto rhs = [](const TPZVec<REAL>&loc, TPZVec<STATE> &u){
       const REAL &x = loc[0];
       const REAL &y = loc[1];
@@ -276,6 +343,77 @@ namespace testiterative{
     return cmesh;
   }
 
+  TPZCompMesh *CreateCMesh1D()
+  {
+    /*
+      this rhs corresponds to the solution (x^2-1)^2
+     */
+    constexpr int rhsPOrder{2};
+    const auto rhs = [](const TPZVec<REAL>&loc, TPZVec<STATE> &u){
+      const REAL &x = loc[0];
+      const REAL &y = loc[1];
+      u[0] = 4-12*x*x;
+    };
+    
+    //dimension of the problem
+    constexpr int dim{1};
+    //number of elements
+    constexpr int nEls{5};
+    //all geometric coordinates in NeoPZ are in the 3D space
+
+    //left boundary
+    constexpr REAL minX{-1};
+    //right boundary
+    constexpr REAL maxX{ 1};
+
+    /*vector for storing materials(regions) identifiers
+     * for the TPZGeoMeshTools::CreateGeoMeshOnGrid function.
+     * In this example, we have different materials in each
+     * section of the boundary*/
+    TPZManVector<int,5> matIdVec={1,-1,-1};
+    //whether to create boundary elements
+    constexpr bool genBoundEls{true};
+    //type of elements
+    constexpr MMeshType meshType{MMeshType::ETriangular};
+
+
+    //polynomial order of elements
+    constexpr int pOrder{3};
+    //defining the geometry of the problem
+    //TPZAutoPointer is a smart pointer from the NeoPZ library
+    TPZAutoPointer<TPZGeoMesh> gmesh =
+      TPZGeoMeshTools::CreateGeoMesh1D(minX, maxX, nEls, matIdVec, genBoundEls);
+
+    TPZCompMesh *cmesh = new TPZCompMesh(gmesh);
+
+    auto *mat = new TPZMatPoisson<STATE>(matIdVec[0],dim);
+    //TPZMatLaplacian solves div(k grad(u)) = -f
+    mat->SetForcingFunction(rhs,rhsPOrder);
+    cmesh->InsertMaterialObject(mat);
+
+    //now we insert the boundary conditions
+    {
+      //TPZFMatrix<T> implements a full matrix of type T
+      /* val1 and val2 are used for calculating the boundary
+       * conditions. val1 goes in the matrix and val2 in the rhs.
+       * for dirichlet boundary conditions, only the value of 
+       * val2 is used.*/
+      TPZFMatrix<STATE> val1(1,1,0.);
+      TPZManVector<STATE,1> val2={0};
+      //dirichlet=0,neumann=1,robin=2
+      constexpr int boundType{0};
+      //TPZBndCond is a material type for boundary conditions
+      TPZBndCond * bnd = mat->CreateBC(mat,matIdVec[1],boundType,val1,val2);
+      cmesh->InsertMaterialObject(bnd);
+
+    }
+    
+    cmesh->SetAllCreateFunctionsContinuous();
+    cmesh->SetDefaultOrder(pOrder);
+    cmesh->AutoBuild();
+    return cmesh;
+  }
+  
   void FilterBoundaryEquations(TPZCompMesh *cmesh,
                                TPZVec<int64_t> &activeEquations) {
     TPZManVector<int64_t, 1000> allConnects;
