bda3893c9e87fc54abedb2e0c54dc09134e8d44e
PhilippeDevloo <phil@fec.unicamp.br>
2023-09-04 17:46:48 -0300

    Generalized the cylinder map

--- SpecialMaps/TPZCylinderMap.h
+++ SpecialMaps/TPZCylinderMap.h
@@ -28,8 +28,6 @@ namespace pzgeom {
         /// node around which we rotate the coordinates
         TPZManVector<REAL,3> fOrigin;
         
-        /// radius of the cylinder
-        REAL fRadius;
         
         /**
            @brief Computes the rotation matrix of the cylinder.
@@ -42,31 +40,31 @@ namespace pzgeom {
         
     public:
         
-        TPZCylinderMap() : TGeo(), fCylindricalCo(2,TGeo::NNodes,-1), fOrigin(3,0.), fRadius(0.), fRotation(3,3,0.)
+        TPZCylinderMap() : TGeo(), fCylindricalCo(3,TGeo::NNodes,-1), fOrigin(3,0.), fRotation(3,3,0.)
         {
             fRotation.Identity();
         }
         
-        TPZCylinderMap(TPZVec<int64_t> &nodeindices) : TGeo(nodeindices), fCylindricalCo(2,TGeo::NNodes,-1), fOrigin(3,0.), fRadius(0.), fRotation(3,3,0.)
+        TPZCylinderMap(TPZVec<int64_t> &nodeindices) : TGeo(nodeindices), fCylindricalCo(3,TGeo::NNodes,-1), fOrigin(3,0.), fRotation(3,3,0.)
         {
             fRotation.Identity();
         }
         
-        TPZCylinderMap(const TPZCylinderMap &cp) : TGeo(cp), fCylindricalCo(cp.fCylindricalCo), fRadius(cp.fRadius), fRotation(cp.fRotation)
+        TPZCylinderMap(const TPZCylinderMap &cp) : TGeo(cp), fCylindricalCo(cp.fCylindricalCo), fRotation(cp.fRotation)
         {
         }
 
         /** @brief Copy constructor for another mesh*/
         TPZCylinderMap(const TPZCylinderMap &cp,
                        TPZGeoMesh &destmesh) : TGeo(cp,destmesh), fCylindricalCo(cp.fCylindricalCo),
-                                               fOrigin(cp.fOrigin), fRadius(cp.fRadius),
+                                               fOrigin(cp.fOrigin),
                                                fRotation(cp.fRotation)
         {}
 
         /** @brief Copy constructor with node map*/
         TPZCylinderMap(const TPZCylinderMap &cp,
                        std::map<int64_t,int64_t> & gl2lcNdMap) : TGeo(cp,gl2lcNdMap), fCylindricalCo(cp.fCylindricalCo),
-                                                                 fOrigin(cp.fOrigin), fRadius(cp.fRadius),
+                                                                 fOrigin(cp.fOrigin),
                                                                  fRotation(cp.fRotation)
         {}
         
@@ -75,7 +73,6 @@ namespace pzgeom {
             TGeo::operator=(cp);
             fCylindricalCo = cp.fCylindricalCo;
             fOrigin = cp.fOrigin;
-            fRadius = cp.fRadius;
             fRotation = cp.fRotation;
             return *this;
         }
@@ -86,14 +83,14 @@ namespace pzgeom {
         }
 
         /// Sets center of the cylinder's base and radius
-        void SetOrigin(const TPZVec<REAL> &origin, REAL radius)
+        void SetOrigin(const TPZVec<REAL> &origin)
         {
             fOrigin = origin;
-            fRadius = radius;
         }
 
         /// axis direction with the vertical axis
-        void SetCylinderAxis(const TPZVec<REAL> &axis);
+        /// the last line indicates the z-direction
+        void SetCylinderAxis(const TPZFMatrix<REAL> &axis);
         
         /// compute the cylindrical coordinates of the corner nodes
         void ComputeCornerCoordinates(TPZGeoMesh &gmesh);
@@ -109,29 +106,46 @@ namespace pzgeom {
             CornerCoordinates(gel, coord);
             X(coord,loc,result);
         }
-        
+ 
+        template<class T>
+        void ComputeGradRX(const TPZVec<T> &ftz, TPZFMatrix<T> &gradrx) const {
+            gradrx(0,0) = cos(ftz[1]);
+            gradrx(0,1) = -ftz[0]*sin(ftz[1]);
+            gradrx(0,2) = ftz[0]*0.;
+            gradrx(1,0) = sin(ftz[1]);
+            gradrx(1,1) = ftz[0]*cos(ftz[1]);
+            gradrx(1,2) = ftz[0]*0.;
+            gradrx(2,0) = ftz[0]*0.;
+            gradrx(2,1) = ftz[0]*0.;
+            gradrx(2,2) = ftz[0]*0.+1.;
+        }
+
         template<class T>
         void GradX(const TPZFMatrix<REAL> &nodes, TPZVec<T> &par, TPZFMatrix<T> &gradx) const
         {
             
             
-            TPZFNMatrix<6,T> DxDphi(3,2,0.), gradphi(2,2), gradxloc(3,2);
-            TPZManVector<T,3> ft(2,0.);
-            TGeo::X(fCylindricalCo,par,ft);
+            TPZFNMatrix<9,T> gradphi(3,3), gradxloc(3,3);
+            TPZManVector<T,3> rtz(3,0.);
+            TGeo::X(fCylindricalCo,par,rtz);
             TGeo::GradX(fCylindricalCo, par, gradphi);
-            
+            ComputeGradRX(rtz,gradxloc);
+            // melhorar
+            /*
             gradxloc(0,0) = -fRadius*sin(ft[0])*gradphi(0,0);
             gradxloc(1,0) = fRadius*cos(ft[0])*gradphi(0,0);
             gradxloc(2,0) = gradphi(1,0);
             gradxloc(0,1) = -fRadius*sin(ft[0])*gradphi(0,1);
             gradxloc(1,1) = fRadius*cos(ft[0])*gradphi(0,1);
             gradxloc(2,1) = gradphi(1,1);
-
+*/
             for (int64_t i=0; i<3; i++) {
-                for (int64_t j=0; j<2; j++) {
+                for (int64_t j=0; j<TGeo::Dimension; j++) {
                     gradx(i,j) = 0.;
                     for (int64_t k=0; k<3; k++) {
-                        gradx(i,j) += fRotation.GetVal(i,k)*gradxloc(k,j);
+                        for (int l = 0; l<3; l++) {
+                            gradx(i,j) += fRotation.GetVal(i,k)*gradxloc(k,l)*gradphi(l,j);
+                        }
                     }
                 }
             }
@@ -140,13 +154,14 @@ namespace pzgeom {
         template<class T>
         void X(const TPZFMatrix<REAL> &nodes,TPZVec<T> &loc,TPZVec<T> &result) const
         {
-            TPZManVector<T,2> localcylinder(2);
+            TPZManVector<T,3> localcylinder(3);
             TGeo::X(this->fCylindricalCo,loc,localcylinder);
             TPZManVector<T,3> localcartesian(3);
-            
-            localcartesian[0] = fRadius*cos(localcylinder[0]);
-            localcartesian[1] = fRadius*sin(localcylinder[0]);
-            localcartesian[2] = localcylinder[1];
+            T Radius = localcylinder[0];
+            T theta = localcylinder[1];
+            localcartesian[0] = Radius*cos(theta);
+            localcartesian[1] = Radius*sin(theta);
+            localcartesian[2] = localcylinder[2];
             
             for (int64_t i=0; i<3; i++) {
                 result[i] = fOrigin[i];
