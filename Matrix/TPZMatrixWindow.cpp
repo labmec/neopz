8e64ddb050764f02e3897de1dfa655ea177f2d21
orlandini <francisco.orlandini@gmail.com>
2024-04-01 17:19:24 -0300

    feat(MatrixWindow): ongoing changes on TPZMatrixWindow

--- Matrix/TPZMatrixWindow.cpp
+++ Matrix/TPZMatrixWindow.cpp
@@ -1,10 +1,18 @@
 #include "TPZMatrixWindow.h"
 #include "pzfmatrix.h"
 
+#ifdef USING_LAPACK
+#include "TPZLapack.h"
+#endif
+
 template<class TVar>
-TPZMatrixWindow<TVar>::TPZMatrixWindow(TVar* mem_area, const int nrows, const int ncols, const int leading_dim)
+TPZMatrixWindow<TVar>::TPZMatrixWindow(TVar* mem_area, const int nrows, const int ncols,
+                                       const int leading_dim, const int size_mem)
   : fStorage(mem_area), fLeadingDim(leading_dim)
 {
+  const int nr_orig = leading_dim;
+  const int nc_orig = size_mem/leading_dim;
+  CheckConstructor(0,0,nrows,ncols,nr_orig,nc_orig);
   this->fRow = nrows;
   this->fCol = ncols;
 }
@@ -14,6 +22,7 @@ TPZMatrixWindow<TVar>::TPZMatrixWindow(TPZFMatrix<TVar> &mat, const int i, const
                                        const int nrows, const int ncols)
   : fLeadingDim(mat.Rows())
 {
+  CheckConstructor(i,j,nrows,ncols,mat.Rows(),mat.Cols());
   //pointer of first window position
   fStorage = mat.Elem() + mat.Rows()*j+i;
   this->fRow = nrows;
@@ -22,13 +31,158 @@ TPZMatrixWindow<TVar>::TPZMatrixWindow(TPZFMatrix<TVar> &mat, const int i, const
 
 template<class TVar>
 void
-TPZMatrixWindow<TVar>::MultAdd(const TPZFMatrix<TVar> &x,const TPZFMatrix<TVar> &y, TPZFMatrix<TVar> &z,
+TPZMatrixWindow<TVar>::MultAdd(const TPZMatrixWindow<TVar> &x,const TPZMatrixWindow<TVar> &y, TPZMatrixWindow<TVar> &z,
                                const TVar alpha,const TVar beta,const int opt_a, const int opt_x) const
 {
-  //implement me
+  if(opt_x==0){
+    //default checks, same as TPZMatrix<T>::MultAddChecks
+    if ((!opt_a && this->Cols() != x.Rows()) || (opt_a && this->Rows() != x.Rows())) {
+      TPZMatrix<TVar>::Error( "TPZFMatrix::MultAdd matrix x with incompatible dimensions>" );
+      return;
+    }
+    if(beta != (TVar)0. && ((!opt_a && this->Rows() != y.Rows()) || (opt_a && this->Cols() != y.Rows()) || y.Cols() != x.Cols())) {
+      TPZMatrix<TVar>::Error( "TPZFMatrix::MultAdd matrix y with incompatible dimensions>" );
+      return;
+    }
+    if(!opt_a) {
+      if(z.Cols() != x.Cols() || z.Rows() != this->Rows()) {
+        z.Redim(this->Rows(),x.Cols());
+      }
+    } else {
+      if(z.Cols() != x.Cols() || z.Rows() != this->Cols()) {
+        z.Redim(this->Cols(),x.Cols());
+      }
+    }
+  }else{
+    //checks for transposed x
+    if ((!opt_a && this->Cols() != x.Cols()) || (opt_a && this->Rows() != x.Cols())) {
+      TPZMatrix<TVar>::Error( "TPZFMatrix::MultAdd matrix x with incompatible dimensions>" );
+      return;
+    }
+    if(beta != (TVar)0. && ((!opt_a && this->Rows() != y.Rows()) || (opt_a && this->Cols() != y.Rows()) || y.Cols() != x.Rows())) {
+      TPZMatrix<TVar>::Error( "TPZFMatrix::MultAdd matrix y with incompatible dimensions>" );
+      return;
+    }
+    if(!opt_a) {
+      if(z.Cols() != x.Rows() || z.Rows() != this->Rows()) {
+        z.Redim(this->Rows(),x.Rows());
+      }
+    } else {
+      if(z.Cols() != x.Rows() || z.Rows() != this->Cols()) {
+        z.Redim(this->Cols(),x.Rows());
+      }
+    }
+  }
   DebugStop();
+  // if (beta != (TVar)0) {
+  //   z = y;
+  // }else{
+  //   z.Zero();
+  // }
+
+  const int64_t rows = this->Rows();
+  const int64_t cols = this->Cols();
+  const int64_t xrows = x.Rows();
+  const int64_t xcols = x.Cols();
+    
+    
+  if(rows == 0 || cols == 0 || xrows == 0 || xcols == 0){
+    if (beta != (TVar)0) {
+      z *= beta;
+    }
+    return;
+  }
+
+#ifdef USING_LAPACK
+    
+  //0: no transpose, 1: transpose, 2: conj trans
+  const CBLAS_TRANSPOSE transp_a =
+    opt_a == 0 ? CblasNoTrans : (opt_a == 1 ? CblasTrans : CblasConjTrans);
+  const auto dim1 = opt_a == 0 ? rows : cols;
+  const auto dim2 = opt_a == 0 ? cols : rows;
+
+  const CBLAS_TRANSPOSE transp_x =
+    opt_x == 0 ? CblasNoTrans : (opt_x == 1 ? CblasTrans : CblasConjTrans);
+  
+  if constexpr (std::is_same_v<TVar,double>){
+    cblas_dgemm(CblasColMajor, transp_a, transp_x, dim1, xcols, dim2,
+                alpha, this->fStorage, this->fLeadingDim, x.fStorage,
+                x.fLeadingDim, beta, z.fStorage, z.fLeadingDim);
+    return;
+  } else if constexpr (std::is_same_v<TVar,float>){
+    cblas_sgemm(CblasColMajor, transp_a, transp_x, dim1, xcols, dim2,
+                alpha, this->fStorage, this->fLeadingDim, x.fStorage,
+                x.fLeadingDim, beta, z.fStorage, z.fLeadingDim);
+    return;
+  } else if constexpr (std::is_same_v<TVar,std::complex<double>>){
+    cblas_zgemm(CblasColMajor, transp_a, transp_x, dim1, xcols, dim2,
+                &alpha, this->fStorage, this->fLeadingDim, x.fStorage,
+                x.fLeadingDim, &beta, z.fStorage, z.fLeadingDim);
+    return;
+  } else if constexpr (std::is_same_v<TVar,std::complex<float>>){
+    cblas_cgemm(CblasColMajor, transp_a, transp_x, dim1, xcols, dim2,
+                &alpha, this->fStorage, this->fLeadingDim, x.fStorage,
+                x.fLeadingDim, &beta, z.fStorage, z.fLeadingDim);
+    return;
+  }
+#endif
+  DebugStop();
+  // if (beta != (TVar)0) {
+  //   z *= beta;
+  // }
+  // for (auto ic = 0; ic < xcols; ic++) {
+  //   if(!opt) {
+  //     for (auto c = 0; c<cols; c++) {
+  //       TVar * zp = &z(0,ic), *zlast = zp+rows;
+  //       TVar * fp = fElem +rows*c;
+  //       const TVar * xp = &x.g(c,ic);
+  //       while(zp < zlast) {
+  //         *zp += alpha* *fp++ * *xp;
+  //         zp ++;
+  //       }
+  //     }
+  //   } else {
+  //     TVar * fp = fElem,  *zp = &z(0,ic);
+  //     for (auto c = 0; c<cols; c++) {
+  //       TVar val = 0.;
+  //       // bug correction philippe 5/2/97
+  //       //					 REAL * xp = &x(0,ic), xlast = xp + numeq;
+  //       const TVar *xp = &x.g(0,ic);
+  //       const TVar *xlast = xp + rows;
+  //       if constexpr (is_complex<TVar>::value){
+  //         if(opt==2){
+  //           while(xp < xlast) {
+  //             val += std::conj(*fp++) * *xp++;
+  //           }
+  //           *zp++ += alpha *val;
+  //           continue;//continue from the for loop
+  //         }
+  //       }
+  //       while(xp < xlast) {
+  //         val += *fp++ * *xp++;
+  //       }
+  //       *zp++ += alpha *val;
+  //     }
+  //   }
+  // }
 }
 
+template<class TVar>
+void
+TPZMatrixWindow<TVar>::CheckConstructor(const int i, const int j, const int nr, const int nc,
+                                        const int nr_orig, const int nc_orig){
+  if(nr < 1 || nc < 1 || i+nr>nr_orig || j+nc>nc_orig || i < 0 || j < 0){
+    PZError<<__PRETTY_FUNCTION__
+           <<"\nInvalid parameters:\n"
+           <<"i "<<i<<'\n'
+           <<"j "<<j<<'\n'
+           <<"nr "<<nr<<'\n'
+           <<"nc "<<nc<<'\n'
+           <<"nr_orig "<<nr_orig<<'\n'
+           <<"nc_orig "<<nc_orig<<std::endl;
+    DebugStop();
+  }
+}
 
 template class TPZMatrixWindow<float >;
 template class TPZMatrixWindow<double >;
--- Matrix/TPZMatrixWindow.h
+++ Matrix/TPZMatrixWindow.h
@@ -12,14 +12,35 @@ class TPZFMatrix;
 template<class TVar>
 class TPZMatrixWindow : public TPZMatrix<TVar>{
 public:
+  //!Copy constructor (what is the expected behaviour?)
+  TPZMatrixWindow(const TPZMatrixWindow<TVar> &mat){
+    DebugStop();
+  }
+  //!Move constructor (what is the expected behaviour?)
+  TPZMatrixWindow(TPZMatrixWindow<TVar> &&mat){
+    DebugStop();
+  }
+  //!Copy assignment operator (what is the expected behaviour?)
+  TPZMatrixWindow<TVar> &operator=(const TPZMatrixWindow<TVar> &cp){
+    DebugStop();
+    return *this;
+  }
+  //!Move assignment operator (what is the expected behaviour?)
+  TPZMatrixWindow<TVar> &operator=(TPZMatrixWindow<TVar> &&cp){
+    DebugStop();
+    return *this;
+  }
+  //! Default destructor (no memory deallocation)
+  ~TPZMatrixWindow() = default;
   /**
      @brief Constructs a window based on a ROW MAJOR memory area
      @param mem_area First position of block to be windowed
      @param nrows Number of rows of window
      @param ncols Number of cols of window
      @param leading_dim Distance in memory between two sucessive rows
+     @param size size of memory area (must be able to accomodate nrows*ncols)
    */
-  TPZMatrixWindow(TVar* mem_area, const int nrows, const int ncols, const int leading_dim);
+  TPZMatrixWindow(TVar* mem_area, const int nrows, const int ncols, const int leading_dim, const int size_mem);
   /**
      @brief Constructs a window based on a memory area
      @param mat original TPZFmatrix
@@ -40,9 +61,143 @@ public:
    * @param opt_a Indicates if A(this) is Transpose or not
    * @param opt_x Indicates if x is Transpose or not
    */
-  void MultAdd(const TPZFMatrix<TVar> &x,const TPZFMatrix<TVar> &y, TPZFMatrix<TVar> &z,
+  void MultAdd(const TPZMatrixWindow<TVar> &x,const TPZMatrixWindow<TVar> &y, TPZMatrixWindow<TVar> &z,
                const TVar alpha=1.,const TVar beta = 0.,const int opt_a = 0, const int opt_x = 0) const;
+  /**
+	 * @brief It computes z = beta * y + alpha * opt(this)*x but z and x can not overlap in memory.
+	 * @param x Is x on the above operation
+	 * @param y Is y on the above operation
+	 * @param z Is z on the above operation
+	 * @param alpha Is alpha on the above operation
+	 * @param beta Is beta on the above operation
+	 * @param opt Indicates if is Transpose or not
+	 */
+  inline void MultAdd(const TPZFMatrix<TVar> & x,const TPZFMatrix<TVar>& y, TPZFMatrix<TVar>& z,
+                      const TVar alpha=1., const TVar beta = 0., const int opt = 0) const override{
+    //is it safe to do so?
+    const TPZMatrixWindow<TVar> x_window(const_cast<TVar*>(x.Elem()),0,0,x.Rows(),x.Cols());
+    const TPZMatrixWindow<TVar> y_window(const_cast<TVar*>(y.Elem()),0,0,y.Rows(),y.Cols());
+    TPZMatrixWindow<TVar> z_window(z.Elem(),0,0,z.Rows(),z.Cols());
+    //forwards to general implementation (opt_x is always zero)
+    MultAdd(x_window,y_window,z_window,alpha,beta,opt,0);
+  }
+
+  inline TVar &operator()(const int64_t row, const int64_t col) {
+#ifndef PZNODEBUG
+	if ( (row >= this->Rows()) || (col >= this->Cols()) || row <0 || col<0 ) {
+		PZError<<__PRETTY_FUNCTION__
+           <<"\nIndex out of range"<<std::endl;
+    DebugStop();
+	}
+#endif
+    return fStorage[fLeadingDim*col+row];
+  }
+  inline TVar operator()(const int64_t row, const int64_t col)const {
+#ifndef PZNODEBUG
+	if ( (row >= this->Rows()) || (col >= this->Cols()) || row <0 || col<0 ) {
+		PZError<<__PRETTY_FUNCTION__
+           <<"\nIndex out of range"<<std::endl;
+    DebugStop();
+	}
+#endif
+    return fStorage[fLeadingDim*col+row];
+  }
+
+  const TVar GetVal(const int64_t row,const int64_t col ) const override{
+    return fStorage[fLeadingDim*col+row];
+  }
+
+  int PutVal(const int64_t row, const int64_t col,const TVar & value ) override{
+    fStorage[col*fLeadingDim+row ] = value;
+    return( 1 );
+  }
+
+  //! We do not allow resizing a TPZMatrixWindow 
+	int Resize(const int64_t newRows, const int64_t newCols ) override{
+		if(newRows!=this->fRow || newCols!=this->fCol){
+      DebugStop();
+    }
+		return 1;
+	}
+  //! Sets all elements to zero
+  int Zero() override{
+    for(int ic = 0; ic < this->fCol; ic++){
+      for(int ir = 0; ir < this->fRow; ir++){
+        this->PutVal(ir,ic,(TVar)0);
+      }
+    }
+    return 1;
+  }
+  
+  //! Not available for this matrix type 
+  inline int64_t Size() const override{
+    /*
+      Theoretically these methods could be implemented,
+      however, if both Size() and Elem() are implemented,
+      then it could lead to the Storage() method, defined at
+      TPZMatrix<T>, being called with no warnings whatsoever.
+      Let us keep it safe for now.
+    */
+    DebugStop();
+    return -1;
+  }
+  //! Not available for this matrix type 
+  inline TVar* &Elem() override{
+    /*
+      Theoretically these methods could be implemented,
+      however, if both Size() and Elem() are implemented,
+      then it could lead to the Storage() method, defined at
+      TPZMatrix<T>, being called with no warnings whatsoever.
+      Let us keep it safe for now.
+    */
+    DebugStop();
+    static TVar* myvar{nullptr};
+    return myvar;
+  }
+  //! Not available for this matrix type 
+  inline const TVar* Elem() const override{
+    /*
+      Theoretically these methods could be implemented,
+      however, if both Size() and Elem() are implemented,
+      then it could lead to the Storage() method, defined at
+      TPZMatrix<T>, being called with no warnings whatsoever.
+      Let us keep it safe for now.
+    */
+    DebugStop();
+    return nullptr;
+  }
+  //! Not available for this matrix type 
+  TPZMatrix<TVar> *Clone() const override{
+    DebugStop();
+    return nullptr;
+  }
+  //! Not available for this matrix type
+  TPZMatrix<TVar> *NewMatrix() const override{
+    DebugStop();
+    return nullptr;
+  }
+  //! Not available for this matrix type
+  int Decompose(const DecomposeType dt) override{
+    DebugStop();
+    return -1;
+  }
+  //! Not available for this matrix type
+  void CopyFrom(const TPZMatrix<TVar> *mat) override{
+    DebugStop();
+  }
+  //! Not available for this matrix type
+  int SolveDirect(TPZFMatrix<TVar>& F , const DecomposeType dt) override{
+    DebugStop();
+    return -1;
+  }
+  //! Not available for this matrix type
+  int SolveDirect(TPZFMatrix<TVar>& F , const DecomposeType dt) const override{
+    DebugStop();
+    return -1;
+  }
 private:
+  //! Auxiliary function for checking input param
+  void CheckConstructor(const int i, const int j, const int nr, const int nc, const int nr_orig, const int nc_orig);
   int64_t fLeadingDim{-1};
   TVar *fStorage{nullptr};
 };
