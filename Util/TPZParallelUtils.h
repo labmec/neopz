b35b9c53af2c8d859b62726fdd571baa6c422160
orlandini <francisco.orlandini@gmail.com>
2024-04-15 16:43:07 -0300

    feat(utils): adds serial option to ParallelFor

--- Util/TPZParallelUtils.h
+++ Util/TPZParallelUtils.h
@@ -28,28 +28,31 @@ as in
 It is the user responsability to use the necessary mutexes and locks.
 */
 template<typename TFunc>
-void ParallelFor( int ibeg, int iend, const TFunc & f )
+void ParallelFor( int ibeg, int iend, const TFunc & f ,const int nt=-1)
 {
-  const auto nthreads = std::thread::hardware_concurrency();
-  std::vector<std::thread> threadvec;
-  const int sz = (iend-ibeg)/nthreads;
-  for (int i=0; i<nthreads; i++)
-  {
-    const int myibeg = ibeg + i*sz;
-    const int myiend = ibeg + (i+1)*sz;
-    threadvec.push_back(
-      std::thread( [myibeg,myiend,&f] ()
-      {
-        //we prevent mkl launching multiple threads inside this thread
-        auto mklthreads = SetNumThreadsLocalMKL(1);
-        for(int it = myibeg; it < myiend; it++)
+  const auto nthreads = nt > -1 ? nt : std::thread::hardware_concurrency();
+  int sz{0};
+  if(nthreads){
+    std::vector<std::thread> threadvec;
+    sz = (iend-ibeg)/nthreads;
+    for (int i=0; i<nthreads; i++)
+    {
+      const int myibeg = ibeg + i*sz;
+      const int myiend = ibeg + (i+1)*sz;
+      threadvec.push_back(
+        std::thread( [myibeg,myiend,&f] ()
+        {
+          //we prevent mkl launching multiple threads inside this thread
+          auto mklthreads = SetNumThreadsLocalMKL(1);
+          for(int it = myibeg; it < myiend; it++)
           {f(it);}
-        SetNumThreadsLocalMKL(mklthreads);
-      }));
-  }
+          SetNumThreadsLocalMKL(mklthreads);
+        }));
+    }
 
-  for (int i=0; i<nthreads; i++)
+    for (int i=0; i<nthreads; i++)
     {threadvec[i].join();}
+  }
   //possible remainder
   const int lastbeg = ibeg + nthreads*sz;
   for(int it = lastbeg; it < iend; it++){
