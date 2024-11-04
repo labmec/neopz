ff88d8385fa28d74c11d08380e51343fef2fbb53
PhilippeDevloo <phil@fec.unicamp.br>
2024-11-03 21:42:09 -0300

    The error is now computed with MaxOrder

--- Mesh/pzinterpolationspace.cpp
+++ Mesh/pzinterpolationspace.cpp
@@ -1216,7 +1216,7 @@ void TPZInterpolationSpace::EvaluateErrorT(TPZVec<REAL> &errors,bool store_error
         }
         return max_int_order;
     }();
-	TPZManVector<int,3> maxorder(dim, maxIntOrder);
+    TPZManVector<int,3> maxorder(dim, intrule->GetMaxOrder());
 	intrule->SetOrder(maxorder);
 	TPZManVector<REAL,10> intpoint(problemdimension), values(NErrors);
 	REAL weight;
@@ -1258,7 +1258,7 @@ void TPZInterpolationSpace::EvaluateErrorT(TPZVec<REAL> &errors,bool store_error
             elvals(index,ier) = errors[ier];
         }
     }
-	intrule->SetOrder(prevOrder);
+//	intrule->SetOrder(prevOrder);
 	
 }//method
 
