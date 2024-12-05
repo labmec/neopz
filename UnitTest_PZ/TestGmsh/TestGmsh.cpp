464c629ee2ce086d6dea1dfb82e88150eab256f9
Francisco Orlandini <francisco.orlandini@gmail.com>
2024-12-05 09:57:40 -0300

    feat(test): TestGmsh compares node ordering

--- UnitTest_PZ/TestGmsh/TestGmsh.cpp
+++ UnitTest_PZ/TestGmsh/TestGmsh.cpp
@@ -4,13 +4,17 @@
  */
 
 #include "TPZGmshReader.h"
+#include "TPZGeoMeshTools.h"
 #include "pzcheckgeom.h"
 
 #include <catch2/catch_test_macros.hpp>
+#include <numeric> //for iota
 
+///check node ordering of every periodic element in mesh
+void TestPeriodicElements(TPZGeoMesh *gmesh, const SPZPeriodicData& periodic_data);
 
 TEST_CASE("Periodic meshes", "[gmsh]"){
-
+  
   /*simple periodic mesh on a unit square,
     testing multiple periodicity*/
   SECTION("Multiple periodicities 2D"){
@@ -19,9 +23,11 @@ TEST_CASE("Periodic meshes", "[gmsh]"){
       meshReader.GeometricGmshMesh("multiple_periodic_2d.msh");
     REQUIRE(true);
 
-
     TPZCheckGeom check(gmesh.operator->());
     REQUIRE(check.CheckIds()==0);
+
+    auto data = meshReader.GetPeriodicData();
+    TestPeriodicElements(gmesh.operator->(), *data);
   }
 
   /**
@@ -37,9 +43,56 @@ TEST_CASE("Periodic meshes", "[gmsh]"){
     TPZAutoPointer<TPZGeoMesh> gmesh =
       meshReader.GeometricGmshMesh("periodic_layer.msh");
     REQUIRE(true);
-
-
+    
     TPZCheckGeom check(gmesh.operator->());
     REQUIRE(check.CheckIds()==0);
+
+    auto data = meshReader.GetPeriodicData();
+    TestPeriodicElements(gmesh.operator->(), *data);
+  }
+}
+
+
+void TestPeriodicElements(TPZGeoMesh *gmesh, const SPZPeriodicData& periodic_data){
+
+  auto &data = periodic_data;
+
+  TPZVec<TPZAutoPointer<std::map<int64_t,int64_t>>> el_map_vec;
+  TPZGeoMeshTools::FindPeriodicElements(gmesh, data.dep_mat_ids, data.indep_mat_ids, data.nodes_map,
+                                        el_map_vec);
+
+  for(auto el_map : el_map_vec){
+    for(auto [dep_idx, indep_idx] : *el_map){
+      auto dep_el = gmesh->Element(dep_idx);
+      auto indep_el = gmesh->Element(indep_idx);
+      REQUIRE(dep_el->NCornerNodes() == indep_el->NCornerNodes());
+      const auto nnodes = dep_el->NCornerNodes();
+      constexpr int max_nnodes{8};
+      TPZManVector<int64_t,max_nnodes> dep_ids(nnodes,0),indep_ids(nnodes,0);;
+      for(auto i=0; i<nnodes; i++) {
+        dep_ids[i] = dep_el->NodePtr(i)->Id();
+        indep_ids[i] = indep_el->NodePtr(i)->Id();
+      }
+
+      //now we compare their ordering
+      TPZManVector<int64_t,max_nnodes> dep_order(nnodes,0), indep_order(nnodes,0);
+      std::iota(dep_order.begin(), dep_order.end(), 0);
+      std::iota(indep_order.begin(), indep_order.end(), 0);
+
+      // sort indexes based on comparing values in v
+      // using std::stable_sort instead of std::sort
+      // to avoid unnecessary index re-orderings
+      // when v contains elements of equal values 
+      std::sort(dep_order.begin(), dep_order.end(),
+                [&dep_ids](const int64_t i1, const int64_t i2)
+                {return dep_ids[i1] < dep_ids[i2];});
+      std::sort(indep_order.begin(), indep_order.end(),
+                [&indep_ids](const int64_t i1, const int64_t i2)
+                {return indep_ids[i1] < indep_ids[i2];});
+
+      for(auto i=0; i<nnodes; i++) {
+        REQUIRE(dep_order[i]==indep_order[i]);
+      }
+    }
   }
 }
